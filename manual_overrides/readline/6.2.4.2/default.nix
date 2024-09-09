1c3e1881412469ded2fb92ad889e38640ddd7dcc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-09 16:29:44 +0200

    autogenerated overwrites for Appium-Python-Client==4.1.0

--- manual_overrides/readline/6.2.4.2/default.nix
+++ manual_overrides/readline/6.2.4.2/default.nix
@@ -1,5 +1,7 @@
 {
-  postPatch = old.postPatch or "" + ''
-    substituteInPlace "setup.py" --replace-fail "/bin/bash" "${pkgs.bash}/bin/bash"
-  '';
+  postPatch =
+    old.postPatch or ""
+    + ''
+      substituteInPlace "setup.py" --replace-fail "/bin/bash" "${pkgs.bash}/bin/bash"
+    '';
 }
