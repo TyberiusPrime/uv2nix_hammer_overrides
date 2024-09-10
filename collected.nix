e88a337345accab75789b6a6fc026215e2db5965
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 18:24:23 +0200

    autogenerated overwrites for imm-source==1.0.30

--- collected.nix
+++ collected.nix
@@ -184,6 +184,9 @@
   igor = {
     "0.3" = import ./overrides/igor/0.3;
   };
+  imm-source = {
+    "1.0.30" = import ./overrides/imm-source/1.0.30;
+  };
   ioctl-opt = {
     "1.3" = import ./overrides/ioctl-opt/1.3;
   };
