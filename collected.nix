22f462803ca26498113eb7a358a73d5cec726dfc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 18:52:25 +0200

    autogenerated overwrites for cs.fs==20240630

--- collected.nix
+++ collected.nix
@@ -80,6 +80,9 @@
   crcmod = {
     "1.7" = import ./overrides/crcmod/1.7;
   };
+  cs-ansi-colour = {
+    "20220227" = import ./overrides/cs-ansi-colour/20220227;
+  };
   cysystemd = {
     "1.6.2" = import ./overrides/cysystemd/1.6.2;
   };
