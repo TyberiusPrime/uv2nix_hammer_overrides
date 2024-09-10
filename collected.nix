1c105eea6ec0a678f29d2bf94379fcfdf5c76b5f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 18:43:21 +0200

    autogenerated overwrites for ofxparse==0.21

--- collected.nix
+++ collected.nix
@@ -236,6 +236,9 @@
     "1.24.4" = import ./overrides/numpy/1.24.4;
     "2.1.0" = import ./overrides/numpy/2.1.0;
   };
+  ofxparse = {
+    "0.21" = import ./overrides/ofxparse/0.21;
+  };
   pandas = {
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
