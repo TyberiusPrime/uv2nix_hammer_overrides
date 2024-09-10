f6d3b464ce33d3eae3b1da5b97d6e4f91e6a7fc0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 17:29:24 +0200

    autogenerated overwrites for rencode==1.0.6

--- collected.nix
+++ collected.nix
@@ -327,6 +327,9 @@
   readline = {
     "6.2.4.2" = import ./overrides/readline/6.2.4.2;
   };
+  rencode = {
+    "1.0.6" = import ./overrides/rencode/1.0.6;
+  };
   reprshed = {
     "1.0.6" = import ./overrides/reprshed/1.0.6;
   };
