7f6335e889812407f33877df014f9b340cc1cb14
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 18:45:21 +0200

    autogenerated overwrites for wiringpi==2.60.1

--- collected.nix
+++ collected.nix
@@ -405,6 +405,9 @@
   webbot = {
     "0.34" = import ./overrides/webbot/0.34;
   };
+  wiringpi = {
+    "2.60.1" = import ./overrides/wiringpi/2.60.1;
+  };
   xtermcolor = {
     "1.3" = import ./overrides/xtermcolor/1.3;
   };
