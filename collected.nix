8159df5f2b0f4dccf17852b93ea9389adb60faa6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-10 17:41:25 +0200

    autogenerated overwrites for pycoin==0.92.20230326

--- collected.nix
+++ collected.nix
@@ -260,6 +260,9 @@
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
+  pycoin = {
+    "0.92.20230326" = import ./overrides/pycoin/0.92.20230326;
+  };
   pydebug = {
     "1.0.3" = import ./overrides/pydebug/1.0.3;
   };
