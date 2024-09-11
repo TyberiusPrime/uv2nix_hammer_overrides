506f10e025427323da26b4258e18f23bc7a360fa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:20:59 +0200

    autogenerated overwrites for sense-core==0.2.93

--- collected.nix
+++ collected.nix
@@ -260,6 +260,9 @@
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
+  pycrypto = {
+    "2.6.1" = import ./overrides/pycrypto/2.6.1;
+  };
   pydebug = {
     "1.0.3" = import ./overrides/pydebug/1.0.3;
   };
