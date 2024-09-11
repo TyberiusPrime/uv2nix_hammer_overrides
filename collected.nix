4bcaecf2dbc7ca7d39da2ea2226a18fbd9f7f55a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:50:47 +0200

    autogenerated overwrites for stable-baselines==2.10.2

--- collected.nix
+++ collected.nix
@@ -169,6 +169,9 @@
   google-apitools = {
     "0.5.31" = import ./overrides/google-apitools/0.5.31;
   };
+  gym = {
+    "0.26.2" = import ./overrides/gym/0.26.2;
+  };
   h5py = {
     "3.11.0" = import ./overrides/h5py/3.11.0;
   };
