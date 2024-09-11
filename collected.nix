79384a6c2380fe1a2016ddef305ba22982102dec
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:06:10 +0200

    autogenerated overwrites for micropython-cpython-micropython==0.1.1

--- collected.nix
+++ collected.nix
@@ -214,6 +214,9 @@
   maxminddb-geolite2 = {
     "2018.703" = import ./overrides/maxminddb-geolite2/2018.703;
   };
+  micropython-cpython-micropython = {
+    "0.1.1" = import ./overrides/micropython-cpython-micropython/0.1.1;
+  };
   msgpack-python = {
     "0.5.6" = import ./overrides/msgpack-python/0.5.6;
   };
