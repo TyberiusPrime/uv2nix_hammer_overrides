37aed641b1b20896b66d129b853a090af155c4b6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 23:34:56 +0200

    autogenerated overwrites for pyschedule==0.2.34

--- collected.nix
+++ collected.nix
@@ -287,6 +287,9 @@
   pyperclip = {
     "1.9.0" = import ./overrides/pyperclip/1.9.0;
   };
+  pyschedule = {
+    "0.2.34" = import ./overrides/pyschedule/0.2.34;
+  };
   pyspark = {
     "3.5.2" = import ./overrides/pyspark/3.5.2;
   };
