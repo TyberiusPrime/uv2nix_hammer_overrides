72cbd24bf53d10f200c40111b787c5a151366206
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 23:48:16 +0200

    autogenerated overwrites for scvelo==0.3.2

--- collected.nix
+++ collected.nix
@@ -336,6 +336,9 @@
   rpi-gpio = {
     "0.7.1" = import ./overrides/rpi-gpio/0.7.1;
   };
+  session-info = {
+    "1.0.0" = import ./overrides/session-info/1.0.0;
+  };
   simplejson = {
     "3.11.1" = import ./overrides/simplejson/3.11.1;
   };
