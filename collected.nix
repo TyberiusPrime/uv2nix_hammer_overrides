f5d954dcfa7935c6a382d5e6849a459597c20f39
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 22:37:41 +0200

    autogenerated overwrites for librouteros==3.2.1

--- collected.nix
+++ collected.nix
@@ -199,6 +199,9 @@
   kfp-server-api = {
     "2.0.5" = import ./overrides/kfp-server-api/2.0.5;
   };
+  librouteros = {
+    "3.2.1" = import ./overrides/librouteros/3.2.1;
+  };
   llist = {
     "0.8" = import ./overrides/llist/0.8;
   };
