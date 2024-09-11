844ccb8b74f4e5d27ba1a3ac42a09dae76c9bf6b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:18:41 +0200

    autogenerated overwrites for fortranformat==2.0.0

--- collected.nix
+++ collected.nix
@@ -153,6 +153,9 @@
   flask-principal = {
     "0.4.0" = import ./overrides/flask-principal/0.4.0;
   };
+  fortranformat = {
+    "2.0.0" = import ./overrides/fortranformat/2.0.0;
+  };
   funcy = {
     "1.10.1" = import ./overrides/funcy/1.10.1;
   };
