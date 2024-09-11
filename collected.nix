442b9b17536128a6957bda1d92170383b3a7a025
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:04:11 +0200

    autogenerated overwrites for cenpy==1.0.1

--- collected.nix
+++ collected.nix
@@ -71,6 +71,9 @@
   celery-once = {
     "3.0.1" = import ./overrides/celery-once/3.0.1;
   };
+  cenpy = {
+    "1.0.1" = import ./overrides/cenpy/1.0.1;
+  };
   clipboard = {
     "0.0.4" = import ./overrides/clipboard/0.0.4;
   };
