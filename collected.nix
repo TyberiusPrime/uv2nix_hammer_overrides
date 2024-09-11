0c8306320e21b24a3f431519420429f6a23bcc86
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:23:02 +0200

    autogenerated overwrites for fastimport==0.9.14

--- collected.nix
+++ collected.nix
@@ -135,6 +135,9 @@
   face-recognition-models = {
     "0.3.0" = import ./overrides/face-recognition-models/0.3.0;
   };
+  fastimport = {
+    "0.9.14" = import ./overrides/fastimport/0.9.14;
+  };
   fcm-django = {
     "2.2.1" = import ./overrides/fcm-django/2.2.1;
   };
