b9c21426f64f7566e9e4da7de860533e2c702160
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 20:47:19 +0200

    autogenerated overwrites for flask-apscheduler==1.13.1

--- collected.nix
+++ collected.nix
@@ -144,6 +144,9 @@
   fishbase = {
     "1.6" = import ./overrides/fishbase/1.6;
   };
+  flask-apscheduler = {
+    "1.13.1" = import ./overrides/flask-apscheduler/1.13.1;
+  };
   flask-kvsession-invenio = {
     "0.6.3" = import ./overrides/flask-kvsession-invenio/0.6.3;
   };
