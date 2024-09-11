c3d32c696b103a37e5828e4a809e2509bc1802e1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 00:14:51 +0200

    autogenerated overwrites for tri.struct==4.1.0

--- collected.nix
+++ collected.nix
@@ -375,6 +375,9 @@
   text2vec = {
     "1.3.0" = import ./overrides/text2vec/1.3.0;
   };
+  tri-struct = {
+    "4.1.0" = import ./overrides/tri-struct/4.1.0;
+  };
   twitter-common-decorators = {
     "0.3.11" = import ./overrides/twitter-common-decorators/0.3.11;
   };
