0a57c85a2cae5387a26035c3a4e903782936deec
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 23:29:03 +0200

    autogenerated overwrites for pytest-watch==4.2.0

--- collected.nix
+++ collected.nix
@@ -293,6 +293,9 @@
   pystemd = {
     "0.13.2" = import ./overrides/pystemd/0.13.2;
   };
+  pytest-watch = {
+    "4.2.0" = import ./overrides/pytest-watch/4.2.0;
+  };
   python-axolotl-curve25519 = {
     "0.4.1.post2" = import ./overrides/python-axolotl-curve25519/0.4.1.post2;
   };
