9656d66d6f28a0bc8631bdc4168deda55d4b74dd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:22:34 +0200

    autogenerated overwrites for pao==1.0.2

--- collected.nix
+++ collected.nix
@@ -239,6 +239,9 @@
   pandas = {
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
+  pao = {
+    "1.0.2" = import ./overrides/pao/1.0.2;
+  };
   pdpyras = {
     "5.2.0" = import ./overrides/pdpyras/5.2.0;
   };
