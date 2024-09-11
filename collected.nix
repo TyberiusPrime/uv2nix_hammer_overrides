149c8d38b4b2aa29fb5fd87d5cc3af847c62f1a3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 21:40:42 +0200

    autogenerated overwrites for datafiles==2.2.3

--- collected.nix
+++ collected.nix
@@ -71,6 +71,9 @@
   celery-once = {
     "3.0.1" = import ./overrides/celery-once/3.0.1;
   };
+  classproperties = {
+    "0.2.0" = import ./overrides/classproperties/0.2.0;
+  };
   clipboard = {
     "0.0.4" = import ./overrides/clipboard/0.0.4;
   };
