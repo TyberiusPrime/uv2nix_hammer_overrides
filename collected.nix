85e18771f2abd23a1d0240880fe701d28674a930
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-11 20:37:44 +0200

    autogenerated overwrites for mimerender==0.6.0

--- collected.nix
+++ collected.nix
@@ -214,6 +214,9 @@
   maxminddb-geolite2 = {
     "2018.703" = import ./overrides/maxminddb-geolite2/2018.703;
   };
+  mimerender = {
+    "0.6.0" = import ./overrides/mimerender/0.6.0;
+  };
   msgpack-python = {
     "0.5.6" = import ./overrides/msgpack-python/0.5.6;
   };
