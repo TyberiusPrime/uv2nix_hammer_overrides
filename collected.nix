f921d6b22772f90077393347fde5ed92acf3f043
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 04:12:06 +0200

    autogenerated overwrites for ha-mqtt-discoverable==0.14.0

--- collected.nix
+++ collected.nix
@@ -236,6 +236,9 @@
     "1.24.4" = import ./overrides/numpy/1.24.4;
     "2.1.0" = import ./overrides/numpy/2.1.0;
   };
+  paho-mqtt = {
+    "1.6.1" = import ./overrides/paho-mqtt/1.6.1;
+  };
   pandas = {
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
