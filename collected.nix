b6e64b8b68b2369201c4450c9eee8d58d204c8fc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 22:32:48 +0000

    autogenerated overwrites for pyppeteer2==0.2.2

--- collected.nix
+++ collected.nix
@@ -1789,6 +1789,9 @@
   webbot = {
     "0.34" = import ./overrides/webbot/0.34;
   };
+  websockets = {
+    "8.1" = import ./overrides/websockets/8.1;
+  };
   wf-minimal-honeycomb-python = {
     "0.6.3" = import ./overrides/wf-minimal-honeycomb-python/0.6.3;
   };
