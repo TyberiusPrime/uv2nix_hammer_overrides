799d7bf08ec6e7980d5dd24c8b3027bd3f87be22
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 06:53:14 +0200

    autogenerated overwrites for pycontracts==1.8.12

--- collected.nix
+++ collected.nix
@@ -260,6 +260,9 @@
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
+  pycontracts = {
+    "1.8.12" = import ./overrides/pycontracts/1.8.12;
+  };
   pydebug = {
     "1.0.3" = import ./overrides/pydebug/1.0.3;
   };
