60d8bc8b37e910c2d78dbf818369e946953eceae
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 03:10:04 +0200

    autogenerated overwrites for pexif==0.15

--- collected.nix
+++ collected.nix
@@ -242,6 +242,9 @@
   pdpyras = {
     "5.2.0" = import ./overrides/pdpyras/5.2.0;
   };
+  pexif = {
+    "0.15" = import ./overrides/pexif/0.15;
+  };
   pillow = {
     "10.4.0" = import ./overrides/pillow/10.4.0;
   };
