6eae3aae5157168618578fe9814beb160c315449
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 06:27:19 +0200

    autogenerated overwrites for opster==5.0

--- collected.nix
+++ collected.nix
@@ -236,6 +236,9 @@
     "1.24.4" = import ./overrides/numpy/1.24.4;
     "2.1.0" = import ./overrides/numpy/2.1.0;
   };
+  opster = {
+    "5.0" = import ./overrides/opster/5.0;
+  };
   pandas = {
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
