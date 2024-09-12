1f746bd9c6811e9513a5a1fe2683796f8e69ae0e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 05:58:33 +0200

    autogenerated overwrites for django-bootstrap3-datetimepicker==2.2.3

--- collected.nix
+++ collected.nix
@@ -108,6 +108,9 @@
   django-bitfield = {
     "2.2.0" = import ./overrides/django-bitfield/2.2.0;
   };
+  django-bootstrap3-datetimepicker = {
+    "2.2.3" = import ./overrides/django-bootstrap3-datetimepicker/2.2.3;
+  };
   django-devserver = {
     "0.8.0" = import ./overrides/django-devserver/0.8.0;
   };
