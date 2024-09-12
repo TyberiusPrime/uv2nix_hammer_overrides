87457a8eae92ceba363092600c5ee7d23d5d499c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 22:00:32 +0000

    autogenerated overwrites for crc16==0.1.1

--- collected.nix
+++ collected.nix
@@ -316,6 +316,9 @@
   cppimport = {
     "22.8.2" = import ./overrides/cppimport/22.8.2;
   };
+  crc16 = {
+    "0.1.1" = import ./overrides/crc16/0.1.1;
+  };
   crcmod = {
     "1.7" = import ./overrides/crcmod/1.7;
   };
