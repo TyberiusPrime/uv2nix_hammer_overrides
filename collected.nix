fc016d242e4da0fa1a96a74fc6f674ab37e3594b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 07:12:24 +0200

    autogenerated overwrites for calysto-scheme==1.4.8

--- collected.nix
+++ collected.nix
@@ -408,6 +408,9 @@
   xtermcolor = {
     "1.3" = import ./overrides/xtermcolor/1.3;
   };
+  yasi = {
+    "2.1.2" = import ./overrides/yasi/2.1.2;
+  };
   zuper-commons-z6 = {
     "6.2.4" = import ./overrides/zuper-commons-z6/6.2.4;
   };
