2c65d5e81a0a51652656dabf9b86b36e9ee603d7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 23:42:28 +0000

    autogenerated overwrites for django-pure-pagination==0.3.0

--- collected.nix
+++ collected.nix
@@ -419,6 +419,9 @@
   django-login-required-middleware = {
     "0.9.0" = import ./overrides/django-login-required-middleware/0.9.0;
   };
+  django-pure-pagination = {
+    "0.3.0" = import ./overrides/django-pure-pagination/0.3.0;
+  };
   django-redis = {
     "4.2.0" = import ./overrides/django-redis/4.2.0;
   };
