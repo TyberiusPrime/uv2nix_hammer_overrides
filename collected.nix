5b10f171566bce1bb0fe615958135dd6d415609a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 05:53:18 +0200

    autogenerated overwrites for captain==4.5.0

--- collected.nix
+++ collected.nix
@@ -65,6 +65,9 @@
   bravado-core = {
     "6.1.1" = import ./overrides/bravado-core/6.1.1;
   };
+  captain = {
+    "4.5.0" = import ./overrides/captain/4.5.0;
+  };
   case-conversion = {
     "2.1.0" = import ./overrides/case-conversion/2.1.0;
   };
