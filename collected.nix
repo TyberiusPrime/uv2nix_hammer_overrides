06c8d3c7132f428803854095972abd6828dc27e8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 21:47:02 +0000

    autogenerated overwrites for pybitcointools==1.1.15

--- collected.nix
+++ collected.nix
@@ -1161,6 +1161,9 @@
   pybars3 = {
     "0.9.7" = import ./overrides/pybars3/0.9.7;
   };
+  pybitcointools = {
+    "1.1.15" = import ./overrides/pybitcointools/1.1.15;
+  };
   pybrowserid = {
     "0.14.0" = import ./overrides/pybrowserid/0.14.0;
   };
