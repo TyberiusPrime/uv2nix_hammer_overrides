8cbc97f6745ce2e8ac3442f3cdeba6cb11807782
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 04:13:02 +0200

    autogenerated overwrites for pyuv==1.4.0

--- collected.nix
+++ collected.nix
@@ -318,6 +318,9 @@
   pyudorandom = {
     "1.0.0" = import ./overrides/pyudorandom/1.0.0;
   };
+  pyuv = {
+    "1.4.0" = import ./overrides/pyuv/1.4.0;
+  };
   pyxdameraulevenshtein = {
     "1.8.0" = import ./overrides/pyxdameraulevenshtein/1.8.0;
   };
