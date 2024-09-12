c8f5953093ae2f98eb764305f4feb2f7eac396a5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 23:58:48 +0000

    autogenerated overwrites for python-logstash==0.4.8

--- collected.nix
+++ collected.nix
@@ -1370,6 +1370,9 @@
   python-ligo-lw = {
     "1.8.3" = import ./overrides/python-ligo-lw/1.8.3;
   };
+  python-logstash = {
+    "0.4.8" = import ./overrides/python-logstash/0.4.8;
+  };
   python-modargs = {
     "1.7" = import ./overrides/python-modargs/1.7;
   };
