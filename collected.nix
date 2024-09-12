bde1060c03838495645704e18d59dc3c75d63af6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 22:09:44 +0000

    autogenerated overwrites for opcua==0.98.13

--- collected.nix
+++ collected.nix
@@ -1027,6 +1027,9 @@
   opal-adal = {
     "1.2.4" = import ./overrides/opal-adal/1.2.4;
   };
+  opcua = {
+    "0.98.13" = import ./overrides/opcua/0.98.13;
+  };
   openpaperwork-core = {
     "2.2.5" = import ./overrides/openpaperwork-core/2.2.5;
   };
