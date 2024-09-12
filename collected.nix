04b6de025499740fd308eaf95b2feeb5e5b2b14c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 06:27:47 +0200

    autogenerated overwrites for cgen==2020.1

--- collected.nix
+++ collected.nix
@@ -71,6 +71,9 @@
   celery-once = {
     "3.0.1" = import ./overrides/celery-once/3.0.1;
   };
+  cgen = {
+    "2020.1" = import ./overrides/cgen/2020.1;
+  };
   clipboard = {
     "0.0.4" = import ./overrides/clipboard/0.0.4;
   };
