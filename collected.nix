335e1ff75ff9e73ed018bfc6983f2a6ab5b968c9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 04:06:33 +0200

    autogenerated overwrites for tsam==2.3.3

--- collected.nix
+++ collected.nix
@@ -375,6 +375,9 @@
   text2vec = {
     "1.3.0" = import ./overrides/text2vec/1.3.0;
   };
+  tsam = {
+    "2.3.3" = import ./overrides/tsam/2.3.3;
+  };
   twitter-common-decorators = {
     "0.3.11" = import ./overrides/twitter-common-decorators/0.3.11;
   };
