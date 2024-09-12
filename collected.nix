ad3f13705a87eab14320aeee3e786fddbaed8afb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 06:21:33 +0200

    autogenerated overwrites for ripser==0.6.10

--- collected.nix
+++ collected.nix
@@ -175,6 +175,9 @@
   hdfs = {
     "2.7.3" = import ./overrides/hdfs/2.7.3;
   };
+  hopcroftkarp = {
+    "1.2.5" = import ./overrides/hopcroftkarp/1.2.5;
+  };
   html2text = {
     "2024.2.26" = import ./overrides/html2text/2024.2.26;
   };
