3d10a455d01f9d89fed6c8de7fcc5f19024518af
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 03:25:34 +0200

    autogenerated overwrites for opal-azure-cli-core==2.0.70

--- collected.nix
+++ collected.nix
@@ -236,6 +236,9 @@
     "1.24.4" = import ./overrides/numpy/1.24.4;
     "2.1.0" = import ./overrides/numpy/2.1.0;
   };
+  opal-adal = {
+    "1.2.4" = import ./overrides/opal-adal/1.2.4;
+  };
   pandas = {
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
