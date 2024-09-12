ca9ee6850ae1e0f60b6cf3097ae6fdbbc5181eb4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 23:59:12 +0000

    autogenerated overwrites for sciunit==0.2.8

--- collected.nix
+++ collected.nix
@@ -1501,6 +1501,9 @@
   scikit-bio = {
     "0.6.2" = import ./overrides/scikit-bio/0.6.2;
   };
+  sciunit = {
+    "0.2.8" = import ./overrides/sciunit/0.2.8;
+  };
   scpclient = {
     "0.7" = import ./overrides/scpclient/0.7;
   };
