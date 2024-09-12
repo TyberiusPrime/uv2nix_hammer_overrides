d8400b9b07e44b660b651f83bd75a556412556e4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 03:33:35 +0200

    autogenerated overwrites for caep==1.1.0

--- collected.nix
+++ collected.nix
@@ -65,6 +65,9 @@
   bravado-core = {
     "6.1.1" = import ./overrides/bravado-core/6.1.1;
   };
+  caep = {
+    "1.1.0" = import ./overrides/caep/1.1.0;
+  };
   case-conversion = {
     "2.1.0" = import ./overrides/case-conversion/2.1.0;
   };
