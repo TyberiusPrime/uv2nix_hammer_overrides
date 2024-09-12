4f358a14b3d3a3645cfb579382e7ac0c23fcd602
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-12 07:09:49 +0200

    autogenerated overwrites for libagent==0.15.0

--- collected.nix
+++ collected.nix
@@ -284,6 +284,9 @@
   pymlconf = {
     "3.0.1" = import ./overrides/pymlconf/3.0.1;
   };
+  pymsgbox = {
+    "1.0.9" = import ./overrides/pymsgbox/1.0.9;
+  };
   pyperclip = {
     "1.9.0" = import ./overrides/pyperclip/1.9.0;
   };
