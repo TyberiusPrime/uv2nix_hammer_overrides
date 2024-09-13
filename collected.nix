7136ece0b56c3f55f4b9ba2c45a75bc448dc9db0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 05:20:10 +0000

    autogenerated overwrites for siphash==0.0.1

--- collected.nix
+++ collected.nix
@@ -1537,6 +1537,9 @@
   singer-python = {
     "5.1.5" = import ./overrides/singer-python/5.1.5;
   };
+  siphash = {
+    "0.0.1" = import ./overrides/siphash/0.0.1;
+  };
   six = {
     "1.16.0" = import ./overrides/six/1.16.0;
   };
