d9637e24c40895176185fc0f19172b2bb11cf661
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 10:24:44 +0200

    fix annoy

--- manual_overrides/manual_overrides/python-augeas/1.1.0/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  postPatch = let
-    libname = "libaugeas${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}";
-  in old.postPatch or "" + ''
-    substituteInPlace augeas/ffi.py \
-    --replace-fail 'ffi.dlopen("augeas")' \
-              'ffi.dlopen("${pkgs.lib.makeLibraryPath [pkgs.augeas]}/${libname}")'
-  '';
-}
--- manual_overrides/manual_overrides/readline/6.2.4.2/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  postPatch =
-    old.postPatch or ""
-    + ''
-      substituteInPlace "setup.py" --replace-fail "/bin/bash" "${pkgs.bash}/bin/bash"
-    '';
-}
