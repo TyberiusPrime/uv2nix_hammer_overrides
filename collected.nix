2471ee7913658f83fd78629304080af105a69a3b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 03:18:19 +0000

    autogenerated overwrites for django-ranged-response==0.2.0

--- collected.nix
+++ collected.nix
@@ -419,6 +419,9 @@
   django-login-required-middleware = {
     "0.9.0" = import ./overrides/django-login-required-middleware/0.9.0;
   };
+  django-ranged-response = {
+    "0.2.0" = import ./overrides/django-ranged-response/0.2.0;
+  };
   django-redis = {
     "4.2.0" = import ./overrides/django-redis/4.2.0;
   };
