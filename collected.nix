c9c4ec66174bae58ec0f98bc1f3a8143fc89d774
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 03:53:14 +0000

    autogenerated overwrites for lesscli==0.2.1

--- collected.nix
+++ collected.nix
@@ -816,6 +816,9 @@
   lazyutils = {
     "0.3.3" = import ./overrides/lazyutils/0.3.3;
   };
+  lesscli = {
+    "0.2.1" = import ./overrides/lesscli/0.2.1;
+  };
   libevdev = {
     "0.11" = import ./overrides/libevdev/0.11;
   };
