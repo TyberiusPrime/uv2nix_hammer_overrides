a67ad7716ab8ed7a3f64b7e9f76771b15486846e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 03:28:46 +0000

    autogenerated overwrites for antlr-plsql==0.9.1

--- collected.nix
+++ collected.nix
@@ -62,6 +62,9 @@
   antlr-ast = {
     "0.7.0" = import ./overrides/antlr-ast/0.7.0;
   };
+  antlr-plsql = {
+    "0.9.1" = import ./overrides/antlr-plsql/0.9.1;
+  };
   antlr-tsql = {
     "0.12.6" = import ./overrides/antlr-tsql/0.12.6;
   };
