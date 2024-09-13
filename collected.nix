e44a2952a909c564524356924f7bf784b82e1414
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 04:27:55 +0000

    autogenerated overwrites for distorm3==3.5.2

--- collected.nix
+++ collected.nix
@@ -380,6 +380,9 @@
   dimagi-utils = {
     "1.2.3.2" = import ./overrides/dimagi-utils/1.2.3.2;
   };
+  distorm3 = {
+    "3.5.2" = import ./overrides/distorm3/3.5.2;
+  };
   django-autocomplete-light = {
     "3.11.0" = import ./overrides/django-autocomplete-light/3.11.0;
   };
