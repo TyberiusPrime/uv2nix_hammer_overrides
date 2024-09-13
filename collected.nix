04bb7053c21d02dfdaeae18d9d58bbd899365876
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 00:33:30 +0000

    autogenerated overwrites for py-ubjson==0.16.1

--- collected.nix
+++ collected.nix
@@ -1146,6 +1146,9 @@
   px-pipeline = {
     "0.1.2" = import ./overrides/px-pipeline/0.1.2;
   };
+  py-ubjson = {
+    "0.16.1" = import ./overrides/py-ubjson/0.16.1;
+  };
   pyactiveresource = {
     "2.2.2" = import ./overrides/pyactiveresource/2.2.2;
   };
