7780e632078b814c167e97b1e8acce388bf2a8f2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 02:28:03 +0000

    autogenerated overwrites for pytils==0.4.1

--- collected.nix
+++ collected.nix
@@ -1397,6 +1397,9 @@
   pytidylib = {
     "0.3.2" = import ./overrides/pytidylib/0.3.2;
   };
+  pytils = {
+    "0.4.1" = import ./overrides/pytils/0.4.1;
+  };
   pytweening = {
     "1.2.0" = import ./overrides/pytweening/1.2.0;
   };
