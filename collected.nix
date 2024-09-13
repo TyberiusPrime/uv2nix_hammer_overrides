fad76f84da1056b74b7b76a619a229bb740c8482
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 02:21:31 +0000

    autogenerated overwrites for dirsync==2.2.5

--- collected.nix
+++ collected.nix
@@ -380,6 +380,9 @@
   dimagi-utils = {
     "1.2.3.2" = import ./overrides/dimagi-utils/1.2.3.2;
   };
+  dirsync = {
+    "2.2.5" = import ./overrides/dirsync/2.2.5;
+  };
   django-autocomplete-light = {
     "3.11.0" = import ./overrides/django-autocomplete-light/3.11.0;
   };
