dad49ece93941ae2c752e49e54a98f34bb5a32e6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-13 05:24:42 +0000

    autogenerated overwrites for jaxlinop==0.0.3

--- collected.nix
+++ collected.nix
@@ -744,6 +744,9 @@
   jaconv = {
     "0.4.0" = import ./overrides/jaconv/0.4.0;
   };
+  jaxlinop = {
+    "0.0.3" = import ./overrides/jaxlinop/0.0.3;
+  };
   jieba = {
     "0.42.1" = import ./overrides/jieba/0.42.1;
   };
