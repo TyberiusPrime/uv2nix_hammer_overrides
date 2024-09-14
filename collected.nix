3231b51ad6823c15bbf36d6338ecd0a2a880b9ea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 20:23:47 +0000

    autogenerated overwrites for mongokit==0.9.1.1

--- collected.nix
+++ collected.nix
@@ -1648,6 +1648,9 @@
   monero = {
     "1.1.1" = import ./overrides/monero/1.1.1;
   };
+  mongokit = {
+    "0.9.1.1" = import ./overrides/mongokit/0.9.1.1;
+  };
   mouseinfo = {
     "0.1.3" = import ./overrides/mouseinfo/0.1.3;
   };
