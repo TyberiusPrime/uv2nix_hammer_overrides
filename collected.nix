f1fc47506bf1062eaf47108327c3ef97fc309e72
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 22:59:11 +0000

    autogenerated overwrites for pytype==2024.4.11

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   pytweening = {
     "1.2.0" = import ./overrides/pytweening/1.2.0;
   };
+  pytype = {
+    "2024.4.11" = import ./overrides/pytype/2024.4.11;
+  };
   pytz = {
     "2024.1" = import ./overrides/pytz/2024.1;
   };
