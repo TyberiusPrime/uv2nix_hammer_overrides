a482540c054c7824b0491822f1fd788687bedd45
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 21:47:19 +0000

    autogenerated overwrites for signify==0.6.1

--- collected.nix
+++ collected.nix
@@ -2675,6 +2675,9 @@
   signals-utils = {
     "1.14.0" = import ./overrides/signals-utils/1.14.0;
   };
+  signify = {
+    "0.6.1" = import ./overrides/signify/0.6.1;
+  };
   simple-rlp = {
     "0.1.3" = import ./overrides/simple-rlp/0.1.3;
   };
