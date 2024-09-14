2f69ceb015cf3164c57fc6519961631e2c27fe8b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 20:15:17 +0000

    autogenerated overwrites for iotlabcli==3.3.0

--- collected.nix
+++ collected.nix
@@ -1288,6 +1288,9 @@
   ionhash = {
     "1.2.1" = import ./overrides/ionhash/1.2.1;
   };
+  iotlabcli = {
+    "3.3.0" = import ./overrides/iotlabcli/3.3.0;
+  };
   ipcalc = {
     "1.1.3" = import ./overrides/ipcalc/1.1.3;
   };
