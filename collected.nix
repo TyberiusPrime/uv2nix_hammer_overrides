b889b0d280310f51a6b26a199b410ade9d5d6a4d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 22:32:07 +0000

    autogenerated overwrites for pykrakenapi==0.3.2

--- collected.nix
+++ collected.nix
@@ -2171,6 +2171,9 @@
   pykmtronic = {
     "0.3.0" = import ./overrides/pykmtronic/0.3.0;
   };
+  pykrakenapi = {
+    "0.3.2" = import ./overrides/pykrakenapi/0.3.2;
+  };
   pykulersky = {
     "0.5.5" = import ./overrides/pykulersky/0.5.5;
   };
