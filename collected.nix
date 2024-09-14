041a8abdd914ae530d503e1746ffe4728f1623de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 19:04:08 +0000

    autogenerated overwrites for simplepam==0.1.5

--- collected.nix
+++ collected.nix
@@ -2684,6 +2684,9 @@
   simplejson = {
     "3.11.1" = import ./overrides/simplejson/3.11.1;
   };
+  simplepam = {
+    "0.1.5" = import ./overrides/simplepam/0.1.5;
+  };
   singer-python = {
     "5.1.5" = import ./overrides/singer-python/5.1.5;
   };
