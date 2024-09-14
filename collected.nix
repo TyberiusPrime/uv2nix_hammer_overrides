0a91ceb4b1f9e44176deef603fde3a2975cb09c8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 15:15:26 +0000

    autogenerated overwrites for uWSGI==2.0.26

--- collected.nix
+++ collected.nix
@@ -3039,6 +3039,9 @@
   utm = {
     "0.7.0" = import ./overrides/utm/0.7.0;
   };
+  uwsgi = {
+    "2.0.26" = import ./overrides/uwsgi/2.0.26;
+  };
   uwsgidecorators = {
     "1.1.0" = import ./overrides/uwsgidecorators/1.1.0;
   };
