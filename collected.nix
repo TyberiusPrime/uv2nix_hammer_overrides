2ed1cfeb1d5870525d030af8e1303a2594885115
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 14:58:04 +0000

    autogenerated overwrites for bar==0.2.1

--- collected.nix
+++ collected.nix
@@ -219,6 +219,9 @@
   backport-collections = {
     "0.1" = import ./overrides/backport-collections/0.1;
   };
+  bar = {
+    "0.2.1" = import ./overrides/bar/0.2.1;
+  };
   barenecessities = {
     "0.2.8" = import ./overrides/barenecessities/0.2.8;
   };
