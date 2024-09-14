762486ce4bc097960f9d46f2a6f4c84dd365a08a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 19:30:30 +0000

    autogenerated overwrites for heudiconv==1.1.6

--- collected.nix
+++ collected.nix
@@ -2934,6 +2934,9 @@
   trading-calendars = {
     "2.1.1" = import ./overrides/trading-calendars/2.1.1;
   };
+  traits = {
+    "6.3.2" = import ./overrides/traits/6.3.2;
+  };
   transpyler = {
     "0.5.0" = import ./overrides/transpyler/0.5.0;
   };
