993eaf1533edcd82a3135e5d5f33d7850b9dcc41
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 15:18:58 +0000

    autogenerated overwrites for flashtext==2.7

--- collected.nix
+++ collected.nix
@@ -936,6 +936,9 @@
   fixture = {
     "1.5.11" = import ./overrides/fixture/1.5.11;
   };
+  flashtext = {
+    "2.7" = import ./overrides/flashtext/2.7;
+  };
   flask = {
     "0.10.1" = import ./overrides/flask/0.10.1;
   };
