15f42f0d460be37212de2c504038483cbfecfeea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 13:15:32 +0000

    autogenerated overwrites for snapcast==2.3.6

--- collected.nix
+++ collected.nix
@@ -2702,6 +2702,9 @@
   slixmpp = {
     "1.8.5" = import ./overrides/slixmpp/1.8.5;
   };
+  snapcast = {
+    "2.3.6" = import ./overrides/snapcast/2.3.6;
+  };
   snownlp = {
     "0.12.3" = import ./overrides/snownlp/0.12.3;
   };
