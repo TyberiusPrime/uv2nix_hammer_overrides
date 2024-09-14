686307da54168288efa329d3c3c7022eeec25d8d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 21:54:08 +0000

    autogenerated overwrites for coala==0.11.0

--- collected.nix
+++ collected.nix
@@ -2840,6 +2840,9 @@
   tensorflow-io-gcs-filesystem = {
     "0.37.1" = import ./overrides/tensorflow-io-gcs-filesystem/0.37.1;
   };
+  termcolor = {
+    "1.1.0" = import ./overrides/termcolor/1.1.0;
+  };
   testgear-api-client = {
     "3.2.0" = import ./overrides/testgear-api-client/3.2.0;
   };
