e8184cac9c0fb80eb3608da6e2f4c5b6b5dd7462
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-14 12:12:46 +0000

    autogenerated overwrites for py_vollib==1.0.1

--- collected.nix
+++ collected.nix
@@ -2023,11 +2023,17 @@
   px-pipeline = {
     "0.1.2" = import ./overrides/px-pipeline/0.1.2;
   };
+  py-lets-be-rational = {
+    "1.0.1" = import ./overrides/py-lets-be-rational/1.0.1;
+  };
   py-sonic = {
     "1.0.1" = import ./overrides/py-sonic/1.0.1;
   };
   py-ubjson = {
     "0.16.1" = import ./overrides/py-ubjson/0.16.1;
+  };
+  py-vollib = {
+    "1.0.1" = import ./overrides/py-vollib/1.0.1;
   };
   pyacoustid = {
     "1.3.0" = import ./overrides/pyacoustid/1.3.0;
