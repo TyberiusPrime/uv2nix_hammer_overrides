e1aef6d94acd11fca89f485930a448a667c4e6c9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 02:45:09 +0000

    autogenerated overwrites for pyutil==3.3.6

--- collected.nix
+++ collected.nix
@@ -2436,6 +2436,9 @@
   pyupdate = {
     "1.4.0" = import ./overrides/pyupdate/1.4.0;
   };
+  pyutil = {
+    "3.3.6" = import ./overrides/pyutil/3.3.6;
+  };
   pyuv = {
     "1.4.0" = import ./overrides/pyuv/1.4.0;
   };
