93a073d95c5248935a51250d8404e86d5c38e139
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 03:05:09 +0000

    autogenerated overwrites for monkeyhex==1.7.4

--- collected.nix
+++ collected.nix
@@ -1648,6 +1648,9 @@
   monero = {
     "1.1.1" = import ./overrides/monero/1.1.1;
   };
+  monkeyhex = {
+    "1.7.4" = import ./overrides/monkeyhex/1.7.4;
+  };
   mouseinfo = {
     "0.1.3" = import ./overrides/mouseinfo/0.1.3;
   };
