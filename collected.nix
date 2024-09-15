67240e6c58a40e9e275e4ebe6a1c47ea875deaa1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 06:43:57 +0000

    autogenerated overwrites for opentracing-instrumentation==3.3.1

--- collected.nix
+++ collected.nix
@@ -1838,6 +1838,9 @@
   opensensemap-api = {
     "0.3.2" = import ./overrides/opensensemap-api/0.3.2;
   };
+  opentracing = {
+    "2.4.0" = import ./overrides/opentracing/2.4.0;
+  };
   opster = {
     "5.0" = import ./overrides/opster/5.0;
   };
@@ -2903,6 +2906,9 @@
   };
   tornadis = {
     "0.8.1" = import ./overrides/tornadis/0.8.1;
+  };
+  tornado = {
+    "5.1.1" = import ./overrides/tornado/5.1.1;
   };
   torpy = {
     "1.1.6" = import ./overrides/torpy/1.1.6;
