41f625bcaa82f82a634c781f756b5b4a7117d238
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 02:13:33 +0000

    autogenerated overwrites for dfdatetime==20240504

--- collected.nix
+++ collected.nix
@@ -605,6 +605,9 @@
   demands = {
     "3.0.0" = import ./overrides/demands/3.0.0;
   };
+  dfdatetime = {
+    "20240504" = import ./overrides/dfdatetime/20240504;
+  };
   dictor = {
     "0.1.12" = import ./overrides/dictor/0.1.12;
   };
