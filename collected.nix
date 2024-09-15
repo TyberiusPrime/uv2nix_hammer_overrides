3c7c1611073137bc7fe2344aef9116af5fa00fbb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 21:57:15 +0000

    autogenerated overwrites for dirlistproc==1.4.5

--- collected.nix
+++ collected.nix
@@ -979,6 +979,9 @@
   dimagi-utils = {
     "1.2.3.2" = import ./overrides/dimagi-utils/1.2.3.2;
   };
+  dirlistproc = {
+    "1.4.5" = import ./overrides/dirlistproc/1.4.5;
+  };
   dirsync = {
     "2.2.5" = import ./overrides/dirsync/2.2.5;
   };
