aa85af15a8cd22b5bc328e34cca96c0d83990ac1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 02:30:47 +0000

    autogenerated overwrites for pynanoleaf==0.1.1

--- collected.nix
+++ collected.nix
@@ -2213,6 +2213,9 @@
   pynagios = {
     "0.1.1" = import ./overrides/pynagios/0.1.1;
   };
+  pynanoleaf = {
+    "0.1.1" = import ./overrides/pynanoleaf/0.1.1;
+  };
   pync = {
     "2.0.3" = import ./overrides/pync/2.0.3;
   };
