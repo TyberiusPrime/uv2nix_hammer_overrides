d8dccde0d291411ff85a12167545138bcff8a5f1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:38:17 +0000

    autogenerated overwrites for pytest-pep8==1.0.6

--- collected.nix
+++ collected.nix
@@ -2318,6 +2318,9 @@
   pytest-logging = {
     "2015.11.4" = import ./overrides/pytest-logging/2015.11.4;
   };
+  pytest-pep8 = {
+    "1.0.6" = import ./overrides/pytest-pep8/1.0.6;
+  };
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
