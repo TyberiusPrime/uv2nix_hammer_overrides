323f5924a642b90d7cb17bf7c92f43d55dff4d5e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 21:10:50 +0000

    autogenerated overwrites for pyftpdlib==2.0.0

--- collected.nix
+++ collected.nix
@@ -3651,6 +3651,9 @@
   pyflic = {
     "2.0.4" = import ./overrides/pyflic/2.0.4;
   };
+  pyftpdlib = {
+    "2.0.0" = import ./overrides/pyftpdlib/2.0.0;
+  };
   pygetwindow = {
     "0.0.9" = import ./overrides/pygetwindow/0.0.9;
   };
