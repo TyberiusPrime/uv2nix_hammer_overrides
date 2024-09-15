4844ac32f48c369ef19134feffbc4891ca95ab55
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:22:52 +0000

    autogenerated overwrites for signalrcore==0.9.5

--- collected.nix
+++ collected.nix
@@ -1678,6 +1678,9 @@
   ms-cv = {
     "0.1.1" = import ./overrides/ms-cv/0.1.1;
   };
+  msgpack = {
+    "1.0.2" = import ./overrides/msgpack/1.0.2;
+  };
   msgpack-python = {
     "0.5.6" = import ./overrides/msgpack-python/0.5.6;
   };
