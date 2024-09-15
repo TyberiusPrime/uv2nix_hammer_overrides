431a4771e74b00d66bc14053170876ce66aacdcf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 06:22:27 +0000

    autogenerated overwrites for pyls-isort==0.2.2

--- collected.nix
+++ collected.nix
@@ -2189,6 +2189,9 @@
   pylint-flask = {
     "0.6" = import ./overrides/pylint-flask/0.6;
   };
+  pyls-isort = {
+    "0.2.2" = import ./overrides/pyls-isort/0.2.2;
+  };
   pylzma = {
     "0.5.0" = import ./overrides/pylzma/0.5.0;
   };
