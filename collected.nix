9d44ec82823afc33feaba063e262ae787227511d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 04:20:45 +0000

    autogenerated overwrites for pactman==2.31.0

--- collected.nix
+++ collected.nix
@@ -1853,6 +1853,9 @@
   osascript = {
     "2020.12.3" = import ./overrides/osascript/2020.12.3;
   };
+  pactman = {
+    "2.31.0" = import ./overrides/pactman/2.31.0;
+  };
   paginator = {
     "0.5.1" = import ./overrides/paginator/0.5.1;
   };
