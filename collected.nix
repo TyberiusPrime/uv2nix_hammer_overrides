5561c74e29f9e6eeacc821340d0564948847eb67
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 05:28:19 +0000

    autogenerated overwrites for tbx==1.9.4

--- collected.nix
+++ collected.nix
@@ -2819,6 +2819,9 @@
   tap-framework = {
     "0.1.1" = import ./overrides/tap-framework/0.1.1;
   };
+  tbx = {
+    "1.9.4" = import ./overrides/tbx/1.9.4;
+  };
   telepath = {
     "0.3.1" = import ./overrides/telepath/0.3.1;
   };
