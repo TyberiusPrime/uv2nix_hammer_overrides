cd680d465d429c010165d5f91d82d0ba55fb2d13
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 00:58:41 +0000

    autogenerated overwrites for django-sendsms==0.5

--- collected.nix
+++ collected.nix
@@ -720,6 +720,9 @@
   django-rtk = {
     "0.6.0" = import ./overrides/django-rtk/0.6.0;
   };
+  django-sendsms = {
+    "0.5" = import ./overrides/django-sendsms/0.5;
+  };
   django-social-api = {
     "0.1.1" = import ./overrides/django-social-api/0.1.1;
   };
