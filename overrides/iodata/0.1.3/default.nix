e1d90a48c52ace642220d9b0a1cfdf531e71ddcf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:11:35 +0000

    autogenerated overwrites for iodata==0.1.3

---
+++ overrides/iodata/0.1.3/default.nix
@@ -0,0 +1,11 @@
+{ final, ... }:
+old:
+if ((old.format or "sdist") == "wheel") then
+  { }
+else
+  {
+    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
+      final.cython_0
+      final.setuptools
+    ];
+  }
