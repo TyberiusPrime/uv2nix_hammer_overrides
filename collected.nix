6b9cd65d3f6826a5c4c0f1bdd3847b7816bbb7b4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 01:20:48 +0000

    autogenerated overwrites for python-fedora==1.1.1

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
     "2.9.0" = import ./overrides/python-dateutil/2.9.0;
     "2.9.0.post0" = import ./overrides/python-dateutil/2.9.0.post0;
   };
+  python-fedora = {
+    "1.1.1" = import ./overrides/python-fedora/1.1.1;
+  };
   python-freeipa = {
     "1.0.8" = import ./overrides/python-freeipa/1.0.8;
   };
