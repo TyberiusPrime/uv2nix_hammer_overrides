4ecd01c3e65ff56b967380ea676bd9cb1a812235
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:59:33 +0000

    autogenerated overwrites for tw2.core==2.3.0

--- collected.nix
+++ collected.nix
@@ -2961,6 +2961,9 @@
   tubeup = {
     "2023.9.19" = import ./overrides/tubeup/2023.9.19;
   };
+  tw2-core = {
+    "2.3.0" = import ./overrides/tw2-core/2.3.0;
+  };
   twitter-common-contextutil = {
     "0.3.11" = import ./overrides/twitter-common-contextutil/0.3.11;
   };
