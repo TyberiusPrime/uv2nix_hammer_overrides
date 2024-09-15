8ea8b8befb85e95dae15389a8c71f1fbcf28df81
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:57:29 +0000

    autogenerated overwrites for simplesam==0.1.4.1

--- collected.nix
+++ collected.nix
@@ -2684,6 +2684,9 @@
   simplejson = {
     "3.11.1" = import ./overrides/simplejson/3.11.1;
   };
+  simplesam = {
+    "0.1.4.1" = import ./overrides/simplesam/0.1.4.1;
+  };
   singer-python = {
     "5.1.5" = import ./overrides/singer-python/5.1.5;
   };
