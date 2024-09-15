cd46fdfb78779f5cac62edbb6bea4a02831db3c2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:49:22 +0000

    autogenerated overwrites for tvdb_api==3.1.0

--- collected.nix
+++ collected.nix
@@ -2961,6 +2961,9 @@
   tubeup = {
     "2023.9.19" = import ./overrides/tubeup/2023.9.19;
   };
+  tvdb-api = {
+    "3.1.0" = import ./overrides/tvdb-api/3.1.0;
+  };
   twitter-common-contextutil = {
     "0.3.11" = import ./overrides/twitter-common-contextutil/0.3.11;
   };
