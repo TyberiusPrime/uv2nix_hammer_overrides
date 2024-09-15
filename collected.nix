14811d1f168a30e456bfb194138ca746159da36d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 06:43:32 +0000

    autogenerated overwrites for django-compat==1.0.15

--- collected.nix
+++ collected.nix
@@ -660,6 +660,9 @@
   django-celery-beat = {
     "2.6.0" = import ./overrides/django-celery-beat/2.6.0;
   };
+  django-compat = {
+    "1.0.15" = import ./overrides/django-compat/1.0.15;
+  };
   django-cratis = {
     "0.9.1" = import ./overrides/django-cratis/0.9.1;
   };
