d908f259b1069f1dfeb7edf61cb50e623aca0377
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:28:33 +0000

    autogenerated overwrites for django-settings-export==1.2.1

--- collected.nix
+++ collected.nix
@@ -720,6 +720,9 @@
   django-rtk = {
     "0.6.0" = import ./overrides/django-rtk/0.6.0;
   };
+  django-settings-export = {
+    "1.2.1" = import ./overrides/django-settings-export/1.2.1;
+  };
   django-social-api = {
     "0.1.1" = import ./overrides/django-social-api/0.1.1;
   };
