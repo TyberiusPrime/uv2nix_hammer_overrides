5b6b22d0ab77af2be50c15274f72ef355e6f7467
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:46:42 +0000

    autogenerated overwrites for rstview==0.4.0

--- collected.nix
+++ collected.nix
@@ -2585,6 +2585,9 @@
   rstgen = {
     "24.4.0" = import ./overrides/rstgen/24.4.0;
   };
+  rstview = {
+    "0.4.0" = import ./overrides/rstview/0.4.0;
+  };
   runcmd = {
     "2020.12.3" = import ./overrides/runcmd/2020.12.3;
   };
