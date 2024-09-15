d2db56df1a0f7a1c3ba4a3f6a0fed5c695e47a87
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:38:32 +0000

    autogenerated overwrites for python-yubico==1.3.3

--- collected.nix
+++ collected.nix
@@ -2406,6 +2406,9 @@
   python-wink = {
     "1.10.5" = import ./overrides/python-wink/1.10.5;
   };
+  python-yubico = {
+    "1.3.3" = import ./overrides/python-yubico/1.3.3;
+  };
   python3-xlib = {
     "0.15" = import ./overrides/python3-xlib/0.15;
   };
