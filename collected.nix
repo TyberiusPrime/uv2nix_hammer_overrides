552ab6efb46274c58aeb7ce3c0c96b9fc25b05d7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 07:33:51 +0000

    autogenerated overwrites for openimis-be-calculation==1.7.1

--- collected.nix
+++ collected.nix
@@ -672,6 +672,9 @@
   django-datetime-widget = {
     "0.9.3" = import ./overrides/django-datetime-widget/0.9.3;
   };
+  django-db-signals = {
+    "0.1.1" = import ./overrides/django-db-signals/0.1.1;
+  };
   django-devserver = {
     "0.8.0" = import ./overrides/django-devserver/0.8.0;
   };
