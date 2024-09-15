eba8f0010c6dba4d5f2faa95cc19e94f8083e767
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-15 00:52:46 +0000

    autogenerated overwrites for unsync==1.4.0

--- collected.nix
+++ collected.nix
@@ -3015,6 +3015,9 @@
   unqlite = {
     "0.9.6" = import ./overrides/unqlite/0.9.6;
   };
+  unsync = {
+    "1.4.0" = import ./overrides/unsync/1.4.0;
+  };
   upyun = {
     "2.5.5" = import ./overrides/upyun/2.5.5;
   };
