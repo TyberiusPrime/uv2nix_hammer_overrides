cdfe643d92ea4aca7d3294560cef6b63ad17d1c3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 07:31:34 +0000

    autogenerated overwrites for Brownie==0.5.1

--- collected.nix
+++ collected.nix
@@ -454,6 +454,9 @@
   brightway2 = {
     "2.4.6" = import ./overrides/brightway2/2.4.6;
   };
+  brownie = {
+    "0.5.1" = import ./overrides/brownie/0.5.1;
+  };
   bs4 = {
     "0.0.1" = import ./overrides/bs4/0.0.1;
   };
