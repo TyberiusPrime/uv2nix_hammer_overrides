6ef9b4e70355bf658793037650bef1e4dde4ed4f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 21:00:34 +0000

    autogenerated overwrites for twitter.common.quantity==0.3.11

--- collected.nix
+++ collected.nix
@@ -5180,6 +5180,9 @@
   twitter-common-options = {
     "0.3.11" = import ./overrides/twitter-common-options/0.3.11;
   };
+  twitter-common-quantity = {
+    "0.3.11" = import ./overrides/twitter-common-quantity/0.3.11;
+  };
   twitter-common-util = {
     "0.3.11" = import ./overrides/twitter-common-util/0.3.11;
   };
