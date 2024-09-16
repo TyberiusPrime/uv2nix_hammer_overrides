e89dd0a0cfb4a7830c89c7d5afcb06a7209e2e10
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 05:42:12 +0000

    autogenerated overwrites for rova==0.4.1

--- collected.nix
+++ collected.nix
@@ -4405,6 +4405,9 @@
   rouge-score = {
     "0.1.2" = import ./overrides/rouge-score/0.1.2;
   };
+  rova = {
+    "0.4.1" = import ./overrides/rova/0.4.1;
+  };
   rpcq = {
     "3.11.0" = import ./overrides/rpcq/3.11.0;
   };
