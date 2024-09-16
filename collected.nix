31d0ca43487cc00b315e9963465da03507dadb5c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 10:55:03 +0000

    autogenerated overwrites for Flask-Uploads==0.2.1

--- collected.nix
+++ collected.nix
@@ -1615,6 +1615,9 @@
   flask-themes2 = {
     "1.0.1" = import ./overrides/flask-themes2/1.0.1;
   };
+  flask-uploads = {
+    "0.2.1" = import ./overrides/flask-uploads/0.2.1;
+  };
   flask-user = {
     "1.0.2.2" = import ./overrides/flask-user/1.0.2.2;
   };
