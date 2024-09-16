b6341cb33e7fa00af1b130f39c38f1047a647174
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 22:11:46 +0000

    autogenerated overwrites for openimis-be-core==1.7.2

--- collected.nix
+++ collected.nix
@@ -2893,6 +2893,9 @@
   neologdn = {
     "0.5.3" = import ./overrides/neologdn/0.5.3;
   };
+  nepalicalendar = {
+    "0.4" = import ./overrides/nepalicalendar/0.4;
+  };
   nested-lookup = {
     "0.2.25" = import ./overrides/nested-lookup/0.2.25;
   };
