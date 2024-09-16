98b0670be1859a07032ae505744bfd673ec4e271
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 09:46:39 +0000

    autogenerated overwrites for cs.sh==20210316

--- collected.nix
+++ collected.nix
@@ -831,6 +831,9 @@
   cs-ansi-colour = {
     "20220227" = import ./overrides/cs-ansi-colour/20220227;
   };
+  cs-sh = {
+    "20210316" = import ./overrides/cs-sh/20210316;
+  };
   cs-timeutils = {
     "20211208" = import ./overrides/cs-timeutils/20211208;
   };
