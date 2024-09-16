dff5b8bd6710faacb7f6cd7d1d20c01aed62e7ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 23:43:45 +0000

    autogenerated overwrites for fastkml==0.12

--- collected.nix
+++ collected.nix
@@ -3651,6 +3651,9 @@
   pyflic = {
     "2.0.4" = import ./overrides/pyflic/2.0.4;
   };
+  pygeoif = {
+    "0.7" = import ./overrides/pygeoif/0.7;
+  };
   pygetwindow = {
     "0.0.9" = import ./overrides/pygetwindow/0.0.9;
   };
