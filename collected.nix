5c1fbd190d45cf443999c2b410c85a462a3430b6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 04:14:55 +0000

    autogenerated overwrites for sphinxcontrib-ditaa==1.0.2

--- collected.nix
+++ collected.nix
@@ -4699,6 +4699,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinxcontrib-ditaa = {
+    "1.0.2" = import ./overrides/sphinxcontrib-ditaa/1.0.2;
+  };
   sphinxcontrib-fulltoc = {
     "1.2.0" = import ./overrides/sphinxcontrib-fulltoc/1.2.0;
   };
