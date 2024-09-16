07efbae66995f8e01d016f174e66daa1b9e2ad01
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 01:51:16 +0000

    autogenerated overwrites for marionette-harness==5.0.0

--- collected.nix
+++ collected.nix
@@ -454,6 +454,9 @@
   brightway2 = {
     "2.4.6" = import ./overrides/brightway2/2.4.6;
   };
+  browsermob-proxy = {
+    "0.8.0" = import ./overrides/browsermob-proxy/0.8.0;
+  };
   bs4 = {
     "0.0.1" = import ./overrides/bs4/0.0.1;
   };
@@ -4195,6 +4198,9 @@
   };
   pywatchman = {
     "2.0.0" = import ./overrides/pywatchman/2.0.0;
+  };
+  pywebsocket3 = {
+    "4.0.2" = import ./overrides/pywebsocket3/4.0.2;
   };
   pywurfl = {
     "7.2.1" = import ./overrides/pywurfl/7.2.1;
