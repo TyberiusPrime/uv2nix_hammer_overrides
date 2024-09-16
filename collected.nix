d31939ef098e9ba28eb18057cf6e30c696ab9e2b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 22:35:02 +0000

    autogenerated overwrites for snakebite==2.11.0

--- collected.nix
+++ collected.nix
@@ -4648,6 +4648,9 @@
   smbus-cffi = {
     "0.5.1" = import ./overrides/smbus-cffi/0.5.1;
   };
+  snakebite = {
+    "2.11.0" = import ./overrides/snakebite/2.11.0;
+  };
   snakecase = {
     "1.0.1" = import ./overrides/snakecase/1.0.1;
   };
