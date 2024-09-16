8a5c854b7002d324f874a80dcba68fe8544bb681
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 23:04:22 +0000

    autogenerated overwrites for django-haystack==3.3.0

---
+++ overrides/django-haystack/3.3.0/default.nix
@@ -0,0 +1,12 @@
+{ final, ... }:
+old:
+if ((old.format or "sdist") == "wheel") then
+  { }
+else
+  {
+    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
+      final.setuptools-scm
+      final.setuptools
+      final.wheel
+    ];
+  }
