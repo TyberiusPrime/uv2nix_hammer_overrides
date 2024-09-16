56649ce87eaf29b6db22ace6cd5213a189d71510
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 04:55:15 +0000

    autogenerated overwrites for localstack-client==2.5

--- collected.nix
+++ collected.nix
@@ -2491,6 +2491,9 @@
   lmtpd = {
     "6.2.0" = import ./overrides/lmtpd/6.2.0;
   };
+  localstack-client = {
+    "2.5" = import ./overrides/localstack-client/2.5;
+  };
   lock = {
     "2018.3.25.2110" = import ./overrides/lock/2018.3.25.2110;
   };
