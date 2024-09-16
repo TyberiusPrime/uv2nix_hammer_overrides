7062a47d43c225faac7e33593002e632e660fcdb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-16 20:24:17 +0000

    autogenerated overwrites for sphinx-serve==1.0.1

--- collected.nix
+++ collected.nix
@@ -4699,6 +4699,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-serve = {
+    "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
+  };
   sphinxcontrib-fulltoc = {
     "1.2.0" = import ./overrides/sphinxcontrib-fulltoc/1.2.0;
   };
