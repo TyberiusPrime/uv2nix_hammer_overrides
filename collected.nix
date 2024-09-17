3b0e05c261655bf7aac4705ac3f021a9efa2f21d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-17 00:54:08 +0000

    autogenerated overwrites for aiologger==0.7.0

--- collected.nix
+++ collected.nix
@@ -74,6 +74,9 @@
   aiolivisi = {
     "0.0.19" = import ./overrides/aiolivisi/0.0.19;
   };
+  aiologger = {
+    "0.7.0" = import ./overrides/aiologger/0.7.0;
+  };
   aioopenssl = {
     "0.6.0" = import ./overrides/aioopenssl/0.6.0;
   };
