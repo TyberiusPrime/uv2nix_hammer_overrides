032d52d3f63f1b08718916e48bd5e8dc784a98ad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-17 09:48:43 +0000

    autogenerated overwrites for statistics==1.0.3.5

--- collected.nix
+++ collected.nix
@@ -4769,6 +4769,9 @@
   static3 = {
     "0.7.0" = import ./overrides/static3/0.7.0;
   };
+  statistics = {
+    "1.0.3.5" = import ./overrides/statistics/1.0.3.5;
+  };
   stdiomask = {
     "0.0.6" = import ./overrides/stdiomask/0.0.6;
   };
