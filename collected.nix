60c33cd9f16f0b4a147a14881b311d330c7039d8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-17 08:02:41 +0000

    autogenerated overwrites for pyforge==1.3.0

--- collected.nix
+++ collected.nix
@@ -3651,6 +3651,9 @@
   pyflic = {
     "2.0.4" = import ./overrides/pyflic/2.0.4;
   };
+  pyforge = {
+    "1.3.0" = import ./overrides/pyforge/1.3.0;
+  };
   pygetwindow = {
     "0.0.9" = import ./overrides/pygetwindow/0.0.9;
   };
