cf71eb3fb40a1d1c7860064d5694df802ffe2887
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-17 07:01:14 +0000

    autogenerated overwrites for platformio==6.1.15

--- collected.nix
+++ collected.nix
@@ -3311,6 +3311,9 @@
   placebo = {
     "0.10.0" = import ./overrides/placebo/0.10.0;
   };
+  platformio = {
+    "6.1.15" = import ./overrides/platformio/6.1.15;
+  };
   plotille = {
     "5.0.0" = import ./overrides/plotille/5.0.0;
   };
