f337daf187327923a0173a28b2d4f984371b7922
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-17 21:18:00 +0000

    autogenerated overwrites for lightparam==0.4.6

---
+++ overrides/pyqt5/5.15.11/default.nix
@@ -0,0 +1,60 @@
+{final, prev, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // {dontWrapQtApps = true;}) (old: old // ( let
+  inherit (pkgs) lib;
+  selectQt5 = version: let
+    selector = builtins.concatStringsSep "" (lib.take 2 (builtins.splitVersion version));
+  in
+    pkgs."qt${selector}" or pkgs.qt5;
+
+  qt5 = selectQt5 prev.pyqt5-qt5.version;
+  pyQt5Modules = qt5:
+    with qt5; [
+      qt3d
+      qtbase
+      qtcharts
+      qtconnectivity
+      qtdatavis3d
+      qtdeclarative
+      qtgamepad
+      qtlocation
+      qtmultimedia
+      qtsensors
+      qtserialport
+      qtsvg
+      qtwebchannel
+      qtwebengine
+      qtwebsockets
+      qtx11extras
+      qtxmlpatterns
+      qtquick3d
+      qtremoteobjects
+      qttools
+
+    ];
+in {
+  postPatch = ''
+    # Confirm license, if project.py exists
+    if test -f project.py; then
+      sed -i s/"if tool == 'pep517':"/"if True:"/ project.py
+    fi
+  '';
+
+  dontConfigure = true;
+  dontWrapQtApps = true;
+  nativeBuildInputs =
+    old.nativeBuildInputs
+    or []
+    ++ pyQt5Modules qt5
+    ++ [
+      final.pyqt-builder
+      final.sip
+    ];
+    buildInputs = old.buildInputs or [] ++ pyQt5Modules qt5;
+  autoPatchelfIgnoreMissingDeps = ["libQt5TextToSpeech.so.5"]; # no clue if this is correct.
+}
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
