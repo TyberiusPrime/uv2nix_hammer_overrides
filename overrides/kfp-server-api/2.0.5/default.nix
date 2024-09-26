1320f6e45b903666d267bdc54dcbd99eb8d8c8e2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-26 12:50:25 +0000

    pyproject.nix pkg 2

--- overrides/kfp-server-api/2.0.5/default.nix
+++ overrides/kfp-server-api/2.0.5/default.nix
@@ -1,6 +1,3 @@
-{ final, ... }:
-old:
-if ((old.format or "sdist") == "wheel") then
-  { }
-else
-  { nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ final.setuptools ]; }
+{resolveBuildSystem, final, ...}
+        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+        
