b9c0a5b17eb77226249baac7a5597f285c8a8025
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-27 12:54:41 +0200

    unzip has been take care of upstream

--- overrides/vapoursynth/70/default.nix
+++ overrides/vapoursynth/70/default.nix
@@ -1,3 +1,3 @@
 {resolveBuildSystem, final, pkgs, ...}
-        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.unzip pkgs.vapoursynth] ++ ( resolveBuildSystem {cython = [];setuptools = [];});}
+        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.vapoursynth] ++ ( resolveBuildSystem {cython = [];setuptools = [];});}
         
