b078aa02fb8da6d9fa2cb36c544cba8b4e4044a5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-27 15:15:29 +0200

    psutils 6.0.0

---
+++ overrides/psutil/6.0.0/default.nix
@@ -0,0 +1,12 @@
+{resolveBuildSystem, ...}: old:
+if ((old.format or "sdist") == "wheel")
+then {}
+else {
+  nativeBuildInputs =
+    old.nativeBuildInputs
+    or []
+    ++ (resolveBuildSystem {
+      setuptools = [];
+      wheel = [];
+    });
+}
