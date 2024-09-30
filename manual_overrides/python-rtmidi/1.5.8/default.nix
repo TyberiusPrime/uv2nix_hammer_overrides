b18c4bf227cb03f26a6d08b862f70c40e8c7e6ee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-09-30 13:54:08 +0000

    python-rtmidi

---
+++ manual_overrides/python-rtmidi/1.5.8/default.nix
@@ -0,0 +1,32 @@
+{
+  nativeBuildInputs =
+    old.nativeBuildInputs
+    or []
+    ++ pkgs.lib.optionals
+    (((old.format or "sdist") == "wheel")
+      && pkgs.stdenv.isLinux) [
+      pkgs.libjack2
+      pkgs.alsa-lib.out
+    ]
+    ++ pkgs.lib.optionals
+    (((old.format or "sdist") == "wheel")
+      && pkgs.stdenv.isDarwin) [
+      pkgs.CoreAudio
+      pkgs.CoreMIDI
+      pkgs.pkgs.CoreServices
+      pkgs.Foundation
+    ];
+  builtInputs =
+    old.builtInputs
+    or []
+    ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+      pkgs.libjack2
+      pkgs.alsa-lib.out
+    ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.CoreAudio
+      pkgs.CoreMIDI
+      pkgs.pkgs.CoreServices
+      pkgs.Foundation
+    ];
+}
