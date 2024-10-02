d2896f669a2e666626ea3134ce1c441c135240a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-02 23:05:18 +0000

    autogenerated overwrites for evdev==1.7.1

---
+++ overrides/evdev/1.7.1/default.nix
@@ -0,0 +1,7 @@
+{resolveBuildSystem, final, pkgs, ...}
+        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.linuxHeaders] ++ ( resolveBuildSystem {setuptools = [];});postPatch = (old.postPatch or "")+(''
+                if [ -e setup.py ]; then
+                     substituteInPlace setup.py --replace-quiet /usr/include ${pkgs.linuxHeaders}/include
+                fi
+                '');}
+        
