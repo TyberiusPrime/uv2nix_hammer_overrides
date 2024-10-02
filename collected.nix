4147569bce15a1b0774d94fac2c2a5ea51779db4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-02 14:52:22 +0000

    autogenerated overwrites for python-lzo==1.15

--- collected.nix
+++ collected.nix
@@ -6109,6 +6109,9 @@
   python-lzf = {
     "0.2.6" = import ./overrides/python-lzf/0.2.6;
   };
+  python-lzo = {
+    "1.15" = import ./overrides/python-lzo/1.15;
+  };
   python-modargs = {
     "1.7" = import ./overrides/python-modargs/1.7;
   };
