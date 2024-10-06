e563253bce8fd69c54b043a93303c27a237d1449
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-06 11:48:17 +0000

    autogenerated overwrites for pin==2.7.0

---
+++ overrides/cmeel-urdfdom/3.1.1.1/default.nix
@@ -0,0 +1,3 @@
+{pkgs, ...}
+        : old: {buildInputs = old.buildInputs or [] ++ [pkgs.console-bridge pkgs.tinyxml];}
+        
---
+++ overrides/hpp-fcl/2.4.4/default.nix
@@ -0,0 +1,16 @@
+{pkgs, final, ...}
+        : old: if ((old.format or "sdist") == "wheel") then {buildInputs = old.buildInputs or [] ++ [
+             (pkgs.boost183.override {
+                 python = final.python;
+                 numpy = final.numpy;
+                 enablePython = true;
+             })
+              final.eigenpy pkgs.assimp pkgs.boost183 pkgs.octomap pkgs.qhull];preFixup = ''addAutoPatchelfSearchPath ${final.eigenpy}/${final.python.sitePackages}/cmeel.prefix/${final.python.sitePackages}
+'';} else {buildInputs = old.buildInputs or [] ++ [
+             (pkgs.boost183.override {
+                 python = final.python;
+                 numpy = final.numpy;
+                 enablePython = true;
+             })
+              final.eigenpy pkgs.assimp pkgs.boost183 pkgs.octomap pkgs.qhull];}
+        
