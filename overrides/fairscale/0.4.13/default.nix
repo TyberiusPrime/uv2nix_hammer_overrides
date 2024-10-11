a3418d98770fe8ca0e20d707133c15129aee0aa0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-11 08:49:04 +0000

    autogenerated overwrites for fairscale==0.4.13

---
+++ overrides/fairscale/0.4.13/default.nix
@@ -0,0 +1,5 @@
+{resolveBuildSystem, final, helpers, ...}
+        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {ninja = [];setuptools = [];wheel = [];});postPatch = (old.postPatch or "")+(''
+                ${helpers.tomlreplace} pyproject.toml build-system.requires "[]"
+        '');}
+        
