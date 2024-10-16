dc53807e19622e0d94681f6f7480eb0c19d0013d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-16 09:51:15 +0000

    autogenerated overwrites (commit to be squashed)

--- overrides/onnx-simplifier/0.4.36/default.nix
+++ overrides/onnx-simplifier/0.4.36/default.nix
@@ -1,3 +1,3 @@
 {resolveBuildSystem, final, ...}
-        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cmake = [];ninja = [];setuptools = [];wheel = [];});}
+        : old: if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cmake = [];ninja = [];pytest-runner = [];setuptools = [];wheel = [];});}
         
