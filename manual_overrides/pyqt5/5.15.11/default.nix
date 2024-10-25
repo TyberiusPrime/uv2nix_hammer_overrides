9422c49dbc996cf932fb856c65a2469a0f31789a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-25 12:57:04 +0000

    autogenerated overwrites for pyqt-resource-helper==0.0.14

--- manual_overrides/pyqt5/5.15.11/default.nix
+++ manual_overrides/pyqt5/5.15.11/default.nix
@@ -1,37 +1,37 @@
-(
-  if (old.format or "sdist" == "sdist")
-  then let
-    inherit (pkgs) lib;
-    selectQt5 = version: let
-      selector = builtins.concatStringsSep "" (lib.take 2 (builtins.splitVersion version));
-    in
-      pkgs."qt${selector}" or pkgs.qt5;
+let
+  inherit (pkgs) lib;
+  selectQt5 = version: let
+    selector = builtins.concatStringsSep "" (lib.take 2 (builtins.splitVersion version));
+  in
+    pkgs."qt${selector}" or pkgs.qt5;
 
-    qt5 = selectQt5 prev.pyqt5-qt5.version;
-    pyQt5Modules = qt5:
-      with qt5; [
-        qt3d
-        qtbase
-        qtcharts
-        qtconnectivity
-        qtdatavis3d
-        qtdeclarative
-        qtgamepad
-        qtlocation
-        qtmultimedia
-        qtsensors
-        qtserialport
-        qtsvg
-        qtwebchannel
-        qtwebengine
-        qtwebsockets
-        qtx11extras
-        qtxmlpatterns
-        qtquick3d
-        qtremoteobjects
-        qttools
-      ];
-  in {
+  qt5 = selectQt5 prev.pyqt5-qt5.version;
+  pyQt5Modules = qt5:
+    with qt5; [
+      qt3d
+      qtbase
+      qtcharts
+      qtconnectivity
+      qtdatavis3d
+      qtdeclarative
+      qtgamepad
+      qtlocation
+      qtmultimedia
+      qtsensors
+      qtserialport
+      qtsvg
+      qtwebchannel
+      qtwebengine
+      qtwebsockets
+      qtx11extras
+      qtxmlpatterns
+      qtquick3d
+      qtremoteobjects
+      qttools
+    ];
+in (
+  if (old.format or "sdist" == "sdist")
+  then {
     postPatch = ''
       # Confirm license, if project.py exists
       if test -f project.py; then
@@ -52,5 +52,8 @@
     buildInputs = old.buildInputs or [] ++ pyQt5Modules qt5;
     autoPatchelfIgnoreMissingDeps = ["libQt5TextToSpeech.so.5"]; # no clue if this is correct.
   }
-  else {}
+  else {
+    buildInputs = old.buildInputs or [] ++ pyQt5Modules qt5;
+    autoPatchelfIgnoreMissingDeps = ["libQt5TextToSpeech.so.5"]; # no clue if this is correct.
+  }
 )
