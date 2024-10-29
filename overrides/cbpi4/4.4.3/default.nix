38f38a9e68754771d996ae9a3ad01492193eebdb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-10-29 12:53:07 +0000

    autogenerated overwrites for cbpi4==4.4.3

--- overrides/cbpi4/4.4.3/default.nix
+++ overrides/cbpi4/4.4.3/default.nix
@@ -1,5 +1,7 @@
-{final, pkgs, ...}
-        : old: {
+{resolveBuildSystem, final, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
   postInstall =
     old.postInstall
     or ""
@@ -7,5 +9,7 @@
       rm -rf $out/${final.python.sitePackages}/tests
     '';
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
