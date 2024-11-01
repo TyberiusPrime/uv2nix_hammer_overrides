f210e9affd63b39f0718205c0baec2ffd4016ca1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-01 09:22:30 +0000

    autogenerated overwrites for web3==7.2.0

--- collected.nix
+++ collected.nix
@@ -5976,6 +5976,9 @@
   pymsteams = {
     "0.2.3" = import ./overrides/pymsteams/0.2.3;
   };
+  pymummer = {
+    "0.11.0" = import ./overrides/pymummer/0.11.0;
+  };
   pynagios = {
     "0.1.1" = import ./overrides/pynagios/0.1.1;
   };
@@ -7127,6 +7130,12 @@
   };
   scapy-python3 = {
     "0.26" = import ./overrides/scapy-python3/0.26;
+  };
+  sceptre-cmd-resolver = {
+    "2.0.0" = import ./overrides/sceptre-cmd-resolver/2.0.0;
+  };
+  sceptre-file-resolver = {
+    "1.0.6" = import ./overrides/sceptre-file-resolver/1.0.6;
   };
   scheduler-cron = {
     "0.1" = import ./overrides/scheduler-cron/0.1;
