4b86a575a869296520b288fd58e6535e94de812b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-01 09:45:55 +0000

    autogenerated overwrites for pyimpfuzzy==0.5

---
+++ overrides/pyimpfuzzy/0.5/default.nix
@@ -0,0 +1,10 @@
+{resolveBuildSystem, final, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
+  buildInputs = [pkgs.ssdeep];
+}
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
