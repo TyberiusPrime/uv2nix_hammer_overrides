4c6cd0a0008ce45666f5dd2c04ee270b596aea4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-05 09:31:36 +0000

    autogenerated overwrites for arcgis==2.3.1

---
+++ manual_overrides/cbra/1.1.10/default.nix
@@ -0,0 +1,10 @@
+{
+  postInstall =
+    old.postInstall
+    or ""
+    + ''
+      rm -rf $out/${final.python.sitePackages}/docs/
+      rm -rf $out/${final.python.sitePackages}/tests/
+      rm -rf $out/${final.python.sitePackages}/examples/
+    '';
+}
---
+++ overrides/arcgis/2.3.1/default.nix
@@ -0,0 +1,12 @@
+{resolveBuildSystem, final, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if ((old.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
+  postPatch = pkgs.lib.optionalString (old.format or "sdist" == "sdist") ''
+    substituteInPlace setup.py --replace-warn "conda_install_mode = False" "conda_install_mode = True"
+  '';
+}
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
