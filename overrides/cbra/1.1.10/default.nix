b773ac0803f9f9345ddd5254e856989c44d2ebe9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-05 10:30:24 +0000

    autogenerated overwrites for sass==2.3

---
+++ overrides/cbra/1.1.10/default.nix
@@ -0,0 +1,13 @@
+{final, pkgs, ...}
+        : old: {
+  postInstall =
+    old.postInstall
+    or ""
+    + ''
+      rm -rf $out/${final.python.sitePackages}/docs/
+      rm -rf $out/${final.python.sitePackages}/tests/
+      rm -rf $out/${final.python.sitePackages}/examples/
+    '';
+}
+
+        
