ebe67a6773606107fe239ca7dade0861a3c906af
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-07 15:25:02 +0000

    autogenerated overwrites for accupy==0.3.6

--- overrides/pyimpfuzzy/0.5/default.nix
+++ overrides/pyimpfuzzy/0.5/default.nix
@@ -1,23 +1,10 @@
-{
-  resolveBuildSystem,
-  final,
-  pkgs,
-  ...
+{resolveBuildSystem, final, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if ((old.passthru.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
+  buildInputs = [pkgs.ssdeep];
 }
-: old: let
-  funcs = [
-    (old:
-      old
-      // (
-        if ((old.passthru.format or "sdist") == "wheel")
-        then {}
-        else {nativeBuildInputs = old.nativeBuildInputs or [] ++ (resolveBuildSystem {setuptools = [];});}
-      ))
-    (old:
-      old
-      // {
-        buildInputs = [pkgs.ssdeep];
-      })
-  ];
-in
-  pkgs.lib.trivial.pipe old funcs
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
---
+++ overrides/torch/2.4.1/default.nix
@@ -0,0 +1,9 @@
+{final, pkgs, ...}
+        : old: {
+  postFixup = ''
+    rm -rf $out/${final.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+    ln -sfn $out/${final.python.sitePackages}/nvidia/*/lib/lib*.so* $out/lib
+  '';
+}
+
+        
