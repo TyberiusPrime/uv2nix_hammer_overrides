2465b551ece9abe951f081f7b22418af95141b5c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-11 11:54:22 +0000

    autogenerated overwrites for pycocotools==2.0.8

--- overrides/cbra/1.1.10/default.nix
+++ overrides/cbra/1.1.10/default.nix
@@ -1,30 +1,17 @@
-{
-  resolveBuildSystem,
-  final,
-  pkgs,
-  ...
+{resolveBuildSystem, final, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if ((old.passthru.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
+  postInstall =
+    old.postInstall
+    or ""
+    + ''
+      rm -rf $out/${final.python.sitePackages}/docs/
+      rm -rf $out/${final.python.sitePackages}/tests/
+      rm -rf $out/${final.python.sitePackages}/examples/
+    '';
 }
-: old: let
-  funcs = [
-    (old:
-      old
-      // (
-        if ((old.passthru.format or "sdist") == "wheel")
-        then {}
-        else {nativeBuildInputs = old.nativeBuildInputs or [] ++ (resolveBuildSystem {setuptools = [];});}
-      ))
-    (old:
-      old
-      // {
-        postInstall =
-          old.postInstall
-          or ""
-          + ''
-            rm -rf $out/${final.python.sitePackages}/docs/
-            rm -rf $out/${final.python.sitePackages}/tests/
-            rm -rf $out/${final.python.sitePackages}/examples/
-          '';
-      })
-  ];
-in
-  pkgs.lib.trivial.pipe old funcs
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
