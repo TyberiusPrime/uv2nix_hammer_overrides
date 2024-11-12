1d9f6ef21d87afdfa16fe5c34e6a8be6669d6ec7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-12 12:08:19 +0000

    autogenerated overwrites for pyjq==2.6.0

--- overrides/opencv-contrib-python/4.9.0.80/default.nix
+++ overrides/opencv-contrib-python/4.9.0.80/default.nix
@@ -9,8 +9,7 @@
     substituteInPlace setup.py --replace-fail '[ r"python/cv2/py.typed" ] if sys.version_info >= (3, 6) else []' "[]" \
     --replace-fail 'rearrange_cmake_output_data["cv2.typing"] = ["python/cv2" + r"/typing/.*\.py"]' "pass"
   '';
-}
-))];
+}))];
             in
             pkgs.lib.trivial.pipe old funcs
     
