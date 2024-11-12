7aa0fb93ca44370908e8099bae463bda6f55808e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-12 15:36:23 +0000

    autogenerated overwrites for torch==2.4.1

--- collected.nix
+++ collected.nix
@@ -8124,6 +8124,9 @@
   toolwrapper = {
     "2.1.0" = import ./overrides/toolwrapper/2.1.0;
   };
+  torch = {
+    "2.4.1" = import ./overrides/torch/2.4.1;
+  };
   torch-scatter = {
     "2.1.2" = import ./overrides/torch-scatter/2.1.2;
   };
