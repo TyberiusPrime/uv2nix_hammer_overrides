51cc5a49d04e22ca070db671ef7eb042d567405e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-13 12:04:34 +0000

    autogenerated overwrites for tbb==2021.13.1

--- overrides/tbb/2021.13.1/default.nix
+++ overrides/tbb/2021.13.1/default.nix
@@ -1,6 +1,10 @@
-{pkgs, ...}
-        : old: {
+{helpers, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( {buildInputs = old.buildInputs or [] ++ [pkgs.hwloc];})) (old: old // ( {
    autoPatchelfIgnoreMissingDeps = ["libhwloc.so.5"]; # it finds libhwloc.so.15, so I'll go with 'this should work'
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
