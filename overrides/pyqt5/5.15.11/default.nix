244c4e1a69f6144a5b8293d8e203b4e14b90e092
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-13 13:15:16 +0000

    autogenerated overwrites for torch-sparse==0.6.18

--- overrides/pyqt5/5.15.11/default.nix
+++ overrides/pyqt5/5.15.11/default.nix
@@ -1,99 +1,67 @@
-{
-  prev,
-  helpers,
-  resolveBuildSystem,
-  pkgs,
-  final,
-  ...
-}:
-old:
-let
-  funcs = [
-    (
-      old:
-      old
-      // (
-        if (helpers.isWheel old) then
-          { dontWrapQtApps = true; }
-        else
-          {
-            dontWrapQtApps = true;
-            nativeBuildInputs =
-              old.nativeBuildInputs or [ ]
-              ++ (resolveBuildSystem {
-                pyqt-builder = [ ];
-                setuptools = [ ];
-                sip = [ ];
-              });
-          }
-      )
-    )
-    (
-      old:
-      old
-      // (
-        let
-          inherit (pkgs) lib;
-          selectQt5 =
-            version:
-            let
-              selector = builtins.concatStringsSep "" (lib.take 2 (builtins.splitVersion version));
-            in
-            pkgs."qt${selector}" or pkgs.qt5;
+{final, helpers, pkgs, prev, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {dontWrapQtApps = true;} else {dontWrapQtApps = true;nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {pyqt-builder = [];setuptools = [];sip = [];});})) (old: old // ( let
+  inherit (pkgs) lib;
+  selectQt5 =
+    version:
+    let
+      selector = builtins.concatStringsSep "" (lib.take 2 (builtins.splitVersion version));
+    in
+    pkgs."qt${selector}" or pkgs.qt5;
 
-          qt5 = selectQt5 prev.pyqt5-qt5.version;
-          pyQt5Modules =
-            qt5: with qt5; [
-              qt3d
-              qtbase
-              qtcharts
-              qtconnectivity
-              qtdatavis3d
-              qtdeclarative
-              qtgamepad
-              qtlocation
-              qtmultimedia
-              qtsensors
-              qtserialport
-              qtsvg
-              qtwebchannel
-              qtwebengine
-              qtwebsockets
-              qtx11extras
-              qtxmlpatterns
-              qtquick3d
-              qtremoteobjects
-              qttools
-            ];
-        in
-        if (!helpers.isWheel old) then
-          {
-            postPatch = ''
-              # Confirm license, if project.py exists
-              if test -f project.py; then
-                sed -i s/"if tool == 'pep517':"/"if True:"/ project.py
-              fi
-            '';
-
-            dontConfigure = true;
-            dontWrapQtApps = true;
-            nativeBuildInputs =
-              old.nativeBuildInputs or [ ]
-              ++ pyQt5Modules qt5
-              ++ [
-                final.pyqt-builder
-                final.sip
-              ];
-            buildInputs = old.buildInputs or [ ] ++ pyQt5Modules qt5;
-            autoPatchelfIgnoreMissingDeps = [ "libQt5TextToSpeech.so.5" ]; # no clue if this is correct.
-          }
-        else
-          {
-            buildInputs = old.buildInputs or [ ] ++ pyQt5Modules qt5;
-            autoPatchelfIgnoreMissingDeps = [ "libQt5TextToSpeech.so.5" ]; # no clue if this is correct.
-          }
-      )
-    )
-  ];
+  qt5 = selectQt5 prev.pyqt5-qt5.version;
+  pyQt5Modules =
+    qt5: with qt5; [
+      qt3d
+      qtbase
+      qtcharts
+      qtconnectivity
+      qtdatavis3d
+      qtdeclarative
+      qtgamepad
+      qtlocation
+      qtmultimedia
+      qtsensors
+      qtserialport
+      qtsvg
+      qtwebchannel
+      qtwebengine
+      qtwebsockets
+      qtx11extras
+      qtxmlpatterns
+      qtquick3d
+      qtremoteobjects
+      qttools
+    ];
 in
-pkgs.lib.trivial.pipe old funcs
+if (!helpers.isWheel old) then
+    {
+      postPatch = ''
+        # Confirm license, if project.py exists
+        if test -f project.py; then
+          sed -i s/"if tool == 'pep517':"/"if True:"/ project.py
+        fi
+      '';
+
+      dontConfigure = true;
+      dontWrapQtApps = true;
+      nativeBuildInputs =
+        old.nativeBuildInputs or [ ]
+        ++ pyQt5Modules qt5
+        ++ [
+          final.pyqt-builder
+          final.sip
+        ];
+      buildInputs = old.buildInputs or [ ] ++ pyQt5Modules qt5;
+      autoPatchelfIgnoreMissingDeps = [ "libQt5TextToSpeech.so.5" ]; # no clue if this is correct.
+    }
+  else
+    {
+      buildInputs = old.buildInputs or [ ] ++ pyQt5Modules qt5;
+      autoPatchelfIgnoreMissingDeps = [ "libQt5TextToSpeech.so.5" ]; # no clue if this is correct.
+    }
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
--- overrides/tbb/2021.13.1/default.nix
+++ overrides/tbb/2021.13.1/default.nix
@@ -1,16 +1,10 @@
-{
-  helpers,
-  pkgs,
-  ...
+{helpers, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( {buildInputs = old.buildInputs or [] ++ [pkgs.hwloc];})) (old: old // ( {
+  autoPatchelfIgnoreMissingDeps = [ "libhwloc.so.5" ]; # it finds libhwloc.so.15, so I'll go with 'this should work'
 }
-: old: let
-  funcs = [
-    (old: old // {buildInputs = old.buildInputs or [] ++ [pkgs.hwloc];})
-    (old:
-      old
-      // {
-        autoPatchelfIgnoreMissingDeps = ["libhwloc.so.5"]; # it finds libhwloc.so.15, so I'll go with 'this should work'
-      })
-  ];
-in
-  pkgs.lib.trivial.pipe old funcs
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
