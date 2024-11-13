5e4a0d758cb15a75413ebc93a194166d3fc7fc0f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-13 12:56:25 +0000

    autogenerated overwrites for gmsh==4.13.1

---
+++ overrides/gmsh/4.13.1/default.nix
@@ -0,0 +1,3 @@
+{helpers, pkgs, ...}
+        : old: {buildInputs = old.buildInputs or [] ++ [pkgs.fontconfig pkgs.libGLU pkgs.xorg.libXcursor pkgs.xorg.libXfixes pkgs.xorg.libXft pkgs.xorg.libXinerama];}
+        
