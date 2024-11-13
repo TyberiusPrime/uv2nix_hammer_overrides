891d410e5a6c9f2c062d15056497503f0c1b9d14
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-13 08:00:18 +0000

    autogenerated overwrites for smac==2.2.0

--- overrides/pyrfr/0.9.0/default.nix
+++ overrides/pyrfr/0.9.0/default.nix
@@ -1,3 +1,3 @@
-{resolveBuildSystem, final, ...}
-        : old: if ((old.passthru.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+{resolveBuildSystem, final, pkgs, ...}
+        : old: if ((old.passthru.format or "sdist") == "wheel") then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.swig] ++ ( resolveBuildSystem {setuptools = [];});}
         
