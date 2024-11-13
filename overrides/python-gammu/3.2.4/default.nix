d04eb87fea710ea5ae57c8701db9d69c3b4ce899
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-13 12:55:08 +0000

    autogenerated overwrites for python-gammu==3.2.4

---
+++ overrides/python-gammu/3.2.4/default.nix
@@ -0,0 +1,3 @@
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.gammu];} else {buildInputs = old.buildInputs or [] ++ [pkgs.gammu];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.pkg-config] ++ ( resolveBuildSystem {setuptools = [];});}
+        
