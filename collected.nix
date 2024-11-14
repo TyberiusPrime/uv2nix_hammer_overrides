f0d5a3471c3c8fa6557b751cd2eac69200252ebd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-14 21:42:26 +0000

    autogenerated overwrites for fdint==2.0.2

--- collected.nix
+++ collected.nix
@@ -2384,6 +2384,9 @@
   fcore = {
     "4.0.0" = import ./overrides/fcore/4.0.0;
   };
+  fdint = {
+    "2.0.2" = import ./overrides/fdint/2.0.2;
+  };
   fdm = {
     "0.4.1" = import ./overrides/fdm/0.4.1;
   };
