3f3b5f0a93f2505ec3df129339a92b21fe7cd48a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-14 15:56:17 +0000

    autogenerated overwrites for slycot==0.6.0

--- overrides/python-openzwave/0.4.19/default.nix
+++ overrides/python-openzwave/0.4.19/default.nix
@@ -1,59 +1,27 @@
-{
-  final,
-  helpers,
-  pkgs,
-  resolveBuildSystem,
-  ...
-}: old: let
-  funcs = [
-    (
-      old:
-        old
-        // (
-          if (helpers.isWheel old)
-          then {}
-          else {nativeBuildInputs = old.nativeBuildInputs or [] ++ (resolveBuildSystem {setuptools = [];});}
-        )
-    )
-    (
-      old:
-        old
-        // {
-          # from nixpkgs
-          nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.pkg-config];
-          buildInputs =
-            old.buildInputs
-            or []
-            ++ [
-              pkgs.systemd
-              pkgs.libyaml
-              pkgs.openzwave
-              final.cython_0
-            ];
-
-          # primary location for the .xml files is in /etc/openzwave so we override the
-          # /usr/local/etc lookup instead as that allows us to dump new .xml files into
-          # /etc/openzwave if needed
-          postPatch =
-            old.postPatch
-            or ""
-            + pkgs.lib.optionalString (!helpers.isWheel old) ''
-              substituteInPlace src-lib/libopenzwave/libopenzwave.pyx \
-                --replace /usr/local/etc/openzwave ${pkgs.openzwave}/etc/openzwave
-            '';
-        }
-    )
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});})) (old: old // ( {
+  # from nixpkgs
+  nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ pkgs.pkg-config ];
+  buildInputs = old.buildInputs or [ ] ++ [
+    pkgs.systemd
+    pkgs.libyaml
+    pkgs.openzwave
+    final.cython_0
   ];
 
   # primary location for the .xml files is in /etc/openzwave so we override the
   # /usr/local/etc lookup instead as that allows us to dump new .xml files into
   # /etc/openzwave if needed
   postPatch =
-    old.postPatch
-    or ""
+    old.postPatch or ""
     + pkgs.lib.optionalString (!helpers.isWheel old) ''
       substituteInPlace src-lib/libopenzwave/libopenzwave.pyx \
         --replace /usr/local/etc/openzwave ${pkgs.openzwave}/etc/openzwave
     '';
-in
-  pkgs.lib.trivial.pipe old funcs
+}
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
---
+++ overrides/slycot/0.6.0/default.nix
@@ -0,0 +1,3 @@
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.blas pkgs.lapack];} else {buildInputs = old.buildInputs or [] ++ [pkgs.blas pkgs.lapack];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.gfortran] ++ ( resolveBuildSystem {cmake = [];numpy = [];scikit-build = [];setuptools = [];setuptools-scm = [];wheel = [];});}
+        
