34c885686c01f423e55ec23ef78b4ef728e82efb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-19 07:04:28 +0000

    autogenerated overwrites for home-assistant-chip-core==2024.7.1

--- overrides/nvidia-cusparse-cu12/12.1.0.106/default.nix
+++ overrides/nvidia-cusparse-cu12/12.1.0.106/default.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
-old:
-let
-  funcs = [
-    (old: old // ({ buildInputs = old.buildInputs or [ ] ++ [ pkgs.cudaPackages.libnvjitlink ]; }))
-    (old: old // ({ dontUsePyprojectBytecode = true; }))
-  ];
-in
-pkgs.lib.trivial.pipe old funcs
+{helpers, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( {buildInputs = old.buildInputs or [] ++ [pkgs.cudaPackages.libnvjitlink];})) (old: old // ( { dontUsePyprojectBytecode = true; }
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
