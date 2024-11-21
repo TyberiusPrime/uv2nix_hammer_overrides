9822eaeaea45eae05cf48b5e277373e967064dc4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-21 09:53:34 +0000

    autogenerated overwrites for gmpy==1.17

---
+++ overrides/gmpy/1.17/default.nix
@@ -0,0 +1,3 @@
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.gmp];} else {buildInputs = old.buildInputs or [] ++ [pkgs.gmp];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.gmp.dev pkgs.pkg-config] ++ ( resolveBuildSystem {setuptools = [];});}
+        
