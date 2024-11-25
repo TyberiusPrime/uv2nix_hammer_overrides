eaae82f04c3a6afe267b9a3578ce00d06d36f3d1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-25 15:50:47 +0000

    autogenerated overwrites for implicit==0.7.2

--- collected.nix
+++ collected.nix
@@ -3430,6 +3430,9 @@
   impacket = {
     "0.11.0" = import ./overrides/impacket/0.11.0;
   };
+  implicit = {
+    "0.7.2" = import ./overrides/implicit/0.7.2;
+  };
   importlib = {
     "1.0.4" = import ./overrides/importlib/1.0.4;
   };
---
+++ overrides/implicit/0.7.2/default.nix
@@ -0,0 +1,3 @@
+{helpers, pkgs, ...}
+        : old: {buildInputs = old.buildInputs or [] ++ [pkgs.cudaPackages.libcurand pkgs.cudaPackages_11.cuda_cudart pkgs.cudaPackages_11.libcublas];}
+        
