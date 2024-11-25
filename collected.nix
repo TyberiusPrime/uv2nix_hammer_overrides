4c10d67e6a64b5a50a84874329ea2bcaf7cb03d7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-25 21:16:35 +0000

    autogenerated overwrites for torchrl==0.5.0

--- collected.nix
+++ collected.nix
@@ -8533,6 +8533,9 @@
   torchnet = {
     "0.0.4" = import ./overrides/torchnet/0.0.4;
   };
+  torchrl = {
+    "0.5.0" = import ./overrides/torchrl/0.5.0;
+  };
   torchtext = {
     "0.18.0" = import ./overrides/torchtext/0.18.0;
   };
