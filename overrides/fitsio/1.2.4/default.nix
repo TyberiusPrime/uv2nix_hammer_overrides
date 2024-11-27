8ed53e62acf4c8e538bcb9862332338dcab9e1f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-27 10:55:38 +0100

    formatting, nvidia

--- overrides/fitsio/1.2.4/default.nix
+++ overrides/fitsio/1.2.4/default.nix
@@ -1,3 +1,33 @@
-{final, helpers, pkgs, resolveBuildSystem, ...}
-        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.cfitsio pkgs.curl pkgs.zlib.out];} else {buildInputs = old.buildInputs or [] ++ [pkgs.cfitsio pkgs.curl pkgs.zlib.out];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.bzip2.dev pkgs.pkg-config] ++ ( resolveBuildSystem {numpy = [];setuptools = [];});}
-        
+{
+  helpers,
+  pkgs,
+  resolveBuildSystem,
+  ...
+}:
+old:
+if (helpers.isWheel old) then
+  {
+    buildInputs = old.buildInputs or [ ] ++ [
+      pkgs.cfitsio
+      pkgs.curl
+      pkgs.zlib.out
+    ];
+  }
+else
+  {
+    buildInputs = old.buildInputs or [ ] ++ [
+      pkgs.cfitsio
+      pkgs.curl
+      pkgs.zlib.out
+    ];
+    nativeBuildInputs =
+      old.nativeBuildInputs or [ ]
+      ++ [
+        pkgs.bzip2.dev
+        pkgs.pkg-config
+      ]
+      ++ (resolveBuildSystem {
+        numpy = [ ];
+        setuptools = [ ];
+      });
+  }
