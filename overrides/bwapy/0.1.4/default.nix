aa4284ed5bf3fe03ff5ebfd265cbae4cdf245952
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-28 13:10:19 +0000

    autogenerated overwrites for wbBase==0.2.9

--- overrides/bwapy/0.1.4/default.nix
+++ overrides/bwapy/0.1.4/default.nix
@@ -1,42 +1,22 @@
-{ helpers, pkgs, ... }:
-old:
-let
-  funcs = [
-    (
-      old:
-      old
-      // (
-        if (helpers.isWheel old) then
-          { buildInputs = old.buildInputs or [ ] ++ [ pkgs.zlib.out ]; }
-        else
-          {
-            buildInputs = old.buildInputs or [ ] ++ [ pkgs.zlib.out ];
-            nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
-              pkgs.pkg-config
-              pkgs.zlib.dev
-            ];
-          }
-      )
-    )
-    (
-      old:
-      old
-      // {
-        postPatch = ''
-          # replace bundled bwa
-          rm -r bwa/*
-          cp ${pkgs.bwa}/lib/*.a ${pkgs.bwa}/include/*.h bwa/
+{helpers, pkgs, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.zlib.out];} else {buildInputs = old.buildInputs or [] ++ [pkgs.zlib.out];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.pkg-config pkgs.zlib.dev];})) (old: old // ( {
+  postPatch = ''
+    # replace bundled bwa
+    rm -r bwa/*
+    cp ${pkgs.bwa}/lib/*.a ${pkgs.bwa}/include/*.h bwa/
 
-          substituteInPlace setup.py \
-            --replace-quiet 'setuptools>=49.2.0' 'setuptools'
-        '';
+    substituteInPlace setup.py \
+      --replace-quiet 'setuptools>=49.2.0' 'setuptools'
+  '';
 
-        buildInputs = old.buildInputs or [ ] ++ [
-          pkgs.zlib
-          pkgs.bwa
-        ];
-      }
-    )
+  buildInputs = old.buildInputs or [ ] ++ [
+    pkgs.zlib
+    pkgs.bwa
   ];
-in
-pkgs.lib.trivial.pipe old funcs
+}
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
