e0b9c4c7805ceff9f55611cd898e979be35edeff
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-11-29 13:56:28 +0000

    autogenerated overwrites for dm.xmlsec.binding==3.0

---
+++ overrides/dm-xmlsec-binding/3.0/default.nix
@@ -0,0 +1,3 @@
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.libtool pkgs.xmlsec.dev];} else {buildInputs = old.buildInputs or [] ++ [pkgs.libtool pkgs.xmlsec.dev];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.libxml2 pkgs.xmlsec.out] ++ ( resolveBuildSystem {lxml = [];setuptools = [];});}
+        
