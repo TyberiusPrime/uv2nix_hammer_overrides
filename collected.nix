babf998f3a550e16f0209e22188a7421307f5720
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:15:14 +0000

    autogenerated overwrites for asyncserial==0.1.0

--- collected.nix
+++ collected.nix
@@ -452,6 +452,9 @@
   asyncpgsa = {
     "0.27.1" = import ./overrides/asyncpgsa/0.27.1;
   };
+  asyncserial = {
+    "0.1.0" = import ./overrides/asyncserial/0.1.0;
+  };
   atomic = {
     "0.7.3" = import ./overrides/atomic/0.7.3;
   };
