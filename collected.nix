c4fe1a62911b6c10f0b4ca6616c917040879a9d8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:41:10 +0000

    autogenerated overwrites for rfc3986==2.0.0

--- collected.nix
+++ collected.nix
@@ -7937,6 +7937,9 @@
   reverse-geocoder = {
     "1.5.1" = import ./overrides/reverse-geocoder/1.5.1;
   };
+  rfc3986 = {
+    "2.0.0" = import ./overrides/rfc3986/2.0.0;
+  };
   rfc7464 = {
     "17.7.0" = import ./overrides/rfc7464/17.7.0;
   };
