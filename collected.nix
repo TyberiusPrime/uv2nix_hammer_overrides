f9d6ed8eacefd730f54264b3cb72eadf7f0eb5e2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 17:58:53 +0000

    autogenerated overwrites for immutabledict==4.2.0

--- collected.nix
+++ collected.nix
@@ -3709,6 +3709,9 @@
   imm-source = {
     "1.0.30" = import ./overrides/imm-source/1.0.30;
   };
+  immutabledict = {
+    "4.2.0" = import ./overrides/immutabledict/4.2.0;
+  };
   impacket = {
     "0.11.0" = import ./overrides/impacket/0.11.0;
   };
