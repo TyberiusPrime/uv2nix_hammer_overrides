aad0c15e5e1fe822bf5e3841903e1fe5cfe166a1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:27:21 +0000

    autogenerated overwrites for snoop==0.4.3

--- collected.nix
+++ collected.nix
@@ -8544,6 +8544,9 @@
   snaptime = {
     "0.2.4" = import ./overrides/snaptime/0.2.4;
   };
+  snoop = {
+    "0.4.3" = import ./overrides/snoop/0.4.3;
+  };
   snownlp = {
     "0.12.3" = import ./overrides/snownlp/0.12.3;
   };
