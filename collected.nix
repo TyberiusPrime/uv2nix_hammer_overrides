f8372ccffc9a5194a63747e859f5d40028f1b889
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:50:59 +0000

    autogenerated overwrites for poppy==1.1.1

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  poppy = {
+    "1.1.1" = import ./overrides/poppy/1.1.1;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
