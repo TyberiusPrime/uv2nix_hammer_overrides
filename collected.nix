7671d47207362ed5d942c3188b9c4120f4743edf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:27:28 +0000

    autogenerated overwrites for django-safe-fields==0.2.2

--- collected.nix
+++ collected.nix
@@ -2129,6 +2129,9 @@
   django-rtk = {
     "0.6.0" = import ./overrides/django-rtk/0.6.0;
   };
+  django-safe-fields = {
+    "0.2.2" = import ./overrides/django-safe-fields/0.2.2;
+  };
   django-sendfile = {
     "0.3.11" = import ./overrides/django-sendfile/0.3.11;
   };
