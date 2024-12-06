229e28d5d6321b32d54474cfb7a35fc12a5f0f96
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:07:41 +0000

    autogenerated overwrites for django-pwa==2.0.0

--- collected.nix
+++ collected.nix
@@ -2102,6 +2102,9 @@
   django-push-notifications = {
     "3.1.0" = import ./overrides/django-push-notifications/3.1.0;
   };
+  django-pwa = {
+    "2.0.0" = import ./overrides/django-pwa/2.0.0;
+  };
   django-qsstats-magic = {
     "1.1.0" = import ./overrides/django-qsstats-magic/1.1.0;
   };
