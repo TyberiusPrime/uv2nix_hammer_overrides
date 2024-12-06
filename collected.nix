b280a46f335b69e8dab1553d303427d72733db11
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:11:49 +0000

    autogenerated overwrites for pytest-plt==1.1.1

--- collected.nix
+++ collected.nix
@@ -7236,6 +7236,9 @@
   pytest-pep8 = {
     "1.0.6" = import ./overrides/pytest-pep8/1.0.6;
   };
+  pytest-plt = {
+    "1.1.1" = import ./overrides/pytest-plt/1.1.1;
+  };
   pytest-raisesregexp = {
     "2.1" = import ./overrides/pytest-raisesregexp/2.1;
   };
