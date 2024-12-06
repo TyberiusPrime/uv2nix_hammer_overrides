2fd3e64c5e680a49f0899b5597d2950d85747c12
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 12:26:28 +0000

    autogenerated overwrites for python-idzip==0.3.9

---
+++ manual_overrides/azureml-train-automl-client/1.57.0/default.nix
@@ -0,0 +1,6 @@
+{
+  postInstall = ''
+    cp ${./__init__.py} "$out/${final.python.sitePackages}/azureml/__init__.py"
+    cp ${./__init__.py} "$out/${final.python.sitePackages}/azureml/train/__init__.py"
+  '';
+}
