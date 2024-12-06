704259f945f60c0f9d00a18501c2e5d4825dcd7f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 17:41:11 +0000

    autogenerated overwrites for types-lxml==2024.8.7

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-lxml = {
+    "2024.8.7" = import ./overrides/types-lxml/2024.8.7;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
