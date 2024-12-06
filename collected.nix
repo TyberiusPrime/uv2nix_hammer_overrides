5841530dfe9adb147273ed722d2f2ad5ec068288
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:01:54 +0000

    autogenerated overwrites for p4a.common==1.1

--- collected.nix
+++ collected.nix
@@ -5635,6 +5635,9 @@
   p-tqdm = {
     "1.4.2" = import ./overrides/p-tqdm/1.4.2;
   };
+  p4a-common = {
+    "1.1" = import ./overrides/p4a-common/1.1;
+  };
   p4a-z2utils = {
     "1.0.2" = import ./overrides/p4a-z2utils/1.0.2;
   };
