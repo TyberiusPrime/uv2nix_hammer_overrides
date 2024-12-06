9c31b59500996ad7828aae9e2a8a2371fd876b3b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:39:59 +0000

    autogenerated overwrites for pytorch-tabnet==4.1.0

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytorch-tabnet = {
+    "4.1.0" = import ./overrides/pytorch-tabnet/4.1.0;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
