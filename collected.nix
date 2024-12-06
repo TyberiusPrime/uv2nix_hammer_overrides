5ae03d5c261b809bf9d33db9d51dedec2529fd0e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:39:11 +0000

    autogenerated overwrites for generallibrary==2.10.0

--- collected.nix
+++ collected.nix
@@ -3110,6 +3110,9 @@
   gdspy = {
     "1.6.13" = import ./overrides/gdspy/1.6.13;
   };
+  generallibrary = {
+    "2.10.0" = import ./overrides/generallibrary/2.10.0;
+  };
   genpy = {
     "2022.1" = import ./overrides/genpy/2022.1;
   };
