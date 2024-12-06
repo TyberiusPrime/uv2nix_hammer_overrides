7f9e2d5743a8e8e88722c326a09c745ff773c8f4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:12:19 +0000

    autogenerated overwrites for nlpcloud==1.1.46

--- collected.nix
+++ collected.nix
@@ -5241,6 +5241,9 @@
   nix-prefetch-github = {
     "7.1.0" = import ./overrides/nix-prefetch-github/7.1.0;
   };
+  nlpcloud = {
+    "1.1.46" = import ./overrides/nlpcloud/1.1.46;
+  };
   nlptoolkit-annotatedsentence = {
     "1.0.38" = import ./overrides/nlptoolkit-annotatedsentence/1.0.38;
   };
