1280da96bfb9ebf62e0be75c5108f6da3de656b2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:02:22 +0000

    autogenerated overwrites for gotenberg-client==0.6.0

--- collected.nix
+++ collected.nix
@@ -3270,6 +3270,9 @@
   googletransx = {
     "2.4.2" = import ./overrides/googletransx/2.4.2;
   };
+  gotenberg-client = {
+    "0.6.0" = import ./overrides/gotenberg-client/0.6.0;
+  };
   gpgrecord = {
     "0.0.4" = import ./overrides/gpgrecord/0.0.4;
     "0.0.5" = import ./overrides/gpgrecord/0.0.5;
