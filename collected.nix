28eaadb9a184abe8b37ea6ac651ae9d10788926c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:11:33 +0000

    autogenerated overwrites for types-toml==0.10.8.20240310

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-toml = {
+    "0.10.8.20240310" = import ./overrides/types-toml/0.10.8.20240310;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
