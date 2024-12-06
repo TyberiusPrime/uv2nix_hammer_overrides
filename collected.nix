d49808e3bd358651e005b66ac3961405d329a1e3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:52:26 +0000

    autogenerated overwrites for pkutils==3.0.2

--- collected.nix
+++ collected.nix
@@ -5955,6 +5955,9 @@
   pkuseg = {
     "0.0.25" = import ./overrides/pkuseg/0.0.25;
   };
+  pkutils = {
+    "3.0.2" = import ./overrides/pkutils/3.0.2;
+  };
   placebo = {
     "0.10.0" = import ./overrides/placebo/0.10.0;
   };
