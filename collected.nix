22754a6dd958078d2520815a37ea796d03c066ef
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:05:27 +0000

    autogenerated overwrites for safe-cast==0.3.4

--- collected.nix
+++ collected.nix
@@ -8102,6 +8102,9 @@
   sacremoses = {
     "0.0.53" = import ./overrides/sacremoses/0.0.53;
   };
+  safe-cast = {
+    "0.3.4" = import ./overrides/safe-cast/0.3.4;
+  };
   safe-pysha3 = {
     "1.0.4" = import ./overrides/safe-pysha3/1.0.4;
   };
