19c25c0409121ab678ab4adb9f8cca0de044e3b1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:47:19 +0000

    autogenerated overwrites for mkdocstrings-python==1.11.1

--- collected.nix
+++ collected.nix
@@ -4835,6 +4835,9 @@
   mkdocs-jupyter = {
     "0.12.0" = import ./overrides/mkdocs-jupyter/0.12.0;
   };
+  mkdocstrings-python = {
+    "1.11.1" = import ./overrides/mkdocstrings-python/1.11.1;
+  };
   ml-collections = {
     "0.1.1" = import ./overrides/ml-collections/0.1.1;
   };
