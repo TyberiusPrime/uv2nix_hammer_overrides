513534d6f5b28206d9bd487accc9cf6895d1acc6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:19:43 +0000

    autogenerated overwrites for py-partiql-parser==0.5.6

--- collected.nix
+++ collected.nix
@@ -6318,6 +6318,9 @@
   py-lets-be-rational = {
     "1.0.1" = import ./overrides/py-lets-be-rational/1.0.1;
   };
+  py-partiql-parser = {
+    "0.5.6" = import ./overrides/py-partiql-parser/0.5.6;
+  };
   py-postgresql = {
     "1.2.1" = import ./overrides/py-postgresql/1.2.1;
   };
