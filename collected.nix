20846248885aca20f0c79262c26a901ccb1e11d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:11:29 +0000

    autogenerated overwrites for pigpio==1.78

--- collected.nix
+++ collected.nix
@@ -5896,6 +5896,9 @@
   piep = {
     "0.10.0" = import ./overrides/piep/0.10.0;
   };
+  pigpio = {
+    "1.78" = import ./overrides/pigpio/1.78;
+  };
   pii-data = {
     "0.5.0" = import ./overrides/pii-data/0.5.0;
   };
