e3aff9ee44928df846c5c6a81748203f35ea1fc2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 17:39:29 +0000

    autogenerated overwrites for todoist-python==8.1.4

--- collected.nix
+++ collected.nix
@@ -9154,6 +9154,9 @@
   tlv8 = {
     "0.10.0" = import ./overrides/tlv8/0.10.0;
   };
+  todoist-python = {
+    "8.1.4" = import ./overrides/todoist-python/8.1.4;
+  };
   toil-lib = {
     "1.1.8" = import ./overrides/toil-lib/1.1.8;
   };
