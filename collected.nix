0402597b692e6396d2c7ce4c9eded73cc2f37d06
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:59:38 +0000

    autogenerated overwrites for editables==0.5

--- collected.nix
+++ collected.nix
@@ -2466,6 +2466,9 @@
   edalize = {
     "0.5.4" = import ./overrides/edalize/0.5.4;
   };
+  editables = {
+    "0.5" = import ./overrides/editables/0.5;
+  };
   editorconfig = {
     "0.12.4" = import ./overrides/editorconfig/0.12.4;
   };
