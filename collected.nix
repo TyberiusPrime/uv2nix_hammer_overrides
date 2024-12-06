c6a1a5950bddeebc6d32a73fc105f4387240bd1a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:16:58 +0000

    autogenerated overwrites for google-resumable-media==2.7.2

--- collected.nix
+++ collected.nix
@@ -3249,6 +3249,9 @@
   google-images-download = {
     "2.8.0" = import ./overrides/google-images-download/2.8.0;
   };
+  google-resumable-media = {
+    "2.7.2" = import ./overrides/google-resumable-media/2.7.2;
+  };
   google-search-results = {
     "2.4.2" = import ./overrides/google-search-results/2.4.2;
   };
