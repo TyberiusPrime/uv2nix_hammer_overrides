5656f55f698ec21038c9c3446ca75ed766dc1b70
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:38:36 +0000

    autogenerated overwrites for mautrix==0.20.6

--- collected.nix
+++ collected.nix
@@ -4661,6 +4661,9 @@
   matrix-api-async = {
     "0.1.0" = import ./overrides/matrix-api-async/0.1.0;
   };
+  mautrix = {
+    "0.20.6" = import ./overrides/mautrix/0.20.6;
+  };
   maxminddb-geolite2 = {
     "2018.703" = import ./overrides/maxminddb-geolite2/2018.703;
   };
