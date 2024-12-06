0eaff874aeeb717b33ea48e9e3bb7636e5ee494b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 10:20:47 +0000

    autogenerated overwrites for azureml-sdk==1.57.0

--- overrides/aleksis/2023.6.1/default.nix
+++ overrides/aleksis/2023.6.1/default.nix
@@ -1,8 +1,10 @@
-{ final, ... }:
-old: {
+{final, pkgs, ...}
+        : old: {
   postInstall =
     old.postInstall or ""
     + ''
       rm -rf $out/${final.python.sitePackages}/*.rst
     '';
 }
+
+        
