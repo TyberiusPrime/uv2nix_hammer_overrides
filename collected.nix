2a166ff893cc7b9a62ed025bd2e53560ee4a8271
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 19:26:19 +0000

    autogenerated overwrites for uqbar==0.7.4

--- collected.nix
+++ collected.nix
@@ -9573,6 +9573,9 @@
   upyun = {
     "2.5.5" = import ./overrides/upyun/2.5.5;
   };
+  uqbar = {
+    "0.7.4" = import ./overrides/uqbar/0.7.4;
+  };
   urlnorm = {
     "1.1.4" = import ./overrides/urlnorm/1.1.4;
   };
