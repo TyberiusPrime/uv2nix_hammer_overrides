48c3deed47c7ee326298d65a27378506eb361b91
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:58:05 +0000

    autogenerated overwrites for smqtk-descriptors==0.19.0

--- collected.nix
+++ collected.nix
@@ -8526,6 +8526,9 @@
   smokesignal = {
     "0.7.0" = import ./overrides/smokesignal/0.7.0;
   };
+  smqtk-descriptors = {
+    "0.19.0" = import ./overrides/smqtk-descriptors/0.19.0;
+  };
   smt = {
     "2.6.3" = import ./overrides/smt/2.6.3;
   };
