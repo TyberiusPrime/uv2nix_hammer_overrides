0acc349b207c1e30bf1c2e7b268eb625aed80852
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:22:55 +0000

    autogenerated overwrites for object-colors==2.3.1

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  object-colors = {
+    "2.3.1" = import ./overrides/object-colors/2.3.1;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
