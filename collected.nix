dc91d8249f514e4cce7119b421694f7c81a6745f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:54:46 +0000

    autogenerated overwrites for types-typed-ast==1.5.8.7

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-typed-ast = {
+    "1.5.8.7" = import ./overrides/types-typed-ast/1.5.8.7;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
