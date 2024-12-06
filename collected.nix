69525b34097d279286b8141b13021f53ee20887a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:13:37 +0000

    autogenerated overwrites for pytest-benchmark==4.0.0

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-benchmark = {
+    "4.0.0" = import ./overrides/pytest-benchmark/4.0.0;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
