843aa5e5afb5f99facbc1960536b933185f858db
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:42:11 +0000

    autogenerated overwrites for django-admin-tools==0.9.3

--- collected.nix
+++ collected.nix
@@ -1870,6 +1870,9 @@
   django-admin-sortable = {
     "2.3" = import ./overrides/django-admin-sortable/2.3;
   };
+  django-admin-tools = {
+    "0.9.3" = import ./overrides/django-admin-tools/0.9.3;
+  };
   django-allauth = {
     "0.54.0" = import ./overrides/django-allauth/0.54.0;
     "64.2.1" = import ./overrides/django-allauth/64.2.1;
