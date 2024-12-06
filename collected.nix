e037f0a87be0c1d76a848a270b93a7ca97f1ce45
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:24:43 +0000

    autogenerated overwrites for enrich==1.2.7

--- collected.nix
+++ collected.nix
@@ -2529,6 +2529,9 @@
   encryptedsocket = {
     "3.0.0" = import ./overrides/encryptedsocket/3.0.0;
   };
+  enrich = {
+    "1.2.7" = import ./overrides/enrich/1.2.7;
+  };
   env = {
     "0.1.0" = import ./overrides/env/0.1.0;
   };
