57900c090592ab54539fa3ae4aa732c3c9426292
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:12:06 +0000

    autogenerated overwrites for syslog-rfc5424-formatter==1.2.3

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  syslog-rfc5424-formatter = {
+    "1.2.3" = import ./overrides/syslog-rfc5424-formatter/1.2.3;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
