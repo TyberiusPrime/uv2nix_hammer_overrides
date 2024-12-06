55107ee9e6a1fada9d47b6a04e7d9383a2b36f75
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 17:42:12 +0000

    autogenerated overwrites for jsonconf==0.1.4

--- collected.nix
+++ collected.nix
@@ -4000,6 +4000,9 @@
   json2html = {
     "1.3.0" = import ./overrides/json2html/1.3.0;
   };
+  jsonconf = {
+    "0.1.4" = import ./overrides/jsonconf/0.1.4;
+  };
   jsondate = {
     "0.1.2" = import ./overrides/jsondate/0.1.2;
   };
