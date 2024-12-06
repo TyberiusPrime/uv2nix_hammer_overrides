35ef34e37da4dd9e832348e26d1016e26d336eb8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:57:35 +0000

    autogenerated overwrites for pylogz==0.0.4

--- collected.nix
+++ collected.nix
@@ -6834,6 +6834,9 @@
   pylint-flask = {
     "0.6" = import ./overrides/pylint-flask/0.6;
   };
+  pylogz = {
+    "0.0.4" = import ./overrides/pylogz/0.0.4;
+  };
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
