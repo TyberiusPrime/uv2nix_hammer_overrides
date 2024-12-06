677ea322adb7ca1a52d2c34771dd4e4ccf3f7d97
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:28:59 +0000

    autogenerated overwrites for kopf==1.37.2

--- collected.nix
+++ collected.nix
@@ -4202,6 +4202,9 @@
   knnimpute = {
     "0.1.0" = import ./overrides/knnimpute/0.1.0;
   };
+  kopf = {
+    "1.37.2" = import ./overrides/kopf/1.37.2;
+  };
   kotti = {
     "2.0.9" = import ./overrides/kotti/2.0.9;
   };
