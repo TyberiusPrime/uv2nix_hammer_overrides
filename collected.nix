cf56f23c2622bbff9a1d85eb71269f096942348c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:34:28 +0000

    autogenerated overwrites for aioruuvigateway==0.1.0

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioruuvigateway = {
+    "0.1.0" = import ./overrides/aioruuvigateway/0.1.0;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
