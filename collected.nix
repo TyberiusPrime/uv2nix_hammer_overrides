ce56bd27ba9d7d1f9d5b76d0564dda509b2d2a88
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:40:10 +0000

    autogenerated overwrites for mypy-boto3-omics==1.35.7

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  mypy-boto3-omics = {
+    "1.35.7" = import ./overrides/mypy-boto3-omics/1.35.7;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
