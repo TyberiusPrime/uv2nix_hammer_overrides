307b423c026b5af6793d3d512c7fd746cf4bc730
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:38:56 +0000

    autogenerated overwrites for pretty-errors==1.2.25

--- collected.nix
+++ collected.nix
@@ -6099,6 +6099,9 @@
   pretty-bad-protocol = {
     "3.1.1" = import ./overrides/pretty-bad-protocol/3.1.1;
   };
+  pretty-errors = {
+    "1.2.25" = import ./overrides/pretty-errors/1.2.25;
+  };
   pretty-midi = {
     "0.2.10" = import ./overrides/pretty-midi/0.2.10;
   };
