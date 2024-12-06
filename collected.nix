381087e6d32c95abd0d6d259359557259b468844
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 17:49:00 +0000

    autogenerated overwrites for wagtail-autocomplete==0.11.0

--- collected.nix
+++ collected.nix
@@ -9793,6 +9793,9 @@
   wagon = {
     "1.0.1" = import ./overrides/wagon/1.0.1;
   };
+  wagtail-autocomplete = {
+    "0.11.0" = import ./overrides/wagtail-autocomplete/0.11.0;
+  };
   wai-common = {
     "0.0.44" = import ./overrides/wai-common/0.0.44;
   };
