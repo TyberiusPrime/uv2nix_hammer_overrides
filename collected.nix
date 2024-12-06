f1561add73bc29db826c0aae64009480366e2d49
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:27:55 +0000

    autogenerated overwrites for maxminddb==2.6.2

--- collected.nix
+++ collected.nix
@@ -4661,6 +4661,9 @@
   matrix-api-async = {
     "0.1.0" = import ./overrides/matrix-api-async/0.1.0;
   };
+  maxminddb = {
+    "2.6.2" = import ./overrides/maxminddb/2.6.2;
+  };
   maxminddb-geolite2 = {
     "2018.703" = import ./overrides/maxminddb-geolite2/2018.703;
   };
