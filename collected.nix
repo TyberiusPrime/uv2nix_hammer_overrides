34ee2c00795f6b667da40e5ef9532b0062f025e3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 22:13:55 +0000

    autogenerated overwrites for face==22.0.0

--- collected.nix
+++ collected.nix
@@ -2646,6 +2646,9 @@
   fabulous = {
     "0.4.0" = import ./overrides/fabulous/0.4.0;
   };
+  face = {
+    "22.0.0" = import ./overrides/face/22.0.0;
+  };
   face-recognition-models = {
     "0.3.0" = import ./overrides/face-recognition-models/0.3.0;
   };
