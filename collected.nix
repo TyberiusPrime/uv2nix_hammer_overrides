0ca9c9dbfaa26ea116f9c7b92763b5238e050700
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:17:47 +0000

    autogenerated overwrites for pulumi-azure-native==2.60.0

--- collected.nix
+++ collected.nix
@@ -6264,6 +6264,9 @@
   pubnub = {
     "8.1.0" = import ./overrides/pubnub/8.1.0;
   };
+  pulumi-azure-native = {
+    "2.60.0" = import ./overrides/pulumi-azure-native/2.60.0;
+  };
   pupynere = {
     "1.0.15" = import ./overrides/pupynere/1.0.15;
   };
