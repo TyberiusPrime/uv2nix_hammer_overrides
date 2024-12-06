4a1d09211ee333f1efdcc7465a83f7859535cde7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:23:52 +0000

    autogenerated overwrites for javaobj-py3==0.4.4

--- collected.nix
+++ collected.nix
@@ -3898,6 +3898,9 @@
   javabridge = {
     "1.0.19" = import ./overrides/javabridge/1.0.19;
   };
+  javaobj-py3 = {
+    "0.4.4" = import ./overrides/javaobj-py3/0.4.4;
+  };
   jax = {
     "0.3.25" = import ./overrides/jax/0.3.25;
   };
