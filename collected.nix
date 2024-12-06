5bb618dc7d0bea0faf07f21a0a62178671dfe8a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:32:03 +0000

    autogenerated overwrites for piccata==2.0.3

--- collected.nix
+++ collected.nix
@@ -5872,6 +5872,9 @@
   phunspell = {
     "0.1.6" = import ./overrides/phunspell/0.1.6;
   };
+  piccata = {
+    "2.0.3" = import ./overrides/piccata/2.0.3;
+  };
   pickle-mixin = {
     "1.0.2" = import ./overrides/pickle-mixin/1.0.2;
   };
