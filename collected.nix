90ffd0f460c1853315f1aa6a45dcb41dde9270c6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 18:29:08 +0000

    autogenerated overwrites for aicssegmentation==0.5.3

--- collected.nix
+++ collected.nix
@@ -86,6 +86,9 @@
   ago = {
     "0.0.95" = import ./overrides/ago/0.0.95;
   };
+  aicssegmentation = {
+    "0.5.3" = import ./overrides/aicssegmentation/0.5.3;
+  };
   aido-protocols-daffy = {
     "6.1.1" = import ./overrides/aido-protocols-daffy/6.1.1;
   };
