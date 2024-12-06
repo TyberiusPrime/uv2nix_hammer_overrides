53095b34d60edb30f303651c37cdc5e1cb2673c8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:14:59 +0000

    autogenerated overwrites for pycares==4.4.0

--- collected.nix
+++ collected.nix
@@ -6444,6 +6444,9 @@
   pycairo = {
     "1.27.0" = import ./overrides/pycairo/1.27.0;
   };
+  pycares = {
+    "4.4.0" = import ./overrides/pycares/4.4.0;
+  };
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
