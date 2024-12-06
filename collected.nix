db09329b7579adf6b17e31e23d8eb427a1c17c43
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 21:36:04 +0000

    autogenerated overwrites for argopt==0.8.2

--- collected.nix
+++ collected.nix
@@ -349,6 +349,9 @@
   argon2-cffi = {
     "16.3.0" = import ./overrides/argon2-cffi/16.3.0;
   };
+  argopt = {
+    "0.8.2" = import ./overrides/argopt/0.8.2;
+  };
   argparse-manpage = {
     "4.6" = import ./overrides/argparse-manpage/4.6;
   };
