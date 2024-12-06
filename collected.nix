2979e882ef6b8fc5360f9e2cdf2606426d6a8977
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 20:12:07 +0000

    autogenerated overwrites for entrypoints==0.4

--- collected.nix
+++ collected.nix
@@ -2529,6 +2529,9 @@
   encryptedsocket = {
     "3.0.0" = import ./overrides/encryptedsocket/3.0.0;
   };
+  entrypoints = {
+    "0.4" = import ./overrides/entrypoints/0.4;
+  };
   env = {
     "0.1.0" = import ./overrides/env/0.1.0;
   };
