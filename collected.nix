4e6952f1600bffeebde12f012d739e6b43958332
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-06 23:25:01 +0000

    autogenerated overwrites for pylint-plugin-utils==0.8.2

--- collected.nix
+++ collected.nix
@@ -6834,6 +6834,9 @@
   pylint-flask = {
     "0.6" = import ./overrides/pylint-flask/0.6;
   };
+  pylint-plugin-utils = {
+    "0.8.2" = import ./overrides/pylint-plugin-utils/0.8.2;
+  };
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
