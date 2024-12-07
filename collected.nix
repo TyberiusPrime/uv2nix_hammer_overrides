ec9cd119f9af3bda42e08f63626629dd657a59e8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:01:05 +0000

    autogenerated overwrites for itemdb==1.2.0

--- collected.nix
+++ collected.nix
@@ -3871,6 +3871,9 @@
   isounidecode = {
     "0.3" = import ./overrides/isounidecode/0.3;
   };
+  itemdb = {
+    "1.2.0" = import ./overrides/itemdb/1.2.0;
+  };
   ivy = {
     "0.0.9.7" = import ./overrides/ivy/0.0.9.7;
   };
