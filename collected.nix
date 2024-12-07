58ffe004c8b5a843a29b827f6474adefcda0442a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:18:45 +0000

    autogenerated overwrites for mizani==0.12.2

--- collected.nix
+++ collected.nix
@@ -4829,6 +4829,9 @@
   misaka = {
     "2.1.1" = import ./overrides/misaka/2.1.1;
   };
+  mizani = {
+    "0.12.2" = import ./overrides/mizani/0.12.2;
+  };
   mkdocs-exclude = {
     "1.0.2" = import ./overrides/mkdocs-exclude/1.0.2;
   };
