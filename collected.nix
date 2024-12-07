afde0a9e87fb0b340d5e3df4c543bd61d2493864
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:13:59 +0000

    autogenerated overwrites for mypy-boto3-opensearchserverless==1.35.2

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  mypy-boto3-opensearchserverless = {
+    "1.35.2" = import ./overrides/mypy-boto3-opensearchserverless/1.35.2;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
