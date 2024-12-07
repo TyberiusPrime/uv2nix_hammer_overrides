e6f0ac87ef28366f48df47ee7af70e0d5b4dad51
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:37:11 +0000

    autogenerated overwrites for alarmdecoder==1.13.11

--- collected.nix
+++ collected.nix
@@ -153,6 +153,9 @@
   aircv = {
     "1.4.6" = import ./overrides/aircv/1.4.6;
   };
+  alarmdecoder = {
+    "1.13.11" = import ./overrides/alarmdecoder/1.13.11;
+  };
   aldryn-boilerplates = {
     "0.8.0" = import ./overrides/aldryn-boilerplates/0.8.0;
   };
