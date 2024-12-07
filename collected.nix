39153c4db49cc60516c06fd14a0f96071a594a60
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:10:12 +0000

    autogenerated overwrites for erddapy==2.2.0

--- collected.nix
+++ collected.nix
@@ -2553,6 +2553,9 @@
   epydoc = {
     "3.0.1" = import ./overrides/epydoc/3.0.1;
   };
+  erddapy = {
+    "2.2.0" = import ./overrides/erddapy/2.2.0;
+  };
   erlastic = {
     "2.0.0" = import ./overrides/erlastic/2.0.0;
   };
