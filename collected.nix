927be6edceabb4ceca7e2c7d4a87ba431efd0273
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:41:43 +0000

    autogenerated overwrites for pypipegraph==0.197

--- collected.nix
+++ collected.nix
@@ -6988,6 +6988,9 @@
   pypillowfight = {
     "0.3.0" = import ./overrides/pypillowfight/0.3.0;
   };
+  pypipegraph = {
+    "0.197" = import ./overrides/pypipegraph/0.197;
+  };
   pypng = {
     "0.0.19" = import ./overrides/pypng/0.0.19;
   };
