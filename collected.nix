6a8eec1c9ee2e6caf2b7dc6e588a3b50c879e411
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:21:08 +0000

    autogenerated overwrites for nbclient==0.10.0

--- collected.nix
+++ collected.nix
@@ -5135,6 +5135,9 @@
   naturalsort = {
     "1.5.1" = import ./overrides/naturalsort/1.5.1;
   };
+  nbclient = {
+    "0.10.0" = import ./overrides/nbclient/0.10.0;
+  };
   nbexec = {
     "0.2.0" = import ./overrides/nbexec/0.2.0;
   };
