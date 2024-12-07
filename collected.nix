7c2ab92934f44d1daae72c2532182023e17d81f4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:05:33 +0000

    autogenerated overwrites for aioquic-mitmproxy==0.9.21.1

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioquic-mitmproxy = {
+    "0.9.21.1" = import ./overrides/aioquic-mitmproxy/0.9.21.1;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
