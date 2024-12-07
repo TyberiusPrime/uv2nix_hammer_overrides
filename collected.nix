bfa2211f3ca5e7175b81b73d236e10fe9a68dc22
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:34:02 +0000

    autogenerated overwrites for aiogithubapi==24.6.0

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aiogithubapi = {
+    "24.6.0" = import ./overrides/aiogithubapi/24.6.0;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
