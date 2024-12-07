28cda8a5becb13e6fa212eb25c86a527a7c5cb88
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-07 00:05:59 +0000

    autogenerated overwrites for django-mathfilters==1.0.0

--- collected.nix
+++ collected.nix
@@ -2039,6 +2039,9 @@
   django-markdown-deux = {
     "1.0.6" = import ./overrides/django-markdown-deux/1.0.6;
   };
+  django-mathfilters = {
+    "1.0.0" = import ./overrides/django-mathfilters/1.0.0;
+  };
   django-mdeditor = {
     "0.1.20" = import ./overrides/django-mdeditor/0.1.20;
   };
