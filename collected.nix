f8ba818a1244b834979b1799c49c8a15ad7676c2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:54:43 +0000

    autogenerated overwrites for types-backports==0.1.3

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-backports = {
+    "0.1.3" = import ./overrides/types-backports/0.1.3;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
