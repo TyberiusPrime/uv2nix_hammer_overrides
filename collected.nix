cc6e4730a90985db34cd2cb84b7a7c1a347cb40a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:43:59 +0000

    autogenerated overwrites for pylru==1.2.1

--- collected.nix
+++ collected.nix
@@ -6834,6 +6834,9 @@
   pylint-flask = {
     "0.6" = import ./overrides/pylint-flask/0.6;
   };
+  pylru = {
+    "1.2.1" = import ./overrides/pylru/1.2.1;
+  };
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
