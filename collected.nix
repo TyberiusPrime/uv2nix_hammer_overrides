06fd39e97b9b90410186d5557f952924d0432a00
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:02:04 +0000

    autogenerated overwrites for docker-pycreds==0.4.0

--- collected.nix
+++ collected.nix
@@ -2303,6 +2303,9 @@
   dnsq = {
     "1.1.6" = import ./overrides/dnsq/1.1.6;
   };
+  docker-pycreds = {
+    "0.4.0" = import ./overrides/docker-pycreds/0.4.0;
+  };
   dockerpty = {
     "0.4.1" = import ./overrides/dockerpty/0.4.1;
   };
