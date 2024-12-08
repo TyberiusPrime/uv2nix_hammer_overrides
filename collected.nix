3e50fa99d7a4489654c1f327877cc8a55112b21c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:51:45 +0000

    autogenerated overwrites for packageurl-python==0.15.6

--- collected.nix
+++ collected.nix
@@ -5638,6 +5638,9 @@
   p4a-z2utils = {
     "1.0.2" = import ./overrides/p4a-z2utils/1.0.2;
   };
+  packageurl-python = {
+    "0.15.6" = import ./overrides/packageurl-python/0.15.6;
+  };
   packbits = {
     "0.6" = import ./overrides/packbits/0.6;
   };
