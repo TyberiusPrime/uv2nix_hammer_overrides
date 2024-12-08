453be70ad09e451fd87938400fbc5fdca0dda36c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:40:16 +0000

    autogenerated overwrites for flwr==1.11.0

--- collected.nix
+++ collected.nix
@@ -2938,6 +2938,9 @@
   fluent = {
     "0.10.0" = import ./overrides/fluent/0.10.0;
   };
+  flwr = {
+    "1.11.0" = import ./overrides/flwr/1.11.0;
+  };
   fnvhash = {
     "0.1.0" = import ./overrides/fnvhash/0.1.0;
   };
