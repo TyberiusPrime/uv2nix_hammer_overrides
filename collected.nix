4a3189de165dae33d559365d8b46a9e4cf67d0b2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:58:39 +0000

    autogenerated overwrites for ipyplot==1.1.2

--- collected.nix
+++ collected.nix
@@ -3853,6 +3853,9 @@
   ipy = {
     "1.1" = import ./overrides/ipy/1.1;
   };
+  ipyplot = {
+    "1.1.2" = import ./overrides/ipyplot/1.1.2;
+  };
   ipyxact = {
     "0.3.2" = import ./overrides/ipyxact/0.3.2;
   };
