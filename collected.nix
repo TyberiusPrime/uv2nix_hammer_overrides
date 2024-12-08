b834b2e3e7a4ae8bf688b133b4c83308d3e7bf33
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:13:09 +0000

    autogenerated overwrites for google-cloud-automl==2.13.5

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-automl = {
+    "2.13.5" = import ./overrides/google-cloud-automl/2.13.5;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
