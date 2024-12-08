e4186ec6fdf9c4ec3597a15f3e6b64dff1a189e5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:16:33 +0000

    autogenerated overwrites for hvplot==0.10.0

--- collected.nix
+++ collected.nix
@@ -3586,6 +3586,9 @@
   husl = {
     "4.0.3" = import ./overrides/husl/4.0.3;
   };
+  hvplot = {
+    "0.10.0" = import ./overrides/hvplot/0.10.0;
+  };
   hwdata = {
     "2.4.1" = import ./overrides/hwdata/2.4.1;
   };
