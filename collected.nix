ebbbb3d39b2834e77b2611db9f687f0abd0a17c4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:01:43 +0000

    autogenerated overwrites for google-cloud-firestore==2.18.0

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-firestore = {
+    "2.18.0" = import ./overrides/google-cloud-firestore/2.18.0;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
