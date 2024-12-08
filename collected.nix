8b84deededb662d83123897e056b03d46628bd77
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:53:48 +0000

    autogenerated overwrites for goodwe==0.4.8

--- collected.nix
+++ collected.nix
@@ -3234,6 +3234,9 @@
   golgi = {
     "2.0.1" = import ./overrides/golgi/2.0.1;
   };
+  goodwe = {
+    "0.4.8" = import ./overrides/goodwe/0.4.8;
+  };
   google-api-python-client = {
     "1.7.11" = import ./overrides/google-api-python-client/1.7.11;
   };
