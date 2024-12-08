072dc862ea3f374000725ea31570d3b694149152
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:52:45 +0000

    autogenerated overwrites for craft-grammar==2.0.0

--- collected.nix
+++ collected.nix
@@ -1482,6 +1482,9 @@
   cracklib = {
     "2.9.6" = import ./overrides/cracklib/2.9.6;
   };
+  craft-grammar = {
+    "2.0.0" = import ./overrides/craft-grammar/2.0.0;
+  };
   crank = {
     "0.8.1" = import ./overrides/crank/0.8.1;
   };
