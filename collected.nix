e4f098d8f1186061c490d62f9dbb811cef3e1610
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:33:35 +0000

    autogenerated overwrites for LTTL==2.0.12

--- collected.nix
+++ collected.nix
@@ -4541,6 +4541,9 @@
   lru-dict = {
     "1.2.0" = import ./overrides/lru-dict/1.2.0;
   };
+  lttl = {
+    "2.0.12" = import ./overrides/lttl/2.0.12;
+  };
   lucene-querybuilder = {
     "0.2" = import ./overrides/lucene-querybuilder/0.2;
   };
