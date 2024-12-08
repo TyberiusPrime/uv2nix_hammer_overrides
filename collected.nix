e3f6e15cdf9cd5646ab83dc5fd9df03bcfeaedad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:26:05 +0000

    autogenerated overwrites for cssselect2==0.7.0

--- collected.nix
+++ collected.nix
@@ -1560,6 +1560,9 @@
   cssmin = {
     "0.2.0" = import ./overrides/cssmin/0.2.0;
   };
+  cssselect2 = {
+    "0.7.0" = import ./overrides/cssselect2/0.7.0;
+  };
   csv342 = {
     "1.0.1" = import ./overrides/csv342/1.0.1;
   };
