d905733526bea87e5e25364016ab2febd4fe8196
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:16:08 +0000

    autogenerated overwrites for typical==2.9.0

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  typical = {
+    "2.9.0" = import ./overrides/typical/2.9.0;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
