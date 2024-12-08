8f001b184856b7b918a81e43751c759289cf6eb5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:49:38 +0000

    autogenerated overwrites for ncbi-genome-download==0.3.3

--- collected.nix
+++ collected.nix
@@ -5138,6 +5138,9 @@
   nbexec = {
     "0.2.0" = import ./overrides/nbexec/0.2.0;
   };
+  ncbi-genome-download = {
+    "0.3.3" = import ./overrides/ncbi-genome-download/0.3.3;
+  };
   ncephes = {
     "1.1.0" = import ./overrides/ncephes/1.1.0;
   };
