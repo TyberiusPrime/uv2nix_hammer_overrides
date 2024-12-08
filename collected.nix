b778d7c63b0c9f911e7718ccae8d08529deee9f2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:40:15 +0000

    autogenerated overwrites for pymarc==5.2.2

--- collected.nix
+++ collected.nix
@@ -6843,6 +6843,9 @@
   pymailgunner = {
     "1.5" = import ./overrides/pymailgunner/1.5;
   };
+  pymarc = {
+    "5.2.2" = import ./overrides/pymarc/5.2.2;
+  };
   pymarktex = {
     "1.4.6" = import ./overrides/pymarktex/1.4.6;
   };
