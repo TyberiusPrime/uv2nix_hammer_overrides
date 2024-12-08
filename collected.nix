3e487233642b4c31312c9d4fb04bfc8ff9c623c8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:07:21 +0000

    autogenerated overwrites for transitions==0.9.2

--- collected.nix
+++ collected.nix
@@ -9293,6 +9293,9 @@
   transifex-client = {
     "0.14.4" = import ./overrides/transifex-client/0.14.4;
   };
+  transitions = {
+    "0.9.2" = import ./overrides/transitions/0.9.2;
+  };
   translitcodec = {
     "0.7.0" = import ./overrides/translitcodec/0.7.0;
   };
