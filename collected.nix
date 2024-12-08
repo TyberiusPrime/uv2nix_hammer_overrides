9bee556c1e3e4a7fd7fa8605036f4c2b91619d34
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:05:54 +0000

    autogenerated overwrites for manifestoo-core==1.7

--- collected.nix
+++ collected.nix
@@ -4598,6 +4598,9 @@
   mandrill = {
     "1.0.60" = import ./overrides/mandrill/1.0.60;
   };
+  manifestoo-core = {
+    "1.7" = import ./overrides/manifestoo-core/1.7;
+  };
   manifestparser = {
     "2.1.0" = import ./overrides/manifestparser/2.1.0;
   };
