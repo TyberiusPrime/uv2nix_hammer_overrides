9720227abb32cf1fa5d6c3cea7a3267c86e84600
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:22:22 +0000

    autogenerated overwrites for ddddocr==1.5.5

--- collected.nix
+++ collected.nix
@@ -1722,6 +1722,9 @@
   dd-aliyun-python-sdk-core = {
     "2.8.2" = import ./overrides/dd-aliyun-python-sdk-core/2.8.2;
   };
+  ddddocr = {
+    "1.5.5" = import ./overrides/ddddocr/1.5.5;
+  };
   deap = {
     "1.4.1" = import ./overrides/deap/1.4.1;
   };
