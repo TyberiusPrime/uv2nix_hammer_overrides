52970075cb4f193702450c6ab47bc0970bc9dc6c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:09:43 +0000

    autogenerated overwrites for pandas-datareader==0.10.0

--- collected.nix
+++ collected.nix
@@ -5681,6 +5681,9 @@
     "1.5.3" = import ./overrides/pandas/1.5.3;
     "2.2.2" = import ./overrides/pandas/2.2.2;
   };
+  pandas-datareader = {
+    "0.10.0" = import ./overrides/pandas-datareader/0.10.0;
+  };
   pandas-ml-common = {
     "0.2.7" = import ./overrides/pandas-ml-common/0.2.7;
   };
