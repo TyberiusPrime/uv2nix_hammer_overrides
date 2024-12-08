749482c5d6f59aae2e4234c319a42768a3edb60d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:18:44 +0000

    autogenerated overwrites for mkdocs-redirects==1.2.1

--- collected.nix
+++ collected.nix
@@ -4835,6 +4835,9 @@
   mkdocs-jupyter = {
     "0.12.0" = import ./overrides/mkdocs-jupyter/0.12.0;
   };
+  mkdocs-redirects = {
+    "1.2.1" = import ./overrides/mkdocs-redirects/1.2.1;
+  };
   ml-collections = {
     "0.1.1" = import ./overrides/ml-collections/0.1.1;
   };
