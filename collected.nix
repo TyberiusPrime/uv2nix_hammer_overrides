3e9bf474e16738e11e8eee211f54f0ee7912dfc0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:26:50 +0000

    autogenerated overwrites for azure-servicebus==7.12.2

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-servicebus = {
+    "7.12.2" = import ./overrides/azure-servicebus/7.12.2;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
