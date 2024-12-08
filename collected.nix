e505a294c77d0e262f15b9bb35dbb88460eea4e3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:54:27 +0000

    autogenerated overwrites for ipyfilechooser==0.6.0

--- collected.nix
+++ collected.nix
@@ -3853,6 +3853,9 @@
   ipy = {
     "1.1" = import ./overrides/ipy/1.1;
   };
+  ipyfilechooser = {
+    "0.6.0" = import ./overrides/ipyfilechooser/0.6.0;
+  };
   ipyxact = {
     "0.3.2" = import ./overrides/ipyxact/0.3.2;
   };
