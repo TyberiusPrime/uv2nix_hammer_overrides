cc0ca6ccbd6cb95f8b1f5072214e580d7645cfdb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:29:52 +0000

    autogenerated overwrites for toposort==1.10

--- collected.nix
+++ collected.nix
@@ -9175,6 +9175,9 @@
   toolwrapper = {
     "2.1.0" = import ./overrides/toolwrapper/2.1.0;
   };
+  toposort = {
+    "1.10" = import ./overrides/toposort/1.10;
+  };
   torch = {
     "2.4.0" = import ./overrides/torch/2.4.0;
     "2.4.1" = import ./overrides/torch/2.4.1;
