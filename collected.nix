a7d02ba4ac4b7581a2ce17834dad3f1c5c297a89
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:52:32 +0000

    autogenerated overwrites for edit-distance==1.0.6

--- collected.nix
+++ collected.nix
@@ -2466,6 +2466,9 @@
   edalize = {
     "0.5.4" = import ./overrides/edalize/0.5.4;
   };
+  edit-distance = {
+    "1.0.6" = import ./overrides/edit-distance/1.0.6;
+  };
   editorconfig = {
     "0.12.4" = import ./overrides/editorconfig/0.12.4;
   };
