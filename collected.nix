4f3b82bd2f25dbaa5beefc4de1cf597a6d89ffe5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:48:40 +0000

    autogenerated overwrites for dominate==2.9.1

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
+  dominate = {
+    "2.9.1" = import ./overrides/dominate/2.9.1;
+  };
   dopy = {
     "0.3.7" = import ./overrides/dopy/0.3.7;
   };
