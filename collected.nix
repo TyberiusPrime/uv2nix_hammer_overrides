acdef6b5d392c102d0ae70430910a3701960e490
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:23:13 +0000

    autogenerated overwrites for rasterstats==0.19.0

--- collected.nix
+++ collected.nix
@@ -7778,6 +7778,9 @@
   rask = {
     "0.1.5" = import ./overrides/rask/0.1.5;
   };
+  rasterstats = {
+    "0.19.0" = import ./overrides/rasterstats/0.19.0;
+  };
   ratelimit = {
     "2.2.1" = import ./overrides/ratelimit/2.2.1;
   };
