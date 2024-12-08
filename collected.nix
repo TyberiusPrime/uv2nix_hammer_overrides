f4ffc91ce60dbdf58817c44b2cc0f7d415cd491c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:13:50 +0000

    autogenerated overwrites for pathlib==1.0.1

--- collected.nix
+++ collected.nix
@@ -5736,6 +5736,9 @@
   path-and-address = {
     "2.0.1" = import ./overrides/path-and-address/2.0.1;
   };
+  pathlib = {
+    "1.0.1" = import ./overrides/pathlib/1.0.1;
+  };
   pathquery = {
     "0.3.0" = import ./overrides/pathquery/0.3.0;
   };
