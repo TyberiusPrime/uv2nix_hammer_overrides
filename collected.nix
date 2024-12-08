7f18b746cb7efc2f2ae59fe1fc14cbf25275dc36
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:57:33 +0000

    autogenerated overwrites for html-sanitizer==2.4.4

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  html-sanitizer = {
+    "2.4.4" = import ./overrides/html-sanitizer/2.4.4;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
