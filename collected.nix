f11f547c56d1df7be6867fd9a44c1f4457564adc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:32:41 +0000

    autogenerated overwrites for cloudmesh-cmd5==5.0.20

--- collected.nix
+++ collected.nix
@@ -1159,6 +1159,9 @@
   cloudinary = {
     "1.41.0" = import ./overrides/cloudinary/1.41.0;
   };
+  cloudmesh-cmd5 = {
+    "5.0.20" = import ./overrides/cloudmesh-cmd5/5.0.20;
+  };
   cloudshell-automation-api = {
     "8.3.0.168355" = import ./overrides/cloudshell-automation-api/8.3.0.168355;
   };
