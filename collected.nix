4ad31a37f3ed967cd284275edbd7dba837068891
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:10:10 +0000

    autogenerated overwrites for dynalite-devices==0.1.48

--- collected.nix
+++ collected.nix
@@ -2427,6 +2427,9 @@
   dyn = {
     "1.8.6" = import ./overrides/dyn/1.8.6;
   };
+  dynalite-devices = {
+    "0.1.48" = import ./overrides/dynalite-devices/0.1.48;
+  };
   dynamic-pytables-where-condition = {
     "1.1.1" = import ./overrides/dynamic-pytables-where-condition/1.1.1;
   };
