431b786000b2c068274e267a394cda904ec3a67b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:05:31 +0000

    autogenerated overwrites for sphinxcontrib-blockdiag==3.0.0

--- collected.nix
+++ collected.nix
@@ -8619,6 +8619,9 @@
   sphinxcontrib-bayesnet = {
     "0.4" = import ./overrides/sphinxcontrib-bayesnet/0.4;
   };
+  sphinxcontrib-blockdiag = {
+    "3.0.0" = import ./overrides/sphinxcontrib-blockdiag/3.0.0;
+  };
   sphinxcontrib-ditaa = {
     "1.0.2" = import ./overrides/sphinxcontrib-ditaa/1.0.2;
   };
