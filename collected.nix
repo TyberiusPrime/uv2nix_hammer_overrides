4aec898b36c8d5a0021636538d43a5a4e1e9daa8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:26:27 +0000

    autogenerated overwrites for django-safedelete==1.4.0

--- collected.nix
+++ collected.nix
@@ -2129,6 +2129,9 @@
   django-rtk = {
     "0.6.0" = import ./overrides/django-rtk/0.6.0;
   };
+  django-safedelete = {
+    "1.4.0" = import ./overrides/django-safedelete/1.4.0;
+  };
   django-sendfile = {
     "0.3.11" = import ./overrides/django-sendfile/0.3.11;
   };
