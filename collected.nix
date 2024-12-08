7bd40389364eeb3edd2e1cd42900fed11408108d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:33:33 +0000

    autogenerated overwrites for llama-index-program-openai==0.2.0

--- collected.nix
+++ collected.nix
@@ -4455,6 +4455,9 @@
   ll-xist = {
     "5.76" = import ./overrides/ll-xist/5.76;
   };
+  llama-index-program-openai = {
+    "0.2.0" = import ./overrides/llama-index-program-openai/0.2.0;
+  };
   llist = {
     "0.8" = import ./overrides/llist/0.8;
   };
