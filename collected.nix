ce87254b67f18186478d1015b7ff616feb225761
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:20:23 +0000

    autogenerated overwrites for gviz-api==1.10.0

--- collected.nix
+++ collected.nix
@@ -3359,6 +3359,9 @@
   gtts-token = {
     "1.1.4" = import ./overrides/gtts-token/1.1.4;
   };
+  gviz-api = {
+    "1.10.0" = import ./overrides/gviz-api/1.10.0;
+  };
   gym = {
     "0.26.2" = import ./overrides/gym/0.26.2;
   };
