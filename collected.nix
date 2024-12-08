e1046e0bf6e860433454f47238d2bc178a6d17ca
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 17:43:31 +0000

    autogenerated overwrites for h2==4.1.0

--- collected.nix
+++ collected.nix
@@ -3362,6 +3362,9 @@
   gym = {
     "0.26.2" = import ./overrides/gym/0.26.2;
   };
+  h2 = {
+    "4.1.0" = import ./overrides/h2/4.1.0;
+  };
   h2o = {
     "3.46.0.5" = import ./overrides/h2o/3.46.0.5;
   };
