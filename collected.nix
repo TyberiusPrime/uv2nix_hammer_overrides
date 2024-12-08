3fcd0cf922ccff9b6b45342d0a420ebc36977e28
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:40:33 +0000

    autogenerated overwrites for opencensus-context==0.1.3

--- collected.nix
+++ collected.nix
@@ -5519,6 +5519,9 @@
   openapi-codec = {
     "1.3.2" = import ./overrides/openapi-codec/1.3.2;
   };
+  opencensus-context = {
+    "0.1.3" = import ./overrides/opencensus-context/0.1.3;
+  };
   opencmiss-utils = {
     "0.4.2" = import ./overrides/opencmiss-utils/0.4.2;
   };
