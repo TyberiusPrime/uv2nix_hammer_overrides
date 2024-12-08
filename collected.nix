534d34893a90d712d8c5ba26812697259fdcf531
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:36:43 +0000

    autogenerated overwrites for crowdstrike-falconpy==1.4.5

--- collected.nix
+++ collected.nix
@@ -1515,6 +1515,9 @@
   crossbar = {
     "22.6.1" = import ./overrides/crossbar/22.6.1;
   };
+  crowdstrike-falconpy = {
+    "1.4.5" = import ./overrides/crowdstrike-falconpy/1.4.5;
+  };
   cryio = {
     "2018.5.30" = import ./overrides/cryio/2018.5.30;
   };
