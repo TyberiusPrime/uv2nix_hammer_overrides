3ffeadee85ae35fe897bb0ee78d10953f7f92219
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:04:56 +0000

    autogenerated overwrites for pies==2.6.7

--- collected.nix
+++ collected.nix
@@ -5896,6 +5896,9 @@
   piep = {
     "0.10.0" = import ./overrides/piep/0.10.0;
   };
+  pies = {
+    "2.6.7" = import ./overrides/pies/2.6.7;
+  };
   pii-data = {
     "0.5.0" = import ./overrides/pii-data/0.5.0;
   };
