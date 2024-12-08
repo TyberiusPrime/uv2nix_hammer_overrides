41de0228ea179e0f0462487c3e2597c61fd81580
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:16:54 +0000

    autogenerated overwrites for devolo-home-control-api==0.18.3

--- collected.nix
+++ collected.nix
@@ -1782,6 +1782,9 @@
   device-detector = {
     "5.0.1" = import ./overrides/device-detector/5.0.1;
   };
+  devolo-home-control-api = {
+    "0.18.3" = import ./overrides/devolo-home-control-api/0.18.3;
+  };
   dexy = {
     "2.0.9" = import ./overrides/dexy/2.0.9;
   };
