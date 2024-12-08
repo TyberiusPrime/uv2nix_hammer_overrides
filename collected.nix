66d186e44a8345257da95d33e3531b82601c77e2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:51:01 +0000

    autogenerated overwrites for yubikey-manager==5.5.1

--- collected.nix
+++ collected.nix
@@ -10097,6 +10097,9 @@
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
+  yubikey-manager = {
+    "5.5.1" = import ./overrides/yubikey-manager/5.5.1;
+  };
   yurl = {
     "1.0.0" = import ./overrides/yurl/1.0.0;
   };
