34b07319963669d0a539554552b608db620c64f2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:19:17 +0000

    autogenerated overwrites for pytest-factoryboy==2.7.0

--- collected.nix
+++ collected.nix
@@ -7224,6 +7224,9 @@
   pytest-expect = {
     "1.1.0" = import ./overrides/pytest-expect/1.1.0;
   };
+  pytest-factoryboy = {
+    "2.7.0" = import ./overrides/pytest-factoryboy/2.7.0;
+  };
   pytest-json = {
     "0.4.0" = import ./overrides/pytest-json/0.4.0;
   };
