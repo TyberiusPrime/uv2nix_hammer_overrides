c79a19395f189ad5b6f4852db3d6c3d908fe904b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:47:42 +0000

    autogenerated overwrites for sqloxide==0.1.48

--- collected.nix
+++ collected.nix
@@ -8689,6 +8689,9 @@
   sqlmap = {
     "1.8.9" = import ./overrides/sqlmap/1.8.9;
   };
+  sqloxide = {
+    "0.1.48" = import ./overrides/sqloxide/0.1.48;
+  };
   sqlsoup = {
     "0.9.1" = import ./overrides/sqlsoup/0.9.1;
   };
