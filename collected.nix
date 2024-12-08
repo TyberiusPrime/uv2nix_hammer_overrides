391dbc93a8880752c141617fa98f6e35ecf018dd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:09:31 +0000

    autogenerated overwrites for types-aiobotocore-alexaforbusiness==2.13.0

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-alexaforbusiness = {
+    "2.13.0" = import ./overrides/types-aiobotocore-alexaforbusiness/2.13.0;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
