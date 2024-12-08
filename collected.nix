d1675007cdf18590c725e02a611cce8e8a389768
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:19:22 +0000

    autogenerated overwrites for pip-autoremove==0.10.0

--- collected.nix
+++ collected.nix
@@ -5925,6 +5925,9 @@
   pinyin = {
     "0.4.0" = import ./overrides/pinyin/0.4.0;
   };
+  pip-autoremove = {
+    "0.10.0" = import ./overrides/pip-autoremove/0.10.0;
+  };
   pip-services3-commons = {
     "3.3.14" = import ./overrides/pip-services3-commons/3.3.14;
   };
