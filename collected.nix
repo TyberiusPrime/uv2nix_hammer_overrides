1bc37b91a81964cf9e9641c4ca99fa50a677e60e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:42:14 +0000

    autogenerated overwrites for types-protobuf==5.27.0.20240907

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-protobuf = {
+    "5.27.0.20240907" = import ./overrides/types-protobuf/5.27.0.20240907;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
