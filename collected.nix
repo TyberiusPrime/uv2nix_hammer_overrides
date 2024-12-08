39ab28b870f723faa5da8c35fe0b60ef470ab54d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:07:58 +0000

    autogenerated overwrites for pytorch-nlp==0.5.0

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytorch-nlp = {
+    "0.5.0" = import ./overrides/pytorch-nlp/0.5.0;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
