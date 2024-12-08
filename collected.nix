f6185caf16b8b356838f832625468dd7c896a29d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:26:02 +0000

    autogenerated overwrites for interpret-community==0.31.0

--- collected.nix
+++ collected.nix
@@ -3796,6 +3796,9 @@
   intermake = {
     "1.0.0.78" = import ./overrides/intermake/1.0.0.78;
   };
+  interpret-community = {
+    "0.31.0" = import ./overrides/interpret-community/0.31.0;
+  };
   interruptingcow = {
     "0.8" = import ./overrides/interruptingcow/0.8;
   };
