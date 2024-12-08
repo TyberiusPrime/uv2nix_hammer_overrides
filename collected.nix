0fbd3fb45417be3fb25aeb85c7cb49dacc81578d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 22:28:47 +0000

    autogenerated overwrites for omnilogic==0.5.0

--- collected.nix
+++ collected.nix
@@ -5480,6 +5480,9 @@
   omnijson = {
     "0.1.2" = import ./overrides/omnijson/0.1.2;
   };
+  omnilogic = {
+    "0.5.0" = import ./overrides/omnilogic/0.5.0;
+  };
   onetimepad = {
     "1.4" = import ./overrides/onetimepad/1.4;
   };
