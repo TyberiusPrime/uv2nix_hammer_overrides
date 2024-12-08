bf30275961c954494f539217783d71900109eba3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:33:44 +0000

    autogenerated overwrites for homeassistant==2024.9.1

--- collected.nix
+++ collected.nix
@@ -3483,6 +3483,9 @@
   home-assistant-chip-core = {
     "2024.7.1" = import ./overrides/home-assistant-chip-core/2024.7.1;
   };
+  homeassistant = {
+    "2024.9.1" = import ./overrides/homeassistant/2024.9.1;
+  };
   homura = {
     "0.1.5" = import ./overrides/homura/0.1.5;
   };
