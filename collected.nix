14c1b7bd7cee67811397d855b3cb69803f2a774f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 21:28:15 +0000

    autogenerated overwrites for detect-secrets==1.5.0

--- collected.nix
+++ collected.nix
@@ -1776,6 +1776,9 @@
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
+  detect-secrets = {
+    "1.5.0" = import ./overrides/detect-secrets/1.5.0;
+  };
   dev0s = {
     "3.36.6" = import ./overrides/dev0s/3.36.6;
   };
