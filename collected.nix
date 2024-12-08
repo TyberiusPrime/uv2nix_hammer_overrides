fe0a778ea619623a3f3ec9adf3973edb2be515cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:13:16 +0000

    autogenerated overwrites for mypy-boto3-emr-containers==1.35.4

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  mypy-boto3-emr-containers = {
+    "1.35.4" = import ./overrides/mypy-boto3-emr-containers/1.35.4;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
