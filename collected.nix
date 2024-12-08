3aa6c53445336ee41130a8d8dcbdb6037bba5960
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 20:12:25 +0000

    autogenerated overwrites for xlutils==2.0.0

--- collected.nix
+++ collected.nix
@@ -10016,6 +10016,9 @@
   xkbcommon = {
     "1.5.1" = import ./overrides/xkbcommon/1.5.1;
   };
+  xlutils = {
+    "2.0.0" = import ./overrides/xlutils/2.0.0;
+  };
   xlwt-future = {
     "0.8.0" = import ./overrides/xlwt-future/0.8.0;
   };
