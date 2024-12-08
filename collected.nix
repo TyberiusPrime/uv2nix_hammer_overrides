e46f89a9c9c8e75aeb4c0bf8e8f08d30fb178ead
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 23:57:38 +0000

    autogenerated overwrites for django-oscar==3.2.4

--- collected.nix
+++ collected.nix
@@ -2072,6 +2072,9 @@
   django-ohm2-handlers-light = {
     "0.4.3" = import ./overrides/django-ohm2-handlers-light/0.4.3;
   };
+  django-oscar = {
+    "3.2.4" = import ./overrides/django-oscar/3.2.4;
+  };
   django-pagination = {
     "1.0.7" = import ./overrides/django-pagination/1.0.7;
   };
