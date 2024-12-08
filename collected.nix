35cd5df47312eabb375582639e40edc8adf72754
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:47:38 +0000

    autogenerated overwrites for aioinflux==0.9.0

--- collected.nix
+++ collected.nix
@@ -117,6 +117,9 @@
   aiohttp-swagger3 = {
     "0.8.0" = import ./overrides/aiohttp-swagger3/0.8.0;
   };
+  aioinflux = {
+    "0.9.0" = import ./overrides/aioinflux/0.9.0;
+  };
   aiolivisi = {
     "0.0.19" = import ./overrides/aiolivisi/0.0.19;
   };
