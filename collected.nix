302ddce0a78a93133259ba7e4b600096df3759fd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:59:51 +0000

    autogenerated overwrites for rosdep==0.25.1

--- collected.nix
+++ collected.nix
@@ -8015,6 +8015,9 @@
   ros-cdk-cxapi = {
     "1.2.0" = import ./overrides/ros-cdk-cxapi/1.2.0;
   };
+  rosdep = {
+    "0.25.1" = import ./overrides/rosdep/0.25.1;
+  };
   rouge-score = {
     "0.1.2" = import ./overrides/rouge-score/0.1.2;
   };
