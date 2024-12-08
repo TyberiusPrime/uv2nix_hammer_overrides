6a2a75df3e171385399d37c761aec9ac8a543239
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 18:51:48 +0000

    autogenerated overwrites for kiwiki-client==0.1.2

--- collected.nix
+++ collected.nix
@@ -4199,6 +4199,9 @@
   kiwi = {
     "10.1.8" = import ./overrides/kiwi/10.1.8;
   };
+  kiwiki-client = {
+    "0.1.2" = import ./overrides/kiwiki-client/0.1.2;
+  };
   knnimpute = {
     "0.1.0" = import ./overrides/knnimpute/0.1.0;
   };
