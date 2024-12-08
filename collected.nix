3d57519caebabba51cf2d7f775cb468d96bd8c99
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-08 19:41:47 +0000

    autogenerated overwrites for grappelli-safe==1.1.1

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  grappelli-safe = {
+    "1.1.1" = import ./overrides/grappelli-safe/1.1.1;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
