d5c742a7104f549f83449d06aaa2ec86932f9316
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:35:13 +0000

    autogenerated overwrites for pynobo==1.8.1

--- collected.nix
+++ collected.nix
@@ -6913,6 +6913,9 @@
     "0.6.4" = import ./overrides/pynisher/0.6.4;
     "1.0.10" = import ./overrides/pynisher/1.0.10;
   };
+  pynobo = {
+    "1.8.1" = import ./overrides/pynobo/1.8.1;
+  };
   pynsq = {
     "0.9.1" = import ./overrides/pynsq/0.9.1;
   };
