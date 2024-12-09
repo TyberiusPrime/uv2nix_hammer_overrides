6a7482d9f9354664963291f6daa378108ff68685
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:41:19 +0000

    autogenerated overwrites for llama-index==0.11.8

--- collected.nix
+++ collected.nix
@@ -4455,6 +4455,9 @@
   ll-xist = {
     "5.76" = import ./overrides/ll-xist/5.76;
   };
+  llama-index = {
+    "0.11.8" = import ./overrides/llama-index/0.11.8;
+  };
   llist = {
     "0.8" = import ./overrides/llist/0.8;
   };
