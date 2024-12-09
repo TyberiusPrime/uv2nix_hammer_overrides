133fa09ca441b48d9200cf1e3f42a7eec75f706a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:55:54 +0000

    autogenerated overwrites for hurry.resource==0.10

--- collected.nix
+++ collected.nix
@@ -3583,6 +3583,9 @@
   hunspell = {
     "0.5.5" = import ./overrides/hunspell/0.5.5;
   };
+  hurry-resource = {
+    "0.10" = import ./overrides/hurry-resource/0.10;
+  };
   husl = {
     "4.0.3" = import ./overrides/husl/4.0.3;
   };
