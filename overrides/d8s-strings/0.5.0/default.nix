82e8b117be74ac09b13b966f52c590fc0bb9a5d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:52:15 +0000

    autogenerated overwrites for d8s-strings==0.5.0

--- overrides/d8s-strings/0.5.0/default.nix
+++ overrides/d8s-strings/0.5.0/default.nix
@@ -1,10 +1,14 @@
-{final, pkgs, ...}
-        : old: {
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];setuptools-scm = [];wheel = [];});})) (old: old // ( {
   postInstall =
     old.postInstall or ""
     + ''
       rm -rf $out/${final.python.sitePackages}/tests
     '';
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
