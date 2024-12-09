b1313033f515f3fb177a4cb2f95d588c61224b33
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:30:25 +0000

    autogenerated overwrites for pylast==5.3.0

--- collected.nix
+++ collected.nix
@@ -6794,6 +6794,9 @@
   pylacrosse = {
     "0.4" = import ./overrides/pylacrosse/0.4;
   };
+  pylast = {
+    "5.3.0" = import ./overrides/pylast/5.3.0;
+  };
   pylatex = {
     "1.4.2" = import ./overrides/pylatex/1.4.2;
   };
