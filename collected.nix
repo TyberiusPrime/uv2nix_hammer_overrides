b3f2ae5fe222d3332bed85b8fe5f4971e805dd56
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:08:06 +0000

    autogenerated overwrites for motioneye-client==0.3.14

--- collected.nix
+++ collected.nix
@@ -4931,6 +4931,9 @@
   mosestokenizer = {
     "1.2.1" = import ./overrides/mosestokenizer/1.2.1;
   };
+  motioneye-client = {
+    "0.3.14" = import ./overrides/motioneye-client/0.3.14;
+  };
   motuclient = {
     "3.0.0" = import ./overrides/motuclient/3.0.0;
   };
