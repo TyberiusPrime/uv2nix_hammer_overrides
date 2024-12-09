87c36868bc93e10c3bfcc7406cbc02ad89b347ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:20:09 +0000

    autogenerated overwrites for rosdistro==0.9.1

--- collected.nix
+++ collected.nix
@@ -8015,6 +8015,9 @@
   ros-cdk-cxapi = {
     "1.2.0" = import ./overrides/ros-cdk-cxapi/1.2.0;
   };
+  rosdistro = {
+    "0.9.1" = import ./overrides/rosdistro/0.9.1;
+  };
   rouge-score = {
     "0.1.2" = import ./overrides/rouge-score/0.1.2;
   };
