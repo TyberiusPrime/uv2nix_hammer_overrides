de7ca88089b73b357f6e43e5140f907ca9502d4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:03:27 +0000

    autogenerated overwrites for astropy-iers-data==0.2024.9.2.0.33.23

--- collected.nix
+++ collected.nix
@@ -419,6 +419,9 @@
   astropy-helpers = {
     "4.0.1" = import ./overrides/astropy-helpers/4.0.1;
   };
+  astropy-iers-data = {
+    "0.2024.9.2.0.33.23" = import ./overrides/astropy-iers-data/0.2024.9.2.0.33.23;
+  };
   astutils = {
     "0.0.6" = import ./overrides/astutils/0.0.6;
   };
