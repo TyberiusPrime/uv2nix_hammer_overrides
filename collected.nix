4491346f00270adeb08f45fe907254a23a080e2f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:50:25 +0000

    autogenerated overwrites for recipe-scrapers==15.1.0

--- collected.nix
+++ collected.nix
@@ -7808,6 +7808,9 @@
   recaptcha-client = {
     "1.0.6" = import ./overrides/recaptcha-client/1.0.6;
   };
+  recipe-scrapers = {
+    "15.1.0" = import ./overrides/recipe-scrapers/15.1.0;
+  };
   reconfigure = {
     "0.1.82" = import ./overrides/reconfigure/0.1.82;
   };
