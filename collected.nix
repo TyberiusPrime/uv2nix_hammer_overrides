04c0d1649723e741713206ed1bcf84fb57fb473c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:52:02 +0000

    autogenerated overwrites for mcuuid==1.1.0

--- collected.nix
+++ collected.nix
@@ -4670,6 +4670,9 @@
   mcfit = {
     "0.0.22" = import ./overrides/mcfit/0.0.22;
   };
+  mcuuid = {
+    "1.1.0" = import ./overrides/mcuuid/1.1.0;
+  };
   mdstat = {
     "1.0.4" = import ./overrides/mdstat/1.0.4;
   };
