78af1b56973ee1f49e32c46ceba454b8338531eb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:17:48 +0000

    autogenerated overwrites for coordinates==0.4.0

--- collected.nix
+++ collected.nix
@@ -1403,6 +1403,9 @@
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
+  coordinates = {
+    "0.4.0" = import ./overrides/coordinates/0.4.0;
+  };
   coqui-trainer = {
     "0.0.5" = import ./overrides/coqui-trainer/0.0.5;
   };
