419d251a28339df768f37e426f02cef2066e3f21
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:13:54 +0000

    autogenerated overwrites for pytest-astropy==0.11.0

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-astropy = {
+    "0.11.0" = import ./overrides/pytest-astropy/0.11.0;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
