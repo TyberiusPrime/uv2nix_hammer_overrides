f0717467ee215405902d40be518bd7aa8bc35204
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:18:15 +0000

    autogenerated overwrites for i-mongodb==2.0.1

--- collected.nix
+++ collected.nix
@@ -3610,6 +3610,9 @@
   hyrule = {
     "0.6.0" = import ./overrides/hyrule/0.6.0;
   };
+  i-mongodb = {
+    "2.0.1" = import ./overrides/i-mongodb/2.0.1;
+  };
   i3-instant-layout = {
     "0.1.12" = import ./overrides/i3-instant-layout/0.1.12;
   };
