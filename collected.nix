02785b8003317875ccd144652f5e40f41c9c88ae
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:29:36 +0000

    autogenerated overwrites for opentelemetry-exporter-gcp-trace==1.7.0

--- collected.nix
+++ collected.nix
@@ -5559,6 +5559,9 @@
   opensensemap-api = {
     "0.3.2" = import ./overrides/opensensemap-api/0.3.2;
   };
+  opentelemetry-exporter-gcp-trace = {
+    "1.7.0" = import ./overrides/opentelemetry-exporter-gcp-trace/1.7.0;
+  };
   opentracing = {
     "1.3.0" = import ./overrides/opentracing/1.3.0;
     "2.4.0" = import ./overrides/opentracing/2.4.0;
