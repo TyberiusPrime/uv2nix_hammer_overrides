a562af1b0d69d5b8a0ac195c760337070724ad10
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:58:36 +0000

    autogenerated overwrites for pytorch-lightning-bolts==0.3.2.post1

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytorch-lightning-bolts = {
+    "0.3.2.post1" = import ./overrides/pytorch-lightning-bolts/0.3.2.post1;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
