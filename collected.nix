416255430fec04bcdddd41bb8e03e8b50b6475ff
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:08:44 +0000

    autogenerated overwrites for bincopy==20.0.0

--- collected.nix
+++ collected.nix
@@ -707,6 +707,9 @@
   binary2strings = {
     "0.1.13" = import ./overrides/binary2strings/0.1.13;
   };
+  bincopy = {
+    "20.0.0" = import ./overrides/bincopy/20.0.0;
+  };
   bintrees = {
     "2.2.0" = import ./overrides/bintrees/2.2.0;
   };
