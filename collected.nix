c2ec6794152a8f7a3007d64d3bf0f4560436dbad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:07:02 +0000

    autogenerated overwrites for mitogen==0.3.9

--- collected.nix
+++ collected.nix
@@ -4829,6 +4829,9 @@
   misaka = {
     "2.1.1" = import ./overrides/misaka/2.1.1;
   };
+  mitogen = {
+    "0.3.9" = import ./overrides/mitogen/0.3.9;
+  };
   mkdocs-exclude = {
     "1.0.2" = import ./overrides/mkdocs-exclude/1.0.2;
   };
