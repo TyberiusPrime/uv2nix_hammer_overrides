37ea6d58589532a9bf51c03dad1150d0919c1189
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:08:14 +0000

    autogenerated overwrites for azure-mgmt-storage==21.2.1

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-mgmt-storage = {
+    "21.2.1" = import ./overrides/azure-mgmt-storage/21.2.1;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
