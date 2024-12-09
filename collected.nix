a784002112b6564afe006b64f99f0b414e3eef85
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:29:27 +0000

    autogenerated overwrites for imbalanced-learn==0.12.3

--- collected.nix
+++ collected.nix
@@ -3682,6 +3682,9 @@
   imantics = {
     "0.1.12" = import ./overrides/imantics/0.1.12;
   };
+  imbalanced-learn = {
+    "0.12.3" = import ./overrides/imbalanced-learn/0.12.3;
+  };
   img2pdf = {
     "0.5.1" = import ./overrides/img2pdf/0.5.1;
   };
