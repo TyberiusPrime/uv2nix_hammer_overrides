7487888f28caab460aaa38eabd65d37f597e35b6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:18:35 +0000

    autogenerated overwrites for youseedee==0.5.3

--- collected.nix
+++ collected.nix
@@ -10094,6 +10094,9 @@
   yolov5 = {
     "7.0.13" = import ./overrides/yolov5/7.0.13;
   };
+  youseedee = {
+    "0.5.3" = import ./overrides/youseedee/0.5.3;
+  };
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
