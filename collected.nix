6dd019271f88dc5dd15f9c51d44a2131a552ce2f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:40:25 +0000

    autogenerated overwrites for pyramid-tm==2.5

--- collected.nix
+++ collected.nix
@@ -7042,6 +7042,9 @@
   pyramid-redis-sessions = {
     "1.0.1" = import ./overrides/pyramid-redis-sessions/1.0.1;
   };
+  pyramid-tm = {
+    "2.5" = import ./overrides/pyramid-tm/2.5;
+  };
   pyreact = {
     "0.6.0" = import ./overrides/pyreact/0.6.0;
   };
