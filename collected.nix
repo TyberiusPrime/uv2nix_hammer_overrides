e2428cc5272cd6be38e79cedfbc581d2134ba48a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:42:10 +0000

    autogenerated overwrites for lastversion==3.5.6

--- collected.nix
+++ collected.nix
@@ -4251,6 +4251,9 @@
     "0.7.8" = import ./overrides/lark-parser/0.7.8;
     "0.9.0" = import ./overrides/lark-parser/0.9.0;
   };
+  lastversion = {
+    "3.5.6" = import ./overrides/lastversion/3.5.6;
+  };
   latex = {
     "0.7.0" = import ./overrides/latex/0.7.0;
   };
