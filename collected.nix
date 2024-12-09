b0a4df01f2cf287be2ff80fba8b783b6d9ac3c06
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:31:49 +0000

    autogenerated overwrites for datadog==0.50.0

--- collected.nix
+++ collected.nix
@@ -1686,6 +1686,9 @@
   datadbs = {
     "0.1.8" = import ./overrides/datadbs/0.1.8;
   };
+  datadog = {
+    "0.50.0" = import ./overrides/datadog/0.50.0;
+  };
   datapipelines = {
     "1.0.7" = import ./overrides/datapipelines/1.0.7;
   };
