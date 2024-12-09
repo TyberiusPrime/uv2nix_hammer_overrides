da1174cb3ee6ab7c852a54c011fda2cbde6d7b7a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:04:11 +0000

    autogenerated overwrites for pydicom==3.0.0

--- collected.nix
+++ collected.nix
@@ -6563,6 +6563,9 @@
   pydevd-pycharm = {
     "242.21829.153" = import ./overrides/pydevd-pycharm/242.21829.153;
   };
+  pydicom = {
+    "3.0.0" = import ./overrides/pydicom/3.0.0;
+  };
   pydoe = {
     "0.3.8" = import ./overrides/pydoe/0.3.8;
   };
