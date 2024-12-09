1357883c2d20e0118ef55fe0fee0009d4e44e83e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:59:34 +0000

    autogenerated overwrites for pylutron-caseta==0.21.1

--- collected.nix
+++ collected.nix
@@ -6837,6 +6837,9 @@
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
+  pylutron-caseta = {
+    "0.21.1" = import ./overrides/pylutron-caseta/0.21.1;
+  };
   pylzma = {
     "0.5.0" = import ./overrides/pylzma/0.5.0;
   };
