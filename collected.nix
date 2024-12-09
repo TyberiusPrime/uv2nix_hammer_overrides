ad05f7255fba6c4b0852d4fe53f9de222bff59fe
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:25:10 +0000

    autogenerated overwrites for neo==0.13.3

--- collected.nix
+++ collected.nix
@@ -5147,6 +5147,9 @@
   negspacy = {
     "1.0.4" = import ./overrides/negspacy/1.0.4;
   };
+  neo = {
+    "0.13.3" = import ./overrides/neo/0.13.3;
+  };
   neo4j = {
     "5.19.0" = import ./overrides/neo4j/5.19.0;
   };
