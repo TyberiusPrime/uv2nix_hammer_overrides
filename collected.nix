0f813b1538cf69974e5d7c49b84b259630eb92d4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:20:21 +0000

    autogenerated overwrites for xdg==6.0.0

--- collected.nix
+++ collected.nix
@@ -9998,6 +9998,9 @@
   xcffib = {
     "1.5.0" = import ./overrides/xcffib/1.5.0;
   };
+  xdg = {
+    "6.0.0" = import ./overrides/xdg/6.0.0;
+  };
   xdis = {
     "6.1.1" = import ./overrides/xdis/6.1.1;
   };
