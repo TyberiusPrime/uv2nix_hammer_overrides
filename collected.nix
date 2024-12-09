93dbe994b382799905aa999aa0a7c4669169bb7d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:26:15 +0000

    autogenerated overwrites for cert-schema==3.6.4

--- collected.nix
+++ collected.nix
@@ -978,6 +978,9 @@
   cenpy = {
     "1.0.1" = import ./overrides/cenpy/1.0.1;
   };
+  cert-schema = {
+    "3.6.4" = import ./overrides/cert-schema/3.6.4;
+  };
   cf-units = {
     "3.2.0" = import ./overrides/cf-units/3.2.0;
   };
