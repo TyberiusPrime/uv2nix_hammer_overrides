cc468871ccbb273b36b7c9dd2cdf34f5e8ee80a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:31:45 +0000

    autogenerated overwrites for pytest-subprocess==1.5.2

--- collected.nix
+++ collected.nix
@@ -7245,6 +7245,9 @@
   pytest-services = {
     "2.2.1" = import ./overrides/pytest-services/2.2.1;
   };
+  pytest-subprocess = {
+    "1.5.2" = import ./overrides/pytest-subprocess/1.5.2;
+  };
   pytest-subtesthack = {
     "0.2.0" = import ./overrides/pytest-subtesthack/0.2.0;
   };
