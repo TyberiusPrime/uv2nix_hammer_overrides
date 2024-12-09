2281d2fc6d66910e0b167e45c480fdd30ee44717
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:16:20 +0000

    autogenerated overwrites for whois==1.20240129.2

--- collected.nix
+++ collected.nix
@@ -9883,6 +9883,9 @@
   whisper = {
     "1.1.10" = import ./overrides/whisper/1.1.10;
   };
+  whois = {
+    "1.20240129.2" = import ./overrides/whois/1.20240129.2;
+  };
   whylogs-sketching = {
     "3.4.1.dev3" = import ./overrides/whylogs-sketching/3.4.1.dev3;
   };
