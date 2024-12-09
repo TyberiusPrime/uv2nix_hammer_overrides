5d46cf54b4ca3b3e91dd856dc57777922a1cd615
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 16:24:28 +0000

    autogenerated overwrites for netdisco==3.0.0

--- collected.nix
+++ collected.nix
@@ -5187,6 +5187,9 @@
   nestle = {
     "0.2.0" = import ./overrides/nestle/0.2.0;
   };
+  netdisco = {
+    "3.0.0" = import ./overrides/netdisco/3.0.0;
+  };
   netifaces = {
     "0.11.0" = import ./overrides/netifaces/0.11.0;
   };
