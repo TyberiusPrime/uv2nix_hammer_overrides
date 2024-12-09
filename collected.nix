a8b4c4dce101fdd0ff21ea04956b57726299e732
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:18:25 +0000

    autogenerated overwrites for djangorestframework-types==0.8.0

--- collected.nix
+++ collected.nix
@@ -2240,6 +2240,9 @@
   djangorestframework-jsonschema = {
     "0.1.1" = import ./overrides/djangorestframework-jsonschema/0.1.1;
   };
+  djangorestframework-types = {
+    "0.8.0" = import ./overrides/djangorestframework-types/0.8.0;
+  };
   djangotoolbox = {
     "1.8.0" = import ./overrides/djangotoolbox/1.8.0;
   };
