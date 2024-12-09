4d00ca9eccd90b3b9418136ca70639ed2bdc056f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:46:45 +0000

    autogenerated overwrites for greenery==4.2.2

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  greenery = {
+    "4.2.2" = import ./overrides/greenery/4.2.2;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
