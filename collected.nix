e45a335597c4a15cb7843de3ca7ae32cf4c7aa5b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:06:25 +0000

    autogenerated overwrites for pytomlpp==1.0.13

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytomlpp = {
+    "1.0.13" = import ./overrides/pytomlpp/1.0.13;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
