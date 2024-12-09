0fffbc8fdc9dec7560344731c25b31d2e8d7e344
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:01:59 +0000

    autogenerated overwrites for asyncsleepiq==1.5.3

--- collected.nix
+++ collected.nix
@@ -452,6 +452,9 @@
   asyncpgsa = {
     "0.27.1" = import ./overrides/asyncpgsa/0.27.1;
   };
+  asyncsleepiq = {
+    "1.5.3" = import ./overrides/asyncsleepiq/1.5.3;
+  };
   atomic = {
     "0.7.3" = import ./overrides/atomic/0.7.3;
   };
