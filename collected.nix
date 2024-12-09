fb3cb8ca2deecd6b3ae08e2a36937230d9db3659
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:47:31 +0000

    autogenerated overwrites for dufte==0.2.29

--- collected.nix
+++ collected.nix
@@ -2406,6 +2406,9 @@
   duckietown-docker-utils-daffy = {
     "6.1.1" = import ./overrides/duckietown-docker-utils-daffy/6.1.1;
   };
+  dufte = {
+    "0.2.29" = import ./overrides/dufte/0.2.29;
+  };
   dugong = {
     "3.8.2" = import ./overrides/dugong/3.8.2;
   };
