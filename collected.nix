950d8723ef53ab46ab0dacffb8fb8efed5bc6064
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:33:29 +0000

    autogenerated overwrites for param==2.1.1

--- collected.nix
+++ collected.nix
@@ -5711,6 +5711,9 @@
   papis-python-rofi = {
     "1.0.3" = import ./overrides/papis-python-rofi/1.0.3;
   };
+  param = {
+    "2.1.1" = import ./overrides/param/2.1.1;
+  };
   parametrizer = {
     "0.0.3" = import ./overrides/parametrizer/0.0.3;
   };
