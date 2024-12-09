5fa01a9b9d4076c53f87520e15475a5e16c8f60d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:06:52 +0000

    autogenerated overwrites for parver==0.5

--- collected.nix
+++ collected.nix
@@ -5726,6 +5726,9 @@
   parsimonious = {
     "0.9.0" = import ./overrides/parsimonious/0.9.0;
   };
+  parver = {
+    "0.5" = import ./overrides/parver/0.5;
+  };
   patch = {
     "1.16" = import ./overrides/patch/1.16;
   };
