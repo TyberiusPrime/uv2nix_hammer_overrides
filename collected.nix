eb8d7a2226f5149c89a0d28b8c460ec0a92ead43
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:30:41 +0000

    autogenerated overwrites for typed-argument-parser==1.10.1

--- collected.nix
+++ collected.nix
@@ -9437,6 +9437,9 @@
   typechecks = {
     "0.1.0" = import ./overrides/typechecks/0.1.0;
   };
+  typed-argument-parser = {
+    "1.10.1" = import ./overrides/typed-argument-parser/1.10.1;
+  };
   typedecorator = {
     "0.0.5" = import ./overrides/typedecorator/0.0.5;
   };
