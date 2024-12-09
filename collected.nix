8263fe9c0de391d18b5794996c48bc887da3f54c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:43:22 +0000

    autogenerated overwrites for pyairnow==1.2.2

--- collected.nix
+++ collected.nix
@@ -6363,6 +6363,9 @@
   pyaes = {
     "1.6.1" = import ./overrides/pyaes/1.6.1;
   };
+  pyairnow = {
+    "1.2.2" = import ./overrides/pyairnow/1.2.2;
+  };
   pyalsaaudio = {
     "0.11.0" = import ./overrides/pyalsaaudio/0.11.0;
   };
