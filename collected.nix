75ff84f7293d7672648967a685c4e39ce23e08fd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:51:17 +0000

    autogenerated overwrites for recommonmark==0.7.1

--- collected.nix
+++ collected.nix
@@ -7808,6 +7808,9 @@
   recaptcha-client = {
     "1.0.6" = import ./overrides/recaptcha-client/1.0.6;
   };
+  recommonmark = {
+    "0.7.1" = import ./overrides/recommonmark/0.7.1;
+  };
   reconfigure = {
     "0.1.82" = import ./overrides/reconfigure/0.1.82;
   };
