7249c7f241f43217b5fcb3bb2b64655bc31a7491
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:53:34 +0000

    autogenerated overwrites for dwdwfsapi==1.1.0

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dwdwfsapi = {
+    "1.1.0" = import ./overrides/dwdwfsapi/1.1.0;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
