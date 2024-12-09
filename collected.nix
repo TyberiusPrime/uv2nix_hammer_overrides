2791adfa4b100d851b16d844176be7e73fcb0131
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:23:31 +0000

    autogenerated overwrites for squarify==0.4.4

--- collected.nix
+++ collected.nix
@@ -8695,6 +8695,9 @@
   sqre-apikit = {
     "0.1.3" = import ./overrides/sqre-apikit/0.1.3;
   };
+  squarify = {
+    "0.4.4" = import ./overrides/squarify/0.4.4;
+  };
   srt = {
     "3.5.3" = import ./overrides/srt/3.5.3;
   };
