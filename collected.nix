37c39f8ce21e781ec13de1abc3771ea1262450d2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:24:49 +0000

    autogenerated overwrites for aiosonic==0.21.0

--- collected.nix
+++ collected.nix
@@ -141,6 +141,9 @@
   aiosocks = {
     "0.2.6" = import ./overrides/aiosocks/0.2.6;
   };
+  aiosonic = {
+    "0.21.0" = import ./overrides/aiosonic/0.21.0;
+  };
   aioxmpp = {
     "0.13.3" = import ./overrides/aioxmpp/0.13.3;
   };
