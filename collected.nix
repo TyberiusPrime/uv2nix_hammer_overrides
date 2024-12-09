b7a1b4421bcb8586957343b2668d06d4c0f74f75
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:51:00 +0000

    autogenerated overwrites for django-coverage-plugin==3.1.0

--- collected.nix
+++ collected.nix
@@ -1925,6 +1925,9 @@
   django-concurrency = {
     "2.5" = import ./overrides/django-concurrency/2.5;
   };
+  django-coverage-plugin = {
+    "3.1.0" = import ./overrides/django-coverage-plugin/3.1.0;
+  };
   django-cratis = {
     "0.9.1" = import ./overrides/django-cratis/0.9.1;
   };
