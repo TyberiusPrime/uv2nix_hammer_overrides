4c1c63136c7158c75be3a40ec14656317f080d5c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:36:20 +0000

    autogenerated overwrites for streamlit-aggrid==1.0.5

--- collected.nix
+++ collected.nix
@@ -8795,6 +8795,9 @@
   streamlabswater = {
     "1.0.1" = import ./overrides/streamlabswater/1.0.1;
   };
+  streamlit-aggrid = {
+    "1.0.5" = import ./overrides/streamlit-aggrid/1.0.5;
+  };
   streql = {
     "3.0.2" = import ./overrides/streql/3.0.2;
   };
