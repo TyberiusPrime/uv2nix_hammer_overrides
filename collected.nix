fc3268e2e862d6b15dc711642334603ccb7ddd93
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:48:47 +0000

    autogenerated overwrites for suntime==1.3.2

--- collected.nix
+++ collected.nix
@@ -8834,6 +8834,9 @@
   sunburnt = {
     "0.6" = import ./overrides/sunburnt/0.6;
   };
+  suntime = {
+    "1.3.2" = import ./overrides/suntime/1.3.2;
+  };
   sunwatcher = {
     "0.2.1" = import ./overrides/sunwatcher/0.2.1;
   };
