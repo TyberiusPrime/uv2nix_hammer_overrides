11009bf70f0c6b642c65cd2824a4958298b7608b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:09:33 +0000

    autogenerated overwrites for neomodel==5.3.2

--- collected.nix
+++ collected.nix
@@ -5163,6 +5163,9 @@
   neologdn = {
     "0.5.3" = import ./overrides/neologdn/0.5.3;
   };
+  neomodel = {
+    "5.3.2" = import ./overrides/neomodel/5.3.2;
+  };
   neotermcolor = {
     "2.0.10" = import ./overrides/neotermcolor/2.0.10;
   };
