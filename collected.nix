9b8bfbe0e829b153931e671228690f6fc3613ea2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:51:21 +0000

    autogenerated overwrites for ppscore==1.3.0

--- collected.nix
+++ collected.nix
@@ -6075,6 +6075,9 @@
   pprofile = {
     "2.2.0" = import ./overrides/pprofile/2.2.0;
   };
+  ppscore = {
+    "1.3.0" = import ./overrides/ppscore/1.3.0;
+  };
   pptree = {
     "3.1" = import ./overrides/pptree/3.1;
   };
