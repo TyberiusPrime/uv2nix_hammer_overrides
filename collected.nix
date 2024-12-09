bc029105c120ae2d2c805ce43497f1230de75d33
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 16:54:11 +0000

    autogenerated overwrites for django-cachalot==2.6.3

--- collected.nix
+++ collected.nix
@@ -1904,6 +1904,9 @@
   django-bower = {
     "5.2.0" = import ./overrides/django-bower/5.2.0;
   };
+  django-cachalot = {
+    "2.6.3" = import ./overrides/django-cachalot/2.6.3;
+  };
   django-cacheops = {
     "7.0.2" = import ./overrides/django-cacheops/7.0.2;
   };
