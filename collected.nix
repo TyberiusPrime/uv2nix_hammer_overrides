ddcf68807461ce33265d93604bc0ea3da87d36d9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:50:09 +0000

    autogenerated overwrites for your==0.6.7

--- collected.nix
+++ collected.nix
@@ -10094,6 +10094,9 @@
   yolov5 = {
     "7.0.13" = import ./overrides/yolov5/7.0.13;
   };
+  your = {
+    "0.6.7" = import ./overrides/your/0.6.7;
+  };
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
