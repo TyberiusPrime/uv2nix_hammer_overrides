2db410ad4b5fbbc5cdc04fe8454131963783dec8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:47:41 +0000

    autogenerated overwrites for xiaomi-ble==0.32.0

--- collected.nix
+++ collected.nix
@@ -10010,6 +10010,9 @@
   xformers = {
     "0.0.27.post2" = import ./overrides/xformers/0.0.27.post2;
   };
+  xiaomi-ble = {
+    "0.32.0" = import ./overrides/xiaomi-ble/0.32.0;
+  };
   xinabox-core = {
     "0.0.11" = import ./overrides/xinabox-core/0.0.11;
   };
