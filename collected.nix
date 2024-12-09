a7d06cf5ffa30770290864c9f1966922e391075a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:49:59 +0000

    autogenerated overwrites for qth==0.7.1

--- collected.nix
+++ collected.nix
@@ -7715,6 +7715,9 @@
   qrcode-terminal = {
     "0.8" = import ./overrides/qrcode-terminal/0.8;
   };
+  qth = {
+    "0.7.1" = import ./overrides/qth/0.7.1;
+  };
   qtsnbl = {
     "2018.6.8" = import ./overrides/qtsnbl/2018.6.8;
   };
