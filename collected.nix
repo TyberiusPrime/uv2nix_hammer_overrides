836412dac876b5e943521ba05f00ee5ef81f0a1d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:33:38 +0000

    autogenerated overwrites for elastic-apm==6.23.0

--- collected.nix
+++ collected.nix
@@ -2490,6 +2490,9 @@
   eigenpy = {
     "3.5.1" = import ./overrides/eigenpy/3.5.1;
   };
+  elastic-apm = {
+    "6.23.0" = import ./overrides/elastic-apm/6.23.0;
+  };
   elasticsearch-async = {
     "6.2.0" = import ./overrides/elasticsearch-async/6.2.0;
   };
