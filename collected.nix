736841acc596f788744cf426dd3fd60792afbbb9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:59:34 +0000

    autogenerated overwrites for obspy==1.4.1

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  obspy = {
+    "1.4.1" = import ./overrides/obspy/1.4.1;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
