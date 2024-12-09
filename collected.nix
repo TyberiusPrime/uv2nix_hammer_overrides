7693a1cf864afbd218503dc8dbaa5cfa8d9f5602
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:06:17 +0000

    autogenerated overwrites for depinfo==2.2.0

--- collected.nix
+++ collected.nix
@@ -1773,6 +1773,9 @@
   demjson3 = {
     "3.0.6" = import ./overrides/demjson3/3.0.6;
   };
+  depinfo = {
+    "2.2.0" = import ./overrides/depinfo/2.2.0;
+  };
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
