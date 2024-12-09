db1a86d3c869511c3dffaf0ee0d421a5c090a112
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:37:48 +0000

    autogenerated overwrites for vonage-jwt==1.1.1

--- collected.nix
+++ collected.nix
@@ -9771,6 +9771,9 @@
   volvooncall = {
     "0.10.4" = import ./overrides/volvooncall/0.10.4;
   };
+  vonage-jwt = {
+    "1.1.1" = import ./overrides/vonage-jwt/1.1.1;
+  };
   vortexpy = {
     "3.0.0" = import ./overrides/vortexpy/3.0.0;
     "3.1.0" = import ./overrides/vortexpy/3.1.0;
