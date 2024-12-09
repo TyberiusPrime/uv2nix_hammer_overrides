0612eccad2f1cdf1eb033dc80cfefeca44e77fbf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:34:13 +0000

    autogenerated overwrites for dcmstack==0.9.0

--- collected.nix
+++ collected.nix
@@ -1719,6 +1719,9 @@
   dbutils = {
     "1.3" = import ./overrides/dbutils/1.3;
   };
+  dcmstack = {
+    "0.9.0" = import ./overrides/dcmstack/0.9.0;
+  };
   dd-aliyun-python-sdk-core = {
     "2.8.2" = import ./overrides/dd-aliyun-python-sdk-core/2.8.2;
   };
