16709283632fb3882daf0d2b74eda7ef4664fb9d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:23:03 +0000

    autogenerated overwrites for osmnx==1.9.4

--- collected.nix
+++ collected.nix
@@ -5617,6 +5617,9 @@
   osm2geojson = {
     "0.2.5" = import ./overrides/osm2geojson/0.2.5;
   };
+  osmnx = {
+    "1.9.4" = import ./overrides/osmnx/1.9.4;
+  };
   oss2 = {
     "2.19.0" = import ./overrides/oss2/2.19.0;
   };
