399a4247464cef22275628f1a2d66a6055473d44
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:14:58 +0000

    autogenerated overwrites for django-cache-memoize==0.2.0

--- collected.nix
+++ collected.nix
@@ -1904,6 +1904,9 @@
   django-bower = {
     "5.2.0" = import ./overrides/django-bower/5.2.0;
   };
+  django-cache-memoize = {
+    "0.2.0" = import ./overrides/django-cache-memoize/0.2.0;
+  };
   django-cacheops = {
     "7.0.2" = import ./overrides/django-cacheops/7.0.2;
   };
