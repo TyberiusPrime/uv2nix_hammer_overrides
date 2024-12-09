2761db8c2ee0b0ba9c459007b29f6256201f8307
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:36:34 +0000

    autogenerated overwrites for discord==2.3.2

--- collected.nix
+++ collected.nix
@@ -1837,6 +1837,9 @@
   discid = {
     "1.2.0" = import ./overrides/discid/1.2.0;
   };
+  discord = {
+    "2.3.2" = import ./overrides/discord/2.3.2;
+  };
   distance = {
     "0.1.3" = import ./overrides/distance/0.1.3;
   };
