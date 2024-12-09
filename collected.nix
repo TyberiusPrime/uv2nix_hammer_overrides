c11f29b6cc0d02af01a224121d466561780a2354
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:21:01 +0000

    autogenerated overwrites for sphinx-rtd-theme==2.0.0

--- collected.nix
+++ collected.nix
@@ -8613,6 +8613,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-rtd-theme = {
+    "2.0.0" = import ./overrides/sphinx-rtd-theme/2.0.0;
+  };
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
