82412a2d14d72c835208ad2fa8b2be47fde48428
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:07:07 +0000

    autogenerated overwrites for pep440==0.1.2

--- collected.nix
+++ collected.nix
@@ -5812,6 +5812,9 @@
   pep362 = {
     "0.6.2" = import ./overrides/pep362/0.6.2;
   };
+  pep440 = {
+    "0.1.2" = import ./overrides/pep440/0.1.2;
+  };
   percol = {
     "0.2.1" = import ./overrides/percol/0.2.1;
   };
