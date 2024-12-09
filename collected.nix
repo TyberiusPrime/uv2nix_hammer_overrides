0464a588558a64dcae7ba5345579b6edb37c31d7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:33:53 +0000

    autogenerated overwrites for future-fstrings==1.2.0

--- collected.nix
+++ collected.nix
@@ -3044,6 +3044,9 @@
     "0.16.0" = import ./overrides/future/0.16.0;
     "0.18.3" = import ./overrides/future/0.18.3;
   };
+  future-fstrings = {
+    "1.2.0" = import ./overrides/future-fstrings/1.2.0;
+  };
   fuzzingbook = {
     "1.2.1" = import ./overrides/fuzzingbook/1.2.1;
   };
