6caebed61b12919a1643d4140982095e858862b8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:07:58 +0000

    autogenerated overwrites for sphinxcontrib-seqdiag==3.0.0

--- collected.nix
+++ collected.nix
@@ -8640,6 +8640,9 @@
   sphinxcontrib-plantuml = {
     "0.30" = import ./overrides/sphinxcontrib-plantuml/0.30;
   };
+  sphinxcontrib-seqdiag = {
+    "3.0.0" = import ./overrides/sphinxcontrib-seqdiag/3.0.0;
+  };
   sphinxcontrib-tikz = {
     "0.4.20" = import ./overrides/sphinxcontrib-tikz/0.4.20;
   };
