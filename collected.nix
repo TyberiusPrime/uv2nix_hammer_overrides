fd4f4732ee282acdc9fec1c4edd100a485e23452
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:34:57 +0000

    autogenerated overwrites for bumpversion==0.6.0

--- collected.nix
+++ collected.nix
@@ -861,6 +861,9 @@
   btrfs = {
     "14.1" = import ./overrides/btrfs/14.1;
   };
+  bumpversion = {
+    "0.6.0" = import ./overrides/bumpversion/0.6.0;
+  };
   busrt = {
     "0.1.2" = import ./overrides/busrt/0.1.2;
   };
