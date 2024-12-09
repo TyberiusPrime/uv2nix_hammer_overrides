6b25435a7d4bfb923e7749658cf83c3bfb93a620
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:34:47 +0000

    autogenerated overwrites for pip-shims==0.7.3

--- collected.nix
+++ collected.nix
@@ -5937,6 +5937,9 @@
   pip-services3-rpc = {
     "3.3.4" = import ./overrides/pip-services3-rpc/3.3.4;
   };
+  pip-shims = {
+    "0.7.3" = import ./overrides/pip-shims/0.7.3;
+  };
   pipfile = {
     "0.0.2" = import ./overrides/pipfile/0.0.2;
   };
