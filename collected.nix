7c7dce150ce0297b65f8436d2acb55e3b7e6aa80
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:04:29 +0000

    autogenerated overwrites for python-coveralls==2.9.3

--- collected.nix
+++ collected.nix
@@ -7282,6 +7282,9 @@
   python-constraint = {
     "1.4.0" = import ./overrides/python-constraint/1.4.0;
   };
+  python-coveralls = {
+    "2.9.3" = import ./overrides/python-coveralls/2.9.3;
+  };
   python-ctags3 = {
     "1.6.0" = import ./overrides/python-ctags3/1.6.0;
   };
