b3cc7eb55046291a382863a7c386ce1d5d910c94
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:09:44 +0000

    autogenerated overwrites for opensesame-core==4.0.29

--- collected.nix
+++ collected.nix
@@ -5559,6 +5559,9 @@
   opensensemap-api = {
     "0.3.2" = import ./overrides/opensensemap-api/0.3.2;
   };
+  opensesame-core = {
+    "4.0.29" = import ./overrides/opensesame-core/4.0.29;
+  };
   opentracing = {
     "1.3.0" = import ./overrides/opentracing/1.3.0;
     "2.4.0" = import ./overrides/opentracing/2.4.0;
