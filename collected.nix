262c5d404e556c1c0dc66bfbc80aa353b7292d24
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:11:07 +0000

    autogenerated overwrites for lupa==2.2

--- collected.nix
+++ collected.nix
@@ -4547,6 +4547,9 @@
   luhn = {
     "0.2.0" = import ./overrides/luhn/0.2.0;
   };
+  lupa = {
+    "2.2" = import ./overrides/lupa/2.2;
+  };
   lyrebird = {
     "3.0.5" = import ./overrides/lyrebird/3.0.5;
   };
