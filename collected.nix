39420c1507490a1e6224d6fc01f08954da9a5f79
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:48:33 +0000

    autogenerated overwrites for infi.cwrap==0.2.14

--- collected.nix
+++ collected.nix
@@ -3727,6 +3727,9 @@
   imutils = {
     "0.5.4" = import ./overrides/imutils/0.5.4;
   };
+  infi-cwrap = {
+    "0.2.14" = import ./overrides/infi-cwrap/0.2.14;
+  };
   infi-execute = {
     "0.1.8" = import ./overrides/infi-execute/0.1.8;
   };
