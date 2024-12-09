b0e09c0316a461b40534fee2b81faebdea6bd7e7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:00:33 +0000

    autogenerated overwrites for dxfgrabber==1.0.1

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dxfgrabber = {
+    "1.0.1" = import ./overrides/dxfgrabber/1.0.1;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
