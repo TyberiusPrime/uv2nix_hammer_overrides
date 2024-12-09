15c96a9b4d8f17ed8058149c89e36568e31d7390
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:54:51 +0000

    autogenerated overwrites for pvl==1.3.2

--- collected.nix
+++ collected.nix
@@ -6282,6 +6282,9 @@
   pusherclient = {
     "0.3.0" = import ./overrides/pusherclient/0.3.0;
   };
+  pvl = {
+    "1.3.2" = import ./overrides/pvl/1.3.2;
+  };
   pwinput = {
     "1.0.3" = import ./overrides/pwinput/1.0.3;
   };
