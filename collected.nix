546581a62a1ed75fea7d1e7e23626d8b6be3421e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:12:40 +0000

    autogenerated overwrites for invenio-vocabularies==5.0.3

--- collected.nix
+++ collected.nix
@@ -3811,6 +3811,9 @@
   invenio-rdm-records = {
     "12.1.0" = import ./overrides/invenio-rdm-records/12.1.0;
   };
+  invenio-vocabularies = {
+    "5.0.3" = import ./overrides/invenio-vocabularies/5.0.3;
+  };
   iocapture = {
     "0.1.2" = import ./overrides/iocapture/0.1.2;
   };
