3572da673aac467db259747e9b6a2324e8b88eb2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:50:55 +0000

    autogenerated overwrites for blackduck==1.1.3

--- collected.nix
+++ collected.nix
@@ -764,6 +764,9 @@
   blackboxauditing = {
     "0.1.54" = import ./overrides/blackboxauditing/0.1.54;
   };
+  blackduck = {
+    "1.1.3" = import ./overrides/blackduck/1.1.3;
+  };
   blaze = {
     "0.10.1" = import ./overrides/blaze/0.10.1;
   };
