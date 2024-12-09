b3da3ec27e630562259353d51b56e36cf2a68ee2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:32:50 +0000

    autogenerated overwrites for backwork==0.3.1

--- collected.nix
+++ collected.nix
@@ -578,6 +578,9 @@
   backports-zoneinfo = {
     "0.2.1" = import ./overrides/backports-zoneinfo/0.2.1;
   };
+  backwork = {
+    "0.3.1" = import ./overrides/backwork/0.3.1;
+  };
   bambu-mail = {
     "3.4" = import ./overrides/bambu-mail/3.4;
   };
