439429a59ec7213553e2fc64d879c5ff58a42105
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:31:51 +0000

    autogenerated overwrites for flake8-length==0.3.1

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-length = {
+    "0.3.1" = import ./overrides/flake8-length/0.3.1;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
