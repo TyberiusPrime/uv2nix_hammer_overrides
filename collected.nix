0b08f34bd6000fa61dcf915d9aea4c6cf3e73cfd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:14:18 +0000

    autogenerated overwrites for matplotlib-label-lines==0.7.0

--- collected.nix
+++ collected.nix
@@ -4655,6 +4655,9 @@
   matplotlib = {
     "3.4.2" = import ./overrides/matplotlib/3.4.2;
   };
+  matplotlib-label-lines = {
+    "0.7.0" = import ./overrides/matplotlib-label-lines/0.7.0;
+  };
   matplotlib-venn = {
     "1.1.1" = import ./overrides/matplotlib-venn/1.1.1;
   };
