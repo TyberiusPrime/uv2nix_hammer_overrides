856f2d8358a41044db497f0bbea525030171791f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:33:12 +0000

    autogenerated overwrites for types-aiobotocore-ram==2.14.0

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-ram = {
+    "2.14.0" = import ./overrides/types-aiobotocore-ram/2.14.0;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
