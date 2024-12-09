d8e027803638cc98b18bca93cb1b2a30acd03c92
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:02:39 +0000

    autogenerated overwrites for pgspecial==2.1.2

--- collected.nix
+++ collected.nix
@@ -5857,6 +5857,9 @@
   pgsanity = {
     "0.2.9" = import ./overrides/pgsanity/0.2.9;
   };
+  pgspecial = {
+    "2.1.2" = import ./overrides/pgspecial/2.1.2;
+  };
   pgsu = {
     "0.2.4" = import ./overrides/pgsu/0.2.4;
   };
