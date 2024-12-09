debb8e50ba5c764e52ce78b4116524d1b6b033b9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:24:20 +0000

    autogenerated overwrites for cxxfilt==0.3.0

--- collected.nix
+++ collected.nix
@@ -1596,6 +1596,9 @@
   cwlgen = {
     "0.4.2" = import ./overrides/cwlgen/0.4.2;
   };
+  cxxfilt = {
+    "0.3.0" = import ./overrides/cxxfilt/0.3.0;
+  };
   cydets = {
     "1.1" = import ./overrides/cydets/1.1;
   };
