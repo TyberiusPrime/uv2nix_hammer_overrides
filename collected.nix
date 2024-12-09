beaaeb897f49ce9ad9a612d0fba7edb331938399
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:21:28 +0000

    autogenerated overwrites for rnc2rng==2.7.0

--- collected.nix
+++ collected.nix
@@ -7970,6 +7970,9 @@
   rmtest = {
     "0.7.0" = import ./overrides/rmtest/0.7.0;
   };
+  rnc2rng = {
+    "2.7.0" = import ./overrides/rnc2rng/2.7.0;
+  };
   roadtools = {
     "0.0.1" = import ./overrides/roadtools/0.0.1;
   };
