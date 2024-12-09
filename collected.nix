1327fe3cd3ae3255a5f24b79d1bcd46e48d62ea7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:36:06 +0000

    autogenerated overwrites for dolmen.file==0.6

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
+  dolmen-file = {
+    "0.6" = import ./overrides/dolmen-file/0.6;
+  };
   dopy = {
     "0.3.7" = import ./overrides/dopy/0.3.7;
   };
