1bbd431f95037a8264a1a5513ea97aa2011b647e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:54:47 +0000

    autogenerated overwrites for mkdocs-literate-nav==0.6.1

--- collected.nix
+++ collected.nix
@@ -4835,6 +4835,9 @@
   mkdocs-jupyter = {
     "0.12.0" = import ./overrides/mkdocs-jupyter/0.12.0;
   };
+  mkdocs-literate-nav = {
+    "0.6.1" = import ./overrides/mkdocs-literate-nav/0.6.1;
+  };
   ml-collections = {
     "0.1.1" = import ./overrides/ml-collections/0.1.1;
   };
