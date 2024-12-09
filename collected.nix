940a7cefcbc9627a4f65eef038e5d76ccb112a22
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:35:07 +0000

    autogenerated overwrites for fhirclient==4.2.1

--- collected.nix
+++ collected.nix
@@ -2742,6 +2742,9 @@
   fgame = {
     "0.6.5" = import ./overrides/fgame/0.6.5;
   };
+  fhirclient = {
+    "4.2.1" = import ./overrides/fhirclient/4.2.1;
+  };
   fibheap = {
     "0.2.1" = import ./overrides/fibheap/0.2.1;
   };
