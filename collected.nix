f78e7759c411c3f4d07225aee68b93f4b07f4040
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:23:29 +0000

    autogenerated overwrites for gltflib==1.0.13

--- collected.nix
+++ collected.nix
@@ -3210,6 +3210,9 @@
   glpk = {
     "0.4.7" = import ./overrides/glpk/0.4.7;
   };
+  gltflib = {
+    "1.0.13" = import ./overrides/gltflib/1.0.13;
+  };
   gluonnlp = {
     "0.10.0" = import ./overrides/gluonnlp/0.10.0;
   };
