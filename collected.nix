f31f2463c9576b62b88f2b5782fc8ceb38ffe12d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:14:50 +0000

    autogenerated overwrites for brunns-row==2.1.0

--- collected.nix
+++ collected.nix
@@ -843,6 +843,9 @@
   browsermob-proxy = {
     "0.8.0" = import ./overrides/browsermob-proxy/0.8.0;
   };
+  brunns-row = {
+    "2.1.0" = import ./overrides/brunns-row/2.1.0;
+  };
   bs4 = {
     "0.0.1" = import ./overrides/bs4/0.0.1;
   };
