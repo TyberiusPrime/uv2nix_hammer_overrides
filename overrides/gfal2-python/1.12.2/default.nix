16e3530e822692958a8261179ef800368264013d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:17:45 +0000

    autogenerated overwrites for gfal2-python==1.12.2

--- overrides/gfal2-python/1.12.2/default.nix
+++ overrides/gfal2-python/1.12.2/default.nix
@@ -1,29 +1,3 @@
-{
-  resolveBuildSystem,
-  helpers,
-  final,
-  pkgs,
-  ...
-}:
-old:
-if (helpers.isWheel old) then
-  { }
-else
-  {
-    dontUseCmakeConfigure = true;
-    nativeBuildInputs =
-      old.nativeBuildInputs or [ ]
-      ++ [
-        (pkgs.boost.override {
-          inherit (final) python;
-          inherit (final) numpy;
-          enablePython = true;
-        })
-        pkgs.cmake
-        pkgs.gfal2
-        pkgs.glib
-        pkgs.pcre2
-        pkgs.pkg-config
-      ]
-      ++ (resolveBuildSystem { setuptools = [ ]; });
-  }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {} else {dontUseCmakeConfigure = true;nativeBuildInputs = old.nativeBuildInputs or [] ++ [(pkgs.boost.override {python = final.python; numpy=final.numpy; enablePython=true;}) pkgs.cmake pkgs.gfal2 pkgs.glib pkgs.pcre2 pkgs.pkg-config] ++ ( resolveBuildSystem {setuptools = [];});}
+        
