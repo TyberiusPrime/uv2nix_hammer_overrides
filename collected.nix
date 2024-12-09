4bc9404fad514d0ce4be397c5f1a173187d962fa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:20:44 +0000

    autogenerated overwrites for google-cloud-iam-logging==1.3.5

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-iam-logging = {
+    "1.3.5" = import ./overrides/google-cloud-iam-logging/1.3.5;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
