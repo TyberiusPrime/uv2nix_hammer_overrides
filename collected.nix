3e346e7ebc5baee0756c417e3a5017d8e3199b77
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:00:08 +0000

    autogenerated overwrites for prometheus-fastapi-instrumentator==7.0.0

--- collected.nix
+++ collected.nix
@@ -6180,6 +6180,9 @@
   projectq = {
     "0.8.0" = import ./overrides/projectq/0.8.0;
   };
+  prometheus-fastapi-instrumentator = {
+    "7.0.0" = import ./overrides/prometheus-fastapi-instrumentator/7.0.0;
+  };
   promise = {
     "2.3" = import ./overrides/promise/2.3;
   };
