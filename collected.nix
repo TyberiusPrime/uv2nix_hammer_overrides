77a872ed04e6be424cf791090173d80b2548e5ea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:01:13 +0000

    autogenerated overwrites for django-waffle==4.1.0

--- collected.nix
+++ collected.nix
@@ -2222,6 +2222,9 @@
   django-vkontakte-users = {
     "0.8.7" = import ./overrides/django-vkontakte-users/0.8.7;
   };
+  django-waffle = {
+    "4.1.0" = import ./overrides/django-waffle/4.1.0;
+  };
   djangoapidec = {
     "1.8" = import ./overrides/djangoapidec/1.8;
   };
