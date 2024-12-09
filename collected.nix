8643e0b6aae99a50ec1d4223dcd329eafda6f035
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:49:57 +0000

    autogenerated overwrites for python-semantic-release==9.8.8

--- collected.nix
+++ collected.nix
@@ -7457,6 +7457,9 @@
   python-rtmidi = {
     "1.5.8" = import ./overrides/python-rtmidi/1.5.8;
   };
+  python-semantic-release = {
+    "9.8.8" = import ./overrides/python-semantic-release/9.8.8;
+  };
   python-simple-hipchat = {
     "0.4.0" = import ./overrides/python-simple-hipchat/0.4.0;
   };
