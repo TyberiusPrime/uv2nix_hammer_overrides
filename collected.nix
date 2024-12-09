394b281efe718467dac5c1fa607987eaebe6171d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:31:28 +0000

    autogenerated overwrites for ndh==6.9.1

--- collected.nix
+++ collected.nix
@@ -5141,6 +5141,9 @@
   ncephes = {
     "1.1.0" = import ./overrides/ncephes/1.1.0;
   };
+  ndh = {
+    "6.9.1" = import ./overrides/ndh/6.9.1;
+  };
   ndms2-client = {
     "0.1.3" = import ./overrides/ndms2-client/0.1.3;
   };
