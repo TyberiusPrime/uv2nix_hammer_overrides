0574d736835406f96c51e3ad85abe68a42874da6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:35:21 +0000

    autogenerated overwrites for ipythonblocks==1.9.1

--- collected.nix
+++ collected.nix
@@ -3853,6 +3853,9 @@
   ipy = {
     "1.1" = import ./overrides/ipy/1.1;
   };
+  ipythonblocks = {
+    "1.9.1" = import ./overrides/ipythonblocks/1.9.1;
+  };
   ipyxact = {
     "0.3.2" = import ./overrides/ipyxact/0.3.2;
   };
