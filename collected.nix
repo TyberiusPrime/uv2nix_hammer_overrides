9b021b6d8ef5cd0f9024db63cf60328c0bb0d996
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:14:46 +0000

    autogenerated overwrites for graphql-ws==0.4.4

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  graphql-ws = {
+    "0.4.4" = import ./overrides/graphql-ws/0.4.4;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
