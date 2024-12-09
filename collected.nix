a8df862a2f3205678a9152cd86ef5e54d46a7d75
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:02:48 +0000

    autogenerated overwrites for aliyun-python-sdk-r-kvstore==2.20.13

--- collected.nix
+++ collected.nix
@@ -187,6 +187,9 @@
   aliyun-python-sdk-core-v3 = {
     "2.13.33" = import ./overrides/aliyun-python-sdk-core-v3/2.13.33;
   };
+  aliyun-python-sdk-r-kvstore = {
+    "2.20.13" = import ./overrides/aliyun-python-sdk-r-kvstore/2.20.13;
+  };
   allset = {
     "1.0.1" = import ./overrides/allset/1.0.1;
   };
