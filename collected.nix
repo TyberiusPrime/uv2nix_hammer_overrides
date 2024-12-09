3a5c3e7d4901d48ff244f9f45be6b3c188f0a23b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:41:48 +0000

    autogenerated overwrites for pyctr==0.7.5

--- collected.nix
+++ collected.nix
@@ -6530,6 +6530,9 @@
   pycryptodomex = {
     "3.9.9" = import ./overrides/pycryptodomex/3.9.9;
   };
+  pyctr = {
+    "0.7.5" = import ./overrides/pyctr/0.7.5;
+  };
   pycuda = {
     "2024.1.2" = import ./overrides/pycuda/2024.1.2;
   };
