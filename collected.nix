0b5f4b44f56b426f909a95064d36afa41e61b841
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:15:53 +0000

    autogenerated overwrites for onesignal-sdk==2.0.0

--- collected.nix
+++ collected.nix
@@ -5480,6 +5480,9 @@
   omnijson = {
     "0.1.2" = import ./overrides/omnijson/0.1.2;
   };
+  onesignal-sdk = {
+    "2.0.0" = import ./overrides/onesignal-sdk/2.0.0;
+  };
   onetimepad = {
     "1.4" = import ./overrides/onetimepad/1.4;
   };
