de03d9fb0060a74f12dd1dd3eb9ad37120ba4fd7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:55:08 +0000

    autogenerated overwrites for pydantic==2.9.1

--- collected.nix
+++ collected.nix
@@ -6536,6 +6536,9 @@
   pycups = {
     "2.0.4" = import ./overrides/pycups/2.0.4;
   };
+  pydantic = {
+    "2.9.1" = import ./overrides/pydantic/2.9.1;
+  };
   pydaqmx = {
     "1.4.6" = import ./overrides/pydaqmx/1.4.6;
   };
