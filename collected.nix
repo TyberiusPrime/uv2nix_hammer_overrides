ea65039c30240951177fa4a3b9c4b4a8db5a8172
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:51:15 +0000

    autogenerated overwrites for aiohttp-openmetrics==0.0.12

--- collected.nix
+++ collected.nix
@@ -105,6 +105,9 @@
   aiohttp-devtools = {
     "1.1.2" = import ./overrides/aiohttp-devtools/1.1.2;
   };
+  aiohttp-openmetrics = {
+    "0.0.12" = import ./overrides/aiohttp-openmetrics/0.0.12;
+  };
   aiohttp-route-decorator = {
     "0.1.4" = import ./overrides/aiohttp-route-decorator/0.1.4;
   };
