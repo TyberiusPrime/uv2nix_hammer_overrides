7f2b5ec224b6f058b71824a8095edab0f0e1360a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:58:43 +0000

    autogenerated overwrites for django-mysql==4.14.0

--- collected.nix
+++ collected.nix
@@ -2051,6 +2051,9 @@
   django-mp-pagination = {
     "0.3.0" = import ./overrides/django-mp-pagination/0.3.0;
   };
+  django-mysql = {
+    "4.14.0" = import ./overrides/django-mysql/4.14.0;
+  };
   django-next-prev = {
     "1.1.0" = import ./overrides/django-next-prev/1.1.0;
   };
