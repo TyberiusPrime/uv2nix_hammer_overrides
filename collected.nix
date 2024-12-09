1d028aac7c170f2ce0b1d8d6b3daa3b819b12d6a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:38:36 +0000

    autogenerated overwrites for vk-api==11.9.9

--- collected.nix
+++ collected.nix
@@ -9735,6 +9735,9 @@
   vivisect-vstruct-wb = {
     "1.0.3" = import ./overrides/vivisect-vstruct-wb/1.0.3;
   };
+  vk-api = {
+    "11.9.9" = import ./overrides/vk-api/11.9.9;
+  };
   vkontakte = {
     "1.3.5" = import ./overrides/vkontakte/1.3.5;
   };
