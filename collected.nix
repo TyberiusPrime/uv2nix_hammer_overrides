25372aa1dd6fd95150a61bebfe17f6dc116035b3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:46:30 +0000

    autogenerated overwrites for shellescape==3.8.1

--- collected.nix
+++ collected.nix
@@ -8339,6 +8339,9 @@
   sharedmem = {
     "0.3.8" = import ./overrides/sharedmem/0.3.8;
   };
+  shellescape = {
+    "3.8.1" = import ./overrides/shellescape/3.8.1;
+  };
   shinkenplugins = {
     "0.4.2" = import ./overrides/shinkenplugins/0.4.2;
   };
