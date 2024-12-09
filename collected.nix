a021d2b3ff384f48d9d35b068f1942d8f4124972
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:41:03 +0000

    autogenerated overwrites for bit==0.8.0

--- collected.nix
+++ collected.nix
@@ -731,6 +731,9 @@
   bisos2-common = {
     "0.47" = import ./overrides/bisos2-common/0.47;
   };
+  bit = {
+    "0.8.0" = import ./overrides/bit/0.8.0;
+  };
   bitbucket-api = {
     "0.5.0" = import ./overrides/bitbucket-api/0.5.0;
   };
