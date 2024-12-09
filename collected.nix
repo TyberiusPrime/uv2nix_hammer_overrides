0877096cafc60096b05d3a5f5ab4004e9403be3a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:51:27 +0000

    autogenerated overwrites for aiocache==0.12.2

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aiocache = {
+    "0.12.2" = import ./overrides/aiocache/0.12.2;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
