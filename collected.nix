383e12d9e01430433e06947a2b22337cb1cac9d3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:09:11 +0000

    autogenerated overwrites for lambdasdk==0.1.4

--- collected.nix
+++ collected.nix
@@ -4229,6 +4229,9 @@
   lac = {
     "2.1.2" = import ./overrides/lac/2.1.2;
   };
+  lambdasdk = {
+    "0.1.4" = import ./overrides/lambdasdk/0.1.4;
+  };
   landez = {
     "2.5.0" = import ./overrides/landez/2.5.0;
   };
