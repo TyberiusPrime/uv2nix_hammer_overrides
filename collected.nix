d998fcf4200a14c4b39bf720194c06102a27631e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:59:48 +0000

    autogenerated overwrites for nicegui==2.1.0

--- collected.nix
+++ collected.nix
@@ -5232,6 +5232,9 @@
   nglview = {
     "3.1.2" = import ./overrides/nglview/3.1.2;
   };
+  nicegui = {
+    "2.1.0" = import ./overrides/nicegui/2.1.0;
+  };
   nilsimsa = {
     "0.3.8" = import ./overrides/nilsimsa/0.3.8;
   };
