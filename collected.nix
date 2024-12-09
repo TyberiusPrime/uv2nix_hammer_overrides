463c71b18cb567fc81116e0c33dcea1fe94e2bc5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:58:02 +0000

    autogenerated overwrites for filecheck==1.0.1

--- collected.nix
+++ collected.nix
@@ -2757,6 +2757,9 @@
   filebytes = {
     "0.10.2" = import ./overrides/filebytes/0.10.2;
   };
+  filecheck = {
+    "1.0.1" = import ./overrides/filecheck/1.0.1;
+  };
   filechunkio = {
     "1.8" = import ./overrides/filechunkio/1.8;
   };
