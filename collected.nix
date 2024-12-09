eaacac4d5b616ac42cdc10fe66f56ab8c692a27f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 16:52:46 +0000

    autogenerated overwrites for pytconf==0.1.14

--- collected.nix
+++ collected.nix
@@ -7203,6 +7203,9 @@
   pytankerkoenig = {
     "0.0.7" = import ./overrides/pytankerkoenig/0.0.7;
   };
+  pytconf = {
+    "0.1.14" = import ./overrides/pytconf/0.1.14;
+  };
   pytdx = {
     "1.72" = import ./overrides/pytdx/1.72;
   };
