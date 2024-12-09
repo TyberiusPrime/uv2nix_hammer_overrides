04136ae7730354cf7386a1fccf72f46c414a413a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:48:59 +0000

    autogenerated overwrites for youtube-search-python==1.6.6

--- collected.nix
+++ collected.nix
@@ -10097,6 +10097,9 @@
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
+  youtube-search-python = {
+    "1.6.6" = import ./overrides/youtube-search-python/1.6.6;
+  };
   yurl = {
     "1.0.0" = import ./overrides/yurl/1.0.0;
   };
