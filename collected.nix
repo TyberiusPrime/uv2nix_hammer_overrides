d3a8875e3f0cc4a98a6c8c9a7f75e6384fe65c42
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 16:28:44 +0000

    autogenerated overwrites for nixio==1.5.3

--- collected.nix
+++ collected.nix
@@ -5241,6 +5241,9 @@
   nix-prefetch-github = {
     "7.1.0" = import ./overrides/nix-prefetch-github/7.1.0;
   };
+  nixio = {
+    "1.5.3" = import ./overrides/nixio/1.5.3;
+  };
   nlptoolkit-annotatedsentence = {
     "1.0.38" = import ./overrides/nlptoolkit-annotatedsentence/1.0.38;
   };
