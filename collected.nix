2204cdd7d2b7d1bf931e97e3cd2d215fa990d01e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:09:26 +0000

    autogenerated overwrites for textblob==0.18.0.post0

--- collected.nix
+++ collected.nix
@@ -9014,6 +9014,9 @@
   textbisect = {
     "0.1.0" = import ./overrides/textbisect/0.1.0;
   };
+  textblob = {
+    "0.18.0.post0" = import ./overrides/textblob/0.18.0.post0;
+  };
   textnets = {
     "0.9.4" = import ./overrides/textnets/0.9.4;
   };
