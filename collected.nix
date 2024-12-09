c5160d267919c57ddd39524e50b60e8a94b5a7d8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:20:26 +0000

    autogenerated overwrites for pamqp==3.3.0

--- collected.nix
+++ collected.nix
@@ -5666,6 +5666,9 @@
   pairtree = {
     "0.8.1" = import ./overrides/pairtree/0.8.1;
   };
+  pamqp = {
+    "3.3.0" = import ./overrides/pamqp/3.3.0;
+  };
   panda3d = {
     "1.10.15" = import ./overrides/panda3d/1.10.15;
   };
