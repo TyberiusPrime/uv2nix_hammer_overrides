76c75d2fb25b52cec40af92fbc9986b4dd8c03ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:39:23 +0000

    autogenerated overwrites for pynng==0.8.0

--- collected.nix
+++ collected.nix
@@ -6913,6 +6913,9 @@
     "0.6.4" = import ./overrides/pynisher/0.6.4;
     "1.0.10" = import ./overrides/pynisher/1.0.10;
   };
+  pynng = {
+    "0.8.0" = import ./overrides/pynng/0.8.0;
+  };
   pynsq = {
     "0.9.1" = import ./overrides/pynsq/0.9.1;
   };
