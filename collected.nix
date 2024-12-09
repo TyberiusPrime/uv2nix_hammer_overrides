0482b070ecdbdff6edc7c5947253f8ba03b04a0c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:15:55 +0000

    autogenerated overwrites for google-cloud-asset==3.26.3

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-asset = {
+    "3.26.3" = import ./overrides/google-cloud-asset/3.26.3;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
