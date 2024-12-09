8dfcbc616883e09245776b84bae0971ed9298a16
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:28:17 +0000

    autogenerated overwrites for slovnet==0.6.0

--- collected.nix
+++ collected.nix
@@ -8495,6 +8495,9 @@
   slixmpp = {
     "1.8.5" = import ./overrides/slixmpp/1.8.5;
   };
+  slovnet = {
+    "0.6.0" = import ./overrides/slovnet/0.6.0;
+  };
   slugify = {
     "0.0.1" = import ./overrides/slugify/0.0.1;
   };
