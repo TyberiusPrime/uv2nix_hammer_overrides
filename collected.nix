c6ab2a4455e54850da5b1b736556be478146953f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:36:18 +0000

    autogenerated overwrites for types-enum34==1.1.8

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-enum34 = {
+    "1.1.8" = import ./overrides/types-enum34/1.1.8;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
