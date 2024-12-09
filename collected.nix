9d5b4ad9fabefd25a2ccb251da169bb5cf3ccf67
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:19:04 +0000

    autogenerated overwrites for aslabs-dependencies==0.0.3

--- collected.nix
+++ collected.nix
@@ -398,6 +398,9 @@
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
   };
+  aslabs-dependencies = {
+    "0.0.3" = import ./overrides/aslabs-dependencies/0.0.3;
+  };
   asn1tools = {
     "0.167.0" = import ./overrides/asn1tools/0.167.0;
   };
