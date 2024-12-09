173094705e36e934d4ff163b7de96cb5c7100c84
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:31:06 +0000

    autogenerated overwrites for pyjks==20.0.0

--- collected.nix
+++ collected.nix
@@ -6755,6 +6755,9 @@
   pyjade = {
     "4.0.0" = import ./overrides/pyjade/4.0.0;
   };
+  pyjks = {
+    "20.0.0" = import ./overrides/pyjks/20.0.0;
+  };
   pyjq = {
     "2.6.0" = import ./overrides/pyjq/2.6.0;
   };
