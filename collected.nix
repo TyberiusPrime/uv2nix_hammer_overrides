2cc8377750dc34626dc8b8d3fbe3b242ebb9a23d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:43:37 +0000

    autogenerated overwrites for web.py==0.62

--- collected.nix
+++ collected.nix
@@ -9826,6 +9826,9 @@
   weakrefmethod = {
     "1.0.3" = import ./overrides/weakrefmethod/1.0.3;
   };
+  web-py = {
+    "0.62" = import ./overrides/web-py/0.62;
+  };
   webapp2 = {
     "2.5.2" = import ./overrides/webapp2/2.5.2;
   };
