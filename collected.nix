a85a7b79257b82880a52678f8921e03641c2d304
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:56:12 +0000

    autogenerated overwrites for django-countries==7.6.1

--- collected.nix
+++ collected.nix
@@ -1925,6 +1925,9 @@
   django-concurrency = {
     "2.5" = import ./overrides/django-concurrency/2.5;
   };
+  django-countries = {
+    "7.6.1" = import ./overrides/django-countries/7.6.1;
+  };
   django-cratis = {
     "0.9.1" = import ./overrides/django-cratis/0.9.1;
   };
