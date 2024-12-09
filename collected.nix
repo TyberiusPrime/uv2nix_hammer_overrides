ff1c076f926ab61ee09c3d55bbaaf20dd6317e14
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:59:19 +0000

    autogenerated overwrites for aiostream==0.6.2

--- collected.nix
+++ collected.nix
@@ -141,6 +141,9 @@
   aiosocks = {
     "0.2.6" = import ./overrides/aiosocks/0.2.6;
   };
+  aiostream = {
+    "0.6.2" = import ./overrides/aiostream/0.6.2;
+  };
   aioxmpp = {
     "0.13.3" = import ./overrides/aioxmpp/0.13.3;
   };
