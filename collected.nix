e581d77609484e888009a9d197bbdac42870ac3d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:17:01 +0000

    autogenerated overwrites for evento==2.0.1

--- collected.nix
+++ collected.nix
@@ -2604,6 +2604,9 @@
   eventemitter = {
     "0.2.0" = import ./overrides/eventemitter/0.2.0;
   };
+  evento = {
+    "2.0.1" = import ./overrides/evento/2.0.1;
+  };
   events = {
     "0.3" = import ./overrides/events/0.3;
   };
