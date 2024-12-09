d5beadf2128d28d0e69a7f32d2f834141e98b1b9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:04:19 +0000

    autogenerated overwrites for python-multipart==0.0.9

--- collected.nix
+++ collected.nix
@@ -7411,6 +7411,7 @@
   };
   python-multipart = {
     "0.0.5" = import ./overrides/python-multipart/0.0.5;
+    "0.0.9" = import ./overrides/python-multipart/0.0.9;
   };
   python-nest = {
     "4.2.0" = import ./overrides/python-nest/4.2.0;
