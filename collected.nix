24490285f417e36ac48148e2148ee5a4fcf40105
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:46:30 +0000

    autogenerated overwrites for pygments-better-html==0.1.5

--- collected.nix
+++ collected.nix
@@ -6695,6 +6695,9 @@
   pygltflib = {
     "1.16.2" = import ./overrides/pygltflib/1.16.2;
   };
+  pygments-better-html = {
+    "0.1.5" = import ./overrides/pygments-better-html/0.1.5;
+  };
   pygments-lexer-solidity = {
     "0.7.0" = import ./overrides/pygments-lexer-solidity/0.7.0;
   };
