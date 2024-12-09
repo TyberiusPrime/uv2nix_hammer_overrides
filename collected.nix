34d0f8382b9c7a64a277b13aabd007eeae31937b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:02:09 +0000

    autogenerated overwrites for python-mpv-jsonipc==1.2.0

--- collected.nix
+++ collected.nix
@@ -7409,6 +7409,9 @@
   python-modargs = {
     "1.7" = import ./overrides/python-modargs/1.7;
   };
+  python-mpv-jsonipc = {
+    "1.2.0" = import ./overrides/python-mpv-jsonipc/1.2.0;
+  };
   python-multipart = {
     "0.0.5" = import ./overrides/python-multipart/0.0.5;
   };
