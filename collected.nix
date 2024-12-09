f550246170088122e35aa87f63afaafed50d5ba4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:46:13 +0000

    autogenerated overwrites for python-minifier==2.9.0

--- collected.nix
+++ collected.nix
@@ -7406,6 +7406,9 @@
   python-lzo = {
     "1.15" = import ./overrides/python-lzo/1.15;
   };
+  python-minifier = {
+    "2.9.0" = import ./overrides/python-minifier/2.9.0;
+  };
   python-modargs = {
     "1.7" = import ./overrides/python-modargs/1.7;
   };
