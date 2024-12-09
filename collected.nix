05861f99405de0e96aece82c8340ba01477593d6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:04:03 +0000

    autogenerated overwrites for asdf==3.4.0

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  asdf = {
+    "3.4.0" = import ./overrides/asdf/3.4.0;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
