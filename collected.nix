6fd9fcf0bcc71f7b70a194509f995a500a9f0c29
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:25:42 +0000

    autogenerated overwrites for ansicolors==1.1.8

--- collected.nix
+++ collected.nix
@@ -240,6 +240,9 @@
   ansible-base = {
     "2.10.17" = import ./overrides/ansible-base/2.10.17;
   };
+  ansicolors = {
+    "1.1.8" = import ./overrides/ansicolors/1.1.8;
+  };
   ansiconv = {
     "1.0.0" = import ./overrides/ansiconv/1.0.0;
   };
