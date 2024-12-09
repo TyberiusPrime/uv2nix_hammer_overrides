a2fd3284ad58abb24f0d1ed348d939d5b56624f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:29:17 +0000

    autogenerated overwrites for customtkinter==5.2.2

--- collected.nix
+++ collected.nix
@@ -1587,6 +1587,9 @@
   customerio = {
     "2.1" = import ./overrides/customerio/2.1;
   };
+  customtkinter = {
+    "5.2.2" = import ./overrides/customtkinter/5.2.2;
+  };
   cvargparse = {
     "0.5.4" = import ./overrides/cvargparse/0.5.4;
   };
