1b52e6fa08ac61051ad841e81313241eacead2d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:57:27 +0000

    autogenerated overwrites for zenpy==2.0.52

--- collected.nix
+++ collected.nix
@@ -10139,6 +10139,9 @@
   zeam-form-base = {
     "1.4.1" = import ./overrides/zeam-form-base/1.4.1;
   };
+  zenpy = {
+    "2.0.52" = import ./overrides/zenpy/2.0.52;
+  };
   zeroc-ice = {
     "3.6.5" = import ./overrides/zeroc-ice/3.6.5;
   };
