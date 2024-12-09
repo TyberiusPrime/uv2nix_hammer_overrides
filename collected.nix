35afcc8d08ef0d3d21945dd6173f8c2142d38671
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:47:09 +0000

    autogenerated overwrites for borsh-construct==0.1.0

--- collected.nix
+++ collected.nix
@@ -807,6 +807,9 @@
   bonsai = {
     "1.5.3" = import ./overrides/bonsai/1.5.3;
   };
+  borsh-construct = {
+    "0.1.0" = import ./overrides/borsh-construct/0.1.0;
+  };
   boto3facade = {
     "0.5.9" = import ./overrides/boto3facade/0.5.9;
   };
