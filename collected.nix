33f037d3164619a0078b2cccae500d3d70422f34
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:50:39 +0000

    autogenerated overwrites for marisa-trie==1.2.0

--- collected.nix
+++ collected.nix
@@ -4619,6 +4619,9 @@
   marionette-transport = {
     "1.2.0" = import ./overrides/marionette-transport/1.2.0;
   };
+  marisa-trie = {
+    "1.2.0" = import ./overrides/marisa-trie/1.2.0;
+  };
   markerlib = {
     "0.6.0" = import ./overrides/markerlib/0.6.0;
   };
