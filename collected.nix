cfac624c71620e59727fdf6593963eddd3c932a8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:09:17 +0000

    autogenerated overwrites for seedir==0.5.0

--- collected.nix
+++ collected.nix
@@ -8266,6 +8266,9 @@
   seantis-plonetools = {
     "0.18" = import ./overrides/seantis-plonetools/0.18;
   };
+  seedir = {
+    "0.5.0" = import ./overrides/seedir/0.5.0;
+  };
   selector = {
     "0.10.1" = import ./overrides/selector/0.10.1;
   };
