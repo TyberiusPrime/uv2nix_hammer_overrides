f7099dcb42da58a1ecf6f664518e4cd24579cb30
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:18:59 +0000

    autogenerated overwrites for types-redis==4.6.0.20240903

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-redis = {
+    "4.6.0.20240903" = import ./overrides/types-redis/4.6.0.20240903;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
