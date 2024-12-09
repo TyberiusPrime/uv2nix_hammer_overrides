50725f0399a36569070747e8f3afe879852e0658
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:48:49 +0000

    autogenerated overwrites for crosscompute==0.9.4.13

--- collected.nix
+++ collected.nix
@@ -1515,6 +1515,9 @@
   crossbar = {
     "22.6.1" = import ./overrides/crossbar/22.6.1;
   };
+  crosscompute = {
+    "0.9.4.13" = import ./overrides/crosscompute/0.9.4.13;
+  };
   cryio = {
     "2018.5.30" = import ./overrides/cryio/2018.5.30;
   };
