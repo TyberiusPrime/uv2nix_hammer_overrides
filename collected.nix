dcfcf4ee472a0cf91ff6ef7f2288b4aa32353700
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:03:11 +0000

    autogenerated overwrites for azure-storage-queue==12.11.0

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-storage-queue = {
+    "12.11.0" = import ./overrides/azure-storage-queue/12.11.0;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
