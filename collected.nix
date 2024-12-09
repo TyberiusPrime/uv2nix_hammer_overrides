af120da262926290839e247e8ac2f4dec6b91597
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:30:42 +0000

    autogenerated overwrites for applicationinsights==0.11.10

--- collected.nix
+++ collected.nix
@@ -310,6 +310,9 @@
   applescript = {
     "2021.2.9" = import ./overrides/applescript/2021.2.9;
   };
+  applicationinsights = {
+    "0.11.10" = import ./overrides/applicationinsights/0.11.10;
+  };
   appy = {
     "1.0.18" = import ./overrides/appy/1.0.18;
   };
