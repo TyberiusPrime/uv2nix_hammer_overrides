b44e7d343e0ff3c6d9fedbc14647ad6c0f62fdeb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:04:07 +0000

    autogenerated overwrites for django-enumfield==3.1

--- collected.nix
+++ collected.nix
@@ -1958,6 +1958,9 @@
   django-endless-pagination = {
     "2.0" = import ./overrides/django-endless-pagination/2.0;
   };
+  django-enumfield = {
+    "3.1" = import ./overrides/django-enumfield/3.1;
+  };
   django-extra-views = {
     "0.14.0" = import ./overrides/django-extra-views/0.14.0;
   };
