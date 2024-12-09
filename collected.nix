33fbc17d7a0d1c64533a17323a9a63e09cf44edb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:04:54 +0000

    autogenerated overwrites for aioraven==0.7.0

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioraven = {
+    "0.7.0" = import ./overrides/aioraven/0.7.0;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
