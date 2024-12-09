73b9c218b2ce2d0e70f07820b2609b6faa573715
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:43:12 +0000

    autogenerated overwrites for azure-functions==1.20.0

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-functions = {
+    "1.20.0" = import ./overrides/azure-functions/1.20.0;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
