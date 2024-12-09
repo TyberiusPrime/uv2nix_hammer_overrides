59166474ba6a3c488c7db2ae4d8706decde99b0a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:33:35 +0000

    autogenerated overwrites for pyrainbird==6.0.2

--- collected.nix
+++ collected.nix
@@ -7036,6 +7036,9 @@
   pyrabbit = {
     "1.1.0" = import ./overrides/pyrabbit/1.1.0;
   };
+  pyrainbird = {
+    "6.0.2" = import ./overrides/pyrainbird/6.0.2;
+  };
   pyramid-deform = {
     "0.2" = import ./overrides/pyramid-deform/0.2;
   };
