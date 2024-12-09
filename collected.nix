04c7e233b473b18dff0925f9238649ad28af5d76
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:05:12 +0000

    autogenerated overwrites for azure-storage-file-datalake==12.16.0

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-storage-file-datalake = {
+    "12.16.0" = import ./overrides/azure-storage-file-datalake/12.16.0;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
