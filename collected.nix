66a28cfb4d1c8deaa40db26abc76905bb3d4f927
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:44:56 +0000

    autogenerated overwrites for botlib==0.0.0

--- collected.nix
+++ collected.nix
@@ -807,6 +807,9 @@
   bonsai = {
     "1.5.3" = import ./overrides/bonsai/1.5.3;
   };
+  botlib = {
+    "0.0.0" = import ./overrides/botlib/0.0.0;
+  };
   boto3facade = {
     "0.5.9" = import ./overrides/boto3facade/0.5.9;
   };
