d8227d78d2a2c3fb0fc4dc96d06d40b4a030e247
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:55:56 +0000

    autogenerated overwrites for asgi-csrf==0.10

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  asgi-csrf = {
+    "0.10" = import ./overrides/asgi-csrf/0.10;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
