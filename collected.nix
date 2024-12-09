936a11c23b5558303d559101eb8b8b4d04d22239
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:08:51 +0000

    autogenerated overwrites for asyncua==1.1.5

--- collected.nix
+++ collected.nix
@@ -452,6 +452,9 @@
   asyncpgsa = {
     "0.27.1" = import ./overrides/asyncpgsa/0.27.1;
   };
+  asyncua = {
+    "1.1.5" = import ./overrides/asyncua/1.1.5;
+  };
   atomic = {
     "0.7.3" = import ./overrides/atomic/0.7.3;
   };
