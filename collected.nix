765f4292f3e6c21d45fe5e3f3b1f823933db965b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:19:23 +0000

    autogenerated overwrites for domeneshop==0.4.4

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
+  domeneshop = {
+    "0.4.4" = import ./overrides/domeneshop/0.4.4;
+  };
   dopy = {
     "0.3.7" = import ./overrides/dopy/0.3.7;
   };
