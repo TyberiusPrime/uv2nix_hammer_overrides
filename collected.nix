e5365f3fc3df6921dd4a37c7543429141bc6a090
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:54:54 +0000

    autogenerated overwrites for widgetsnbextension==4.0.13

--- collected.nix
+++ collected.nix
@@ -9886,6 +9886,9 @@
   whylogs-sketching = {
     "3.4.1.dev3" = import ./overrides/whylogs-sketching/3.4.1.dev3;
   };
+  widgetsnbextension = {
+    "4.0.13" = import ./overrides/widgetsnbextension/4.0.13;
+  };
   wikipedia = {
     "1.4.0" = import ./overrides/wikipedia/1.4.0;
   };
