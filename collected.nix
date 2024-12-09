78f092e79bf44ce70713540678711a991e3114f3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:19:17 +0000

    autogenerated overwrites for hiredis==3.0.0

--- collected.nix
+++ collected.nix
@@ -3465,6 +3465,9 @@
   hierplace = {
     "1.1.0" = import ./overrides/hierplace/1.1.0;
   };
+  hiredis = {
+    "3.0.0" = import ./overrides/hiredis/3.0.0;
+  };
   hitchbuild = {
     "0.6.3" = import ./overrides/hitchbuild/0.6.3;
   };
