0c940879ec6d1249490bbec9272b8e6b03932362
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:38:13 +0000

    autogenerated overwrites for tesla-fleet-api==0.7.4

--- collected.nix
+++ collected.nix
@@ -8990,6 +8990,9 @@
   terminaltables = {
     "3.1.0" = import ./overrides/terminaltables/3.1.0;
   };
+  tesla-fleet-api = {
+    "0.7.4" = import ./overrides/tesla-fleet-api/0.7.4;
+  };
   tesseract = {
     "0.1.3" = import ./overrides/tesseract/0.1.3;
   };
