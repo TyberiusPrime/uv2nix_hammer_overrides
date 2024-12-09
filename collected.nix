36ede2951319df9697f0f6777d885e12e3944fdd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:51:18 +0000

    autogenerated overwrites for aioblescan==0.2.14

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aioblescan = {
+    "0.2.14" = import ./overrides/aioblescan/0.2.14;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
