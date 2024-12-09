5e62930d94a6f2c50a38a28e6b6dc7777e74141e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:23:18 +0000

    autogenerated overwrites for envisage==7.0.3

--- collected.nix
+++ collected.nix
@@ -2538,6 +2538,9 @@
   environmental-override = {
     "0.1.2" = import ./overrides/environmental-override/0.1.2;
   };
+  envisage = {
+    "7.0.3" = import ./overrides/envisage/7.0.3;
+  };
   envparse = {
     "0.2.0" = import ./overrides/envparse/0.2.0;
   };
