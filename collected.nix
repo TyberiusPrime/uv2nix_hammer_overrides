65fa1233a90ae2f8254eabd10ff158d160da52ca
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:16:46 +0000

    autogenerated overwrites for s3bz==0.1.27

--- collected.nix
+++ collected.nix
@@ -8093,6 +8093,9 @@
   s3-pit-restore = {
     "0.9" = import ./overrides/s3-pit-restore/0.9;
   };
+  s3bz = {
+    "0.1.27" = import ./overrides/s3bz/0.1.27;
+  };
   s3keyring = {
     "0.2.4" = import ./overrides/s3keyring/0.2.4;
   };
