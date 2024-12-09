b4a0c20bbaf2ba461f4465ee07d761b2e8c4cb5f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:49:32 +0000

    autogenerated overwrites for formulae==0.5.4

--- collected.nix
+++ collected.nix
@@ -2953,6 +2953,9 @@
   formbar = {
     "1.0rc20170131" = import ./overrides/formbar/1.0rc20170131;
   };
+  formulae = {
+    "0.5.4" = import ./overrides/formulae/0.5.4;
+  };
   fortnitepy = {
     "3.6.9" = import ./overrides/fortnitepy/3.6.9;
   };
