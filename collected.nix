9cd2952eb282475afe70e8242f8cb33df5eb3cf1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:54:13 +0000

    autogenerated overwrites for nsz==4.6.1

--- collected.nix
+++ collected.nix
@@ -5343,6 +5343,9 @@
   nsapi = {
     "3.0.5" = import ./overrides/nsapi/3.0.5;
   };
+  nsz = {
+    "4.6.1" = import ./overrides/nsz/4.6.1;
+  };
   numba = {
     "0.53.1" = import ./overrides/numba/0.53.1;
     "0.60.0" = import ./overrides/numba/0.60.0;
