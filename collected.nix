c9621dcb0b246af1043d7e475a9b78fce51f93e9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:23:33 +0000

    autogenerated overwrites for bloom-filter==1.3.3

--- collected.nix
+++ collected.nix
@@ -785,6 +785,9 @@
   blockchain-etl-common = {
     "1.7.1" = import ./overrides/blockchain-etl-common/1.7.1;
   };
+  bloom-filter = {
+    "1.3.3" = import ./overrides/bloom-filter/1.3.3;
+  };
   blosc2 = {
     "2.0.0" = import ./overrides/blosc2/2.0.0;
     "2.7.1" = import ./overrides/blosc2/2.7.1;
