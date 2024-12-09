e69b0018ab34ae60b9aeaf661c289fb084bf0d40
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:39:01 +0000

    autogenerated overwrites for google-cloud-workstations==0.5.8

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-workstations = {
+    "0.5.8" = import ./overrides/google-cloud-workstations/0.5.8;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
