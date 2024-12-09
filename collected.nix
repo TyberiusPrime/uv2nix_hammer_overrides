276948fad3ed4a739d03e962d1b339f2563162ad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:30:38 +0000

    autogenerated overwrites for oca-port==0.15

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  oca-port = {
+    "0.15" = import ./overrides/oca-port/0.15;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
