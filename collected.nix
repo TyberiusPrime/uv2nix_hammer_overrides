93aec8cf3be5cddc927505543515ac1f962955d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:18:52 +0000

    autogenerated overwrites for pydantic-openapi-helper==0.2.11

--- collected.nix
+++ collected.nix
@@ -6536,6 +6536,9 @@
   pycups = {
     "2.0.4" = import ./overrides/pycups/2.0.4;
   };
+  pydantic-openapi-helper = {
+    "0.2.11" = import ./overrides/pydantic-openapi-helper/0.2.11;
+  };
   pydaqmx = {
     "1.4.6" = import ./overrides/pydaqmx/1.4.6;
   };
