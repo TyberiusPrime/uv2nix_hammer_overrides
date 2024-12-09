0b907c108640bf703b8fd1b9d240b7f621d49f9a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:21:04 +0000

    autogenerated overwrites for mara-page==1.7.0

--- collected.nix
+++ collected.nix
@@ -4607,6 +4607,9 @@
   mappy = {
     "2.28" = import ./overrides/mappy/2.28;
   };
+  mara-page = {
+    "1.7.0" = import ./overrides/mara-page/1.7.0;
+  };
   mariadb = {
     "1.1.10" = import ./overrides/mariadb/1.1.10;
   };
