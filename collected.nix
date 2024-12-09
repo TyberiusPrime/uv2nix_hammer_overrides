dded94910d25f4b2c4535b79d5d22c31bf4ccac4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:05:18 +0000

    autogenerated overwrites for dash-iconify==0.1.2

--- collected.nix
+++ collected.nix
@@ -1671,6 +1671,9 @@
   dash-daq = {
     "0.5.0" = import ./overrides/dash-daq/0.5.0;
   };
+  dash-iconify = {
+    "0.1.2" = import ./overrides/dash-iconify/0.1.2;
+  };
   dash-renderer = {
     "1.9.1" = import ./overrides/dash-renderer/1.9.1;
   };
