b88498b4b4aad0f1551f7703bc19a18219d854ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:20:59 +0000

    autogenerated overwrites for census==0.8.22

--- collected.nix
+++ collected.nix
@@ -978,6 +978,9 @@
   cenpy = {
     "1.0.1" = import ./overrides/cenpy/1.0.1;
   };
+  census = {
+    "0.8.22" = import ./overrides/census/0.8.22;
+  };
   cf-units = {
     "3.2.0" = import ./overrides/cf-units/3.2.0;
   };
