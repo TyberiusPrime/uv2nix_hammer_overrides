7ad40d4624472888c3e354851e4d78d3e4f25252
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:50:02 +0000

    autogenerated overwrites for aiogram==3.13.0

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aiogram = {
+    "3.13.0" = import ./overrides/aiogram/3.13.0;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
