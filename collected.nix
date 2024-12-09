125b2509ad3d87dbf257e4cbe14c7a1d76f7ec74
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:41:03 +0000

    autogenerated overwrites for google-cloud-spanner==3.49.1

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-spanner = {
+    "3.49.1" = import ./overrides/google-cloud-spanner/3.49.1;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
