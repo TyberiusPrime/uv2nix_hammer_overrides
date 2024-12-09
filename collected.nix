6ee14b61b206a0c185e9d8c888912a0ba74b1b14
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:42:22 +0000

    autogenerated overwrites for dependency-injector==4.42.0

--- collected.nix
+++ collected.nix
@@ -1773,6 +1773,9 @@
   demjson3 = {
     "3.0.6" = import ./overrides/demjson3/3.0.6;
   };
+  dependency-injector = {
+    "4.42.0" = import ./overrides/dependency-injector/4.42.0;
+  };
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
