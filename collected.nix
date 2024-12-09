e678907087cb1f24eddfa8c13b026e7a9ef7e8ed
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:22:51 +0000

    autogenerated overwrites for yubico-client==1.13.0

--- collected.nix
+++ collected.nix
@@ -10097,6 +10097,9 @@
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
+  yubico-client = {
+    "1.13.0" = import ./overrides/yubico-client/1.13.0;
+  };
   yurl = {
     "1.0.0" = import ./overrides/yurl/1.0.0;
   };
