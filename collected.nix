4d9665fbd46931efe9a1dc20d041a1c88fc80564
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:28:14 +0000

    autogenerated overwrites for cftime==1.6.4

--- collected.nix
+++ collected.nix
@@ -984,6 +984,9 @@
   cfgraph = {
     "0.2.1" = import ./overrides/cfgraph/0.2.1;
   };
+  cftime = {
+    "1.6.4" = import ./overrides/cftime/1.6.4;
+  };
   cfunits = {
     "3.3.7" = import ./overrides/cfunits/3.3.7;
   };
