2e75b901e8fc93313582679e9f6546250a585299
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:37:08 +0000

    autogenerated overwrites for azure-servicemanagement-legacy==0.20.7

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-servicemanagement-legacy = {
+    "0.20.7" = import ./overrides/azure-servicemanagement-legacy/0.20.7;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
