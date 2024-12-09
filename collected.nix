63a420c436b67c61d017a0f389b4df720054871f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:18:44 +0000

    autogenerated overwrites for django-modelcluster==6.3

--- collected.nix
+++ collected.nix
@@ -2045,6 +2045,9 @@
   django-model-utils = {
     "3.0.0" = import ./overrides/django-model-utils/3.0.0;
   };
+  django-modelcluster = {
+    "6.3" = import ./overrides/django-modelcluster/6.3;
+  };
   django-mongoengine = {
     "0.5.6" = import ./overrides/django-mongoengine/0.5.6;
   };
