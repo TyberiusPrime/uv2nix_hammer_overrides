f4ba736d3e4c959f7447ce8c67ebcc3c849485ca
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:00:49 +0000

    autogenerated overwrites for fixtures==4.1.0

--- collected.nix
+++ collected.nix
@@ -2790,6 +2790,9 @@
   fixture = {
     "1.5.11" = import ./overrides/fixture/1.5.11;
   };
+  fixtures = {
+    "4.1.0" = import ./overrides/fixtures/4.1.0;
+  };
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
