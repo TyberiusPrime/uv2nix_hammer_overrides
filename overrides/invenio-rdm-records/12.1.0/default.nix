e7c2a3d3553c62ad635198c540c3670452744dfb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:38:24 +0000

    autogenerated overwrites for invenio-rdm-records==12.1.0

--- overrides/invenio-rdm-records/12.1.0/default.nix
+++ overrides/invenio-rdm-records/12.1.0/default.nix
@@ -1,10 +1,14 @@
-{final, pkgs, ...}
-        : old: {
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {babel = [];setuptools = [];wheel = [];});})) (old: old // ( {
   postInstall =
     old.postInstall or ""
     + ''
       rm -rf $out/${final.python.sitePackages}/tests
     '';
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
