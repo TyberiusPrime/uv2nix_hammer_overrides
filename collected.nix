bd44acf3bbdc07998fa40981e79e79b480622ad5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:39:53 +0000

    autogenerated overwrites for aiohttp-middlewares==2.4.0

--- collected.nix
+++ collected.nix
@@ -105,6 +105,9 @@
   aiohttp-devtools = {
     "1.1.2" = import ./overrides/aiohttp-devtools/1.1.2;
   };
+  aiohttp-middlewares = {
+    "2.4.0" = import ./overrides/aiohttp-middlewares/2.4.0;
+  };
   aiohttp-route-decorator = {
     "0.1.4" = import ./overrides/aiohttp-route-decorator/0.1.4;
   };
