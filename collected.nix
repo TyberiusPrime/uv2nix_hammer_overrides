cb1c021ef3a4eb30f7b5bc08b240c76ebd43bfa4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 16:55:23 +0000

    autogenerated overwrites for rcssmin==1.1.2

--- collected.nix
+++ collected.nix
@@ -7790,6 +7790,9 @@
   rcsb-utils-io = {
     "1.49" = import ./overrides/rcsb-utils-io/1.49;
   };
+  rcssmin = {
+    "1.1.2" = import ./overrides/rcssmin/1.1.2;
+  };
   rdp = {
     "0.8" = import ./overrides/rdp/0.8;
   };
