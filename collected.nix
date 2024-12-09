5ac8653a18e6c6e4843944d2234bc7f59dc952a1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:22:49 +0000

    autogenerated overwrites for pystrict==1.3

--- collected.nix
+++ collected.nix
@@ -7185,6 +7185,9 @@
   pystemmer = {
     "2.2.0.1" = import ./overrides/pystemmer/2.2.0.1;
   };
+  pystrict = {
+    "1.3" = import ./overrides/pystrict/1.3;
+  };
   pyswarm = {
     "0.6" = import ./overrides/pyswarm/0.6;
   };
