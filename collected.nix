dccc5ba3ef511291fac83b078d23c10a7890ea46
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:00:40 +0000

    autogenerated overwrites for pyramid_layout==1.0

--- collected.nix
+++ collected.nix
@@ -7039,6 +7039,9 @@
   pyramid-deform = {
     "0.2" = import ./overrides/pyramid-deform/0.2;
   };
+  pyramid-layout = {
+    "1.0" = import ./overrides/pyramid-layout/1.0;
+  };
   pyramid-redis-sessions = {
     "1.0.1" = import ./overrides/pyramid-redis-sessions/1.0.1;
   };
