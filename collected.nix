c382901c2fe659baaa7c9365275ebd4ecf61107f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:22:16 +0000

    autogenerated overwrites for pykka==4.0.2

--- collected.nix
+++ collected.nix
@@ -6779,6 +6779,9 @@
   pykerberos = {
     "1.2.4" = import ./overrides/pykerberos/1.2.4;
   };
+  pykka = {
+    "4.0.2" = import ./overrides/pykka/4.0.2;
+  };
   pykmtronic = {
     "0.3.0" = import ./overrides/pykmtronic/0.3.0;
   };
