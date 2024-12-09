fe81edf99f38e180c25679eda3dc3aed1a7808e9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 00:25:22 +0000

    autogenerated overwrites for riemann-tx==2.1.0

--- collected.nix
+++ collected.nix
@@ -7949,6 +7949,9 @@
   rhasspy-silence = {
     "0.4.0" = import ./overrides/rhasspy-silence/0.4.0;
   };
+  riemann-tx = {
+    "2.1.0" = import ./overrides/riemann-tx/2.1.0;
+  };
   rig = {
     "2.4.1" = import ./overrides/rig/2.4.1;
   };
