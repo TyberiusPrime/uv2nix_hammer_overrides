cd901a5f13f18ae8edd1a914c38295f3a3725c54
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:18:51 +0000

    autogenerated overwrites for hikvision==2.0.4

--- collected.nix
+++ collected.nix
@@ -3465,6 +3465,9 @@
   hierplace = {
     "1.1.0" = import ./overrides/hierplace/1.1.0;
   };
+  hikvision = {
+    "2.0.4" = import ./overrides/hikvision/2.0.4;
+  };
   hitchbuild = {
     "0.6.3" = import ./overrides/hitchbuild/0.6.3;
   };
