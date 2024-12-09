153f570d28b7437fbda276d0065b999a6b5925f5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:27:52 +0000

    autogenerated overwrites for python-resize-image==1.1.20

--- collected.nix
+++ collected.nix
@@ -7454,6 +7454,9 @@
   python-qpid-proton = {
     "0.39.0" = import ./overrides/python-qpid-proton/0.39.0;
   };
+  python-resize-image = {
+    "1.1.20" = import ./overrides/python-resize-image/1.1.20;
+  };
   python-rtmidi = {
     "1.5.8" = import ./overrides/python-rtmidi/1.5.8;
   };
