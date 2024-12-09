33bf0f51d1abd7e542e8ee798a291a34521c578f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:07:01 +0000

    autogenerated overwrites for dpdata==0.2.20

--- collected.nix
+++ collected.nix
@@ -2349,6 +2349,9 @@
   dpcontracts = {
     "0.6.0" = import ./overrides/dpcontracts/0.6.0;
   };
+  dpdata = {
+    "0.2.20" = import ./overrides/dpdata/0.2.20;
+  };
   dppd = {
     "0.27" = import ./overrides/dppd/0.27;
   };
