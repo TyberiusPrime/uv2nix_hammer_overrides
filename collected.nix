c2356f9cbe7564dc33751f65ff4d45d28694dada
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:11:00 +0000

    autogenerated overwrites for django-storages==1.14.4

--- collected.nix
+++ collected.nix
@@ -2165,6 +2165,9 @@
   django-staticfiles = {
     "1.2.1" = import ./overrides/django-staticfiles/1.2.1;
   };
+  django-storages = {
+    "1.14.4" = import ./overrides/django-storages/1.14.4;
+  };
   django-summernote = {
     "0.8.20.0" = import ./overrides/django-summernote/0.8.20.0;
   };
