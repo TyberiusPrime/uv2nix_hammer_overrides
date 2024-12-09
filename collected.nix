fdae6e4ded8789fe6a42cc6e61c945b961649eda
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:27:54 +0000

    autogenerated overwrites for k3utfjson==0.1.1

--- collected.nix
+++ collected.nix
@@ -4078,6 +4078,9 @@
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
+  k3utfjson = {
+    "0.1.1" = import ./overrides/k3utfjson/0.1.1;
+  };
   kabaret = {
     "2.3.0" = import ./overrides/kabaret/2.3.0;
   };
