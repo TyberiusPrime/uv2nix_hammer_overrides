fdc3a5ab92342b928865205cfc23d5ec4373f6b3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:37:04 +0000

    autogenerated overwrites for pytest-select==0.1.2

--- collected.nix
+++ collected.nix
@@ -7242,6 +7242,9 @@
   pytest-remfiles = {
     "0.0.2" = import ./overrides/pytest-remfiles/0.0.2;
   };
+  pytest-select = {
+    "0.1.2" = import ./overrides/pytest-select/0.1.2;
+  };
   pytest-services = {
     "2.2.1" = import ./overrides/pytest-services/2.2.1;
   };
