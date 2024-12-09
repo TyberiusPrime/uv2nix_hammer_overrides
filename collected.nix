c426acc1e55b7a1086262951d1824ab7f7988313
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:28:45 +0000

    autogenerated overwrites for magicgui==0.9.1

--- collected.nix
+++ collected.nix
@@ -4577,6 +4577,9 @@
   madmom = {
     "0.16.1" = import ./overrides/madmom/0.16.1;
   };
+  magicgui = {
+    "0.9.1" = import ./overrides/magicgui/0.9.1;
+  };
   mailchecker = {
     "6.0.8" = import ./overrides/mailchecker/6.0.8;
   };
