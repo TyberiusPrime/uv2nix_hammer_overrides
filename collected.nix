691c57a60fc2aee1ed96ea39cbacdbbd391f8681
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:49:19 +0000

    autogenerated overwrites for requests-futures==1.0.1

--- collected.nix
+++ collected.nix
@@ -7898,6 +7898,9 @@
   requests-ftp = {
     "0.3.1" = import ./overrides/requests-ftp/0.3.1;
   };
+  requests-futures = {
+    "1.0.1" = import ./overrides/requests-futures/1.0.1;
+  };
   requests-oauth = {
     "0.4.1" = import ./overrides/requests-oauth/0.4.1;
   };
