fdf9a2e407962f48f6599b58871559cdfa827d52
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:53:36 +0000

    autogenerated overwrites for altair==5.4.1

--- collected.nix
+++ collected.nix
@@ -193,6 +193,9 @@
   alphatwirl = {
     "0.23.0" = import ./overrides/alphatwirl/0.23.0;
   };
+  altair = {
+    "5.4.1" = import ./overrides/altair/5.4.1;
+  };
   amazon-dax-client = {
     "2.0.3" = import ./overrides/amazon-dax-client/2.0.3;
   };
