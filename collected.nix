f4d82b0b1b15df7c39976d050e1e2837d98dbadf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:02:49 +0000

    autogenerated overwrites for whiteboxgui==2.3.0

--- collected.nix
+++ collected.nix
@@ -9883,6 +9883,9 @@
   whisper = {
     "1.1.10" = import ./overrides/whisper/1.1.10;
   };
+  whiteboxgui = {
+    "2.3.0" = import ./overrides/whiteboxgui/2.3.0;
+  };
   whylogs-sketching = {
     "3.4.1.dev3" = import ./overrides/whylogs-sketching/3.4.1.dev3;
   };
