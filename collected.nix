7183bbac792264febe8b08d502ccf7a2c80ecbdf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 11:34:04 +0000

    autogenerated overwrites for ndjson==0.3.1

--- collected.nix
+++ collected.nix
@@ -5141,6 +5141,9 @@
   ncephes = {
     "1.1.0" = import ./overrides/ncephes/1.1.0;
   };
+  ndjson = {
+    "0.3.1" = import ./overrides/ndjson/0.3.1;
+  };
   ndms2-client = {
     "0.1.3" = import ./overrides/ndms2-client/0.1.3;
   };
