aa9a35714ca9bd134c492391940ca1e52240b98b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:06:00 +0000

    autogenerated overwrites for cf-units==3.2.0

--- overrides/cf-units/3.2.0/default.nix
+++ overrides/cf-units/3.2.0/default.nix
@@ -1,25 +1,3 @@
-{
-  resolveBuildSystem,
-  helpers,
-  pkgs,
-  ...
-}:
-old:
-if (helpers.isWheel old) then
-  { buildInputs = old.buildInputs or [ ] ++ [ pkgs.udunits ]; }
-else
-  {
-    buildInputs = old.buildInputs or [ ] ++ [ pkgs.udunits ];
-    env = {
-      "UDUNITS2_XML_PATH" = "${pkgs.udunits}/share/udunits/udunits2.xml";
-    };
-    nativeBuildInputs =
-      old.nativeBuildInputs or [ ]
-      ++ (resolveBuildSystem {
-        "cython_0" = [ ];
-        oldest-supported-numpy = [ ];
-        setuptools = [ ];
-        setuptools-scm = [ ];
-        wheel = [ ];
-      });
-  }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.udunits];} else {buildInputs = old.buildInputs or [] ++ [pkgs.udunits];env = {"UDUNITS2_XML_PATH" = "${pkgs.udunits}/share/udunits/udunits2.xml";};nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {"cython_0" = [];oldest-supported-numpy = [];setuptools = [];setuptools-scm = [];wheel = [];});}
+        
