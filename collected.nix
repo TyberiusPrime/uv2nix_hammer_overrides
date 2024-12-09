ed5d3ee3f23abfe8582ede22660499227fe9a1cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:43:10 +0000

    autogenerated overwrites for pysmi==1.5.0

--- collected.nix
+++ collected.nix
@@ -7149,6 +7149,9 @@
   pysmb = {
     "1.2.9.1" = import ./overrides/pysmb/1.2.9.1;
   };
+  pysmi = {
+    "1.5.0" = import ./overrides/pysmi/1.5.0;
+  };
   pysocks = {
     "1.6.8" = import ./overrides/pysocks/1.6.8;
   };
