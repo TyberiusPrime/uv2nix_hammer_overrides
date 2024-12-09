59165a717ec935e893a4d3e337c697cc9e9b73cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:05:47 +0000

    autogenerated overwrites for ontospy==2.1.1

--- collected.nix
+++ collected.nix
@@ -5501,6 +5501,9 @@
   onnxsim = {
     "0.4.36" = import ./overrides/onnxsim/0.4.36;
   };
+  ontospy = {
+    "2.1.1" = import ./overrides/ontospy/2.1.1;
+  };
   onvif-zeep = {
     "0.2.12" = import ./overrides/onvif-zeep/0.2.12;
   };
