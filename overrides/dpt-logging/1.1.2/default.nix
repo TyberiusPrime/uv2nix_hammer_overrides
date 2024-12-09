8a9a0bdd37ca1822ac418bd80cbe5b34a0a494f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 21:20:10 +0000

    autogenerated overwrites for dpt-logging==1.1.2

--- overrides/dpt-logging/1.1.2/default.nix
+++ overrides/dpt-logging/1.1.2/default.nix
@@ -1,10 +1,14 @@
-{pkgs, ...}
-        : old: {
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];wheel = [];});})) (old: old // ( {
   postInstall =
     old.postInstall or ""
     + ''
       rm $out/docs/* -r
     '';
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
