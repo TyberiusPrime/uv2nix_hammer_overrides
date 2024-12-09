8754d42574ac52311968e0ec56416e13909fc993
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:56:01 +0000

    autogenerated overwrites for openlineage-integration-common==1.22.0

--- collected.nix
+++ collected.nix
@@ -5543,6 +5543,9 @@
   openidc-client = {
     "0.6.0" = import ./overrides/openidc-client/0.6.0;
   };
+  openlineage-integration-common = {
+    "1.22.0" = import ./overrides/openlineage-integration-common/1.22.0;
+  };
   openmdao = {
     "3.34.2" = import ./overrides/openmdao/3.34.2;
   };
