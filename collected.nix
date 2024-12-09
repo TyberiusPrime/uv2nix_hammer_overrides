9e2cb235fa530fdd46662a2e011d8123aa2aa60f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:06:53 +0000

    autogenerated overwrites for toml==0.10.2

--- collected.nix
+++ collected.nix
@@ -9160,6 +9160,9 @@
   tokenleaderclient = {
     "1.5.1" = import ./overrides/tokenleaderclient/1.5.1;
   };
+  toml = {
+    "0.10.2" = import ./overrides/toml/0.10.2;
+  };
   toml-py = {
     "0.1.7" = import ./overrides/toml-py/0.1.7;
   };
