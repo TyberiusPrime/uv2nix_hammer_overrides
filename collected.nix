8a547e6f21ff1bb1ee21353ea71b237e46fab943
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:11:24 +0000

    autogenerated overwrites for virtualenv==20.26.4

--- collected.nix
+++ collected.nix
@@ -9711,6 +9711,9 @@
   virtualchain = {
     "20.0.1.0" = import ./overrides/virtualchain/20.0.1.0;
   };
+  virtualenv = {
+    "20.26.4" = import ./overrides/virtualenv/20.26.4;
+  };
   visaplan-plone-tools = {
     "1.5.3" = import ./overrides/visaplan-plone-tools/1.5.3;
   };
