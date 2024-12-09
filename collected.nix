1b2cf43b24daa8e1178947a3e760969dbe0a74a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 14:11:14 +0000

    autogenerated overwrites for xmodem==0.4.7

--- collected.nix
+++ collected.nix
@@ -10025,6 +10025,9 @@
   xmlbuilder = {
     "1.0" = import ./overrides/xmlbuilder/1.0;
   };
+  xmodem = {
+    "0.4.7" = import ./overrides/xmodem/0.4.7;
+  };
   xmpppy = {
     "0.7.1" = import ./overrides/xmpppy/0.7.1;
   };
