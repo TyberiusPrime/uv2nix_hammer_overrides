5fa32f00a43c8d0d7f22888a3c3eb06c5348aa63
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:44:20 +0000

    autogenerated overwrites for miller==0.1.8

--- collected.nix
+++ collected.nix
@@ -4793,6 +4793,9 @@
   migen = {
     "0.9.2" = import ./overrides/migen/0.9.2;
   };
+  miller = {
+    "0.1.8" = import ./overrides/miller/0.1.8;
+  };
   millify = {
     "0.1.1" = import ./overrides/millify/0.1.1;
   };
