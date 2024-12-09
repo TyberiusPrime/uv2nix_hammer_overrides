39bc03ed90d06c61a6f179fd175d2abef6d3dda3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:08:19 +0000

    autogenerated overwrites for fastapi-restful==0.6.0

--- collected.nix
+++ collected.nix
@@ -2679,6 +2679,9 @@
   fastapi-cli = {
     "0.0.5" = import ./overrides/fastapi-cli/0.0.5;
   };
+  fastapi-restful = {
+    "0.6.0" = import ./overrides/fastapi-restful/0.6.0;
+  };
   fastcache = {
     "1.1.0" = import ./overrides/fastcache/1.1.0;
   };
