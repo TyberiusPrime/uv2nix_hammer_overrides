96f8ecc9b39b85e54833057b1fc3cee9796afee9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:06:55 +0000

    autogenerated overwrites for kcu==0.0.73

--- collected.nix
+++ collected.nix
@@ -4105,6 +4105,9 @@
   kavenegar = {
     "1.1.2" = import ./overrides/kavenegar/1.1.2;
   };
+  kcu = {
+    "0.0.73" = import ./overrides/kcu/0.0.73;
+  };
   kedro = {
     "0.19.8" = import ./overrides/kedro/0.19.8;
   };
