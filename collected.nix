0931c3d43e2bb0ac22f54d24bc3f1d3223b15abe
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:02:39 +0000

    autogenerated overwrites for line-bot-sdk==3.13.0

--- collected.nix
+++ collected.nix
@@ -4413,6 +4413,9 @@
   limix-plot = {
     "0.1.2" = import ./overrides/limix-plot/0.1.2;
   };
+  line-bot-sdk = {
+    "3.13.0" = import ./overrides/line-bot-sdk/3.13.0;
+  };
   linear-tsv = {
     "1.1.0" = import ./overrides/linear-tsv/1.1.0;
   };
