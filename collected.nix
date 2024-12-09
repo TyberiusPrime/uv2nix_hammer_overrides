608efd0886a4fcff937753eb73a4f251ab2e9a50
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 08:45:54 +0000

    autogenerated overwrites for pytorch-lightning==2.4.0

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytorch-lightning = {
+    "2.4.0" = import ./overrides/pytorch-lightning/2.4.0;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
