6a56d267e26b28a66bbf2885fd22bc13b6a6ac11
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:49:55 +0000

    autogenerated overwrites for cachy==0.3.0

--- collected.nix
+++ collected.nix
@@ -888,6 +888,9 @@
   cached-ipaddress = {
     "0.5.0" = import ./overrides/cached-ipaddress/0.5.0;
   };
+  cachy = {
+    "0.3.0" = import ./overrides/cachy/0.3.0;
+  };
   caep = {
     "1.1.0" = import ./overrides/caep/1.1.0;
   };
