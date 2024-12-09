b19eb3f6ce8ab34cb4e06957898178bfce30637e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:14:18 +0000

    autogenerated overwrites for graze==0.1.24

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  graze = {
+    "0.1.24" = import ./overrides/graze/0.1.24;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
