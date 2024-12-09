c08ffd99d50f7d6d1eac3588cc063d0af124b5b8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:33:04 +0000

    autogenerated overwrites for invenio-communities==15.0.0

--- collected.nix
+++ collected.nix
@@ -3805,6 +3805,9 @@
   intervaltree = {
     "3.1.0" = import ./overrides/intervaltree/3.1.0;
   };
+  invenio-communities = {
+    "15.0.0" = import ./overrides/invenio-communities/15.0.0;
+  };
   invenio-github = {
     "1.5.2" = import ./overrides/invenio-github/1.5.2;
   };
