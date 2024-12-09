116b74a5e43a86a791980d75277844b83a5445fa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:58:38 +0000

    autogenerated overwrites for py65==1.2.0

--- collected.nix
+++ collected.nix
@@ -6351,6 +6351,9 @@
   py3rijndael = {
     "0.3.3" = import ./overrides/py3rijndael/0.3.3;
   };
+  py65 = {
+    "1.2.0" = import ./overrides/py65/1.2.0;
+  };
   pyacoustid = {
     "1.3.0" = import ./overrides/pyacoustid/1.3.0;
   };
