d49ade255dea9ad049a8492f67d651ea7c8c5a59
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:21:04 +0000

    autogenerated overwrites for imio.migrator==1.34

--- collected.nix
+++ collected.nix
@@ -3701,6 +3701,7 @@
     "1.27" = import ./overrides/imio-history/1.27;
   };
   imio-migrator = {
+    "1.34" = import ./overrides/imio-migrator/1.34;
     "1.38" = import ./overrides/imio-migrator/1.38;
   };
   imio-prettylink = {
