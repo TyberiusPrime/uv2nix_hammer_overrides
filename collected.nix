b4cc162bf4e8ebec0f930e01678e059aab1a04de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:06:06 +0000

    autogenerated overwrites for mudata==0.3.1

--- collected.nix
+++ collected.nix
@@ -5013,6 +5013,9 @@
   mtranslate = {
     "1.8" = import ./overrides/mtranslate/1.8;
   };
+  mudata = {
+    "0.3.1" = import ./overrides/mudata/0.3.1;
+  };
   mullvad-closest = {
     "0.0.3" = import ./overrides/mullvad-closest/0.0.3;
   };
