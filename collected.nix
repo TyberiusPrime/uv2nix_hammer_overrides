1e6bb3820cf675709ee366172d7d4a52a5c7e286
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:51:25 +0000

    autogenerated overwrites for objectpath==0.6.1

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  objectpath = {
+    "0.6.1" = import ./overrides/objectpath/0.6.1;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
