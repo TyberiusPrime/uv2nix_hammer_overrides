66b40d944954ea408cc4ab41c1e2088929bd16fa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:39:26 +0000

    autogenerated overwrites for greeclimate==2.1.0

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  greeclimate = {
+    "2.1.0" = import ./overrides/greeclimate/2.1.0;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
