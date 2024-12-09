0a42a01cdb35101970b8d8207ec1158fa3506b94
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:15:06 +0000

    autogenerated overwrites for msg-parser==1.2.0

--- collected.nix
+++ collected.nix
@@ -5001,6 +5001,9 @@
   ms-cv = {
     "0.1.1" = import ./overrides/ms-cv/0.1.1;
   };
+  msg-parser = {
+    "1.2.0" = import ./overrides/msg-parser/1.2.0;
+  };
   msgpack = {
     "1.0.2" = import ./overrides/msgpack/1.0.2;
   };
