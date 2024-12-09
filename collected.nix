d4e8babd0a2949adde9c6408a7d4779fac1e38e0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:04:31 +0000

    autogenerated overwrites for lbt-ladybug==0.27.104

--- collected.nix
+++ collected.nix
@@ -4287,6 +4287,9 @@
   lbmessaging = {
     "1.2.4" = import ./overrides/lbmessaging/1.2.4;
   };
+  lbt-ladybug = {
+    "0.27.104" = import ./overrides/lbt-ladybug/0.27.104;
+  };
   lcogt-logging = {
     "0.3.2" = import ./overrides/lcogt-logging/0.3.2;
   };
