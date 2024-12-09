1397ca44bef3d2a007209bbf92eeab68cce441d7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:46:14 +0000

    autogenerated overwrites for zeroconf==0.134.0

--- collected.nix
+++ collected.nix
@@ -10142,6 +10142,9 @@
   zeroc-ice = {
     "3.6.5" = import ./overrides/zeroc-ice/3.6.5;
   };
+  zeroconf = {
+    "0.134.0" = import ./overrides/zeroconf/0.134.0;
+  };
   zerotk-zops = {
     "0.5.3" = import ./overrides/zerotk-zops/0.5.3;
   };
