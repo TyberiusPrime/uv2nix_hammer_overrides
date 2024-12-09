432f6699adae4a8fa14e51d40235fc2f2b2a55a8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:24:42 +0000

    autogenerated overwrites for google-nest-sdm==5.0.1

--- collected.nix
+++ collected.nix
@@ -3249,6 +3249,9 @@
   google-images-download = {
     "2.8.0" = import ./overrides/google-images-download/2.8.0;
   };
+  google-nest-sdm = {
+    "5.0.1" = import ./overrides/google-nest-sdm/5.0.1;
+  };
   google-search-results = {
     "2.4.2" = import ./overrides/google-search-results/2.4.2;
   };
