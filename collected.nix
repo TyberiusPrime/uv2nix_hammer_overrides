9383293202b1dae631d243eccecef7f6cabea963
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:26:07 +0000

    autogenerated overwrites for desktop-notifier==6.0.0

--- collected.nix
+++ collected.nix
@@ -1776,6 +1776,9 @@
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
+  desktop-notifier = {
+    "6.0.0" = import ./overrides/desktop-notifier/6.0.0;
+  };
   dev0s = {
     "3.36.6" = import ./overrides/dev0s/3.36.6;
   };
