5cbe3524edab4eb1ad6366903baec08430917631
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:41:06 +0000

    autogenerated overwrites for dataclasses-json==0.6.7

--- collected.nix
+++ collected.nix
@@ -1680,6 +1680,9 @@
   data = {
     "0.4" = import ./overrides/data/0.4;
   };
+  dataclasses-json = {
+    "0.6.7" = import ./overrides/dataclasses-json/0.6.7;
+  };
   datacleaner = {
     "0.1.5" = import ./overrides/datacleaner/0.1.5;
   };
