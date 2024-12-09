13bfdf0ed829d044da8cc77583f38390c329e5bf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:36:00 +0000

    autogenerated overwrites for azure-cli-telemetry==1.1.0

--- collected.nix
+++ collected.nix
@@ -539,6 +539,9 @@
   axial-positional-embedding = {
     "0.2.1" = import ./overrides/axial-positional-embedding/0.2.1;
   };
+  azure-cli-telemetry = {
+    "1.1.0" = import ./overrides/azure-cli-telemetry/1.1.0;
+  };
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
