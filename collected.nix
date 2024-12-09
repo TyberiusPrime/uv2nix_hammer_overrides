39d04bde7580387b13f003f422192122961b51d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:27:17 +0000

    autogenerated overwrites for supar==1.1.4

--- collected.nix
+++ collected.nix
@@ -8837,6 +8837,9 @@
   sunwatcher = {
     "0.2.1" = import ./overrides/sunwatcher/0.2.1;
   };
+  supar = {
+    "1.1.4" = import ./overrides/supar/1.1.4;
+  };
   sure = {
     "3.0a2" = import ./overrides/sure/3.0a2;
   };
