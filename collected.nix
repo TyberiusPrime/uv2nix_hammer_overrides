4269273623f0ccc206e6c1b58555118dc968dd05
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:12:32 +0000

    autogenerated overwrites for pytorch-fid==0.3.0

--- collected.nix
+++ collected.nix
@@ -7544,6 +7544,9 @@
   pytmpdir = {
     "1.1.0" = import ./overrides/pytmpdir/1.1.0;
   };
+  pytorch-fid = {
+    "0.3.0" = import ./overrides/pytorch-fid/0.3.0;
+  };
   pytorchvideo = {
     "0.1.5" = import ./overrides/pytorchvideo/0.1.5;
   };
