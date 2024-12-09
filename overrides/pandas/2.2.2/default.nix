c9200c21164a258b5df605b6be5d46cb469af646
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 12:54:33 +0000

    autogenerated overwrites for pandas==2.2.2

--- overrides/pandas/2.2.2/default.nix
+++ overrides/pandas/2.2.2/default.nix
@@ -1,22 +1,5 @@
-{ resolveBuildSystem, helpers, ... }:
-old:
-if (helpers.isWheel old) then
-  { }
-else
-  {
-    nativeBuildInputs =
-      old.nativeBuildInputs or [ ]
-      ++ (resolveBuildSystem {
-        cython = [ ];
-        meson = [ ];
-        meson-python = [ ];
-        numpy = [ ];
-        versioneer = [ ];
-        wheel = [ ];
-      });
-    postPatch =
-      (old.postPatch or "")
-      + ''
-        ${helpers.tomlreplace} pyproject.toml build-system.requires "[]"
-      '';
-  }
+{final, helpers, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cython = [];meson = [];meson-python = [];ninja = [];numpy = [];versioneer = [];wheel = [];});postPatch = (old.postPatch or "")+(''
+                ${helpers.tomlreplace} pyproject.toml build-system.requires "[]"
+        '');}
+        
