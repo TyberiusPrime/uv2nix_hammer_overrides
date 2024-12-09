aa3a2d3874925503b0a6e3a8b38fdb5c5bdc2942
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:56:36 +0000

    autogenerated overwrites for wagtail-cache==2.5.1

--- collected.nix
+++ collected.nix
@@ -9793,6 +9793,9 @@
   wagon = {
     "1.0.1" = import ./overrides/wagon/1.0.1;
   };
+  wagtail-cache = {
+    "2.5.1" = import ./overrides/wagtail-cache/2.5.1;
+  };
   wai-common = {
     "0.0.44" = import ./overrides/wai-common/0.0.44;
   };
