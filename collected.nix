f903036284ceaf43138422c03f5ea9fc64ecb7a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 05:05:00 +0000

    autogenerated overwrites for icmplib==3.0.4

--- collected.nix
+++ collected.nix
@@ -3637,6 +3637,9 @@
   ibm-watson = {
     "8.1.0" = import ./overrides/ibm-watson/8.1.0;
   };
+  icmplib = {
+    "3.0.4" = import ./overrides/icmplib/3.0.4;
+  };
   icoextract = {
     "0.1.5" = import ./overrides/icoextract/0.1.5;
   };
