a2e43e5ebd1d2d4b533672c8cb11aed9ed1b2117
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 18:19:32 +0000

    autogenerated overwrites for azure-mgmt-servicefabric==2.1.0

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-mgmt-servicefabric = {
+    "2.1.0" = import ./overrides/azure-mgmt-servicefabric/2.1.0;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
