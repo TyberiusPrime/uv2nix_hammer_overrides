83d055909598c25f97066ab47a3878b90e3ef034
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:38:18 +0000

    autogenerated overwrites for voluptuous-serialize==2.6.0

--- collected.nix
+++ collected.nix
@@ -9768,6 +9768,9 @@
   volkszaehler = {
     "0.4.0" = import ./overrides/volkszaehler/0.4.0;
   };
+  voluptuous-serialize = {
+    "2.6.0" = import ./overrides/voluptuous-serialize/2.6.0;
+  };
   volvooncall = {
     "0.10.4" = import ./overrides/volvooncall/0.10.4;
   };
