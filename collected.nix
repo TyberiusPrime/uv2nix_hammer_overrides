cabfe8f83ed3c36d1ece0a65f28114833b2efcf8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:50:06 +0000

    autogenerated overwrites for msal-extensions==1.2.0

--- collected.nix
+++ collected.nix
@@ -5001,6 +5001,9 @@
   ms-cv = {
     "0.1.1" = import ./overrides/ms-cv/0.1.1;
   };
+  msal-extensions = {
+    "1.2.0" = import ./overrides/msal-extensions/1.2.0;
+  };
   msgpack = {
     "1.0.2" = import ./overrides/msgpack/1.0.2;
   };
