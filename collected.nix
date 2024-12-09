39cb20e64a1483dde36de1cd09a64d5c5037b4ec
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 20:17:45 +0000

    autogenerated overwrites for dol==0.2.71

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
+  dol = {
+    "0.2.71" = import ./overrides/dol/0.2.71;
+  };
   dopy = {
     "0.3.7" = import ./overrides/dopy/0.3.7;
   };
