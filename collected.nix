5b6bcb8e2b9cf92afef93ca7226d9375a18d5c05
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 06:58:02 +0000

    autogenerated overwrites for pyrate-limiter==3.7.0

--- collected.nix
+++ collected.nix
@@ -7042,6 +7042,9 @@
   pyramid-redis-sessions = {
     "1.0.1" = import ./overrides/pyramid-redis-sessions/1.0.1;
   };
+  pyrate-limiter = {
+    "3.7.0" = import ./overrides/pyrate-limiter/3.7.0;
+  };
   pyreact = {
     "0.6.0" = import ./overrides/pyreact/0.6.0;
   };
