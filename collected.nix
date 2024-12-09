aef0b526558b742a3a910793629f59e5d0970e56
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:52:36 +0000

    autogenerated overwrites for mmh3==4.1.0

--- collected.nix
+++ collected.nix
@@ -4847,6 +4847,9 @@
   mmcv-full = {
     "1.7.2" = import ./overrides/mmcv-full/1.7.2;
   };
+  mmh3 = {
+    "4.1.0" = import ./overrides/mmh3/4.1.0;
+  };
   mmhash3 = {
     "3.0.1" = import ./overrides/mmhash3/3.0.1;
   };
