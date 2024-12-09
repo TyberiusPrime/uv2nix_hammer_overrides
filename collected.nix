b082e8ca7490d8f97e6be5dfa7ef752a47ddef10
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:47:25 +0000

    autogenerated overwrites for pyramid-multiauth==1.0.2

--- collected.nix
+++ collected.nix
@@ -7039,6 +7039,9 @@
   pyramid-deform = {
     "0.2" = import ./overrides/pyramid-deform/0.2;
   };
+  pyramid-multiauth = {
+    "1.0.2" = import ./overrides/pyramid-multiauth/1.0.2;
+  };
   pyramid-redis-sessions = {
     "1.0.1" = import ./overrides/pyramid-redis-sessions/1.0.1;
   };
