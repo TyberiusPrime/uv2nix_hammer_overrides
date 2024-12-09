4ba7f4ac91b6f50ed3cd255b5a00548b1817872b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:42:56 +0000

    autogenerated overwrites for notifications-android-tv==1.2.2

--- collected.nix
+++ collected.nix
@@ -5328,6 +5328,9 @@
   noseapp = {
     "2.2.2" = import ./overrides/noseapp/2.2.2;
   };
+  notifications-android-tv = {
+    "1.2.2" = import ./overrides/notifications-android-tv/1.2.2;
+  };
   notmuch2 = {
     "0.1" = import ./overrides/notmuch2/0.1;
   };
