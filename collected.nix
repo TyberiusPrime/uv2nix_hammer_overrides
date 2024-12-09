0de76cb657930781f7f48204093c1cbceccc9150
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 09:20:33 +0000

    autogenerated overwrites for aiobroadlink==0.1.3

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aiobroadlink = {
+    "0.1.3" = import ./overrides/aiobroadlink/0.1.3;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
