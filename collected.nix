95548d280719406c1640a50875f7e3981bc0cd6a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:16:19 +0000

    autogenerated overwrites for streamlit==1.38.0

--- collected.nix
+++ collected.nix
@@ -8795,6 +8795,9 @@
   streamlabswater = {
     "1.0.1" = import ./overrides/streamlabswater/1.0.1;
   };
+  streamlit = {
+    "1.38.0" = import ./overrides/streamlit/1.38.0;
+  };
   streql = {
     "3.0.2" = import ./overrides/streql/3.0.2;
   };
