b6cfcda5655579554bf52cdb1db5ed14ee2bd56c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:53:04 +0000

    autogenerated overwrites for geos==0.2.3

--- collected.nix
+++ collected.nix
@@ -3137,6 +3137,9 @@
   geometric = {
     "1.0.2" = import ./overrides/geometric/1.0.2;
   };
+  geos = {
+    "0.2.3" = import ./overrides/geos/0.2.3;
+  };
   getch = {
     "1.0" = import ./overrides/getch/1.0;
   };
