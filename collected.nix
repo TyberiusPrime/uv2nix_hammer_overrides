6e4ffce217d120f61abd17eb55672d16a190e9ad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 04:13:41 +0000

    autogenerated overwrites for phe==1.5.0

--- collected.nix
+++ collected.nix
@@ -5860,6 +5860,9 @@
   pgsu = {
     "0.2.4" = import ./overrides/pgsu/0.2.4;
   };
+  phe = {
+    "1.5.0" = import ./overrides/phe/1.5.0;
+  };
   phonetics = {
     "1.0.5" = import ./overrides/phonetics/1.0.5;
   };
