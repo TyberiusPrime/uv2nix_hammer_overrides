7f7a9606fb904206f7a158ac302a14cc93bfdb4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:04:29 +0000

    autogenerated overwrites for argon2-cffi-bindings==21.2.0

--- collected.nix
+++ collected.nix
@@ -349,6 +349,9 @@
   argon2-cffi = {
     "16.3.0" = import ./overrides/argon2-cffi/16.3.0;
   };
+  argon2-cffi-bindings = {
+    "21.2.0" = import ./overrides/argon2-cffi-bindings/21.2.0;
+  };
   argparse-manpage = {
     "4.6" = import ./overrides/argparse-manpage/4.6;
   };
