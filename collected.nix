b057ed07b7c74788b322eb9bb961cf08fdb13ff8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 10:21:20 +0000

    autogenerated overwrites for justpy==0.14.0

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  justpy = {
+    "0.14.0" = import ./overrides/justpy/0.14.0;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
