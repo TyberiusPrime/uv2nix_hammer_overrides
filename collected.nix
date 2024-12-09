7ef50a116c1d483dfa9f9ca1f1c1a07212b6f9f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:44:32 +0000

    autogenerated overwrites for schwimmbad==0.4.2

--- collected.nix
+++ collected.nix
@@ -8192,6 +8192,9 @@
   schiene = {
     "0.26" = import ./overrides/schiene/0.26;
   };
+  schwimmbad = {
+    "0.4.2" = import ./overrides/schwimmbad/0.4.2;
+  };
   sciencedates = {
     "1.5.0" = import ./overrides/sciencedates/1.5.0;
   };
