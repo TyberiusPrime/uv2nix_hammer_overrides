9e46f8479dd3eb1496bb86149903b4eac574278f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 15:08:50 +0000

    autogenerated overwrites for falcon==3.1.3

--- collected.nix
+++ collected.nix
@@ -2664,6 +2664,9 @@
   fairseq = {
     "0.12.2" = import ./overrides/fairseq/0.12.2;
   };
+  falcon = {
+    "3.1.3" = import ./overrides/falcon/3.1.3;
+  };
   falcon-cors = {
     "1.1.7" = import ./overrides/falcon-cors/1.1.7;
   };
