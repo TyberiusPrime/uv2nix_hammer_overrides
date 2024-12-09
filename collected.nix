cc5ec627927e465154f26f03c1161f06490b3025
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 01:10:23 +0000

    autogenerated overwrites for mypy-boto3-lambda==1.35.3

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  mypy-boto3-lambda = {
+    "1.35.3" = import ./overrides/mypy-boto3-lambda/1.35.3;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
