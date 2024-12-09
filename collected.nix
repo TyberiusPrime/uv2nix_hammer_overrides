843130ef6bc87bdea74910302951ab395e975d1e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:40:08 +0000

    autogenerated overwrites for types-freezegun==1.1.10

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-freezegun = {
+    "1.1.10" = import ./overrides/types-freezegun/1.1.10;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
