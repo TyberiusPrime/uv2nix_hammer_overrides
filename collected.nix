9912a54880fe5fdc9b888c88528dcf6c207c071c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 03:35:48 +0000

    autogenerated overwrites for dicom2nifti==2.4.11

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  dicom2nifti = {
+    "2.4.11" = import ./overrides/dicom2nifti/2.4.11;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
