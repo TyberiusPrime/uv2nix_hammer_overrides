b7f252ea373f5b421330c73b466b8b2701db46b7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 02:03:35 +0000

    autogenerated overwrites for cyclonedx-bom==4.5.0

--- collected.nix
+++ collected.nix
@@ -1596,6 +1596,9 @@
   cwlgen = {
     "0.4.2" = import ./overrides/cwlgen/0.4.2;
   };
+  cyclonedx-bom = {
+    "4.5.0" = import ./overrides/cyclonedx-bom/4.5.0;
+  };
   cydets = {
     "1.1" = import ./overrides/cydets/1.1;
   };
