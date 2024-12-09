ccdce2cefa93f419fb3df010ffb8495576eeabf8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 13:07:17 +0000

    autogenerated overwrites for vega==4.0.0

--- collected.nix
+++ collected.nix
@@ -9675,6 +9675,9 @@
   vedo = {
     "2024.5.2" = import ./overrides/vedo/2024.5.2;
   };
+  vega = {
+    "4.0.0" = import ./overrides/vega/4.0.0;
+  };
   venn = {
     "0.1.3" = import ./overrides/venn/0.1.3;
   };
