53dbe0692041389f3385fe5ada8fd7fbd2ea0f0d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 07:29:09 +0000

    autogenerated overwrites for irc3==1.1.10

--- collected.nix
+++ collected.nix
@@ -3856,6 +3856,9 @@
   ipyxact = {
     "0.3.2" = import ./overrides/ipyxact/0.3.2;
   };
+  irc3 = {
+    "1.1.10" = import ./overrides/irc3/1.1.10;
+  };
   iso-639 = {
     "0.4.5" = import ./overrides/iso-639/0.4.5;
   };
