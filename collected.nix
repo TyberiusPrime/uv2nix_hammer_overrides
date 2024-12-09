c2d33ef68ff64a7c66e9fc4bd5080c24d65e8535
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 17:17:46 +0000

    autogenerated overwrites for dvc-gs==3.0.1

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dvc-gs = {
+    "3.0.1" = import ./overrides/dvc-gs/3.0.1;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
