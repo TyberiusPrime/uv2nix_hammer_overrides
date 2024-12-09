a8e61610a560979c0199b3432e6abef8097a7927
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-09 19:06:31 +0000

    autogenerated overwrites for imgkit==1.2.3

--- collected.nix
+++ collected.nix
@@ -3688,6 +3688,9 @@
   imgcat = {
     "0.5.0" = import ./overrides/imgcat/0.5.0;
   };
+  imgkit = {
+    "1.2.3" = import ./overrides/imgkit/1.2.3;
+  };
   imgviz = {
     "1.7.5" = import ./overrides/imgviz/1.7.5;
   };
