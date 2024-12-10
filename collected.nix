9e823cecde9631c2a42384ca1f169161fcdd9c9c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 17:01:54 +0000

    autogenerated overwrites for minichain==0.3.3

--- collected.nix
+++ collected.nix
@@ -4808,6 +4808,9 @@
   minepy = {
     "1.2.6" = import ./overrides/minepy/1.2.6;
   };
+  minichain = {
+    "0.3.3" = import ./overrides/minichain/0.3.3;
+  };
   minidb = {
     "2.0.8" = import ./overrides/minidb/2.0.8;
   };
