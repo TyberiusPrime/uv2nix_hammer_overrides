4aad540d7e12dc0b9001613adcc0fc838430d2fb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:48:51 +0000

    autogenerated overwrites for vprof==0.38

--- collected.nix
+++ collected.nix
@@ -9781,6 +9781,9 @@
   voxcell = {
     "3.1.9" = import ./overrides/voxcell/3.1.9;
   };
+  vprof = {
+    "0.38" = import ./overrides/vprof/0.38;
+  };
   vsts-cd-manager = {
     "1.0.2" = import ./overrides/vsts-cd-manager/1.0.2;
   };
