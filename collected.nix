d2dfe5023896b834ae44f6ecd7b8955be7f9c317
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:13:23 +0000

    autogenerated overwrites for fastapi-slim==0.114.0

--- collected.nix
+++ collected.nix
@@ -2679,6 +2679,9 @@
   fastapi-cli = {
     "0.0.5" = import ./overrides/fastapi-cli/0.0.5;
   };
+  fastapi-slim = {
+    "0.114.0" = import ./overrides/fastapi-slim/0.114.0;
+  };
   fastcache = {
     "1.1.0" = import ./overrides/fastcache/1.1.0;
   };
