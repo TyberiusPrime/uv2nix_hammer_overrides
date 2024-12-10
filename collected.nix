f3452e31c14cd287de461630aadf700d0e93efbf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:09:28 +0000

    autogenerated overwrites for ld2410-ble==0.2.0

--- collected.nix
+++ collected.nix
@@ -4290,6 +4290,9 @@
   lcogt-logging = {
     "0.3.2" = import ./overrides/lcogt-logging/0.3.2;
   };
+  ld2410-ble = {
+    "0.2.0" = import ./overrides/ld2410-ble/0.2.0;
+  };
   lepl = {
     "5.1.3" = import ./overrides/lepl/5.1.3;
   };
