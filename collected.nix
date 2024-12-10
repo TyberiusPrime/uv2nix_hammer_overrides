59f46d07d58a21fe6716182a246d5e65fbd13090
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:01:55 +0000

    autogenerated overwrites for adafruit-circuitpython-esp32spi==8.4.0

--- collected.nix
+++ collected.nix
@@ -35,6 +35,9 @@
   active-alchemy = {
     "1.1.0" = import ./overrides/active-alchemy/1.1.0;
   };
+  adafruit-circuitpython-esp32spi = {
+    "8.4.0" = import ./overrides/adafruit-circuitpython-esp32spi/8.4.0;
+  };
   adafruit-extended-bus = {
     "1.0.2" = import ./overrides/adafruit-extended-bus/1.0.2;
   };
