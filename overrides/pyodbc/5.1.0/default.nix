9454b094a8eec6a71b43c4c8666347d8fb52f9b1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 19:07:27 +0000

    autogenerated overwrites for pyodbc==5.1.0

--- overrides/pyodbc/5.1.0/default.nix
+++ overrides/pyodbc/5.1.0/default.nix
@@ -1,1 +1,3 @@
-{ pkgs, ... }: old: { buildInputs = old.buildInputs or [ ] ++ [ pkgs.unixODBC ]; }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.unixODBC];} else {buildInputs = old.buildInputs or [] ++ [pkgs.unixODBC];nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+        
