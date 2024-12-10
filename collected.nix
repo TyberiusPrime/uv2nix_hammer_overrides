0aa495543278de278ed4a6d8d312b3967b68c946
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:30:24 +0000

    autogenerated overwrites for symspellpy==6.7.8

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  symspellpy = {
+    "6.7.8" = import ./overrides/symspellpy/6.7.8;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
