0ae487f4409a8ecebadac45b2d21b7282317a7d0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 15:12:00 +0000

    autogenerated overwrites for exoplanet==0.6.0

--- collected.nix
+++ collected.nix
@@ -2625,6 +2625,9 @@
   execo = {
     "2.8" = import ./overrides/execo/2.8;
   };
+  exoplanet = {
+    "0.6.0" = import ./overrides/exoplanet/0.6.0;
+  };
   expects = {
     "0.9.0" = import ./overrides/expects/0.9.0;
   };
