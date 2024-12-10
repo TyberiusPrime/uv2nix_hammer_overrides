baa5f37acee6ba95a45c88d8c011b2030a83003c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:33:34 +0000

    autogenerated overwrites for jupyter-ui-poll==1.0.0

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyter-ui-poll = {
+    "1.0.0" = import ./overrides/jupyter-ui-poll/1.0.0;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
