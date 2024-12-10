20d5a1c4b48411249cac05ea766d072021e9e539
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:27:39 +0000

    autogenerated overwrites for edx-rest-api-client==6.0.0

--- collected.nix
+++ collected.nix
@@ -2472,6 +2472,9 @@
   editorium = {
     "0.0.0.30" = import ./overrides/editorium/0.0.0.30;
   };
+  edx-rest-api-client = {
+    "6.0.0" = import ./overrides/edx-rest-api-client/6.0.0;
+  };
   ee-extra = {
     "0.0.15" = import ./overrides/ee-extra/0.0.15;
   };
