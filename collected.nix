e14ea49da58c969d2605c8007f12edc52d345386
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:38:28 +0000

    autogenerated overwrites for google-cloud-scheduler==2.13.5

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-scheduler = {
+    "2.13.5" = import ./overrides/google-cloud-scheduler/2.13.5;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
