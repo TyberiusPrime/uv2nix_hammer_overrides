de346a96f83090ed618091c7fbc8897affbf49cd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:15:55 +0000

    autogenerated overwrites for googlesearch-python==1.2.5

--- collected.nix
+++ collected.nix
@@ -3264,6 +3264,9 @@
   googlemaps = {
     "4.10.0" = import ./overrides/googlemaps/4.10.0;
   };
+  googlesearch-python = {
+    "1.2.5" = import ./overrides/googlesearch-python/1.2.5;
+  };
   googletrans = {
     "3.0.0" = import ./overrides/googletrans/3.0.0;
   };
