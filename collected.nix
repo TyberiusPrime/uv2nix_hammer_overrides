8dabf9dc29742ee705902907529a92547399ba10
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:42:44 +0000

    autogenerated overwrites for pydyf==0.11.0

--- collected.nix
+++ collected.nix
@@ -6578,6 +6578,9 @@
   pyds9 = {
     "1.8.1" = import ./overrides/pyds9/1.8.1;
   };
+  pydyf = {
+    "0.11.0" = import ./overrides/pydyf/0.11.0;
+  };
   pyeapi = {
     "1.0.4" = import ./overrides/pyeapi/1.0.4;
   };
