e8fbdfbf21ba484cbc62eb1c5ed62c82e32fd41f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:43:53 +0000

    autogenerated overwrites for galpy==1.10.0

--- collected.nix
+++ collected.nix
@@ -3071,6 +3071,9 @@
   gaegraph = {
     "3.9" = import ./overrides/gaegraph/3.9;
   };
+  galpy = {
+    "1.10.0" = import ./overrides/galpy/1.10.0;
+  };
   gammapy = {
     "1.2" = import ./overrides/gammapy/1.2;
   };
