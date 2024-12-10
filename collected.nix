119829f7cb3dd1c3a32dab305dc109d483133bfe
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:32:00 +0000

    autogenerated overwrites for lazy-property==0.0.1

--- collected.nix
+++ collected.nix
@@ -4269,6 +4269,9 @@
   lazy-import = {
     "0.2.2" = import ./overrides/lazy-import/0.2.2;
   };
+  lazy-property = {
+    "0.0.1" = import ./overrides/lazy-property/0.0.1;
+  };
   lazy-string = {
     "1.0.0" = import ./overrides/lazy-string/1.0.0;
   };
