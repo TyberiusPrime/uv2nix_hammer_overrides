c30a393d47283c2c6e43aa201dd94b25eddd2ddf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:31:24 +0000

    autogenerated overwrites for guidata==3.6.3

--- collected.nix
+++ collected.nix
@@ -3359,6 +3359,9 @@
   gtts-token = {
     "1.1.4" = import ./overrides/gtts-token/1.1.4;
   };
+  guidata = {
+    "3.6.3" = import ./overrides/guidata/3.6.3;
+  };
   gym = {
     "0.26.2" = import ./overrides/gym/0.26.2;
   };
