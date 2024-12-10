5b059ca0ad8cceebe82fbddf0efb85fca19f3838
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 15:34:31 +0000

    autogenerated overwrites for flake8-expression-complexity==0.0.11

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-expression-complexity = {
+    "0.0.11" = import ./overrides/flake8-expression-complexity/0.0.11;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
