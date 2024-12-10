89ac91b6c27a1de72f16c39d60e643dc42b64e0c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:11:40 +0000

    autogenerated overwrites for spacy-pkuseg==1.0.0

--- collected.nix
+++ collected.nix
@@ -8589,6 +8589,9 @@
   sox = {
     "1.5.0" = import ./overrides/sox/1.5.0;
   };
+  spacy-pkuseg = {
+    "1.0.0" = import ./overrides/spacy-pkuseg/1.0.0;
+  };
   spanners = {
     "1.20" = import ./overrides/spanners/1.20;
   };
