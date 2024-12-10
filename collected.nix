c5df0fd00e0a482011374daeb3ac78594a685b01
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:18:51 +0000

    autogenerated overwrites for awscli==1.34.16

--- collected.nix
+++ collected.nix
@@ -527,6 +527,9 @@
   aws-psycopg2 = {
     "1.3.8" = import ./overrides/aws-psycopg2/1.3.8;
   };
+  awscli = {
+    "1.34.16" = import ./overrides/awscli/1.34.16;
+  };
   awscli-cwlogs = {
     "1.4.6" = import ./overrides/awscli-cwlogs/1.4.6;
   };
