d1316def84f29d6756e0a654a7f8280c4dfe9bb0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:37:33 +0000

    autogenerated overwrites for pyephember==0.4.0

--- collected.nix
+++ collected.nix
@@ -6599,6 +6599,9 @@
   pyephem = {
     "9.99" = import ./overrides/pyephem/9.99;
   };
+  pyephember = {
+    "0.4.0" = import ./overrides/pyephember/0.4.0;
+  };
   pyes = {
     "0.99.6" = import ./overrides/pyes/0.99.6;
   };
