cda0441907e86ebc6da0a7fbc8175e2b6fbfb96c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:53:50 +0000

    autogenerated overwrites for pyemvue==0.18.6

--- collected.nix
+++ collected.nix
@@ -6596,6 +6596,9 @@
   pyelliptic = {
     "1.5.8" = import ./overrides/pyelliptic/1.5.8;
   };
+  pyemvue = {
+    "0.18.6" = import ./overrides/pyemvue/0.18.6;
+  };
   pyephem = {
     "9.99" = import ./overrides/pyephem/9.99;
   };
