f84f2a99c6cad92d6920743005e0d777390c8469
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:55:12 +0000

    autogenerated overwrites for cwcwidth==0.1.9

--- collected.nix
+++ collected.nix
@@ -1593,6 +1593,9 @@
   cvzone = {
     "1.6.1" = import ./overrides/cvzone/1.6.1;
   };
+  cwcwidth = {
+    "0.1.9" = import ./overrides/cwcwidth/0.1.9;
+  };
   cwlgen = {
     "0.4.2" = import ./overrides/cwlgen/0.4.2;
   };
