93542872d450511101ba0e5812e8dfe851350217
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:12:56 +0000

    autogenerated overwrites for pydiscourse==1.7.0

--- collected.nix
+++ collected.nix
@@ -6563,6 +6563,9 @@
   pydevd-pycharm = {
     "242.21829.153" = import ./overrides/pydevd-pycharm/242.21829.153;
   };
+  pydiscourse = {
+    "1.7.0" = import ./overrides/pydiscourse/1.7.0;
+  };
   pydoe = {
     "0.3.8" = import ./overrides/pydoe/0.3.8;
   };
