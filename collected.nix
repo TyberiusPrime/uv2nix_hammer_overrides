388d7fb38a2846eaddc4c4da423bb6323907cce8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:55:06 +0000

    autogenerated overwrites for aliyun-python-sdk-dbfs==2.0.7

--- collected.nix
+++ collected.nix
@@ -187,6 +187,9 @@
   aliyun-python-sdk-core-v3 = {
     "2.13.33" = import ./overrides/aliyun-python-sdk-core-v3/2.13.33;
   };
+  aliyun-python-sdk-dbfs = {
+    "2.0.7" = import ./overrides/aliyun-python-sdk-dbfs/2.0.7;
+  };
   allset = {
     "1.0.1" = import ./overrides/allset/1.0.1;
   };
