5ddfb55b060a234d9ae83583e05fe42b50837c5c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:47:54 +0000

    autogenerated overwrites for consolekit==1.7.1

--- collected.nix
+++ collected.nix
@@ -1379,6 +1379,9 @@
   cons = {
     "0.4.6" = import ./overrides/cons/0.4.6;
   };
+  consolekit = {
+    "1.7.1" = import ./overrides/consolekit/1.7.1;
+  };
   consolemsg = {
     "0.3.3" = import ./overrides/consolemsg/0.3.3;
   };
