d2298d2bdafd8d8fc29fba643552ab36c091dfc8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:35:43 +0000

    autogenerated overwrites for scrapy-splash==0.9.0

--- collected.nix
+++ collected.nix
@@ -8242,6 +8242,9 @@
   scrapeasy = {
     "0.12" = import ./overrides/scrapeasy/0.12;
   };
+  scrapy-splash = {
+    "0.9.0" = import ./overrides/scrapy-splash/0.9.0;
+  };
   screed = {
     "1.1.3" = import ./overrides/screed/1.1.3;
   };
