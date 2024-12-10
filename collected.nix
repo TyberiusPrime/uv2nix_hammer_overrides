0d4d44379fcc483d63596a0a89e25631b4fddd9d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:46:28 +0000

    autogenerated overwrites for types-orjson==3.6.2

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-orjson = {
+    "3.6.2" = import ./overrides/types-orjson/3.6.2;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
