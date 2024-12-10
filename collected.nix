ec72f3ef2e233505a6f28ac2aa92778df3aa4148
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 19:02:38 +0000

    autogenerated overwrites for copulas==0.11.1

--- collected.nix
+++ collected.nix
@@ -1403,6 +1403,9 @@
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
+  copulas = {
+    "0.11.1" = import ./overrides/copulas/0.11.1;
+  };
   coqui-trainer = {
     "0.0.5" = import ./overrides/coqui-trainer/0.0.5;
   };
