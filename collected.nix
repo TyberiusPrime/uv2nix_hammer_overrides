ad8207d37909c6c5c68a872446479c89fb53890f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:44:37 +0000

    autogenerated overwrites for pytest-arraydiff==0.6.1

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-arraydiff = {
+    "0.6.1" = import ./overrides/pytest-arraydiff/0.6.1;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
