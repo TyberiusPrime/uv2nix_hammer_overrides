cb1ba3f64e92b66aa386cd23fef3ea86668bde8c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:58:38 +0000

    autogenerated overwrites for sphinxcontrib-youtube==1.4.1

--- collected.nix
+++ collected.nix
@@ -8643,6 +8643,9 @@
   sphinxcontrib-tikz = {
     "0.4.20" = import ./overrides/sphinxcontrib-tikz/0.4.20;
   };
+  sphinxcontrib-youtube = {
+    "1.4.1" = import ./overrides/sphinxcontrib-youtube/1.4.1;
+  };
   sphinxjp-themecore = {
     "0.2.0" = import ./overrides/sphinxjp-themecore/0.2.0;
   };
