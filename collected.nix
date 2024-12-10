32057a39dfb729a6c82c0b6389816751cb0d81f6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:23:58 +0000

    autogenerated overwrites for freetype-py==2.5.1

--- collected.nix
+++ collected.nix
@@ -2980,6 +2980,9 @@
   freesasa = {
     "2.2.1" = import ./overrides/freesasa/2.2.1;
   };
+  freetype-py = {
+    "2.5.1" = import ./overrides/freetype-py/2.5.1;
+  };
   frida-tools = {
     "12.5.1" = import ./overrides/frida-tools/12.5.1;
   };
