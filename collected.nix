e3a47b4b3ca561ca8add33a87da292534e4d53cc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:06:42 +0000

    autogenerated overwrites for parsl==2024.9.9

--- collected.nix
+++ collected.nix
@@ -5726,6 +5726,9 @@
   parsimonious = {
     "0.9.0" = import ./overrides/parsimonious/0.9.0;
   };
+  parsl = {
+    "2024.9.9" = import ./overrides/parsl/2024.9.9;
+  };
   patch = {
     "1.16" = import ./overrides/patch/1.16;
   };
