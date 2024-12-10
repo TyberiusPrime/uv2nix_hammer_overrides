37f4a97ae62f6ea5a648e6ca03a631b6da73da5b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:05:16 +0000

    autogenerated overwrites for types-aiobotocore-s3outposts==2.15.0

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-s3outposts = {
+    "2.15.0" = import ./overrides/types-aiobotocore-s3outposts/2.15.0;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
