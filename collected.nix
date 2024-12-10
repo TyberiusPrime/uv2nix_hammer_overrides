a3d5c6a4ec4cb78777a1588efb428b479b442d97
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:19:39 +0000

    autogenerated overwrites for pytest-httpx==0.30.0

--- collected.nix
+++ collected.nix
@@ -7224,6 +7224,9 @@
   pytest-expect = {
     "1.1.0" = import ./overrides/pytest-expect/1.1.0;
   };
+  pytest-httpx = {
+    "0.30.0" = import ./overrides/pytest-httpx/0.30.0;
+  };
   pytest-json = {
     "0.4.0" = import ./overrides/pytest-json/0.4.0;
   };
