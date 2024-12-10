c9acb61ccac2df5b41c89914872e12c586d1219f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:56:15 +0000

    autogenerated overwrites for shellcheck-py==0.10.0.1

--- collected.nix
+++ collected.nix
@@ -8339,6 +8339,9 @@
   sharedmem = {
     "0.3.8" = import ./overrides/sharedmem/0.3.8;
   };
+  shellcheck-py = {
+    "0.10.0.1" = import ./overrides/shellcheck-py/0.10.0.1;
+  };
   shinkenplugins = {
     "0.4.2" = import ./overrides/shinkenplugins/0.4.2;
   };
