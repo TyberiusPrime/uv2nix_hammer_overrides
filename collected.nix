acb2c1255a54add6b1e717815cadab139e871c6e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:43:58 +0000

    autogenerated overwrites for panacotta==0.2

--- collected.nix
+++ collected.nix
@@ -5666,6 +5666,9 @@
   pairtree = {
     "0.8.1" = import ./overrides/pairtree/0.8.1;
   };
+  panacotta = {
+    "0.2" = import ./overrides/panacotta/0.2;
+  };
   panda3d = {
     "1.10.15" = import ./overrides/panda3d/1.10.15;
   };
