4568affc377eae5e55367921f09925e6abe598f3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:34:30 +0000

    autogenerated overwrites for semver==3.0.2

--- collected.nix
+++ collected.nix
@@ -8275,6 +8275,9 @@
   semgrep = {
     "1.68.0" = import ./overrides/semgrep/1.68.0;
   };
+  semver = {
+    "3.0.2" = import ./overrides/semver/3.0.2;
+  };
   sentinels = {
     "1.0.0" = import ./overrides/sentinels/1.0.0;
   };
