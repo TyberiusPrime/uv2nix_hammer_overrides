501f7428dd4c294d5ceb3776459046956cd26734
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:30:32 +0000

    autogenerated overwrites for taxii2-client==2.3.0

--- collected.nix
+++ collected.nix
@@ -8900,6 +8900,9 @@
   tasklib = {
     "2.5.1" = import ./overrides/tasklib/2.5.1;
   };
+  taxii2-client = {
+    "2.3.0" = import ./overrides/taxii2-client/2.3.0;
+  };
   tba-api-v3client = {
     "3.8.0" = import ./overrides/tba-api-v3client/3.8.0;
   };
