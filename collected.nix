d1ad2d2787fe3bf24252fe3d9b2f8f4dd9e8d949
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:31:34 +0000

    autogenerated overwrites for sahi==0.11.18

--- collected.nix
+++ collected.nix
@@ -8108,6 +8108,9 @@
   safeio = {
     "1.2" = import ./overrides/safeio/1.2;
   };
+  sahi = {
+    "0.11.18" = import ./overrides/sahi/0.11.18;
+  };
   salsa20 = {
     "0.3.0" = import ./overrides/salsa20/0.3.0;
   };
