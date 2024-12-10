f4dce619e7b0453c6028ecf2d24c352c550c89ee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:11:55 +0000

    autogenerated overwrites for qiskit-nature==0.7.2

--- overrides/qiskit-nature/0.7.2/default.nix
+++ overrides/qiskit-nature/0.7.2/default.nix
@@ -1,23 +1,3 @@
-{ helpers, pkgs, ... }:
-old:
-if (helpers.isWheel old) then
-  {
-    buildInputs = old.buildInputs or [ ] ++ [
-      pkgs.gfortran13
-      pkgs.gfortran13.cc
-      pkgs.gfortran13.out
-    ];
-  }
-else
-  {
-    buildInputs = old.buildInputs or [ ] ++ [
-      pkgs.gfortran13
-      pkgs.gfortran13.cc
-      pkgs.gfortran13.out
-    ];
-    nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
-      pkgs.gfortran13.cc
-      pkgs.gfortran13.cc.lib
-      pkgs.gfortran13.out
-    ];
-  }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.gfortran13 pkgs.gfortran13.cc pkgs.gfortran13.out];} else {buildInputs = old.buildInputs or [] ++ [pkgs.gfortran13 pkgs.gfortran13.cc pkgs.gfortran13.out];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.gfortran13.cc pkgs.gfortran13.cc.lib pkgs.gfortran13.out] ++ ( resolveBuildSystem {setuptools = [];});}
+        
