9ce524168c19fda5aa0bbced89196b3622edd752
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 17:27:44 +0000

    autogenerated overwrites for pycangjie==0.0.10

--- collected.nix
+++ collected.nix
@@ -6444,6 +6444,9 @@
   pycairo = {
     "1.27.0" = import ./overrides/pycairo/1.27.0;
   };
+  pycangjie = {
+    "0.0.10" = import ./overrides/pycangjie/0.0.10;
+  };
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
