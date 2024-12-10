a3f7add36eb4c3c7e423847fc9d34c1a0116f9fc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:38:50 +0000

    autogenerated overwrites for pype-base==0.3.0

--- collected.nix
+++ collected.nix
@@ -6967,6 +6967,9 @@
   pypdftk = {
     "0.5" = import ./overrides/pypdftk/0.5;
   };
+  pype-base = {
+    "0.3.0" = import ./overrides/pype-base/0.3.0;
+  };
   pypeg2 = {
     "2.15.2" = import ./overrides/pypeg2/2.15.2;
   };
