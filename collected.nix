f56bfc03adf5a95dee743f28c8910952e198ec35
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:44:46 +0000

    autogenerated overwrites for utwist==0.1.3

--- collected.nix
+++ collected.nix
@@ -9621,6 +9621,9 @@
   utm = {
     "0.7.0" = import ./overrides/utm/0.7.0;
   };
+  utwist = {
+    "0.1.3" = import ./overrides/utwist/0.1.3;
+  };
   uuid = {
     "1.30" = import ./overrides/uuid/1.30;
   };
