9d033355433fd4bf1a981523f4f4e5e6b593eea0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:07:44 +0000

    autogenerated overwrites for epson-projector==0.5.1

--- collected.nix
+++ collected.nix
@@ -2550,6 +2550,9 @@
   episcanpy = {
     "0.4.0" = import ./overrides/episcanpy/0.4.0;
   };
+  epson-projector = {
+    "0.5.1" = import ./overrides/epson-projector/0.5.1;
+  };
   epydoc = {
     "3.0.1" = import ./overrides/epydoc/3.0.1;
   };
