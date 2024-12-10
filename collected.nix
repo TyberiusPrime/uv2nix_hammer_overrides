2dc7e632fe7785c9ce472c7449f0009a2cb0e2e1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:56:53 +0000

    autogenerated overwrites for oauthenticator==17.0.0

--- collected.nix
+++ collected.nix
@@ -5420,6 +5420,9 @@
   oauth2client = {
     "3.0.0" = import ./overrides/oauth2client/3.0.0;
   };
+  oauthenticator = {
+    "17.0.0" = import ./overrides/oauthenticator/17.0.0;
+  };
   obd = {
     "0.7.2" = import ./overrides/obd/0.7.2;
   };
