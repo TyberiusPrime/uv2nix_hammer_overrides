deea3ff0eb0a595b7edd159a29c39b3a3eb93911
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:19:41 +0000

    autogenerated overwrites for djangoajax==3.3

--- collected.nix
+++ collected.nix
@@ -2222,6 +2222,9 @@
   django-vkontakte-users = {
     "0.8.7" = import ./overrides/django-vkontakte-users/0.8.7;
   };
+  djangoajax = {
+    "3.3" = import ./overrides/djangoajax/3.3;
+  };
   djangoapidec = {
     "1.8" = import ./overrides/djangoapidec/1.8;
   };
