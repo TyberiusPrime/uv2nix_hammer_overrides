971fccbf8cd7468d493d61387bcd85a2e68abc45
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:22:31 +0000

    autogenerated overwrites for observable==1.0.3

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  observable = {
+    "1.0.3" = import ./overrides/observable/1.0.3;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
