96a4a86eb6e8e539c1a17c0ed0bb4c53be90c54f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 15:15:44 +0000

    autogenerated overwrites for buienradar==1.0.6

--- collected.nix
+++ collected.nix
@@ -861,6 +861,9 @@
   btrfs = {
     "14.1" = import ./overrides/btrfs/14.1;
   };
+  buienradar = {
+    "1.0.6" = import ./overrides/buienradar/1.0.6;
+  };
   busrt = {
     "0.1.2" = import ./overrides/busrt/0.1.2;
   };
