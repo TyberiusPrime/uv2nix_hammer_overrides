9b55b7e1e8ecf26bf9ce9d5fcd8e86c7bb277806
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:32:06 +0000

    autogenerated overwrites for invenio-jsonschemas==1.1.4

--- collected.nix
+++ collected.nix
@@ -3808,6 +3808,9 @@
   invenio-github = {
     "1.5.2" = import ./overrides/invenio-github/1.5.2;
   };
+  invenio-jsonschemas = {
+    "1.1.4" = import ./overrides/invenio-jsonschemas/1.1.4;
+  };
   invenio-rdm-records = {
     "12.1.0" = import ./overrides/invenio-rdm-records/12.1.0;
   };
