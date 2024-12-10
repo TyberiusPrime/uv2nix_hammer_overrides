e1c6a565165fb9dc9e44fe3b5bb48a04234f5996
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:36:05 +0000

    autogenerated overwrites for sphinxext-opengraph==0.9.1

--- collected.nix
+++ collected.nix
@@ -8643,6 +8643,9 @@
   sphinxcontrib-tikz = {
     "0.4.20" = import ./overrides/sphinxcontrib-tikz/0.4.20;
   };
+  sphinxext-opengraph = {
+    "0.9.1" = import ./overrides/sphinxext-opengraph/0.9.1;
+  };
   sphinxjp-themecore = {
     "0.2.0" = import ./overrides/sphinxjp-themecore/0.2.0;
   };
