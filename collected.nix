b4387e54cd4745868ae1c46db591abe04f3a1521
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:36:45 +0000

    autogenerated overwrites for django-simplemde==0.1.4

--- collected.nix
+++ collected.nix
@@ -2141,6 +2141,9 @@
   django-shortuuidfield = {
     "0.1.3" = import ./overrides/django-shortuuidfield/0.1.3;
   };
+  django-simplemde = {
+    "0.1.4" = import ./overrides/django-simplemde/0.1.4;
+  };
   django-sites = {
     "0.11" = import ./overrides/django-sites/0.11;
   };
