5142455c06bc101bc6a2baf64391eb9aab69aa1e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 15:19:50 +0000

    autogenerated overwrites for treelog==1.0

--- collected.nix
+++ collected.nix
@@ -9311,6 +9311,9 @@
   tree-format = {
     "0.1.2" = import ./overrides/tree-format/0.1.2;
   };
+  treelog = {
+    "1.0" = import ./overrides/treelog/1.0;
+  };
   trello = {
     "0.9.7.3" = import ./overrides/trello/0.9.7.3;
   };
