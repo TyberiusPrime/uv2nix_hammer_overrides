afa2b0c950da184c94f4a1840794c6a08d78923b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:14:16 +0000

    autogenerated overwrites for orange-widget-base==4.24.0

--- collected.nix
+++ collected.nix
@@ -5587,6 +5587,9 @@
   opuslib = {
     "3.0.1" = import ./overrides/opuslib/3.0.1;
   };
+  orange-widget-base = {
+    "4.24.0" = import ./overrides/orange-widget-base/4.24.0;
+  };
   orcid = {
     "1.0.3" = import ./overrides/orcid/1.0.3;
   };
