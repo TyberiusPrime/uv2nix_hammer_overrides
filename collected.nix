2a9e1aa26c048c7db78957b96a969d72741ba4d4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:44:34 +0000

    autogenerated overwrites for flake8-mock==0.4

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-mock = {
+    "0.4" = import ./overrides/flake8-mock/0.4;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
