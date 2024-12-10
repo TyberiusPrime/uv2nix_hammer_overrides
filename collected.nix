5b3b1a681fb4b41fe793e5ec00af3e98cab07934
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 17:03:31 +0000

    autogenerated overwrites for gearbox==0.2.2

--- collected.nix
+++ collected.nix
@@ -3110,6 +3110,9 @@
   gdspy = {
     "1.6.13" = import ./overrides/gdspy/1.6.13;
   };
+  gearbox = {
+    "0.2.2" = import ./overrides/gearbox/0.2.2;
+  };
   genpy = {
     "2022.1" = import ./overrides/genpy/2022.1;
   };
