c645658bbf6155c56ac9e48003995a890f0abb4c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 17:07:15 +0000

    autogenerated overwrites for types-aiobotocore-codestar==2.13.3

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-codestar = {
+    "2.13.3" = import ./overrides/types-aiobotocore-codestar/2.13.3;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
