b3ea94b19e9f2a4c5639a59ca5924efd6f193455
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:30:37 +0000

    autogenerated overwrites for dialogflow==2.0.0

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  dialogflow = {
+    "2.0.0" = import ./overrides/dialogflow/2.0.0;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
