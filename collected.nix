e042ac17194cd9a117fa64756d49a48ee9cac144
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:05:25 +0000

    autogenerated overwrites for safe-eth-py==5.8.0

--- collected.nix
+++ collected.nix
@@ -8102,6 +8102,9 @@
   sacremoses = {
     "0.0.53" = import ./overrides/sacremoses/0.0.53;
   };
+  safe-eth-py = {
+    "5.8.0" = import ./overrides/safe-eth-py/5.8.0;
+  };
   safe-pysha3 = {
     "1.0.4" = import ./overrides/safe-pysha3/1.0.4;
   };
