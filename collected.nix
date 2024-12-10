096aab6d18e3debfb80ac3eef886f56803621c57
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 18:25:24 +0000

    autogenerated overwrites for dvc-data==3.16.5

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dvc-data = {
+    "3.16.5" = import ./overrides/dvc-data/3.16.5;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
