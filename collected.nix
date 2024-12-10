5cf8fcd58d126956b686e0bb1afe661abd8b9116
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:48:40 +0000

    autogenerated overwrites for sphinx-testing==1.0.1

--- collected.nix
+++ collected.nix
@@ -8616,6 +8616,9 @@
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
+  sphinx-testing = {
+    "1.0.1" = import ./overrides/sphinx-testing/1.0.1;
+  };
   sphinxcontrib-bayesnet = {
     "0.4" = import ./overrides/sphinxcontrib-bayesnet/0.4;
   };
