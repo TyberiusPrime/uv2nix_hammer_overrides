590f2368b9d531cbeb618d61c12d520a5d30c0c9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 12:59:32 +0000

    autogenerated overwrites for google-auth-oauthlib==1.2.1

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-auth-oauthlib = {
+    "1.2.1" = import ./overrides/google-auth-oauthlib/1.2.1;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
