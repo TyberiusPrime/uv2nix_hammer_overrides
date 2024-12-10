2b612ba0ca1a214e06a0a6bd48e6816812ba85ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:52:42 +0000

    autogenerated overwrites for urlnormalizer==1.2.5

--- collected.nix
+++ collected.nix
@@ -9576,6 +9576,9 @@
   urlnorm = {
     "1.1.4" = import ./overrides/urlnorm/1.1.4;
   };
+  urlnormalizer = {
+    "1.2.5" = import ./overrides/urlnormalizer/1.2.5;
+  };
   urlobject = {
     "2.4.3" = import ./overrides/urlobject/2.4.3;
   };
