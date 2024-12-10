69b0afc680ac9c2f4cdf9169459f2336428cc604
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 11:48:25 +0000

    autogenerated overwrites for sbol3==1.1

--- collected.nix
+++ collected.nix
@@ -8165,6 +8165,9 @@
   satpy = {
     "0.51.0" = import ./overrides/satpy/0.51.0;
   };
+  sbol3 = {
+    "1.1" = import ./overrides/sbol3/1.1;
+  };
   sbtab = {
     "1.0.8" = import ./overrides/sbtab/1.0.8;
   };
