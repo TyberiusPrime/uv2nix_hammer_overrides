b4236e6de04698a12855330c45e452e3a2c76041
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 16:08:12 +0000

    autogenerated overwrites for pytest-aioresponses==0.2.0

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-aioresponses = {
+    "0.2.0" = import ./overrides/pytest-aioresponses/0.2.0;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
