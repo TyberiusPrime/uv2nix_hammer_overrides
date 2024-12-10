39b71a14714e45a05a49cf2220f36c5343b20b39
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:14:43 +0000

    autogenerated overwrites for aiohttp-isal==0.3.2

--- collected.nix
+++ collected.nix
@@ -105,6 +105,9 @@
   aiohttp-devtools = {
     "1.1.2" = import ./overrides/aiohttp-devtools/1.1.2;
   };
+  aiohttp-isal = {
+    "0.3.2" = import ./overrides/aiohttp-isal/0.3.2;
+  };
   aiohttp-route-decorator = {
     "0.1.4" = import ./overrides/aiohttp-route-decorator/0.1.4;
   };
