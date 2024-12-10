c25cb279e421ccbc9c224e6f8d0ba8584d40288d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 14:51:13 +0000

    autogenerated overwrites for genanki==0.13.1

--- collected.nix
+++ collected.nix
@@ -3110,6 +3110,9 @@
   gdspy = {
     "1.6.13" = import ./overrides/gdspy/1.6.13;
   };
+  genanki = {
+    "0.13.1" = import ./overrides/genanki/0.13.1;
+  };
   genpy = {
     "2022.1" = import ./overrides/genpy/2022.1;
   };
