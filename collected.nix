5f173ec6647b9684bf6f93c41818ecd98a78b23d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 13:28:04 +0000

    autogenerated overwrites for z3c.json==0.5.5

--- collected.nix
+++ collected.nix
@@ -10106,6 +10106,9 @@
   z3 = {
     "0.2.0" = import ./overrides/z3/0.2.0;
   };
+  z3c-json = {
+    "0.5.5" = import ./overrides/z3c-json/0.5.5;
+  };
   z3c-language-switch = {
     "1.1.0" = import ./overrides/z3c-language-switch/1.1.0;
   };
