3a65c971a9cd0ca1399faf471521bfff56988ef1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-10 15:30:06 +0000

    autogenerated overwrites for types-psycopg2==2.9.21.20240819

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-psycopg2 = {
+    "2.9.21.20240819" = import ./overrides/types-psycopg2/2.9.21.20240819;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
