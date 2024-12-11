83f2380446cfad943b97f1f4c9fb2ae5e80969d3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:58:27 +0000

    autogenerated overwrites for prometheus-client==0.20.0

--- collected.nix
+++ collected.nix
@@ -6180,6 +6180,9 @@
   projectq = {
     "0.8.0" = import ./overrides/projectq/0.8.0;
   };
+  prometheus-client = {
+    "0.20.0" = import ./overrides/prometheus-client/0.20.0;
+  };
   promise = {
     "2.3" = import ./overrides/promise/2.3;
   };
