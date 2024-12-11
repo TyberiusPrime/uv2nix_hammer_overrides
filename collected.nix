3bad832ed163d90b63477084e6274b006cc59c76
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:57:40 +0000

    autogenerated overwrites for stups-pierone==1.1.56

--- collected.nix
+++ collected.nix
@@ -8819,6 +8819,9 @@
   stubserver = {
     "1.1" = import ./overrides/stubserver/1.1;
   };
+  stups-pierone = {
+    "1.1.56" = import ./overrides/stups-pierone/1.1.56;
+  };
   stylus = {
     "0.1.2" = import ./overrides/stylus/0.1.2;
   };
