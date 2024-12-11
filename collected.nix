070ae276f59219541436911ad19871643c0f4136
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:03:29 +0000

    autogenerated overwrites for ufmt==2.7.2

--- collected.nix
+++ collected.nix
@@ -9467,6 +9467,9 @@
   uflash = {
     "2.0.0" = import ./overrides/uflash/2.0.0;
   };
+  ufmt = {
+    "2.7.2" = import ./overrides/ufmt/2.7.2;
+  };
   ujson = {
     "1.35" = import ./overrides/ujson/1.35;
   };
