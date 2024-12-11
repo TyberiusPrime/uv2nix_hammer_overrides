a5ccc458a0e3047fa1b301eac1e6deeda63c624b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:57:11 +0000

    autogenerated overwrites for ashes==24.0.0

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  ashes = {
+    "24.0.0" = import ./overrides/ashes/24.0.0;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
