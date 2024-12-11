adb5f0e5ec722bff8dc4e0957fca48fe0788ccf5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:08:34 +0000

    autogenerated overwrites for financedatabase==2.2.2

--- collected.nix
+++ collected.nix
@@ -2769,6 +2769,9 @@
   filterpy = {
     "1.4.5" = import ./overrides/filterpy/1.4.5;
   };
+  financedatabase = {
+    "2.2.2" = import ./overrides/financedatabase/2.2.2;
+  };
   findlibs = {
     "0.0.5" = import ./overrides/findlibs/0.0.5;
   };
