b970b15aefa4c42fb4c7ed6071b7ab0cf4bb7214
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:39:46 +0000

    autogenerated overwrites for nicegui-highcharts==2.0.2

--- collected.nix
+++ collected.nix
@@ -5232,6 +5232,9 @@
   nglview = {
     "3.1.2" = import ./overrides/nglview/3.1.2;
   };
+  nicegui-highcharts = {
+    "2.0.2" = import ./overrides/nicegui-highcharts/2.0.2;
+  };
   nilsimsa = {
     "0.3.8" = import ./overrides/nilsimsa/0.3.8;
   };
