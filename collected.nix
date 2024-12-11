b11502dab123e006eeb505d3385964589f2089b2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:50:44 +0000

    autogenerated overwrites for minio==7.2.8

--- collected.nix
+++ collected.nix
@@ -4820,6 +4820,9 @@
   minimal-snowplow-tracker = {
     "0.0.2" = import ./overrides/minimal-snowplow-tracker/0.0.2;
   };
+  minio = {
+    "7.2.8" = import ./overrides/minio/7.2.8;
+  };
   mir-eval = {
     "0.7" = import ./overrides/mir-eval/0.7;
   };
