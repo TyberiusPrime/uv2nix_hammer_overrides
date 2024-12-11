20db4314bd2035fa7e22cf8c7e37191d1fb01735
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:43:44 +0000

    autogenerated overwrites for bareutils==4.0.2

--- collected.nix
+++ collected.nix
@@ -596,6 +596,9 @@
   barenecessities = {
     "0.2.8" = import ./overrides/barenecessities/0.2.8;
   };
+  bareutils = {
+    "4.0.2" = import ./overrides/bareutils/4.0.2;
+  };
   base32hex = {
     "1.0.2" = import ./overrides/base32hex/1.0.2;
   };
