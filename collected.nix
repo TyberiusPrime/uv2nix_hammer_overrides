0fb2835162a081f02dc7de214c0244e1a1f94d2f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:28:54 +0000

    autogenerated overwrites for resampy==0.4.3

--- collected.nix
+++ collected.nix
@@ -7904,6 +7904,9 @@
   requests-wsgi-adapter = {
     "0.4.1" = import ./overrides/requests-wsgi-adapter/0.4.1;
   };
+  resampy = {
+    "0.4.3" = import ./overrides/resampy/0.4.3;
+  };
   reserved = {
     "0.1.1" = import ./overrides/reserved/0.1.1;
   };
