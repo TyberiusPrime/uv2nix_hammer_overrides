10119b7315049a8b80372ed2f0ac1a3ab29f75f1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:21:59 +0000

    autogenerated overwrites for diapyr==30

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  diapyr = {
+    "30" = import ./overrides/diapyr/30;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
