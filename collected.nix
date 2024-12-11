8c85ccc8c0b4c9f7a2036ac91cef212ec9adcfdc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 10:13:34 +0000

    autogenerated overwrites for snakemake-interface-executor-plugins==9.2.0

--- collected.nix
+++ collected.nix
@@ -8538,6 +8538,9 @@
   snakemake = {
     "7.32.4" = import ./overrides/snakemake/7.32.4;
   };
+  snakemake-interface-executor-plugins = {
+    "9.2.0" = import ./overrides/snakemake-interface-executor-plugins/9.2.0;
+  };
   snapcast = {
     "2.3.6" = import ./overrides/snapcast/2.3.6;
   };
