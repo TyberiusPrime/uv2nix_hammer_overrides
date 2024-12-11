ef5f349a3264535991c9e33f65727462499c604f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:58:08 +0000

    autogenerated overwrites for dwave-greedy==0.3.0

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dwave-greedy = {
+    "0.3.0" = import ./overrides/dwave-greedy/0.3.0;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
