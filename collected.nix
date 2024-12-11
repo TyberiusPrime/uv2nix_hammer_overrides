dc20ccb4d1e8acb199b52b8d73181f2e592eef89
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:47:55 +0000

    autogenerated overwrites for mendeleev==0.17.0

--- collected.nix
+++ collected.nix
@@ -4703,6 +4703,9 @@
   memoizer = {
     "0.0.1" = import ./overrides/memoizer/0.0.1;
   };
+  mendeleev = {
+    "0.17.0" = import ./overrides/mendeleev/0.17.0;
+  };
   menpo = {
     "0.11.1" = import ./overrides/menpo/0.11.1;
   };
