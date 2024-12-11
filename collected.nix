ab4325deb5430d570a42ed5809f8f7b39455b8dd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 10:18:18 +0000

    autogenerated overwrites for hsluv==5.0.4

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  hsluv = {
+    "5.0.4" = import ./overrides/hsluv/5.0.4;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
