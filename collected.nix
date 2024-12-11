be19da345fc78ca8a5cbb78cfdc09d2cbb60e92f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 10:17:53 +0000

    autogenerated overwrites for netgraph==4.13.2

--- collected.nix
+++ collected.nix
@@ -5187,6 +5187,9 @@
   nestle = {
     "0.2.0" = import ./overrides/nestle/0.2.0;
   };
+  netgraph = {
+    "4.13.2" = import ./overrides/netgraph/4.13.2;
+  };
   netifaces = {
     "0.11.0" = import ./overrides/netifaces/0.11.0;
   };
