1c7d150bee59d805f06b527de1040c661b74c704
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:27:05 +0000

    autogenerated overwrites for geojson==3.1.0

--- collected.nix
+++ collected.nix
@@ -3134,6 +3134,9 @@
   geoid = {
     "1.1.5" = import ./overrides/geoid/1.1.5;
   };
+  geojson = {
+    "3.1.0" = import ./overrides/geojson/3.1.0;
+  };
   geometric = {
     "1.0.2" = import ./overrides/geometric/1.0.2;
   };
