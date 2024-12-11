797a0c3ade8bc056ddae8d1464c8ddd03a7f4745
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 09:46:54 +0000

    autogenerated overwrites for aiida-core==2.6.2

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aiida-core = {
+    "2.6.2" = import ./overrides/aiida-core/2.6.2;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
