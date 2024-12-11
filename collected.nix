615c1d7b2997b91348a73e26f87e8b4577e2015f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:01:31 +0000

    autogenerated overwrites for smhi-pkg==1.0.17

--- collected.nix
+++ collected.nix
@@ -8523,6 +8523,9 @@
   smbus-cffi = {
     "0.5.1" = import ./overrides/smbus-cffi/0.5.1;
   };
+  smhi-pkg = {
+    "1.0.17" = import ./overrides/smhi-pkg/1.0.17;
+  };
   smokesignal = {
     "0.7.0" = import ./overrides/smokesignal/0.7.0;
   };
