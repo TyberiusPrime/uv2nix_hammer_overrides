e0be2c221d4acae96abbf9870ec04e36e91d0114
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:46:59 +0000

    autogenerated overwrites for flake8-coding==1.3.2

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-coding = {
+    "1.3.2" = import ./overrides/flake8-coding/1.3.2;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
