bb33b1b212d3db417b7c08337e35b390687b89cd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 10:24:50 +0000

    autogenerated overwrites for ytmusicapi==1.8.1

--- collected.nix
+++ collected.nix
@@ -10097,6 +10097,9 @@
   youtokentome = {
     "1.0.6" = import ./overrides/youtokentome/1.0.6;
   };
+  ytmusicapi = {
+    "1.8.1" = import ./overrides/ytmusicapi/1.8.1;
+  };
   yurl = {
     "1.0.0" = import ./overrides/yurl/1.0.0;
   };
