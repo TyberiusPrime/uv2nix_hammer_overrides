87b920d8a385e404a75cf9ba9132945ef2281607
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:44:35 +0000

    autogenerated overwrites for types-xmltodict==0.13.0.3

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-xmltodict = {
+    "0.13.0.3" = import ./overrides/types-xmltodict/0.13.0.3;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
