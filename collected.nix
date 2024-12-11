02622ea7dc702c8944b6cc5a978db2c99b6eb4ae
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:21:08 +0000

    autogenerated overwrites for conda-pack==0.8.0

--- collected.nix
+++ collected.nix
@@ -1349,6 +1349,9 @@
   conan = {
     "2.7.0" = import ./overrides/conan/2.7.0;
   };
+  conda-pack = {
+    "0.8.0" = import ./overrides/conda-pack/0.8.0;
+  };
   configobj = {
     "5.0.9" = import ./overrides/configobj/5.0.9;
   };
