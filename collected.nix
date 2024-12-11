a3e756f7245948023451bbed8de8db4a1de325ef
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 10:03:53 +0000

    autogenerated overwrites for pyairtable==2.3.3

--- collected.nix
+++ collected.nix
@@ -6363,6 +6363,9 @@
   pyaes = {
     "1.6.1" = import ./overrides/pyaes/1.6.1;
   };
+  pyairtable = {
+    "2.3.3" = import ./overrides/pyairtable/2.3.3;
+  };
   pyalsaaudio = {
     "0.11.0" = import ./overrides/pyalsaaudio/0.11.0;
   };
