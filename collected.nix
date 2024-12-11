e189cd8e9ea4d0ba11f8178334f3aa1b9cc4a4d3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 08:13:52 +0000

    autogenerated overwrites for django-polymorphic==3.1.0

--- collected.nix
+++ collected.nix
@@ -2090,6 +2090,9 @@
   django-piston = {
     "0.2.3" = import ./overrides/django-piston/0.2.3;
   };
+  django-polymorphic = {
+    "3.1.0" = import ./overrides/django-polymorphic/3.1.0;
+  };
   django-pragmatic = {
     "5.0.0" = import ./overrides/django-pragmatic/5.0.0;
   };
