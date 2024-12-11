88516f323f6f3d1176837ef2e70ea06ca06436de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-11 07:53:18 +0000

    autogenerated overwrites for webdriverdownloader==1.1.0.4

--- collected.nix
+++ collected.nix
@@ -9838,6 +9838,9 @@
   webdavclient3 = {
     "3.14.6" = import ./overrides/webdavclient3/3.14.6;
   };
+  webdriverdownloader = {
+    "1.1.0.4" = import ./overrides/webdriverdownloader/1.1.0.4;
+  };
   webexteamssdk = {
     "1.7" = import ./overrides/webexteamssdk/1.7;
   };
