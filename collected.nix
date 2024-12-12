6cd10204b95ce1bd5c069e4f2976c397bf70d882
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 09:48:32 +0000

    autogenerated overwrites for itly-sdk==0.1.24

--- collected.nix
+++ collected.nix
@@ -3871,6 +3871,9 @@
   isounidecode = {
     "0.3" = import ./overrides/isounidecode/0.3;
   };
+  itly-sdk = {
+    "0.1.24" = import ./overrides/itly-sdk/0.1.24;
+  };
   ivy = {
     "0.0.9.7" = import ./overrides/ivy/0.0.9.7;
   };
