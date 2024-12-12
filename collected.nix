1ab0e5d8d507b0d39b87c337c950bde5b5455b42
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 10:48:24 +0000

    autogenerated overwrites for azure-mgmt-sql==3.0.1

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-mgmt-sql = {
+    "3.0.1" = import ./overrides/azure-mgmt-sql/3.0.1;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
