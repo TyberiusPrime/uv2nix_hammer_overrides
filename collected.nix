beaf0f196c857f4a50320e8443745cf8ea87a1a8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:12:30 +0000

    autogenerated overwrites for datasette-publish-fly==1.3.1

--- collected.nix
+++ collected.nix
@@ -1689,6 +1689,9 @@
   datapipelines = {
     "1.0.7" = import ./overrides/datapipelines/1.0.7;
   };
+  datasette-publish-fly = {
+    "1.3.1" = import ./overrides/datasette-publish-fly/1.3.1;
+  };
   datashape = {
     "0.5.2" = import ./overrides/datashape/0.5.2;
   };
