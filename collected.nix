6541fc46b92ec1d6320c209dfada0e27838634cf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:24:37 +0000

    autogenerated overwrites for libkeepass==0.3.1.post1

--- collected.nix
+++ collected.nix
@@ -4329,6 +4329,9 @@
   libfsntfs-python = {
     "20240501" = import ./overrides/libfsntfs-python/20240501;
   };
+  libkeepass = {
+    "0.3.1.post1" = import ./overrides/libkeepass/0.3.1.post1;
+  };
   libknot = {
     "3.4.0" = import ./overrides/libknot/3.4.0;
   };
