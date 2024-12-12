fddff647252a08ae3d80ee25ddc9060e202ba780
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:28:12 +0000

    autogenerated overwrites for djangorestframework-gis==1.1

--- collected.nix
+++ collected.nix
@@ -2237,6 +2237,9 @@
   djangorestframework-extras = {
     "0.3.0" = import ./overrides/djangorestframework-extras/0.3.0;
   };
+  djangorestframework-gis = {
+    "1.1" = import ./overrides/djangorestframework-gis/1.1;
+  };
   djangorestframework-jsonschema = {
     "0.1.1" = import ./overrides/djangorestframework-jsonschema/0.1.1;
   };
