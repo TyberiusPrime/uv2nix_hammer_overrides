28c203d875bd32f50f306ff62df40cec309a698e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:38:35 +0000

    autogenerated overwrites for napari-assistant==0.4.7

--- collected.nix
+++ collected.nix
@@ -5120,6 +5120,9 @@
   nanotime = {
     "0.5.2" = import ./overrides/nanotime/0.5.2;
   };
+  napari-assistant = {
+    "0.4.7" = import ./overrides/napari-assistant/0.4.7;
+  };
   nats-py = {
     "2.9.0" = import ./overrides/nats-py/2.9.0;
   };
