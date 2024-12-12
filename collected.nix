bf661dc6bad59c2408f6a56f90ca3465e9bb6a08
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 14:30:52 +0000

    autogenerated overwrites for csaps==1.2.0

--- collected.nix
+++ collected.nix
@@ -1548,6 +1548,9 @@
   cs-units = {
     "20220311" = import ./overrides/cs-units/20220311;
   };
+  csaps = {
+    "1.2.0" = import ./overrides/csaps/1.2.0;
+  };
   csrmesh = {
     "0.10.0" = import ./overrides/csrmesh/0.10.0;
   };
