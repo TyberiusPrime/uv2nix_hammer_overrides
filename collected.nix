0ec6257b3a470357ea2c313d258363dc3b081344
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:30:03 +0000

    autogenerated overwrites for napari-workflows==0.2.11

--- collected.nix
+++ collected.nix
@@ -5120,6 +5120,9 @@
   nanotime = {
     "0.5.2" = import ./overrides/nanotime/0.5.2;
   };
+  napari-workflows = {
+    "0.2.11" = import ./overrides/napari-workflows/0.2.11;
+  };
   nats-py = {
     "2.9.0" = import ./overrides/nats-py/2.9.0;
   };
