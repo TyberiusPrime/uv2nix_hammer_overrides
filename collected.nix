c9cbc51743a389dc1d70cea33d69f55b3dbf9c5a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:43:43 +0000

    autogenerated overwrites for dicttoxml2==2.1.0

--- collected.nix
+++ collected.nix
@@ -1800,6 +1800,9 @@
   dictor = {
     "0.1.12" = import ./overrides/dictor/0.1.12;
   };
+  dicttoxml2 = {
+    "2.1.0" = import ./overrides/dicttoxml2/2.1.0;
+  };
   diffimg = {
     "0.3.0" = import ./overrides/diffimg/0.3.0;
   };
