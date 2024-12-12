8502d4e58b85e9f204df4300d7c273169b0408cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:13:46 +0000

    autogenerated overwrites for bolton-eris==0.2.3

--- collected.nix
+++ collected.nix
@@ -804,6 +804,9 @@
   bolt-python = {
     "0.7.1" = import ./overrides/bolt-python/0.7.1;
   };
+  bolton-eris = {
+    "0.2.3" = import ./overrides/bolton-eris/0.2.3;
+  };
   bonsai = {
     "1.5.3" = import ./overrides/bonsai/1.5.3;
   };
