2c10ecbbc2dbbc2cc5b6744c4821ccffd1038fd0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 19:20:35 +0000

    autogenerated overwrites for python-subunit==1.4.4

--- collected.nix
+++ collected.nix
@@ -7475,6 +7475,9 @@
   python-status = {
     "1.0.1" = import ./overrides/python-status/1.0.1;
   };
+  python-subunit = {
+    "1.4.4" = import ./overrides/python-subunit/1.4.4;
+  };
   python-tds = {
     "1.15.0" = import ./overrides/python-tds/1.15.0;
   };
