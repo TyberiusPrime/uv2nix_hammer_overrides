d717db3a2088c79d199982f0f72d866b7c7a1f0a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:35:28 +0000

    autogenerated overwrites for aiortsp==1.3.7

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aiortsp = {
+    "1.3.7" = import ./overrides/aiortsp/1.3.7;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
