72426505719e18db61c25723a1cce53210ac925f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:43:18 +0000

    autogenerated overwrites for aiolip==1.1.6

--- collected.nix
+++ collected.nix
@@ -117,6 +117,9 @@
   aiohttp-swagger3 = {
     "0.8.0" = import ./overrides/aiohttp-swagger3/0.8.0;
   };
+  aiolip = {
+    "1.1.6" = import ./overrides/aiolip/1.1.6;
+  };
   aiolivisi = {
     "0.0.19" = import ./overrides/aiolivisi/0.0.19;
   };
