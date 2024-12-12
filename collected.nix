baea880664a8ced390752006b4750ac1d4f694d4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:42:53 +0000

    autogenerated overwrites for brotlipy==0.7.0

--- collected.nix
+++ collected.nix
@@ -837,6 +837,9 @@
   brightway2 = {
     "2.4.6" = import ./overrides/brightway2/2.4.6;
   };
+  brotlipy = {
+    "0.7.0" = import ./overrides/brotlipy/0.7.0;
+  };
   brownie = {
     "0.5.1" = import ./overrides/brownie/0.5.1;
   };
