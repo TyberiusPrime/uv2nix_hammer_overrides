d23296306f49fedd43f47ad7bd1994da130fb91f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:26:09 +0000

    autogenerated overwrites for blosc2==2.7.1

--- overrides/blosc2/2.7.1/default.nix
+++ overrides/blosc2/2.7.1/default.nix
@@ -1,10 +1,14 @@
-{helpers, pkgs, ...}
-        : old: {
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        :
+            old:
+            let funcs = [(old: old // ( if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cmake = [];cython = [];ninja = [];numpy = [];scikit-build = [];setuptools = [];});})) (old: old // ( {
   postInstall =
     (old.postInstall or "")
     + (pkgs.lib.optionalString (helpers.isWheel old) ''
       echo idk
     '');
 }
-
-        
+))];
+            in
+            pkgs.lib.trivial.pipe old funcs
+    
