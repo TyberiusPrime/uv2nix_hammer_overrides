176d47ee3e71447d63d45909cc53d6f940d71a9e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:42:11 +0000

    autogenerated overwrites for approvaltests==14.0.0

--- collected.nix
+++ collected.nix
@@ -310,6 +310,9 @@
   applescript = {
     "2021.2.9" = import ./overrides/applescript/2021.2.9;
   };
+  approvaltests = {
+    "14.0.0" = import ./overrides/approvaltests/14.0.0;
+  };
   appy = {
     "1.0.18" = import ./overrides/appy/1.0.18;
   };
