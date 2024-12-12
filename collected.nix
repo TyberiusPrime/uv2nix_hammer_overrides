f4fbfe8768b4d4eadc32e4203132634043de414c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:17:37 +0000

    autogenerated overwrites for django-hitcount==1.3.5

--- collected.nix
+++ collected.nix
@@ -1988,6 +1988,9 @@
   django-haystack = {
     "3.3.0" = import ./overrides/django-haystack/3.3.0;
   };
+  django-hitcount = {
+    "1.3.5" = import ./overrides/django-hitcount/1.3.5;
+  };
   django-htmlmin = {
     "0.11.0" = import ./overrides/django-htmlmin/0.11.0;
   };
