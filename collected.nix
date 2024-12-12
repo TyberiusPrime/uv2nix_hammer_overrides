1b7177477ef1f3b8543166df1e896e8e791f8698
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:29:43 +0000

    autogenerated overwrites for aiodocker==0.22.2

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aiodocker = {
+    "0.22.2" = import ./overrides/aiodocker/0.22.2;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
