c903cbdb50e0ad59326f98b2b00fe2bc592d4cd1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:31:43 +0000

    autogenerated overwrites for spacy-lookups-data==1.0.5

--- collected.nix
+++ collected.nix
@@ -8589,6 +8589,9 @@
   sox = {
     "1.5.0" = import ./overrides/sox/1.5.0;
   };
+  spacy-lookups-data = {
+    "1.0.5" = import ./overrides/spacy-lookups-data/1.0.5;
+  };
   spanners = {
     "1.20" = import ./overrides/spanners/1.20;
   };
