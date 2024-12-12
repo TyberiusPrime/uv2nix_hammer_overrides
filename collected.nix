526d76c0a9283bbeb4c3a9d23e1ab5bc248565cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:01:36 +0000

    autogenerated overwrites for pyright==1.1.379

--- collected.nix
+++ collected.nix
@@ -7075,6 +7075,9 @@
   pyric = {
     "0.1.6.3" = import ./overrides/pyric/0.1.6.3;
   };
+  pyright = {
+    "1.1.379" = import ./overrides/pyright/1.1.379;
+  };
   pyroute2-core = {
     "0.6.13" = import ./overrides/pyroute2-core/0.6.13;
   };
