9415a471c92066278f60123b846abdffc60fef11
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 23:24:11 +0000

    autogenerated overwrites for aiotractive==0.6.0

--- collected.nix
+++ collected.nix
@@ -141,6 +141,9 @@
   aiosocks = {
     "0.2.6" = import ./overrides/aiosocks/0.2.6;
   };
+  aiotractive = {
+    "0.6.0" = import ./overrides/aiotractive/0.6.0;
+  };
   aioxmpp = {
     "0.13.3" = import ./overrides/aioxmpp/0.13.3;
   };
