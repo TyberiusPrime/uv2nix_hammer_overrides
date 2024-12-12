733429e2deada57bf04c0cc5112b04822ffd34ca
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:17:35 +0000

    autogenerated overwrites for pycxx==0.0.4

--- collected.nix
+++ collected.nix
@@ -6536,6 +6536,9 @@
   pycups = {
     "2.0.4" = import ./overrides/pycups/2.0.4;
   };
+  pycxx = {
+    "0.0.4" = import ./overrides/pycxx/0.0.4;
+  };
   pydaqmx = {
     "1.4.6" = import ./overrides/pydaqmx/1.4.6;
   };
