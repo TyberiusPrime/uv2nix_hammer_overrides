e7d4ce43883a543c82b87f24f73381fc6b543a9f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 16:55:04 +0000

    autogenerated overwrites for systematic==4.8.7

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  systematic = {
+    "4.8.7" = import ./overrides/systematic/4.8.7;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
