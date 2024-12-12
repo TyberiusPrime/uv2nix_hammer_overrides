84b30a5245ad99407ce17a8db60641b0670738cf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:43:13 +0000

    autogenerated overwrites for nidaqmx==1.0.1

--- collected.nix
+++ collected.nix
@@ -5232,6 +5232,9 @@
   nglview = {
     "3.1.2" = import ./overrides/nglview/3.1.2;
   };
+  nidaqmx = {
+    "1.0.1" = import ./overrides/nidaqmx/1.0.1;
+  };
   nilsimsa = {
     "0.3.8" = import ./overrides/nilsimsa/0.3.8;
   };
