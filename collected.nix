4ea542576417bd1d2ba9a482db93e10355da9e70
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:58:35 +0000

    autogenerated overwrites for whispers==2.3.0

--- collected.nix
+++ collected.nix
@@ -9883,6 +9883,9 @@
   whisper = {
     "1.1.10" = import ./overrides/whisper/1.1.10;
   };
+  whispers = {
+    "2.3.0" = import ./overrides/whispers/2.3.0;
+  };
   whylogs-sketching = {
     "3.4.1.dev3" = import ./overrides/whylogs-sketching/3.4.1.dev3;
   };
