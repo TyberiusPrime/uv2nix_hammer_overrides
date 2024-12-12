26272dcdb7e07760e9c58767a167d9128dae262d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 23:52:58 +0000

    autogenerated overwrites for clumper==0.2.15

--- collected.nix
+++ collected.nix
@@ -1190,6 +1190,9 @@
   cloudshell-snmp = {
     "3.3.0" = import ./overrides/cloudshell-snmp/3.3.0;
   };
+  clumper = {
+    "0.2.15" = import ./overrides/clumper/0.2.15;
+  };
   cmapy = {
     "0.6.6" = import ./overrides/cmapy/0.6.6;
   };
