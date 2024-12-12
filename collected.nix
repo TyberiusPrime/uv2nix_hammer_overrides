d62a63fb4556d9d87c9cbf08f9128bd6e191f1b3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:37:23 +0000

    autogenerated overwrites for google-cloud-redis==2.15.5

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-redis = {
+    "2.15.5" = import ./overrides/google-cloud-redis/2.15.5;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
