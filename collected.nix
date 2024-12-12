b26ce5b181be15bec89b7be240edfb748058e409
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:07:13 +0000

    autogenerated overwrites for macaddress==2.0.2

--- collected.nix
+++ collected.nix
@@ -4574,6 +4574,9 @@
   m9s-trytond = {
     "7.0.7" = import ./overrides/m9s-trytond/7.0.7;
   };
+  macaddress = {
+    "2.0.2" = import ./overrides/macaddress/2.0.2;
+  };
   madmom = {
     "0.16.1" = import ./overrides/madmom/0.16.1;
   };
