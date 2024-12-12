f135c9ac19b87bdafb03e8a00a0fb7781a3da570
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:12:26 +0000

    autogenerated overwrites for pylsp-rope==0.1.16

--- collected.nix
+++ collected.nix
@@ -6837,6 +6837,9 @@
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
+  pylsp-rope = {
+    "0.1.16" = import ./overrides/pylsp-rope/0.1.16;
+  };
   pylzma = {
     "0.5.0" = import ./overrides/pylzma/0.5.0;
   };
