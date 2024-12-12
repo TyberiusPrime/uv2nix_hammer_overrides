8a98808687e4e701e89bf3677f56017c803c5ea4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:46:05 +0000

    autogenerated overwrites for requests-http-signature==0.7.1

--- collected.nix
+++ collected.nix
@@ -7898,6 +7898,9 @@
   requests-ftp = {
     "0.3.1" = import ./overrides/requests-ftp/0.3.1;
   };
+  requests-http-signature = {
+    "0.7.1" = import ./overrides/requests-http-signature/0.7.1;
+  };
   requests-oauth = {
     "0.4.1" = import ./overrides/requests-oauth/0.4.1;
   };
