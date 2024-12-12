15f461cec85c3ca6519f812f874054411390731b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:36:23 +0000

    autogenerated overwrites for txmongo==23.0.0

--- collected.nix
+++ collected.nix
@@ -9422,6 +9422,9 @@
   txhttputil = {
     "1.3.0" = import ./overrides/txhttputil/1.3.0;
   };
+  txmongo = {
+    "23.0.0" = import ./overrides/txmongo/23.0.0;
+  };
   txredis = {
     "2.4" = import ./overrides/txredis/2.4;
   };
