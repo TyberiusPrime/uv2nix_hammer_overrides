2c0a7d5ebed6aeb16bd1f514cefc3de8239ece27
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:46:43 +0000

    autogenerated overwrites for python-xlib==0.33

--- collected.nix
+++ collected.nix
@@ -7508,6 +7508,9 @@
   python-xbrl = {
     "1.1.1" = import ./overrides/python-xbrl/1.1.1;
   };
+  python-xlib = {
+    "0.33" = import ./overrides/python-xlib/0.33;
+  };
   python-yubico = {
     "1.3.3" = import ./overrides/python-yubico/1.3.3;
   };
