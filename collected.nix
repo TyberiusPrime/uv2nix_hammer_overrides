80c043a15826b5620b20d609a36b66126b22b88c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 16:49:17 +0000

    autogenerated overwrites for nexmo==2.5.2

--- collected.nix
+++ collected.nix
@@ -5205,6 +5205,9 @@
   neuronpy = {
     "0.1.6" = import ./overrides/neuronpy/0.1.6;
   };
+  nexmo = {
+    "2.5.2" = import ./overrides/nexmo/2.5.2;
+  };
   nextcord = {
     "2.6.0" = import ./overrides/nextcord/2.6.0;
   };
