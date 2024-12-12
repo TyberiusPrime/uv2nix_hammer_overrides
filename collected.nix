8dffca355495a407d7a779838994bc722a9ab431
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 10:33:33 +0000

    autogenerated overwrites for deriva==1.7.3

--- collected.nix
+++ collected.nix
@@ -1773,6 +1773,9 @@
   demjson3 = {
     "3.0.6" = import ./overrides/demjson3/3.0.6;
   };
+  deriva = {
+    "1.7.3" = import ./overrides/deriva/1.7.3;
+  };
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
