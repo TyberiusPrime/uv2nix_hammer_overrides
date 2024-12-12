702d1069568254fc2fd2ea8592278b6e04d64f82
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:40:21 +0000

    autogenerated overwrites for blake3==0.4.1

--- collected.nix
+++ collected.nix
@@ -764,6 +764,9 @@
   blackboxauditing = {
     "0.1.54" = import ./overrides/blackboxauditing/0.1.54;
   };
+  blake3 = {
+    "0.4.1" = import ./overrides/blake3/0.4.1;
+  };
   blaze = {
     "0.10.1" = import ./overrides/blaze/0.10.1;
   };
