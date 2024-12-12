0130e51db0ad04ce395af2dd598e89405b211697
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:09:34 +0000

    autogenerated overwrites for azure-mgmt-redis==14.4.0

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-mgmt-redis = {
+    "14.4.0" = import ./overrides/azure-mgmt-redis/14.4.0;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
