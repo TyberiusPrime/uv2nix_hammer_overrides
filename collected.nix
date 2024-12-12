5b2981de8ae31133ef65a93047bcc994ed258c27
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:21:37 +0000

    autogenerated overwrites for forcediphttpsadapter==1.1.0

--- collected.nix
+++ collected.nix
@@ -2947,6 +2947,9 @@
   forbiddenfruit = {
     "0.1.4" = import ./overrides/forbiddenfruit/0.1.4;
   };
+  forcediphttpsadapter = {
+    "1.1.0" = import ./overrides/forcediphttpsadapter/1.1.0;
+  };
   formatizer = {
     "0.1.1" = import ./overrides/formatizer/0.1.1;
   };
