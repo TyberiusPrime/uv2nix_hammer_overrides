5f1ea94e45baaf1154f45b0e19ce375c0dd1c878
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:43:03 +0000

    autogenerated overwrites for google-cloud-os-config==1.17.5

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-os-config = {
+    "1.17.5" = import ./overrides/google-cloud-os-config/1.17.5;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
