762d764f38d9c7dbb20c8a0113e0923149c0072a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:51:16 +0000

    autogenerated overwrites for localimport==1.7.6

--- collected.nix
+++ collected.nix
@@ -4465,6 +4465,9 @@
   lmtpd = {
     "6.2.0" = import ./overrides/lmtpd/6.2.0;
   };
+  localimport = {
+    "1.7.6" = import ./overrides/localimport/1.7.6;
+  };
   localstack = {
     "3.7.2" = import ./overrides/localstack/3.7.2;
   };
