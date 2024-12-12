02218011f95d8e4e35ead0b2f3f406a68ff3a4ce
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:40:52 +0000

    autogenerated overwrites for animation==0.0.7

--- collected.nix
+++ collected.nix
@@ -222,6 +222,9 @@
     "9.2.117" = import ./overrides/angr/9.2.117;
     "9.2.120" = import ./overrides/angr/9.2.120;
   };
+  animation = {
+    "0.0.7" = import ./overrides/animation/0.0.7;
+  };
   anitopy = {
     "2.1.1" = import ./overrides/anitopy/2.1.1;
   };
