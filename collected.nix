a273cfd9e694305e057365844eca9b0f44abe4ea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:18:28 +0000

    autogenerated overwrites for hatch-jupyter-builder==0.9.1

--- collected.nix
+++ collected.nix
@@ -3408,6 +3408,9 @@
   hash-ring = {
     "1.3.1" = import ./overrides/hash-ring/1.3.1;
   };
+  hatch-jupyter-builder = {
+    "0.9.1" = import ./overrides/hatch-jupyter-builder/0.9.1;
+  };
   hbmqtt = {
     "0.9.6" = import ./overrides/hbmqtt/0.9.6;
   };
