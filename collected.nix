d0123e8ebebfbb2180157f6ae11e51b70ecd96a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:20:39 +0000

    autogenerated overwrites for push-receiver==0.1.1

--- collected.nix
+++ collected.nix
@@ -6279,6 +6279,9 @@
   purepng = {
     "0.2.0" = import ./overrides/purepng/0.2.0;
   };
+  push-receiver = {
+    "0.1.1" = import ./overrides/push-receiver/0.1.1;
+  };
   pusherclient = {
     "0.3.0" = import ./overrides/pusherclient/0.3.0;
   };
