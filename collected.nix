824469103045abd3869e1970e3611137100c4d0e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 10:50:36 +0000

    autogenerated overwrites for dagster-graphql==1.8.6

--- collected.nix
+++ collected.nix
@@ -1659,6 +1659,9 @@
   daff = {
     "1.3.46" = import ./overrides/daff/1.3.46;
   };
+  dagster-graphql = {
+    "1.8.6" = import ./overrides/dagster-graphql/1.8.6;
+  };
   dark-matter = {
     "4.0.89" = import ./overrides/dark-matter/4.0.89;
   };
