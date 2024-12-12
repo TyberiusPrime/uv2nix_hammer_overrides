a823a7e9c8b5cf74adabb089693d1d05fc4bbaec
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:23:54 +0000

    autogenerated overwrites for pyro-ppl==1.9.1

--- collected.nix
+++ collected.nix
@@ -7075,6 +7075,9 @@
   pyric = {
     "0.1.6.3" = import ./overrides/pyric/0.1.6.3;
   };
+  pyro-ppl = {
+    "1.9.1" = import ./overrides/pyro-ppl/1.9.1;
+  };
   pyroute2-core = {
     "0.6.13" = import ./overrides/pyroute2-core/0.6.13;
   };
