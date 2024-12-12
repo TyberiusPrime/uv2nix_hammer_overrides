c8518f78024729eccff16d2dbbeb667987a6d489
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:04:12 +0000

    autogenerated overwrites for i2==0.1.33

--- collected.nix
+++ collected.nix
@@ -3610,6 +3610,9 @@
   hyrule = {
     "0.6.0" = import ./overrides/hyrule/0.6.0;
   };
+  i2 = {
+    "0.1.33" = import ./overrides/i2/0.1.33;
+  };
   i3-instant-layout = {
     "0.1.12" = import ./overrides/i3-instant-layout/0.1.12;
   };
