dce3830c1b14a9c5652c4202b0a4b9ffdf54a718
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:06:44 +0000

    autogenerated overwrites for dendroid==1.6.1

--- collected.nix
+++ collected.nix
@@ -1773,6 +1773,9 @@
   demjson3 = {
     "3.0.6" = import ./overrides/demjson3/3.0.6;
   };
+  dendroid = {
+    "1.6.1" = import ./overrides/dendroid/1.6.1;
+  };
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
