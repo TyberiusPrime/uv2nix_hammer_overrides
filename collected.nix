d7d61ae31db4480b151aceea23805fadd0252009
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 19:31:35 +0000

    autogenerated overwrites for moreorless==0.4.0

--- collected.nix
+++ collected.nix
@@ -4925,6 +4925,9 @@
   mordred = {
     "1.2.0" = import ./overrides/mordred/1.2.0;
   };
+  moreorless = {
+    "0.4.0" = import ./overrides/moreorless/0.4.0;
+  };
   morph = {
     "0.1.5" = import ./overrides/morph/0.1.5;
   };
