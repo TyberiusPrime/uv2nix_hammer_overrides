ef2b80e8c1b3c05f5c10affa8eec055d4f27b843
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:15:33 +0000

    autogenerated overwrites for milksnake==0.1.6

--- collected.nix
+++ collected.nix
@@ -4793,6 +4793,9 @@
   migen = {
     "0.9.2" = import ./overrides/migen/0.9.2;
   };
+  milksnake = {
+    "0.1.6" = import ./overrides/milksnake/0.1.6;
+  };
   millify = {
     "0.1.1" = import ./overrides/millify/0.1.1;
   };
