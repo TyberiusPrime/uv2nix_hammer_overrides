d6a6a6c4f10082afc1a573b5d5175e27f0c03b09
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:11:59 +0000

    autogenerated overwrites for aioredlock==0.7.3

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioredlock = {
+    "0.7.3" = import ./overrides/aioredlock/0.7.3;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
