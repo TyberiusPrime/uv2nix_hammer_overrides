dfc5975701df77fa1de3d1f29b5853a458585413
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:52:59 +0000

    autogenerated overwrites for qiskit-aqua==0.9.5

--- overrides/qiskit-aqua/0.9.5/default.nix
+++ overrides/qiskit-aqua/0.9.5/default.nix
@@ -1,7 +1,3 @@
-{ pkgs, ... }:
-old: {
-  buildInputs = old.buildInputs or [ ] ++ [
-    pkgs.gfortran13.cc
-    pkgs.gfortran13.out
-  ];
-}
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.gfortran13.cc pkgs.gfortran13.out];} else {buildInputs = old.buildInputs or [] ++ [pkgs.gfortran13.cc pkgs.gfortran13.out];nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+        
