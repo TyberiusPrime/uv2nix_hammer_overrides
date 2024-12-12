75a87d1dfb6822dd9f081002252e04a6881075e4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:52:18 +0000

    autogenerated overwrites for aio-geojson-usgs-earthquakes==0.3

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aio-geojson-usgs-earthquakes = {
+    "0.3" = import ./overrides/aio-geojson-usgs-earthquakes/0.3;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
