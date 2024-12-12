a219fbbb6f398593af22c08ad326af2870f6ea8e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:49:05 +0000

    autogenerated overwrites for hashids==1.3.1

--- collected.nix
+++ collected.nix
@@ -3408,6 +3408,9 @@
   hash-ring = {
     "1.3.1" = import ./overrides/hash-ring/1.3.1;
   };
+  hashids = {
+    "1.3.1" = import ./overrides/hashids/1.3.1;
+  };
   hbmqtt = {
     "0.9.6" = import ./overrides/hbmqtt/0.9.6;
   };
