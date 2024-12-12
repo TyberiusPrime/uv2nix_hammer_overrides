4a49804da3e43050cedf1ee5c3bd973c198955c3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 17:05:29 +0000

    autogenerated overwrites for gpflow==2.9.2

--- collected.nix
+++ collected.nix
@@ -3270,6 +3270,9 @@
   googletransx = {
     "2.4.2" = import ./overrides/googletransx/2.4.2;
   };
+  gpflow = {
+    "2.9.2" = import ./overrides/gpflow/2.9.2;
+  };
   gpgrecord = {
     "0.0.4" = import ./overrides/gpgrecord/0.0.4;
     "0.0.5" = import ./overrides/gpgrecord/0.0.5;
