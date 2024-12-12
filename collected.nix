5e69ccd6e8cc547bbc3e4e0d6177b3a517841cf8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:37:55 +0000

    autogenerated overwrites for django-polymorphic-tree==2.1

--- collected.nix
+++ collected.nix
@@ -2090,6 +2090,9 @@
   django-piston = {
     "0.2.3" = import ./overrides/django-piston/0.2.3;
   };
+  django-polymorphic-tree = {
+    "2.1" = import ./overrides/django-polymorphic-tree/2.1;
+  };
   django-pragmatic = {
     "5.0.0" = import ./overrides/django-pragmatic/5.0.0;
   };
