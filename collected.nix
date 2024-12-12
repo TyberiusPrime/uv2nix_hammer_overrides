ad0997b08e6f95cf069bb3a26e020e3f3906357b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:46:52 +0000

    autogenerated overwrites for mdformat-toc==0.3.0

--- collected.nix
+++ collected.nix
@@ -4670,6 +4670,9 @@
   mcfit = {
     "0.0.22" = import ./overrides/mcfit/0.0.22;
   };
+  mdformat-toc = {
+    "0.3.0" = import ./overrides/mdformat-toc/0.3.0;
+  };
   mdstat = {
     "1.0.4" = import ./overrides/mdstat/1.0.4;
   };
