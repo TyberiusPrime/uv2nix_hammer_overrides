9b39ce369f752587ced890b64736d9d033276dae
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:45:29 +0000

    autogenerated overwrites for configclass==0.2.0

--- collected.nix
+++ collected.nix
@@ -1349,6 +1349,9 @@
   conan = {
     "2.7.0" = import ./overrides/conan/2.7.0;
   };
+  configclass = {
+    "0.2.0" = import ./overrides/configclass/0.2.0;
+  };
   configobj = {
     "5.0.9" = import ./overrides/configobj/5.0.9;
   };
