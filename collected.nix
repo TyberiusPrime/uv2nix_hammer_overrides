799e8b49c1e2390dbfd94ce8abd54f706f9b8ccc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:05:34 +0000

    autogenerated overwrites for pytest-trio==0.8.0

--- collected.nix
+++ collected.nix
@@ -7248,6 +7248,9 @@
   pytest-subtesthack = {
     "0.2.0" = import ./overrides/pytest-subtesthack/0.2.0;
   };
+  pytest-trio = {
+    "0.8.0" = import ./overrides/pytest-trio/0.8.0;
+  };
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
