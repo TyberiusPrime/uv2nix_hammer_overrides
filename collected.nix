7d4be2852cea23c73ad0f6bb7e68d02b1271bc25
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 19:51:02 +0000

    autogenerated overwrites for myhome==0.3.0

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  myhome = {
+    "0.3.0" = import ./overrides/myhome/0.3.0;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
