a3428936e3b283d4d490009183e26f66e5a77826
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:19:50 +0000

    autogenerated overwrites for dcor==0.6

--- collected.nix
+++ collected.nix
@@ -1719,6 +1719,9 @@
   dbutils = {
     "1.3" = import ./overrides/dbutils/1.3;
   };
+  dcor = {
+    "0.6" = import ./overrides/dcor/0.6;
+  };
   dd-aliyun-python-sdk-core = {
     "2.8.2" = import ./overrides/dd-aliyun-python-sdk-core/2.8.2;
   };
