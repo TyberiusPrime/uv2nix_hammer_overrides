d24a2d1bee9576d131be97e4083e6aa55859fda4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 17:36:16 +0000

    autogenerated overwrites for phoenixdb==1.2.2

--- collected.nix
+++ collected.nix
@@ -5860,6 +5860,9 @@
   pgsu = {
     "0.2.4" = import ./overrides/pgsu/0.2.4;
   };
+  phoenixdb = {
+    "1.2.2" = import ./overrides/phoenixdb/1.2.2;
+  };
   phonetics = {
     "1.0.5" = import ./overrides/phonetics/1.0.5;
   };
