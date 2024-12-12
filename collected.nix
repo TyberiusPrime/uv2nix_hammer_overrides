9ea0aedf0490d48b37297fdfe63531f5ae1b8449
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:52:22 +0000

    autogenerated overwrites for gherkin-official==29.0.0

--- collected.nix
+++ collected.nix
@@ -3155,6 +3155,9 @@
   ghdiff = {
     "0.4" = import ./overrides/ghdiff/0.4;
   };
+  gherkin-official = {
+    "29.0.0" = import ./overrides/gherkin-official/29.0.0;
+  };
   gif2numpy = {
     "1.3" = import ./overrides/gif2numpy/1.3;
   };
