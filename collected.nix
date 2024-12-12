b6bc9a5eda72758ea226712df2c9094ce24549a4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 16:53:26 +0000

    autogenerated overwrites for diazo==2.0.2

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  diazo = {
+    "2.0.2" = import ./overrides/diazo/2.0.2;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
