dec3f678b0ccb3209b737f3b745d79fbc1531b52
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 19:07:48 +0000

    autogenerated overwrites for pyisy==3.1.14

--- collected.nix
+++ collected.nix
@@ -6752,6 +6752,9 @@
   pyipma = {
     "3.0.7" = import ./overrides/pyipma/3.0.7;
   };
+  pyisy = {
+    "3.1.14" = import ./overrides/pyisy/3.1.14;
+  };
   pyjade = {
     "4.0.0" = import ./overrides/pyjade/4.0.0;
   };
