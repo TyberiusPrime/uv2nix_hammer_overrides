f1a34f61771bb6aceb972da719cd5167b476b8e4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:50:04 +0000

    autogenerated overwrites for python-editor==1.0.4

--- collected.nix
+++ collected.nix
@@ -7303,6 +7303,9 @@
   python-doi = {
     "0.2.0" = import ./overrides/python-doi/0.2.0;
   };
+  python-editor = {
+    "1.0.4" = import ./overrides/python-editor/1.0.4;
+  };
   python-etcd = {
     "0.4.5" = import ./overrides/python-etcd/0.4.5;
   };
