103dbff1e1ccebb5050e4473e86a5c5f95039186
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:43:54 +0000

    autogenerated overwrites for djangomix==1.1.3

--- collected.nix
+++ collected.nix
@@ -2228,6 +2228,9 @@
   djangocodemirror = {
     "2.1.0" = import ./overrides/djangocodemirror/2.1.0;
   };
+  djangomix = {
+    "1.1.3" = import ./overrides/djangomix/1.1.3;
+  };
   djangorestframework-camel-case = {
     "1.4.2" = import ./overrides/djangorestframework-camel-case/1.4.2;
   };
