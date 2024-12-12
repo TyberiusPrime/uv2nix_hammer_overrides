80b369c8abfa79144e77b809e8054cc656bb3472
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:34:53 +0000

    autogenerated overwrites for jupyter-contrib-nbextensions==0.7.0

--- overrides/jupyter-contrib-nbextensions/0.7.0/default.nix
+++ overrides/jupyter-contrib-nbextensions/0.7.0/default.nix
@@ -1,10 +1,3 @@
-{ helpers, ... }:
-old:
-if (helpers.isWheel old) then
-  { }
-else
-  {
-    env = {
-      dontCheckRuntimeDeps = true;
-    };
-  }
+{final, helpers, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {} else {env = {dontCheckRuntimeDeps = true;};nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+        
