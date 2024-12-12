cabd6076b35a82298f89340313b9dab4950d9bd9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:28:27 +0000

    autogenerated overwrites for rawkit==0.6.0

--- collected.nix
+++ collected.nix
@@ -7787,6 +7787,9 @@
   rauth = {
     "0.7.3" = import ./overrides/rauth/0.7.3;
   };
+  rawkit = {
+    "0.6.0" = import ./overrides/rawkit/0.6.0;
+  };
   rcsb-utils-io = {
     "1.49" = import ./overrides/rcsb-utils-io/1.49;
   };
