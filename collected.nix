48ed0d2c2ddf6978dedf215ca8835bc4e6bbd60d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:13:40 +0000

    autogenerated overwrites for poolsense==0.1.0

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  poolsense = {
+    "0.1.0" = import ./overrides/poolsense/0.1.0;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
