084697bc9e479809c7f1c3c046e080f82965d02e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:24:00 +0000

    autogenerated overwrites for sslib==0.2.0

--- collected.nix
+++ collected.nix
@@ -8708,6 +8708,9 @@
   ssh = {
     "1.8.0" = import ./overrides/ssh/1.8.0;
   };
+  sslib = {
+    "0.2.0" = import ./overrides/sslib/0.2.0;
+  };
   sslyze = {
     "6.0.0" = import ./overrides/sslyze/6.0.0;
   };
