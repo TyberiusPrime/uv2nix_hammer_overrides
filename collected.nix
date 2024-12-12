d3872c8360c9c325f21bf74da581fa78aa9a3f45
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 23:00:59 +0000

    autogenerated overwrites for psyplot==1.5.1

--- collected.nix
+++ collected.nix
@@ -6237,6 +6237,9 @@
   psycopg2cffi = {
     "2.9.0" = import ./overrides/psycopg2cffi/2.9.0;
   };
+  psyplot = {
+    "1.5.1" = import ./overrides/psyplot/1.5.1;
+  };
   ptable = {
     "0.9.2" = import ./overrides/ptable/0.9.2;
   };
