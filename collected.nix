74b4d4fc0190a789dafe7825e4bc5ccb0df1f639
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:14:32 +0000

    autogenerated overwrites for collective.prettydate==1.2.2

--- collected.nix
+++ collected.nix
@@ -1283,6 +1283,9 @@
   collective-js-jqueryui = {
     "2.2.0" = import ./overrides/collective-js-jqueryui/2.2.0;
   };
+  collective-prettydate = {
+    "1.2.2" = import ./overrides/collective-prettydate/1.2.2;
+  };
   collective-quickupload = {
     "1.11.1" = import ./overrides/collective-quickupload/1.11.1;
   };
