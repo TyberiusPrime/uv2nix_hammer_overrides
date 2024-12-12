fbea7c14e96b7ab48f44d2cb6d2271a2e2c5b10e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:31:20 +0000

    autogenerated overwrites for click-configfile==0.2.3

--- collected.nix
+++ collected.nix
@@ -1113,6 +1113,9 @@
   click-completion = {
     "0.5.2" = import ./overrides/click-completion/0.5.2;
   };
+  click-configfile = {
+    "0.2.3" = import ./overrides/click-configfile/0.2.3;
+  };
   click-didyoumean = {
     "0.0.3" = import ./overrides/click-didyoumean/0.0.3;
   };
