15577ceb7fe16c4deda10ef9a4b7d525df329c12
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:10:49 +0000

    autogenerated overwrites for netmiko==4.4.0

--- collected.nix
+++ collected.nix
@@ -5190,6 +5190,9 @@
   netifaces = {
     "0.11.0" = import ./overrides/netifaces/0.11.0;
   };
+  netmiko = {
+    "4.4.0" = import ./overrides/netmiko/4.4.0;
+  };
   netstruct = {
     "1.1.2" = import ./overrides/netstruct/1.1.2;
   };
