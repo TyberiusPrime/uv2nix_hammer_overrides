0d31a32f28840b1669158d3ad7d63d0f1b56b79c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 09:57:27 +0000

    autogenerated overwrites for jsonschema-specifications==2023.12.1

--- collected.nix
+++ collected.nix
@@ -4051,6 +4051,9 @@
   jsonrpcserver = {
     "5.0.9" = import ./overrides/jsonrpcserver/5.0.9;
   };
+  jsonschema-specifications = {
+    "2023.12.1" = import ./overrides/jsonschema-specifications/2023.12.1;
+  };
   jsonsir = {
     "0.0.2" = import ./overrides/jsonsir/0.0.2;
   };
