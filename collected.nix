26bb45b24f375483ef9d8f69a79d48c7bae143e3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:02:14 +0000

    autogenerated overwrites for vipy==1.14.4

--- collected.nix
+++ collected.nix
@@ -9702,6 +9702,9 @@
   vincenty = {
     "0.1.4" = import ./overrides/vincenty/0.1.4;
   };
+  vipy = {
+    "1.14.4" = import ./overrides/vipy/1.14.4;
+  };
   virtstrap-core = {
     "0.3.16" = import ./overrides/virtstrap-core/0.3.16;
   };
