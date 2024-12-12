572d30df97a715b5794542b229c53e1bb61a2473
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:26:40 +0000

    autogenerated overwrites for python-lsp-ruff==2.2.2

--- collected.nix
+++ collected.nix
@@ -7397,6 +7397,9 @@
   python-louvain = {
     "0.16" = import ./overrides/python-louvain/0.16;
   };
+  python-lsp-ruff = {
+    "2.2.2" = import ./overrides/python-lsp-ruff/2.2.2;
+  };
   python-ly = {
     "0.9.7" = import ./overrides/python-ly/0.9.7;
   };
