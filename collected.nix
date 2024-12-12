030941937f1ece3e8a00c98b71af2d344a94a351
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 19:58:54 +0000

    autogenerated overwrites for aioresponses==0.7.6

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioresponses = {
+    "0.7.6" = import ./overrides/aioresponses/0.7.6;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
