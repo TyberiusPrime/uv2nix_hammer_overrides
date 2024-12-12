0d9928403fe3215403b106875993001a95ddb399
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:11:33 +0000

    autogenerated overwrites for deid==0.3.24

--- collected.nix
+++ collected.nix
@@ -1758,6 +1758,9 @@
   defopt = {
     "6.4.0" = import ./overrides/defopt/6.4.0;
   };
+  deid = {
+    "0.3.24" = import ./overrides/deid/0.3.24;
+  };
   delfick-error = {
     "1.8" = import ./overrides/delfick-error/1.8;
   };
