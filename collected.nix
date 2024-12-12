d799c85209483663a180a56ab547c62b1a916d98
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 10:48:52 +0000

    autogenerated overwrites for daiquiri==3.2.5.1

--- collected.nix
+++ collected.nix
@@ -1659,6 +1659,9 @@
   daff = {
     "1.3.46" = import ./overrides/daff/1.3.46;
   };
+  daiquiri = {
+    "3.2.5.1" = import ./overrides/daiquiri/3.2.5.1;
+  };
   dark-matter = {
     "4.0.89" = import ./overrides/dark-matter/4.0.89;
   };
