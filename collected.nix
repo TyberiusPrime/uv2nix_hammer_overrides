141fad7e8481ce2e21d95c052511c5975b9c56db
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 21:50:10 +0000

    autogenerated overwrites for trezor==0.13.9

--- collected.nix
+++ collected.nix
@@ -9314,6 +9314,9 @@
   trello = {
     "0.9.7.3" = import ./overrides/trello/0.9.7.3;
   };
+  trezor = {
+    "0.13.9" = import ./overrides/trezor/0.13.9;
+  };
   tri-struct = {
     "4.1.0" = import ./overrides/tri-struct/4.1.0;
   };
