76ac1077e97b904484341763318735aa88bcd5a9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 11:23:19 +0000

    autogenerated overwrites for pytest-logger==1.1.1

--- collected.nix
+++ collected.nix
@@ -7227,6 +7227,9 @@
   pytest-json = {
     "0.4.0" = import ./overrides/pytest-json/0.4.0;
   };
+  pytest-logger = {
+    "1.1.1" = import ./overrides/pytest-logger/1.1.1;
+  };
   pytest-logging = {
     "2015.11.4" = import ./overrides/pytest-logging/2015.11.4;
   };
