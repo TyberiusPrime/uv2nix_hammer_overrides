8d87bcc2d5a979a04c7c52b2cd7faa648f5783e9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 12:29:12 +0000

    autogenerated overwrites for tomorrow3==1.2.2

--- collected.nix
+++ collected.nix
@@ -9163,6 +9163,9 @@
   toml-py = {
     "0.1.7" = import ./overrides/toml-py/0.1.7;
   };
+  tomorrow3 = {
+    "1.2.2" = import ./overrides/tomorrow3/1.2.2;
+  };
   tonyg-rfc3339 = {
     "0.1" = import ./overrides/tonyg-rfc3339/0.1;
   };
