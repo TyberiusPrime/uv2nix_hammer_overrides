866394fae8d66807ce21618de331e75658ee5efa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:59:05 +0000

    autogenerated overwrites for spectral-cube==0.6.5

--- collected.nix
+++ collected.nix
@@ -8598,6 +8598,9 @@
   spectra = {
     "0.0.11" = import ./overrides/spectra/0.0.11;
   };
+  spectral-cube = {
+    "0.6.5" = import ./overrides/spectral-cube/0.6.5;
+  };
   speg = {
     "0.3" = import ./overrides/speg/0.3;
   };
