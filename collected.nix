5a2dd6af31107fb4efee363fc273d374480bb2cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 09:13:15 +0000

    autogenerated overwrites for python-roborock==2.6.0

--- collected.nix
+++ collected.nix
@@ -7454,6 +7454,9 @@
   python-qpid-proton = {
     "0.39.0" = import ./overrides/python-qpid-proton/0.39.0;
   };
+  python-roborock = {
+    "2.6.0" = import ./overrides/python-roborock/2.6.0;
+  };
   python-rtmidi = {
     "1.5.8" = import ./overrides/python-rtmidi/1.5.8;
   };
