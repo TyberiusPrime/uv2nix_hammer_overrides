92d42a83c45575bbb4d53a35660523940fbb1dcd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 14:09:31 +0000

    autogenerated overwrites for adafruit-ampy==1.1.0

--- collected.nix
+++ collected.nix
@@ -35,6 +35,9 @@
   active-alchemy = {
     "1.1.0" = import ./overrides/active-alchemy/1.1.0;
   };
+  adafruit-ampy = {
+    "1.1.0" = import ./overrides/adafruit-ampy/1.1.0;
+  };
   adafruit-extended-bus = {
     "1.0.2" = import ./overrides/adafruit-extended-bus/1.0.2;
   };
