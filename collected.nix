ae89e5be359045af5036f09689e8d5b001253585
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:10:50 +0000

    autogenerated overwrites for opytimark==1.0.8

--- collected.nix
+++ collected.nix
@@ -5587,6 +5587,9 @@
   opuslib = {
     "3.0.1" = import ./overrides/opuslib/3.0.1;
   };
+  opytimark = {
+    "1.0.8" = import ./overrides/opytimark/1.0.8;
+  };
   orcid = {
     "1.0.3" = import ./overrides/orcid/1.0.3;
   };
