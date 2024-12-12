1e936d72eba2be2954e2a660fa0cb8f14ed970b2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:40:24 +0000

    autogenerated overwrites for cleanco==2.3

--- collected.nix
+++ collected.nix
@@ -1101,6 +1101,9 @@
   classproperties = {
     "0.2.0" = import ./overrides/classproperties/0.2.0;
   };
+  cleanco = {
+    "2.3" = import ./overrides/cleanco/2.3;
+  };
   cleandev-config-loader = {
     "0.3.5" = import ./overrides/cleandev-config-loader/0.3.5;
   };
