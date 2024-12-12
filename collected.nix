d44caff447ae3209e44d28322cb42a7fd807577d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:49:16 +0000

    autogenerated overwrites for md-toc==9.0.0

--- collected.nix
+++ collected.nix
@@ -4670,6 +4670,9 @@
   mcfit = {
     "0.0.22" = import ./overrides/mcfit/0.0.22;
   };
+  md-toc = {
+    "9.0.0" = import ./overrides/md-toc/9.0.0;
+  };
   mdstat = {
     "1.0.4" = import ./overrides/mdstat/1.0.4;
   };
