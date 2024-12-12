6f444d5b0737046635f9843c86918093fc960dc0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:16:00 +0000

    autogenerated overwrites for aiotools==1.7.0

--- collected.nix
+++ collected.nix
@@ -141,6 +141,9 @@
   aiosocks = {
     "0.2.6" = import ./overrides/aiosocks/0.2.6;
   };
+  aiotools = {
+    "1.7.0" = import ./overrides/aiotools/1.7.0;
+  };
   aioxmpp = {
     "0.13.3" = import ./overrides/aioxmpp/0.13.3;
   };
