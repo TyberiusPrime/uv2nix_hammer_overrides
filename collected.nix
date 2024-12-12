4a1d2a86f271ec9fcf0f6b9276b37e2e5663b976
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 22:58:39 +0000

    autogenerated overwrites for django-widget-tweaks==1.5.0

--- collected.nix
+++ collected.nix
@@ -2222,6 +2222,9 @@
   django-vkontakte-users = {
     "0.8.7" = import ./overrides/django-vkontakte-users/0.8.7;
   };
+  django-widget-tweaks = {
+    "1.5.0" = import ./overrides/django-widget-tweaks/1.5.0;
+  };
   djangoapidec = {
     "1.8" = import ./overrides/djangoapidec/1.8;
   };
