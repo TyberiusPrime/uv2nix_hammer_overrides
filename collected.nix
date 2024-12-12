0aa878e1016144de6385b4da4fa63dc14b8e9c0b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 09:41:56 +0000

    autogenerated overwrites for httplib2==0.22.0

--- collected.nix
+++ collected.nix
@@ -3556,6 +3556,9 @@
   httpexceptor = {
     "1.4.0" = import ./overrides/httpexceptor/1.4.0;
   };
+  httplib2 = {
+    "0.22.0" = import ./overrides/httplib2/0.22.0;
+  };
   httplib2shim = {
     "0.0.3" = import ./overrides/httplib2shim/0.0.3;
   };
