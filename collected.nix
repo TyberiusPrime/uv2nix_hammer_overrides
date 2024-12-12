5b3ce80579cf827f1f8fece6a743c2be23d12f2c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:18:14 +0000

    autogenerated overwrites for finalfusion==0.7.1

--- collected.nix
+++ collected.nix
@@ -2769,6 +2769,9 @@
   filterpy = {
     "1.4.5" = import ./overrides/filterpy/1.4.5;
   };
+  finalfusion = {
+    "0.7.1" = import ./overrides/finalfusion/0.7.1;
+  };
   findlibs = {
     "0.0.5" = import ./overrides/findlibs/0.0.5;
   };
