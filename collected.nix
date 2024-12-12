064839473c0912b2de2e22a902e82c4be1e09298
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 17:44:27 +0000

    autogenerated overwrites for types-chardet==5.0.4.6

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-chardet = {
+    "5.0.4.6" = import ./overrides/types-chardet/5.0.4.6;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
