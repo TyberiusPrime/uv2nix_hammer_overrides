e85fd50c11882fc42c759072712a0c21d304f96f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:25:53 +0000

    autogenerated overwrites for pyramid-mako==1.1.0

--- collected.nix
+++ collected.nix
@@ -7039,6 +7039,9 @@
   pyramid-deform = {
     "0.2" = import ./overrides/pyramid-deform/0.2;
   };
+  pyramid-mako = {
+    "1.1.0" = import ./overrides/pyramid-mako/1.1.0;
+  };
   pyramid-redis-sessions = {
     "1.0.1" = import ./overrides/pyramid-redis-sessions/1.0.1;
   };
