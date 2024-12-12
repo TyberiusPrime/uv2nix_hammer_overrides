c678d2a352025634ad1070b06b26437409ba3b82
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 09:39:59 +0000

    autogenerated overwrites for pipen==0.15.5

--- collected.nix
+++ collected.nix
@@ -5937,6 +5937,9 @@
   pip-services3-rpc = {
     "3.3.4" = import ./overrides/pip-services3-rpc/3.3.4;
   };
+  pipen = {
+    "0.15.5" = import ./overrides/pipen/0.15.5;
+  };
   pipfile = {
     "0.0.2" = import ./overrides/pipfile/0.0.2;
   };
