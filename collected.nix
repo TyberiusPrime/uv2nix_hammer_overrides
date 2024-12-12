d9c976ac6d1f9eed43babb51be096e2a3207907c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 20:20:49 +0000

    autogenerated overwrites for prometheus-pandas==0.3.3

--- collected.nix
+++ collected.nix
@@ -6180,6 +6180,9 @@
   projectq = {
     "0.8.0" = import ./overrides/projectq/0.8.0;
   };
+  prometheus-pandas = {
+    "0.3.3" = import ./overrides/prometheus-pandas/0.3.3;
+  };
   promise = {
     "2.3" = import ./overrides/promise/2.3;
   };
