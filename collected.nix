955c781594e8cf6677febc3d68f26fdc3b665854
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 18:01:31 +0000

    autogenerated overwrites for queenbee==1.28.0

--- collected.nix
+++ collected.nix
@@ -7727,6 +7727,9 @@
   quantiphyse-fabber = {
     "0.9.2" = import ./overrides/quantiphyse-fabber/0.9.2;
   };
+  queenbee = {
+    "1.28.0" = import ./overrides/queenbee/1.28.0;
+  };
   query = {
     "0.1.4" = import ./overrides/query/0.1.4;
   };
