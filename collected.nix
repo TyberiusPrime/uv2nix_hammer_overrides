eeebe6dc900b86e05ca39774d71eb68e39b38e4e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 15:09:34 +0000

    autogenerated overwrites for httpx==0.27.2

--- collected.nix
+++ collected.nix
@@ -3562,6 +3562,9 @@
   httpretty = {
     "1.1.4" = import ./overrides/httpretty/1.1.4;
   };
+  httpx = {
+    "0.27.2" = import ./overrides/httpx/0.27.2;
+  };
   huawei-lte-api = {
     "1.9.3" = import ./overrides/huawei-lte-api/1.9.3;
   };
