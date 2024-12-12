7e5dda2332b969d3f6b6365e4d489b862dbbd98b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 13:46:41 +0000

    autogenerated overwrites for google-cloud-runtimeconfig==0.34.0

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-runtimeconfig = {
+    "0.34.0" = import ./overrides/google-cloud-runtimeconfig/0.34.0;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
