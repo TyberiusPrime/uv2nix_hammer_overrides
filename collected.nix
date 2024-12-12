bd2f65d107e9e6f0b7a57edb7f16b2a7c25e5139
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-12 16:47:20 +0000

    autogenerated overwrites for toonapi==0.3.0

--- collected.nix
+++ collected.nix
@@ -9175,6 +9175,9 @@
   toolwrapper = {
     "2.1.0" = import ./overrides/toolwrapper/2.1.0;
   };
+  toonapi = {
+    "0.3.0" = import ./overrides/toonapi/0.3.0;
+  };
   torch = {
     "2.4.0" = import ./overrides/torch/2.4.0;
     "2.4.1" = import ./overrides/torch/2.4.1;
