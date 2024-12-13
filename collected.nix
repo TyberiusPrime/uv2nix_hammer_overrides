9b67d3c31c01e73484038935ef3ca18c61ffb674
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:45:08 +0000

    autogenerated overwrites for asgi-logger==0.1.0

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  asgi-logger = {
+    "0.1.0" = import ./overrides/asgi-logger/0.1.0;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
