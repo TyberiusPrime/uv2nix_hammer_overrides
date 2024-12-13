2cec3b65f5d5f4252602beb1560918134caf1a4a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:51:52 +0000

    autogenerated overwrites for duecredit==0.10.2

--- collected.nix
+++ collected.nix
@@ -2406,6 +2406,9 @@
   duckietown-docker-utils-daffy = {
     "6.1.1" = import ./overrides/duckietown-docker-utils-daffy/6.1.1;
   };
+  duecredit = {
+    "0.10.2" = import ./overrides/duecredit/0.10.2;
+  };
   dugong = {
     "3.8.2" = import ./overrides/dugong/3.8.2;
   };
