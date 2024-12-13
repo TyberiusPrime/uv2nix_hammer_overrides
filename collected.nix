172a375626ee839b2ff5062700de14af3e095ae6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:07:20 +0000

    autogenerated overwrites for scikit-lego==0.9.1

--- collected.nix
+++ collected.nix
@@ -8211,6 +8211,9 @@
     "0.24.2" = import ./overrides/scikit-learn/0.24.2;
     "1.2.2" = import ./overrides/scikit-learn/1.2.2;
   };
+  scikit-lego = {
+    "0.9.1" = import ./overrides/scikit-lego/0.9.1;
+  };
   scikit-surgerycore = {
     "0.7.2" = import ./overrides/scikit-surgerycore/0.7.2;
   };
