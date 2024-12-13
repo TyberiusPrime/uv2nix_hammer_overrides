81ea3b8adb98c8d05b5b5ee102567bd8f3da4cf1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:26:17 +0000

    autogenerated overwrites for pooch==1.8.2

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  pooch = {
+    "1.8.2" = import ./overrides/pooch/1.8.2;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
