b36a53d2c7853c51495c48ac521613c5ca8b819e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:46:52 +0000

    autogenerated overwrites for eth-hash==0.7.0

--- collected.nix
+++ collected.nix
@@ -2580,6 +2580,9 @@
   etgen = {
     "1.1.4" = import ./overrides/etgen/1.1.4;
   };
+  eth-hash = {
+    "0.7.0" = import ./overrides/eth-hash/0.7.0;
+  };
   ethereum = {
     "2.3.2" = import ./overrides/ethereum/2.3.2;
   };
