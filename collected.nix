6635b5eed181b5896094a1383d64787a86c949a6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:26:46 +0000

    autogenerated overwrites for awesomeversion==24.6.0

--- collected.nix
+++ collected.nix
@@ -518,6 +518,9 @@
   awesome-slugify = {
     "1.6.5" = import ./overrides/awesome-slugify/1.6.5;
   };
+  awesomeversion = {
+    "24.6.0" = import ./overrides/awesomeversion/24.6.0;
+  };
   awkward = {
     "1.10.5" = import ./overrides/awkward/1.10.5;
   };
