ba60fe815c0c1f5c261eec421aa12de8cb2f7948
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:52:12 +0000

    autogenerated overwrites for syncedlyrics==1.0.1

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  syncedlyrics = {
+    "1.0.1" = import ./overrides/syncedlyrics/1.0.1;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
