5e8664fc3ccca4eec5d1a79d30c2734fa77899a2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:38:27 +0000

    autogenerated overwrites for fastapi-jwt-auth==0.5.0

--- collected.nix
+++ collected.nix
@@ -2679,6 +2679,9 @@
   fastapi-cli = {
     "0.0.5" = import ./overrides/fastapi-cli/0.0.5;
   };
+  fastapi-jwt-auth = {
+    "0.5.0" = import ./overrides/fastapi-jwt-auth/0.5.0;
+  };
   fastcache = {
     "1.1.0" = import ./overrides/fastcache/1.1.0;
   };
