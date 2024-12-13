0ba97f8d27e6ff84eb5960fc5d34561fa0704c54
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:00:01 +0000

    autogenerated overwrites for xopen==2.0.2

--- collected.nix
+++ collected.nix
@@ -10031,6 +10031,9 @@
   xobjects = {
     "0.4.4" = import ./overrides/xobjects/0.4.4;
   };
+  xopen = {
+    "2.0.2" = import ./overrides/xopen/2.0.2;
+  };
   xrootd = {
     "5.7.1" = import ./overrides/xrootd/5.7.1;
   };
