3b4f0dbf7d4895ec10bca28eb81902531757308f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:12:33 +0000

    autogenerated overwrites for django-parler==2.3

--- collected.nix
+++ collected.nix
@@ -2081,6 +2081,9 @@
   django-paintstore = {
     "0.2" = import ./overrides/django-paintstore/0.2;
   };
+  django-parler = {
+    "2.3" = import ./overrides/django-parler/2.3;
+  };
   django-pglocks = {
     "1.0.4" = import ./overrides/django-pglocks/1.0.4;
   };
