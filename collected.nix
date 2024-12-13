c1ecf63e63b5bc4c88959b9e1112e6eeafdce9d6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:41:42 +0000

    autogenerated overwrites for dronekit==2.9.2

--- collected.nix
+++ collected.nix
@@ -2388,6 +2388,9 @@
   drfdocs = {
     "0.0.11" = import ./overrides/drfdocs/0.0.11;
   };
+  dronekit = {
+    "2.9.2" = import ./overrides/dronekit/2.9.2;
+  };
   dsinternals = {
     "1.2.4" = import ./overrides/dsinternals/1.2.4;
   };
