ed3e66be0cb9f85844ec23b65dbf9b8dd9ad4240
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:49:03 +0000

    autogenerated overwrites for convertapi==1.8.0

--- collected.nix
+++ collected.nix
@@ -1403,6 +1403,9 @@
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
+  convertapi = {
+    "1.8.0" = import ./overrides/convertapi/1.8.0;
+  };
   coqui-trainer = {
     "0.0.5" = import ./overrides/coqui-trainer/0.0.5;
   };
