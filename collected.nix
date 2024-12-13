d4554151322941381c48da451625ef7d457d42bf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 16:04:50 +0000

    autogenerated overwrites for google-cloud-vision==3.7.4

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-vision = {
+    "3.7.4" = import ./overrides/google-cloud-vision/3.7.4;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
