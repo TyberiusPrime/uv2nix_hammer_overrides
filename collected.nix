5fd0b26eb78ae6e9405963790e8f01b5ae88e4e4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:08:08 +0000

    autogenerated overwrites for intermine==1.13.0

--- collected.nix
+++ collected.nix
@@ -3796,6 +3796,9 @@
   intermake = {
     "1.0.0.78" = import ./overrides/intermake/1.0.0.78;
   };
+  intermine = {
+    "1.13.0" = import ./overrides/intermine/1.13.0;
+  };
   interruptingcow = {
     "0.8" = import ./overrides/interruptingcow/0.8;
   };
