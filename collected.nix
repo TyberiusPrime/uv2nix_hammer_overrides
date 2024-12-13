7f72067be503cd85a1eaf2828342415464b91c17
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:04:12 +0000

    autogenerated overwrites for readme-renderer==44.0

--- collected.nix
+++ collected.nix
@@ -7799,6 +7799,9 @@
   readline = {
     "6.2.4.2" = import ./overrides/readline/6.2.4.2;
   };
+  readme-renderer = {
+    "44.0" = import ./overrides/readme-renderer/44.0;
+  };
   readtime = {
     "3.0.0" = import ./overrides/readtime/3.0.0;
   };
