f300e58c11ca44a25dfc98271279816675e2fc1b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:34:14 +0000

    autogenerated overwrites for pyevilgenius==2.0.0

--- collected.nix
+++ collected.nix
@@ -6605,6 +6605,9 @@
   pyethash = {
     "0.1.27" = import ./overrides/pyethash/0.1.27;
   };
+  pyevilgenius = {
+    "2.0.0" = import ./overrides/pyevilgenius/2.0.0;
+  };
   pyexcel = {
     "0.4.5" = import ./overrides/pyexcel/0.4.5;
   };
