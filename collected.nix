dc3265545f5a74876354868b1add4a27854f8ade
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:55:16 +0000

    autogenerated overwrites for hazm==0.10.0

--- collected.nix
+++ collected.nix
@@ -3408,6 +3408,9 @@
   hash-ring = {
     "1.3.1" = import ./overrides/hash-ring/1.3.1;
   };
+  hazm = {
+    "0.10.0" = import ./overrides/hazm/0.10.0;
+  };
   hbmqtt = {
     "0.9.6" = import ./overrides/hbmqtt/0.9.6;
   };
