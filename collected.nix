eb721742570e6835b5758147665b4b21a5fc13bd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:57:56 +0000

    autogenerated overwrites for geemap==0.34.1

--- collected.nix
+++ collected.nix
@@ -3110,6 +3110,9 @@
   gdspy = {
     "1.6.13" = import ./overrides/gdspy/1.6.13;
   };
+  geemap = {
+    "0.34.1" = import ./overrides/geemap/0.34.1;
+  };
   genpy = {
     "2022.1" = import ./overrides/genpy/2022.1;
   };
