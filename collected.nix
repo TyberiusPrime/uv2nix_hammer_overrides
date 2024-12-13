e43a8de3090f82d0de118b84ececc94cb238d1fb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:40:49 +0000

    autogenerated overwrites for azure-mgmt-cosmosdb==9.5.1

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-mgmt-cosmosdb = {
+    "9.5.1" = import ./overrides/azure-mgmt-cosmosdb/9.5.1;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
