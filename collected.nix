e2119945d2da8ff72152cd87da16505b5c8af833
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:32:08 +0000

    autogenerated overwrites for webencodings==0.5.1

--- collected.nix
+++ collected.nix
@@ -9838,6 +9838,9 @@
   webdavclient3 = {
     "3.14.6" = import ./overrides/webdavclient3/3.14.6;
   };
+  webencodings = {
+    "0.5.1" = import ./overrides/webencodings/0.5.1;
+  };
   webexteamssdk = {
     "1.7" = import ./overrides/webexteamssdk/1.7;
   };
