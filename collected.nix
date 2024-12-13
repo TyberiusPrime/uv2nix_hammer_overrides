194d64efa6f51f67fde76ff3e2685e5ce27fe536
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:59:36 +0000

    autogenerated overwrites for aiounifi==80

--- collected.nix
+++ collected.nix
@@ -141,6 +141,9 @@
   aiosocks = {
     "0.2.6" = import ./overrides/aiosocks/0.2.6;
   };
+  aiounifi = {
+    "80" = import ./overrides/aiounifi/80;
+  };
   aioxmpp = {
     "0.13.3" = import ./overrides/aioxmpp/0.13.3;
   };
