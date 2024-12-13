e9f448a7177482e0baab33f8e556021c7d418e37
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 19:27:15 +0000

    autogenerated overwrites for static-ffmpeg==2.7

--- collected.nix
+++ collected.nix
@@ -8726,6 +8726,9 @@
   static = {
     "1.1.1" = import ./overrides/static/1.1.1;
   };
+  static-ffmpeg = {
+    "2.7" = import ./overrides/static-ffmpeg/2.7;
+  };
   static3 = {
     "0.7.0" = import ./overrides/static3/0.7.0;
   };
