adb46cea6333ef665fe527c7f15178c1bf0a637c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:24:38 +0000

    autogenerated overwrites for pytest-asyncio==0.24.0

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-asyncio = {
+    "0.24.0" = import ./overrides/pytest-asyncio/0.24.0;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
