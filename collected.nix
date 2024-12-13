ab0e48257da7bde1566751a699edd500bbb1fae8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:11:31 +0000

    autogenerated overwrites for bayserver-core==2.3.5

--- collected.nix
+++ collected.nix
@@ -644,6 +644,9 @@
   bayespy = {
     "0.6.2" = import ./overrides/bayespy/0.6.2;
   };
+  bayserver-core = {
+    "2.3.5" = import ./overrides/bayserver-core/2.3.5;
+  };
   bcf = {
     "1.9.0" = import ./overrides/bcf/1.9.0;
   };
