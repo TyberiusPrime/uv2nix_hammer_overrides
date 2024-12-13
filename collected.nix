7c6479da5e4dc6450d6b4038095e285ede98aeac
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:19:18 +0000

    autogenerated overwrites for mixpanel==4.10.1

--- collected.nix
+++ collected.nix
@@ -4829,6 +4829,9 @@
   misaka = {
     "2.1.1" = import ./overrides/misaka/2.1.1;
   };
+  mixpanel = {
+    "4.10.1" = import ./overrides/mixpanel/4.10.1;
+  };
   mkdocs-exclude = {
     "1.0.2" = import ./overrides/mkdocs-exclude/1.0.2;
   };
