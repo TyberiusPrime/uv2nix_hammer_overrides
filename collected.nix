a61b73c1dc3ab0253d8e5620ddeb7d24b992de11
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:38:12 +0000

    autogenerated overwrites for wtforms-bootstrap5==0.3.0

--- collected.nix
+++ collected.nix
@@ -9955,6 +9955,9 @@
   wtf-peewee = {
     "3.0.5" = import ./overrides/wtf-peewee/3.0.5;
   };
+  wtforms-bootstrap5 = {
+    "0.3.0" = import ./overrides/wtforms-bootstrap5/0.3.0;
+  };
   wuttjamaican = {
     "0.13.3" = import ./overrides/wuttjamaican/0.13.3;
   };
