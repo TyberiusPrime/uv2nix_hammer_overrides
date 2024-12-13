18c7e35f6bf27bd71bf96211f224b9de155b231c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:22:44 +0000

    autogenerated overwrites for httptools==0.6.1

--- collected.nix
+++ collected.nix
@@ -3562,6 +3562,9 @@
   httpretty = {
     "1.1.4" = import ./overrides/httpretty/1.1.4;
   };
+  httptools = {
+    "0.6.1" = import ./overrides/httptools/0.6.1;
+  };
   huawei-lte-api = {
     "1.9.3" = import ./overrides/huawei-lte-api/1.9.3;
   };
