ce72fd4d27d3163f757159e3fe2725bdb667af5f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:45:43 +0000

    autogenerated overwrites for oci==2.133.0

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  oci = {
+    "2.133.0" = import ./overrides/oci/2.133.0;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
