9122ef15f2f0170cf3db2a9c777810ae915ba1bb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:25:13 +0000

    autogenerated overwrites for types-paramiko==3.4.0.20240423

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-paramiko = {
+    "3.4.0.20240423" = import ./overrides/types-paramiko/3.4.0.20240423;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
