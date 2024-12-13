ab86e0917f3f2d8d3f512277248badbda7b8ec3f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:17:59 +0000

    autogenerated overwrites for sagemaker==2.231.0

--- collected.nix
+++ collected.nix
@@ -8108,6 +8108,9 @@
   safeio = {
     "1.2" = import ./overrides/safeio/1.2;
   };
+  sagemaker = {
+    "2.231.0" = import ./overrides/sagemaker/2.231.0;
+  };
   salsa20 = {
     "0.3.0" = import ./overrides/salsa20/0.3.0;
   };
