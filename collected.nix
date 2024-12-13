00cc10a1270f5955c21cd0fc2a9f014f3d01f5cb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:35:40 +0000

    autogenerated overwrites for datasets==2.21.0

--- collected.nix
+++ collected.nix
@@ -1689,6 +1689,9 @@
   datapipelines = {
     "1.0.7" = import ./overrides/datapipelines/1.0.7;
   };
+  datasets = {
+    "2.21.0" = import ./overrides/datasets/2.21.0;
+  };
   datashape = {
     "0.5.2" = import ./overrides/datashape/0.5.2;
   };
