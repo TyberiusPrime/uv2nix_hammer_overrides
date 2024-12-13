4b4f6c8a49259c7681a345850906993ba548a188
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 19:08:41 +0000

    autogenerated overwrites for pgpasslib==1.1.0

--- collected.nix
+++ collected.nix
@@ -5848,6 +5848,9 @@
   pfurl = {
     "2.3.0.1" = import ./overrides/pfurl/2.3.0.1;
   };
+  pgpasslib = {
+    "1.1.0" = import ./overrides/pgpasslib/1.1.0;
+  };
   pgpdump = {
     "1.5" = import ./overrides/pgpdump/1.5;
   };
