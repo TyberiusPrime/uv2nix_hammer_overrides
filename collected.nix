395b1ff8fff628b2ca3ded2ef3b910837350837a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:05:29 +0000

    autogenerated overwrites for bpc-utils==0.10.1

--- collected.nix
+++ collected.nix
@@ -816,6 +816,9 @@
   box2d-py = {
     "2.3.8" = import ./overrides/box2d-py/2.3.8;
   };
+  bpc-utils = {
+    "0.10.1" = import ./overrides/bpc-utils/0.10.1;
+  };
   bpycv = {
     "0.4.0" = import ./overrides/bpycv/0.4.0;
   };
