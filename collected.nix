460b58b34e7de0347fd66105c3b739f9df5b0c37
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:12:16 +0000

    autogenerated overwrites for optbinning==0.19.0

--- collected.nix
+++ collected.nix
@@ -5575,6 +5575,9 @@
   opster = {
     "5.0" = import ./overrides/opster/5.0;
   };
+  optbinning = {
+    "0.19.0" = import ./overrides/optbinning/0.19.0;
+  };
   option-merge = {
     "1.6" = import ./overrides/option-merge/1.6;
   };
