5764e32f9bb030f4fe1ad532321ba540b61165af
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:55:08 +0000

    autogenerated overwrites for pydm==1.24.1

--- collected.nix
+++ collected.nix
@@ -6563,6 +6563,9 @@
   pydevd-pycharm = {
     "242.21829.153" = import ./overrides/pydevd-pycharm/242.21829.153;
   };
+  pydm = {
+    "1.24.1" = import ./overrides/pydm/1.24.1;
+  };
   pydoe = {
     "0.3.8" = import ./overrides/pydoe/0.3.8;
   };
