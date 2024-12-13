e9923a321a660dd1f5c4bf0f743cfc793525313e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:31:04 +0000

    autogenerated overwrites for particle==0.25.0

--- collected.nix
+++ collected.nix
@@ -5726,6 +5726,9 @@
   parsimonious = {
     "0.9.0" = import ./overrides/parsimonious/0.9.0;
   };
+  particle = {
+    "0.25.0" = import ./overrides/particle/0.25.0;
+  };
   patch = {
     "1.16" = import ./overrides/patch/1.16;
   };
