d42e41d64b3cd9495f7e9691b2a11cd2f28622ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:53:26 +0000

    autogenerated overwrites for ifaddr==0.2.0

--- collected.nix
+++ collected.nix
@@ -3649,6 +3649,9 @@
   idx2numpy = {
     "1.2.3" = import ./overrides/idx2numpy/1.2.3;
   };
+  ifaddr = {
+    "0.2.0" = import ./overrides/ifaddr/0.2.0;
+  };
   ifconfig-parser = {
     "0.0.5" = import ./overrides/ifconfig-parser/0.0.5;
   };
