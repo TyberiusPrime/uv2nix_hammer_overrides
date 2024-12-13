5464800014074e8fa3812d05cfd7f5f868b7eaaf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:55:55 +0000

    autogenerated overwrites for async-dns==2.0.0

--- collected.nix
+++ collected.nix
@@ -428,6 +428,9 @@
   async-cache = {
     "1.1.1" = import ./overrides/async-cache/1.1.1;
   };
+  async-dns = {
+    "2.0.0" = import ./overrides/async-dns/2.0.0;
+  };
   async-upnp-client = {
     "0.40.0" = import ./overrides/async-upnp-client/0.40.0;
   };
