87db43272626ff98a9fa31ff8718dab8bcd92934
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:09:04 +0000

    autogenerated overwrites for spacy-transformers==1.3.5

--- collected.nix
+++ collected.nix
@@ -8589,6 +8589,9 @@
   sox = {
     "1.5.0" = import ./overrides/sox/1.5.0;
   };
+  spacy-transformers = {
+    "1.3.5" = import ./overrides/spacy-transformers/1.3.5;
+  };
   spanners = {
     "1.20" = import ./overrides/spanners/1.20;
   };
