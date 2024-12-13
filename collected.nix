9854d484328c5749b78c9281bec5e24b8ccc4e9e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:23:36 +0000

    autogenerated overwrites for discogs-client==2.3.0

--- collected.nix
+++ collected.nix
@@ -1837,6 +1837,9 @@
   discid = {
     "1.2.0" = import ./overrides/discid/1.2.0;
   };
+  discogs-client = {
+    "2.3.0" = import ./overrides/discogs-client/2.3.0;
+  };
   distance = {
     "0.1.3" = import ./overrides/distance/0.1.3;
   };
