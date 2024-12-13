50287293ad497fc51778bd447e3436d11ad809fa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:12:13 +0000

    autogenerated overwrites for amaranth==0.5.2

--- collected.nix
+++ collected.nix
@@ -193,6 +193,9 @@
   alphatwirl = {
     "0.23.0" = import ./overrides/alphatwirl/0.23.0;
   };
+  amaranth = {
+    "0.5.2" = import ./overrides/amaranth/0.5.2;
+  };
   amazon-dax-client = {
     "2.0.3" = import ./overrides/amazon-dax-client/2.0.3;
   };
