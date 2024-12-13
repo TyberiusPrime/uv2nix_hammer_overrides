117417d295c122648a13a594b87b26085efc8660
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:44:52 +0000

    autogenerated overwrites for python-engineio==4.9.1

--- collected.nix
+++ collected.nix
@@ -7303,6 +7303,9 @@
   python-doi = {
     "0.2.0" = import ./overrides/python-doi/0.2.0;
   };
+  python-engineio = {
+    "4.9.1" = import ./overrides/python-engineio/4.9.1;
+  };
   python-etcd = {
     "0.4.5" = import ./overrides/python-etcd/0.4.5;
   };
