e4c9d4232fe737b731c44c73cb328c901ecabd4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:09:52 +0000

    autogenerated overwrites for ground==9.0.0

--- collected.nix
+++ collected.nix
@@ -3323,6 +3323,9 @@
   groestlcoin-hash = {
     "1.0.3" = import ./overrides/groestlcoin-hash/1.0.3;
   };
+  ground = {
+    "9.0.0" = import ./overrides/ground/9.0.0;
+  };
   grpc-google-logging-v2 = {
     "0.11.1" = import ./overrides/grpc-google-logging-v2/0.11.1;
   };
