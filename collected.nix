4c494dbaf2db040dc5ddbea50cd030e531440615
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:01:26 +0000

    autogenerated overwrites for py-cpuinfo==9.0.0

--- collected.nix
+++ collected.nix
@@ -6306,6 +6306,9 @@
   py-bcrypt = {
     "0.4" = import ./overrides/py-bcrypt/0.4;
   };
+  py-cpuinfo = {
+    "9.0.0" = import ./overrides/py-cpuinfo/9.0.0;
+  };
   py-dag = {
     "3.0.1" = import ./overrides/py-dag/3.0.1;
   };
