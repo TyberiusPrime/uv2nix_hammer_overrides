242e5d4fb5ce870d8a065388beab017b51af64ee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:41:32 +0000

    autogenerated overwrites for py-aiger-bv==4.7.7

--- collected.nix
+++ collected.nix
@@ -6300,6 +6300,9 @@
   px-settings = {
     "0.1.5" = import ./overrides/px-settings/0.1.5;
   };
+  py-aiger-bv = {
+    "4.7.7" = import ./overrides/py-aiger-bv/4.7.7;
+  };
   py-air-control-exporter = {
     "0.3.1" = import ./overrides/py-air-control-exporter/0.3.1;
   };
