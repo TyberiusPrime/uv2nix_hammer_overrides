b209690ac67bc4b9e4fe76e2ebcb6b5b06db2309
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:05:43 +0000

    autogenerated overwrites for oarepo-model-builder-tests==4.0.7

--- collected.nix
+++ collected.nix
@@ -5414,6 +5414,9 @@
   oandapyv20 = {
     "0.7.2" = import ./overrides/oandapyv20/0.7.2;
   };
+  oarepo-model-builder-tests = {
+    "4.0.7" = import ./overrides/oarepo-model-builder-tests/4.0.7;
+  };
   oauth = {
     "1.0.1" = import ./overrides/oauth/1.0.1;
   };
