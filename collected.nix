3a31dbca4e8958a5be45a8aa7699fe28f506315a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 04:12:20 +0000

    autogenerated overwrites for owlrl==6.0.2

--- collected.nix
+++ collected.nix
@@ -5632,6 +5632,9 @@
   owlready2 = {
     "0.46" = import ./overrides/owlready2/0.46;
   };
+  owlrl = {
+    "6.0.2" = import ./overrides/owlrl/6.0.2;
+  };
   p-tqdm = {
     "1.4.2" = import ./overrides/p-tqdm/1.4.2;
   };
