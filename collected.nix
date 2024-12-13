7ff0baf284bac387785128d4145d47a7b2048159
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 12:45:06 +0000

    autogenerated overwrites for jupyterthemes==0.20.0

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyterthemes = {
+    "0.20.0" = import ./overrides/jupyterthemes/0.20.0;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
