1ddcd68847f5647910ecbe2967f3d4e986acb4fb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:42:13 +0000

    autogenerated overwrites for qiling==1.4.6

--- collected.nix
+++ collected.nix
@@ -7694,6 +7694,9 @@
   qds-sdk = {
     "1.16.1" = import ./overrides/qds-sdk/1.16.1;
   };
+  qiling = {
+    "1.4.6" = import ./overrides/qiling/1.4.6;
+  };
   qingcloud-sdk = {
     "1.2.15" = import ./overrides/qingcloud-sdk/1.2.15;
   };
