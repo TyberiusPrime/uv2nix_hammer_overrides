696980b7dea9f4e54fecb0a4fa38f4d788138ed9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:53:12 +0000

    autogenerated overwrites for tinygrad==0.9.2

--- collected.nix
+++ collected.nix
@@ -9106,6 +9106,9 @@
   tinyec = {
     "0.4.0" = import ./overrides/tinyec/0.4.0;
   };
+  tinygrad = {
+    "0.9.2" = import ./overrides/tinygrad/0.9.2;
+  };
   tinymongo = {
     "0.2.0" = import ./overrides/tinymongo/0.2.0;
   };
