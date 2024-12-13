4a288e67685d75dbb250f77a878e82554ec98e0b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:03:56 +0000

    autogenerated overwrites for markdown2==2.5.0

--- collected.nix
+++ collected.nix
@@ -4619,6 +4619,9 @@
   marionette-transport = {
     "1.2.0" = import ./overrides/marionette-transport/1.2.0;
   };
+  markdown2 = {
+    "2.5.0" = import ./overrides/markdown2/2.5.0;
+  };
   markerlib = {
     "0.6.0" = import ./overrides/markerlib/0.6.0;
   };
