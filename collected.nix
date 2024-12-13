f72df7c99dad1542bc155dda34e2b6d9a2336a3c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:13:28 +0000

    autogenerated overwrites for lnurl==0.5.3

--- collected.nix
+++ collected.nix
@@ -4465,6 +4465,9 @@
   lmtpd = {
     "6.2.0" = import ./overrides/lmtpd/6.2.0;
   };
+  lnurl = {
+    "0.5.3" = import ./overrides/lnurl/0.5.3;
+  };
   localstack = {
     "3.7.2" = import ./overrides/localstack/3.7.2;
   };
