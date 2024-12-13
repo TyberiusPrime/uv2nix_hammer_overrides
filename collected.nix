377e09d5dd65fc336a0e62833089db9b05be3de0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:05:17 +0000

    autogenerated overwrites for edk2-pytool-library==0.21.10

--- collected.nix
+++ collected.nix
@@ -2472,6 +2472,9 @@
   editorium = {
     "0.0.0.30" = import ./overrides/editorium/0.0.0.30;
   };
+  edk2-pytool-library = {
+    "0.21.10" = import ./overrides/edk2-pytool-library/0.21.10;
+  };
   ee-extra = {
     "0.0.15" = import ./overrides/ee-extra/0.0.15;
   };
