612661d5e38dd9484576c65d9c26b9b957de45cf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:56:35 +0000

    autogenerated overwrites for pulumi-command==1.0.1

--- collected.nix
+++ collected.nix
@@ -6264,6 +6264,9 @@
   pubnub = {
     "8.1.0" = import ./overrides/pubnub/8.1.0;
   };
+  pulumi-command = {
+    "1.0.1" = import ./overrides/pulumi-command/1.0.1;
+  };
   pupynere = {
     "1.0.15" = import ./overrides/pupynere/1.0.15;
   };
