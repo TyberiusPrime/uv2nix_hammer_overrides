0e61d48d2757c62d35f188acebf1e5d31f8e3034
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:32:55 +0000

    autogenerated overwrites for python-frontmatter==1.1.0

--- collected.nix
+++ collected.nix
@@ -7324,6 +7324,9 @@
   python-freeipa = {
     "1.0.8" = import ./overrides/python-freeipa/1.0.8;
   };
+  python-frontmatter = {
+    "1.1.0" = import ./overrides/python-frontmatter/1.1.0;
+  };
   python-gammu = {
     "3.2.4" = import ./overrides/python-gammu/3.2.4;
   };
