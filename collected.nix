66efd04a7bccd41ff84eadb0222b8d71d3c96373
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:40:49 +0000

    autogenerated overwrites for suitcase-utils==0.5.4

--- collected.nix
+++ collected.nix
@@ -8831,6 +8831,9 @@
   suds-py3 = {
     "1.4.1.0" = import ./overrides/suds-py3/1.4.1.0;
   };
+  suitcase-utils = {
+    "0.5.4" = import ./overrides/suitcase-utils/0.5.4;
+  };
   sunburnt = {
     "0.6" = import ./overrides/sunburnt/0.6;
   };
