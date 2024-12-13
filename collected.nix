5b600f0ca5b4e0895076e050fa62f5687a5bec0a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:37:36 +0000

    autogenerated overwrites for rst2ansi==0.1.5

--- collected.nix
+++ collected.nix
@@ -8063,6 +8063,9 @@
   rsl-xsd = {
     "0.2.4" = import ./overrides/rsl-xsd/0.2.4;
   };
+  rst2ansi = {
+    "0.1.5" = import ./overrides/rst2ansi/0.1.5;
+  };
   rstgen = {
     "24.4.0" = import ./overrides/rstgen/24.4.0;
   };
