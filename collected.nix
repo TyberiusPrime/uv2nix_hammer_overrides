8c64ac7bcab7ab8c4f85909bd92d1458a8043fb3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:05:15 +0000

    autogenerated overwrites for mwcli==0.0.3

--- collected.nix
+++ collected.nix
@@ -5061,6 +5061,9 @@
   mutmut = {
     "2.5.1" = import ./overrides/mutmut/2.5.1;
   };
+  mwcli = {
+    "0.0.3" = import ./overrides/mwcli/0.0.3;
+  };
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
