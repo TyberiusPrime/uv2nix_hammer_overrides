730bf952dd752d159e8e2f64e6ecd00d3c1bf61f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:17:47 +0000

    autogenerated overwrites for asyncstdlib==3.12.5

--- collected.nix
+++ collected.nix
@@ -452,6 +452,9 @@
   asyncpgsa = {
     "0.27.1" = import ./overrides/asyncpgsa/0.27.1;
   };
+  asyncstdlib = {
+    "3.12.5" = import ./overrides/asyncstdlib/3.12.5;
+  };
   atomic = {
     "0.7.3" = import ./overrides/atomic/0.7.3;
   };
