47da2aa4983efea0f26718a6b5c497ef621a0b1a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 12:35:04 +0000

    autogenerated overwrites for portend==3.2.0

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  portend = {
+    "3.2.0" = import ./overrides/portend/3.2.0;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
