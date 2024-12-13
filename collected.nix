2198a1bc71892161300bac8911a3257904366c4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:45:36 +0000

    autogenerated overwrites for pytest-golden==0.2.2

--- collected.nix
+++ collected.nix
@@ -7224,6 +7224,9 @@
   pytest-expect = {
     "1.1.0" = import ./overrides/pytest-expect/1.1.0;
   };
+  pytest-golden = {
+    "0.2.2" = import ./overrides/pytest-golden/0.2.2;
+  };
   pytest-json = {
     "0.4.0" = import ./overrides/pytest-json/0.4.0;
   };
