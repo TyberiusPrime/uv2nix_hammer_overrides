4d14a5f384600e28d5c3c4a20560cdc9b1b71957
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:10:04 +0000

    autogenerated overwrites for pysvn==0.1.0

--- collected.nix
+++ collected.nix
@@ -7185,6 +7185,9 @@
   pystemmer = {
     "2.2.0.1" = import ./overrides/pystemmer/2.2.0.1;
   };
+  pysvn = {
+    "0.1.0" = import ./overrides/pysvn/0.1.0;
+  };
   pyswarm = {
     "0.6" = import ./overrides/pyswarm/0.6;
   };
