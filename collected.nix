b6150b47a3127c2449c99ea81cdb52458470e842
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:43:56 +0000

    autogenerated overwrites for html-table-parser-python3==0.3.1

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  html-table-parser-python3 = {
+    "0.3.1" = import ./overrides/html-table-parser-python3/0.3.1;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
