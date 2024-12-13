a8eee80421b1a7c595a6aaf99abf786b792586d2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:48:18 +0000

    autogenerated overwrites for redis==5.0.8

--- collected.nix
+++ collected.nix
@@ -7824,6 +7824,9 @@
     "1.20" = import ./overrides/red-black-tree-mod/1.20;
     "1.22" = import ./overrides/red-black-tree-mod/1.22;
   };
+  redis = {
+    "5.0.8" = import ./overrides/redis/5.0.8;
+  };
   redis2 = {
     "2.10.6.3" = import ./overrides/redis2/2.10.6.3;
   };
