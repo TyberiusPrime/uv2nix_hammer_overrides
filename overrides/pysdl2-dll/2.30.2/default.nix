1188d5063e97e5f4332cccc9c28ff6cb36762848
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:15:46 +0000

    autogenerated overwrites for pysdl2-dll==2.30.2

--- overrides/pysdl2-dll/2.30.2/default.nix
+++ overrides/pysdl2-dll/2.30.2/default.nix
@@ -1,1 +1,3 @@
-{ pkgs, ... }: old: { buildInputs = old.buildInputs or [ ] ++ [ pkgs.SDL2 ]; }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.SDL2];} else {buildInputs = old.buildInputs or [] ++ [pkgs.SDL2];nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {setuptools = [];});}
+        
