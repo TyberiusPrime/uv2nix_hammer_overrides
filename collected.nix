ed74aef303877d67bbf44021a85b0afb026aef33
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:47:18 +0000

    autogenerated overwrites for idasen==0.12.0

--- collected.nix
+++ collected.nix
@@ -3640,6 +3640,9 @@
   icoextract = {
     "0.1.5" = import ./overrides/icoextract/0.1.5;
   };
+  idasen = {
+    "0.12.0" = import ./overrides/idasen/0.12.0;
+  };
   idds-common = {
     "2.1.40" = import ./overrides/idds-common/2.1.40;
   };
