9c19ed32ffdc4f4e940774bdc34e2cc4f44f0dee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:05:56 +0000

    autogenerated overwrites for pymata-express==1.21

--- collected.nix
+++ collected.nix
@@ -6846,6 +6846,9 @@
   pymarktex = {
     "1.4.6" = import ./overrides/pymarktex/1.4.6;
   };
+  pymata-express = {
+    "1.21" = import ./overrides/pymata-express/1.21;
+  };
   pymatsolver = {
     "0.2.0" = import ./overrides/pymatsolver/0.2.0;
   };
