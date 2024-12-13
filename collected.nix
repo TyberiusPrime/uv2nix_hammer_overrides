87ec236e24a951753debdeb8bca826c0ecbf69c8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:45:14 +0000

    autogenerated overwrites for python-pidfile==3.1.1

--- collected.nix
+++ collected.nix
@@ -7442,6 +7442,9 @@
   python-packer = {
     "0.1.2" = import ./overrides/python-packer/0.1.2;
   };
+  python-pidfile = {
+    "3.1.1" = import ./overrides/python-pidfile/3.1.1;
+  };
   python-pipedrive = {
     "0.4.0" = import ./overrides/python-pipedrive/0.4.0;
   };
