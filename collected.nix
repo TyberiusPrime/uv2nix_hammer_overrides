fc05625e733b07675e965d5f4a6c4dc10bd5378f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:10:59 +0000

    autogenerated overwrites for django-contact-form==5.1.0

--- collected.nix
+++ collected.nix
@@ -1925,6 +1925,9 @@
   django-concurrency = {
     "2.5" = import ./overrides/django-concurrency/2.5;
   };
+  django-contact-form = {
+    "5.1.0" = import ./overrides/django-contact-form/5.1.0;
+  };
   django-cratis = {
     "0.9.1" = import ./overrides/django-cratis/0.9.1;
   };
