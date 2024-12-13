96a2921b6764352c610b631c8cdbd5ba0ff29424
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:08:56 +0000

    autogenerated overwrites for zodbpickle==4.0

--- collected.nix
+++ collected.nix
@@ -10169,6 +10169,9 @@
   zodbcode = {
     "3.4.0" = import ./overrides/zodbcode/3.4.0;
   };
+  zodbpickle = {
+    "4.0" = import ./overrides/zodbpickle/4.0;
+  };
   zope-app-cache = {
     "3.7.0" = import ./overrides/zope-app-cache/3.7.0;
   };
