678dbf2e6b9637fa68c36f51acf046f0809b9a91
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:05:51 +0000

    autogenerated overwrites for dploot==3.0.0

--- collected.nix
+++ collected.nix
@@ -2349,6 +2349,9 @@
   dpcontracts = {
     "0.6.0" = import ./overrides/dpcontracts/0.6.0;
   };
+  dploot = {
+    "3.0.0" = import ./overrides/dploot/3.0.0;
+  };
   dppd = {
     "0.27" = import ./overrides/dppd/0.27;
   };
