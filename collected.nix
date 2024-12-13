1965d0aa7efbe531cc1e0cd95a9b1ac570cd9701
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:39:37 +0000

    autogenerated overwrites for sentence-splitter==1.4

--- collected.nix
+++ collected.nix
@@ -8275,6 +8275,9 @@
   semgrep = {
     "1.68.0" = import ./overrides/semgrep/1.68.0;
   };
+  sentence-splitter = {
+    "1.4" = import ./overrides/sentence-splitter/1.4;
+  };
   sentinels = {
     "1.0.0" = import ./overrides/sentinels/1.0.0;
   };
