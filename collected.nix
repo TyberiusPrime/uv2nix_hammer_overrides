8a7207f80b7686b91315a3758d3e7adcafc4b59f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:42:27 +0000

    autogenerated overwrites for jupyter-packaging==0.12.3

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyter-packaging = {
+    "0.12.3" = import ./overrides/jupyter-packaging/0.12.3;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
