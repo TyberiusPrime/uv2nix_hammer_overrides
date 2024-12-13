53c56b48254fedc839028c77a89d860e21a651de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:08:26 +0000

    autogenerated overwrites for solc-select==1.0.4

--- collected.nix
+++ collected.nix
@@ -8568,6 +8568,9 @@
   socksipy-branch = {
     "1.1" = import ./overrides/socksipy-branch/1.1;
   };
+  solc-select = {
+    "1.0.4" = import ./overrides/solc-select/1.0.4;
+  };
   solidpython-ff = {
     "0.1.1" = import ./overrides/solidpython-ff/0.1.1;
   };
