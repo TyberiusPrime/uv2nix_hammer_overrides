16d78d3b894539f4458bbe0415022b7666c28b29
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:14:48 +0000

    autogenerated overwrites for gridnet==5.0.1

--- collected.nix
+++ collected.nix
@@ -3317,6 +3317,9 @@
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
+  gridnet = {
+    "5.0.1" = import ./overrides/gridnet/5.0.1;
+  };
   gripcontrol = {
     "4.2.0" = import ./overrides/gripcontrol/4.2.0;
   };
