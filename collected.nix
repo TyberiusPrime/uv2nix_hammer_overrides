3388c46b1877a641fac30834e4cd031a501664d6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:33:06 +0000

    autogenerated overwrites for dm-env==1.6

--- collected.nix
+++ collected.nix
@@ -2291,6 +2291,9 @@
   dlx = {
     "1.0.4" = import ./overrides/dlx/1.0.4;
   };
+  dm-env = {
+    "1.6" = import ./overrides/dm-env/1.6;
+  };
   dm-reuse = {
     "2.2" = import ./overrides/dm-reuse/2.2;
   };
