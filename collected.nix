5c8e1e78a9c2331665aec75b00243fc8b99cc73f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:04:08 +0000

    autogenerated overwrites for asn1ate==0.6.0

--- collected.nix
+++ collected.nix
@@ -398,6 +398,9 @@
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
   };
+  asn1ate = {
+    "0.6.0" = import ./overrides/asn1ate/0.6.0;
+  };
   asn1tools = {
     "0.167.0" = import ./overrides/asn1tools/0.167.0;
   };
