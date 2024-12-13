cf8ce807e0d0d742855e75b804c22a76a6a81b90
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:37:36 +0000

    autogenerated overwrites for azure-functions-devops-build==0.0.22

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-functions-devops-build = {
+    "0.0.22" = import ./overrides/azure-functions-devops-build/0.0.22;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
