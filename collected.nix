78bd0ef10fa2d6cb20f7956ff809674f3f321f4b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:26:00 +0000

    autogenerated overwrites for rusenttokenize==0.0.5

--- collected.nix
+++ collected.nix
@@ -8081,6 +8081,9 @@
   runipy = {
     "0.1.5" = import ./overrides/runipy/0.1.5;
   };
+  rusenttokenize = {
+    "0.0.5" = import ./overrides/rusenttokenize/0.0.5;
+  };
   rwproperty = {
     "1.0" = import ./overrides/rwproperty/1.0;
   };
