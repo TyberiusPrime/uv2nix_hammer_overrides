ea7511eb79c8f18ffbc05e5ec20b316889561d05
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:53:46 +0000

    autogenerated overwrites for django-braces==1.15.0

--- collected.nix
+++ collected.nix
@@ -1904,6 +1904,9 @@
   django-bower = {
     "5.2.0" = import ./overrides/django-bower/5.2.0;
   };
+  django-braces = {
+    "1.15.0" = import ./overrides/django-braces/1.15.0;
+  };
   django-cacheops = {
     "7.0.2" = import ./overrides/django-cacheops/7.0.2;
   };
