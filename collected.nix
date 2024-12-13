e2172333b301399e33d8c9b488f1756852275df8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:24:15 +0000

    autogenerated overwrites for django-uuslug==2.0.0

--- collected.nix
+++ collected.nix
@@ -2210,6 +2210,9 @@
   django-ultracache = {
     "2.2" = import ./overrides/django-ultracache/2.2;
   };
+  django-uuslug = {
+    "2.0.0" = import ./overrides/django-uuslug/2.0.0;
+  };
   django-vkontakte-api = {
     "0.8.7" = import ./overrides/django-vkontakte-api/0.8.7;
   };
