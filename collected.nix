623856d94b9485ce5ec7dc687e49e146970d2eba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:59:32 +0000

    autogenerated overwrites for numdifftools==0.9.41

--- collected.nix
+++ collected.nix
@@ -5347,6 +5347,9 @@
     "0.53.1" = import ./overrides/numba/0.53.1;
     "0.60.0" = import ./overrides/numba/0.60.0;
   };
+  numdifftools = {
+    "0.9.41" = import ./overrides/numdifftools/0.9.41;
+  };
   numpy = {
     "1.20.3" = import ./overrides/numpy/1.20.3;
     "1.21.6" = import ./overrides/numpy/1.21.6;
