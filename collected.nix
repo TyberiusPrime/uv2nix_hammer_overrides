86b2d71093e52cb023b7c19ff7155380a1c64f26
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:47:12 +0000

    autogenerated overwrites for pytest-xvfb==3.0.0

--- collected.nix
+++ collected.nix
@@ -7251,6 +7251,9 @@
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
+  pytest-xvfb = {
+    "3.0.0" = import ./overrides/pytest-xvfb/3.0.0;
+  };
   pytg = {
     "0.4.10" = import ./overrides/pytg/0.4.10;
   };
