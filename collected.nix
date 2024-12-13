4f8fa231a16b52db453d3d17fec822fa785d6d32
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:50:33 +0000

    autogenerated overwrites for attacut==1.0.6

--- collected.nix
+++ collected.nix
@@ -464,6 +464,9 @@
   atomman = {
     "1.4.11" = import ./overrides/atomman/1.4.11;
   };
+  attacut = {
+    "1.0.6" = import ./overrides/attacut/1.0.6;
+  };
   auroranoaa = {
     "0.0.3" = import ./overrides/auroranoaa/0.0.3;
   };
