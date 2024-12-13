3711e3eed4b0a388d878be547205a1e952408def
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 04:56:49 +0000

    autogenerated overwrites for types-pyasn1==0.6.0.20240824

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-pyasn1 = {
+    "0.6.0.20240824" = import ./overrides/types-pyasn1/0.6.0.20240824;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
