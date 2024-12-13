f36bb3b0d6b9836bbf79fc85a00515d1411cfd49
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:50:14 +0000

    autogenerated overwrites for ckanext-toolbelt==0.4.25

--- collected.nix
+++ collected.nix
@@ -1089,6 +1089,9 @@
   circum = {
     "0.0.6" = import ./overrides/circum/0.0.6;
   };
+  ckanext-toolbelt = {
+    "0.4.25" = import ./overrides/ckanext-toolbelt/0.4.25;
+  };
   ckantoolkit = {
     "0.0.7" = import ./overrides/ckantoolkit/0.0.7;
   };
