0ccbd0a5942a3942bf2a3c829a0e8d8c453bc964
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:58:19 +0000

    autogenerated overwrites for holoviews==1.19.1

--- collected.nix
+++ collected.nix
@@ -3480,6 +3480,9 @@
   hole = {
     "0.8.0" = import ./overrides/hole/0.8.0;
   };
+  holoviews = {
+    "1.19.1" = import ./overrides/holoviews/1.19.1;
+  };
   home-assistant-chip-core = {
     "2024.7.1" = import ./overrides/home-assistant-chip-core/2024.7.1;
   };
