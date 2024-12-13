61cbd971ad7d8b61a47940bd0ac2c8a90b46e546
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:15:50 +0000

    autogenerated overwrites for icdiff==2.0.7

--- collected.nix
+++ collected.nix
@@ -3637,6 +3637,9 @@
   ibm-watson = {
     "8.1.0" = import ./overrides/ibm-watson/8.1.0;
   };
+  icdiff = {
+    "2.0.7" = import ./overrides/icdiff/2.0.7;
+  };
   icoextract = {
     "0.1.5" = import ./overrides/icoextract/0.1.5;
   };
