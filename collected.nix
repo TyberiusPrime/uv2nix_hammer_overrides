94635dc819cdce86b0d70d5cc3deaacc55bda792
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:56:50 +0000

    autogenerated overwrites for notion-client==2.2.1

--- collected.nix
+++ collected.nix
@@ -5328,6 +5328,9 @@
   noseapp = {
     "2.2.2" = import ./overrides/noseapp/2.2.2;
   };
+  notion-client = {
+    "2.2.1" = import ./overrides/notion-client/2.2.1;
+  };
   notmuch2 = {
     "0.1" = import ./overrides/notmuch2/0.1;
   };
