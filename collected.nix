002c5bf9a8cb693617f8216102a9f8fe53c9be76
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:22:55 +0000

    autogenerated overwrites for mediafile==0.12.0

--- collected.nix
+++ collected.nix
@@ -4679,6 +4679,9 @@
   mdv = {
     "1.7.5" = import ./overrides/mdv/1.7.5;
   };
+  mediafile = {
+    "0.12.0" = import ./overrides/mediafile/0.12.0;
+  };
   mediawiki-utilities = {
     "0.4.18" = import ./overrides/mediawiki-utilities/0.4.18;
   };
