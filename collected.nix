0cd2e5158c7fa50b4fe74d2414748d737cf17ef8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 19:05:51 +0000

    autogenerated overwrites for manage-fastapi==1.1.1

--- collected.nix
+++ collected.nix
@@ -4592,6 +4592,9 @@
   mailthon = {
     "0.1.1" = import ./overrides/mailthon/0.1.1;
   };
+  manage-fastapi = {
+    "1.1.1" = import ./overrides/manage-fastapi/1.1.1;
+  };
   manage-py = {
     "0.2.10" = import ./overrides/manage-py/0.2.10;
   };
