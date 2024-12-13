670cbac0f65333314fe09a7db212b86dd53843c0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:48:14 +0000

    autogenerated overwrites for pytest-testmon==2.1.1

--- collected.nix
+++ collected.nix
@@ -7248,6 +7248,9 @@
   pytest-subtesthack = {
     "0.2.0" = import ./overrides/pytest-subtesthack/0.2.0;
   };
+  pytest-testmon = {
+    "2.1.1" = import ./overrides/pytest-testmon/2.1.1;
+  };
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
