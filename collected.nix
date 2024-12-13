630bb32b8807f135a794c8793e2bfa8446953b22
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:20:19 +0000

    autogenerated overwrites for google-cloud-bigquery-datatransfer==3.15.7

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-bigquery-datatransfer = {
+    "3.15.7" = import ./overrides/google-cloud-bigquery-datatransfer/3.15.7;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
