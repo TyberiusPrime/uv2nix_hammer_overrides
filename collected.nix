ce6a9653b2fb4860da78a5a9a51803b557119df5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 12:48:59 +0000

    autogenerated overwrites for sense-energy==0.12.4

--- collected.nix
+++ collected.nix
@@ -8275,6 +8275,9 @@
   semgrep = {
     "1.68.0" = import ./overrides/semgrep/1.68.0;
   };
+  sense-energy = {
+    "0.12.4" = import ./overrides/sense-energy/0.12.4;
+  };
   sentinels = {
     "1.0.0" = import ./overrides/sentinels/1.0.0;
   };
