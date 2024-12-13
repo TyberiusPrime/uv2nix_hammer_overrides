e43a9809dbe235bb911461e044643232b7c34a79
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:23:50 +0000

    autogenerated overwrites for messagebox==0.1.0

--- collected.nix
+++ collected.nix
@@ -4715,6 +4715,9 @@
   merkletools = {
     "1.0.3" = import ./overrides/merkletools/1.0.3;
   };
+  messagebox = {
+    "0.1.0" = import ./overrides/messagebox/0.1.0;
+  };
   messytables = {
     "0.15.2" = import ./overrides/messytables/0.15.2;
   };
