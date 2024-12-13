4f7f3b70505e51ca9dbf2eb395891dd2954de0f6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:12:10 +0000

    autogenerated overwrites for sphinx-panels==0.6.0

--- collected.nix
+++ collected.nix
@@ -8613,6 +8613,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-panels = {
+    "0.6.0" = import ./overrides/sphinx-panels/0.6.0;
+  };
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
