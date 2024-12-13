b98e937b79ed78c160032e4ccc1b61a0e6b20082
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 16:06:53 +0000

    autogenerated overwrites for pycep-parser==0.4.2

--- collected.nix
+++ collected.nix
@@ -6450,6 +6450,9 @@
   pycddlib = {
     "2.1.8.post1" = import ./overrides/pycddlib/2.1.8.post1;
   };
+  pycep-parser = {
+    "0.4.2" = import ./overrides/pycep-parser/0.4.2;
+  };
   pycflow2dot = {
     "0.2.3" = import ./overrides/pycflow2dot/0.2.3;
   };
