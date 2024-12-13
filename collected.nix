3f05591c31418aaa3f90d85d6e8d4ea354d74ddb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:00:22 +0000

    autogenerated overwrites for openapi-spec-validator==0.7.1

--- collected.nix
+++ collected.nix
@@ -5519,6 +5519,9 @@
   openapi-codec = {
     "1.3.2" = import ./overrides/openapi-codec/1.3.2;
   };
+  openapi-spec-validator = {
+    "0.7.1" = import ./overrides/openapi-spec-validator/0.7.1;
+  };
   opencmiss-utils = {
     "0.4.2" = import ./overrides/opencmiss-utils/0.4.2;
   };
