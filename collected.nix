23bbddabf61a8070a86f01ed6bfdcd7882ac11ee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:34:03 +0000

    autogenerated overwrites for missingpy==0.2.0

--- collected.nix
+++ collected.nix
@@ -4829,6 +4829,9 @@
   misaka = {
     "2.1.1" = import ./overrides/misaka/2.1.1;
   };
+  missingpy = {
+    "0.2.0" = import ./overrides/missingpy/0.2.0;
+  };
   mkdocs-exclude = {
     "1.0.2" = import ./overrides/mkdocs-exclude/1.0.2;
   };
