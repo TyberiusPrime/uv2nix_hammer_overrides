0fa7bdd3a0f751b3655c5f3697d39c2d8356b54c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:43:43 +0000

    autogenerated overwrites for orca==1.8

--- collected.nix
+++ collected.nix
@@ -5587,6 +5587,9 @@
   opuslib = {
     "3.0.1" = import ./overrides/opuslib/3.0.1;
   };
+  orca = {
+    "1.8" = import ./overrides/orca/1.8;
+  };
   orcid = {
     "1.0.3" = import ./overrides/orcid/1.0.3;
   };
