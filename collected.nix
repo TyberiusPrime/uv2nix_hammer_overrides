c6d5e4813bede52dede9312b0fb268596acaf7de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:50:26 +0000

    autogenerated overwrites for moderngl==5.11.1

--- collected.nix
+++ collected.nix
@@ -4889,6 +4889,9 @@
   modelcif = {
     "1.0" = import ./overrides/modelcif/1.0;
   };
+  moderngl = {
+    "5.11.1" = import ./overrides/moderngl/5.11.1;
+  };
   modestmaps = {
     "1.4.7" = import ./overrides/modestmaps/1.4.7;
   };
