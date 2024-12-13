1024ee97026eb0e4cd64fb26cbef434e369e5178
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:16:05 +0000

    autogenerated overwrites for google-auth==2.34.0

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-auth = {
+    "2.34.0" = import ./overrides/google-auth/2.34.0;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
