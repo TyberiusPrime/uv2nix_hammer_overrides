9d03120c824dd5bd4cc424f05b806d868fb5e12b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:32:41 +0000

    autogenerated overwrites for scmdata==0.17.0

--- collected.nix
+++ collected.nix
@@ -8230,6 +8230,9 @@
   sciviso = {
     "1.0.9" = import ./overrides/sciviso/1.0.9;
   };
+  scmdata = {
+    "0.17.0" = import ./overrides/scmdata/0.17.0;
+  };
   scoop = {
     "0.7.2.0" = import ./overrides/scoop/0.7.2.0;
   };
