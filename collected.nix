fb00ed6e336fedb09ad35ab930ca814a9647e91a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:42:39 +0000

    autogenerated overwrites for ibis-framework==9.4.0

--- collected.nix
+++ collected.nix
@@ -3622,6 +3622,9 @@
   ibapi = {
     "9.81.1.post1" = import ./overrides/ibapi/9.81.1.post1;
   };
+  ibis-framework = {
+    "9.4.0" = import ./overrides/ibis-framework/9.4.0;
+  };
   ibm-cos-sdk = {
     "2.13.6" = import ./overrides/ibm-cos-sdk/2.13.6;
   };
