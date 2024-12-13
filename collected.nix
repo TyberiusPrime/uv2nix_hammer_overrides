67bb6d954113bc7ab1c7b607e5b12c206b47e280
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:28:48 +0000

    autogenerated overwrites for zigpy-homeassistant==0.19.0

--- collected.nix
+++ collected.nix
@@ -10148,6 +10148,9 @@
   zhconv = {
     "1.4.3" = import ./overrides/zhconv/1.4.3;
   };
+  zigpy-homeassistant = {
+    "0.19.0" = import ./overrides/zigpy-homeassistant/0.19.0;
+  };
   zipseeker = {
     "1.0.11" = import ./overrides/zipseeker/1.0.11;
   };
