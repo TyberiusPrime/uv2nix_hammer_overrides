bb21a0e9b5df458681447041360ddb3dcd61bd48
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:10:51 +0000

    autogenerated overwrites for repeated-test==2.3.3

--- collected.nix
+++ collected.nix
@@ -7871,6 +7871,9 @@
   reparser = {
     "1.4.3" = import ./overrides/reparser/1.4.3;
   };
+  repeated-test = {
+    "2.3.3" = import ./overrides/repeated-test/2.3.3;
+  };
   reportlab-qrcode = {
     "0.1.6" = import ./overrides/reportlab-qrcode/0.1.6;
   };
