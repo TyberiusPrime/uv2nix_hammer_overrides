e6f2ec64f6ec9180bc3f05c3013d4a73753c84d9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:54:26 +0000

    autogenerated overwrites for ziafont==0.9

--- collected.nix
+++ collected.nix
@@ -10148,6 +10148,9 @@
   zhconv = {
     "1.4.3" = import ./overrides/zhconv/1.4.3;
   };
+  ziafont = {
+    "0.9" = import ./overrides/ziafont/0.9;
+  };
   zipseeker = {
     "1.0.11" = import ./overrides/zipseeker/1.0.11;
   };
