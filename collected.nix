208b9977453ad11692ad1ac908b29431a60e3682
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:40:12 +0000

    autogenerated overwrites for uncurl==0.0.11

--- collected.nix
+++ collected.nix
@@ -9485,6 +9485,9 @@
   uncleengineer = {
     "0.2" = import ./overrides/uncleengineer/0.2;
   };
+  uncurl = {
+    "0.0.11" = import ./overrides/uncurl/0.0.11;
+  };
   undetected-chromedriver = {
     "3.5.5" = import ./overrides/undetected-chromedriver/3.5.5;
   };
