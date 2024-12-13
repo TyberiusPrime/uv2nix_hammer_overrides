f09168846809a7e5724a00b699585e7a15cc4e6f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:59:25 +0000

    autogenerated overwrites for google-cloud-audit-log==0.3.0

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-audit-log = {
+    "0.3.0" = import ./overrides/google-cloud-audit-log/0.3.0;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
