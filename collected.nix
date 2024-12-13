18ea9f1180aeea9b336defe49a300577e55415fc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:47:40 +0000

    autogenerated overwrites for rpi-ws281x==5.0.0

--- collected.nix
+++ collected.nix
@@ -8033,6 +8033,9 @@
   rpi-gpio = {
     "0.7.1" = import ./overrides/rpi-gpio/0.7.1;
   };
+  rpi-ws281x = {
+    "5.0.0" = import ./overrides/rpi-ws281x/5.0.0;
+  };
   rpio = {
     "0.10.0" = import ./overrides/rpio/0.10.0;
   };
