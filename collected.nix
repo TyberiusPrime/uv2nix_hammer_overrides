0dad4cf0dc7d668813d9d5698658d7b7074a19f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:57:01 +0000

    autogenerated overwrites for aioauth-client==0.28.1

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aioauth-client = {
+    "0.28.1" = import ./overrides/aioauth-client/0.28.1;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
