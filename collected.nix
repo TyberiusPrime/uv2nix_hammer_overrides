41d9546ed4ac27a5d6cf53ee24b6e9e58badcbb7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 12:39:50 +0000

    autogenerated overwrites for pymorphy2==0.9.1

--- collected.nix
+++ collected.nix
@@ -6885,6 +6885,9 @@
   pymoo = {
     "0.6.1.3" = import ./overrides/pymoo/0.6.1.3;
   };
+  pymorphy2 = {
+    "0.9.1" = import ./overrides/pymorphy2/0.9.1;
+  };
   pymsgbox = {
     "1.0.9" = import ./overrides/pymsgbox/1.0.9;
   };
