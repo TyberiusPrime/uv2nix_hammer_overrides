204277044759016b09e9ad616eddc559180386f4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:26:09 +0000

    autogenerated overwrites for marshmallow-objects==2.3.0

--- collected.nix
+++ collected.nix
@@ -4637,6 +4637,9 @@
   marrow-util = {
     "1.2.3" = import ./overrides/marrow-util/1.2.3;
   };
+  marshmallow-objects = {
+    "2.3.0" = import ./overrides/marshmallow-objects/2.3.0;
+  };
   marshmallow-polyfield = {
     "5.11" = import ./overrides/marshmallow-polyfield/5.11;
   };
