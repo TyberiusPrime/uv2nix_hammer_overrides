500d26451ef2a88345dcb2fc76531838dd482189
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:34:28 +0000

    autogenerated overwrites for types-psutil==6.0.0.20240901

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-psutil = {
+    "6.0.0.20240901" = import ./overrides/types-psutil/6.0.0.20240901;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
