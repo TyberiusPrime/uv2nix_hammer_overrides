9cd7af45e8a7cf93af01e975cfbe439bc4db20b8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 16:10:48 +0000

    autogenerated overwrites for fenics-fiat==2019.1.0

--- collected.nix
+++ collected.nix
@@ -2733,6 +2733,9 @@
   feeluown = {
     "4.1.7" = import ./overrides/feeluown/4.1.7;
   };
+  fenics-fiat = {
+    "2019.1.0" = import ./overrides/fenics-fiat/2019.1.0;
+  };
   fernet = {
     "1.0.1" = import ./overrides/fernet/1.0.1;
   };
