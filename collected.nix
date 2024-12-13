40b60805e8b259bcaebbe36b91a8dfa281bf7130
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:39:17 +0000

    autogenerated overwrites for audiolm-pytorch==2.0.7

--- collected.nix
+++ collected.nix
@@ -464,6 +464,9 @@
   atomman = {
     "1.4.11" = import ./overrides/atomman/1.4.11;
   };
+  audiolm-pytorch = {
+    "2.0.7" = import ./overrides/audiolm-pytorch/2.0.7;
+  };
   auroranoaa = {
     "0.0.3" = import ./overrides/auroranoaa/0.0.3;
   };
