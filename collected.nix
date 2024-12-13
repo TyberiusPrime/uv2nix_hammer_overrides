e0fa49419fcc3cf12314f9b4ad60a1dfdf0cef68
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:14:35 +0000

    autogenerated overwrites for dyools==0.21.3

--- collected.nix
+++ collected.nix
@@ -2430,6 +2430,9 @@
   dynamic-pytables-where-condition = {
     "1.1.1" = import ./overrides/dynamic-pytables-where-condition/1.1.1;
   };
+  dyools = {
+    "0.21.3" = import ./overrides/dyools/0.21.3;
+  };
   easy-config = {
     "0.2.0" = import ./overrides/easy-config/0.2.0;
   };
