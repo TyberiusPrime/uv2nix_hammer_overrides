0b906d66372111ab8c063f8be75d62399aa09407
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:58:09 +0000

    autogenerated overwrites for bookiesports==0.4.18

--- collected.nix
+++ collected.nix
@@ -807,6 +807,9 @@
   bonsai = {
     "1.5.3" = import ./overrides/bonsai/1.5.3;
   };
+  bookiesports = {
+    "0.4.18" = import ./overrides/bookiesports/0.4.18;
+  };
   boto3facade = {
     "0.5.9" = import ./overrides/boto3facade/0.5.9;
   };
