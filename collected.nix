cb041813a27c32b907ca9b3faab57bde2735930d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:42:21 +0000

    autogenerated overwrites for correctionlib==2.6.4

--- collected.nix
+++ collected.nix
@@ -1427,6 +1427,9 @@
   coreschema = {
     "0.0.4" = import ./overrides/coreschema/0.0.4;
   };
+  correctionlib = {
+    "2.6.4" = import ./overrides/correctionlib/2.6.4;
+  };
   cos-python-sdk-v5 = {
     "1.9.31" = import ./overrides/cos-python-sdk-v5/1.9.31;
   };
