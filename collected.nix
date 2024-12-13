e43befee91d059afb74f119e749a1e9c315fc33c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:54:15 +0000

    autogenerated overwrites for edtf==5.0.0

--- collected.nix
+++ collected.nix
@@ -2472,6 +2472,9 @@
   editorium = {
     "0.0.0.30" = import ./overrides/editorium/0.0.0.30;
   };
+  edtf = {
+    "5.0.0" = import ./overrides/edtf/5.0.0;
+  };
   ee-extra = {
     "0.0.15" = import ./overrides/ee-extra/0.0.15;
   };
