ef48802b3b80712ad363fc92b944c79372b89ba6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:26:41 +0000

    autogenerated overwrites for mwoauth==0.4.0

--- collected.nix
+++ collected.nix
@@ -5061,6 +5061,9 @@
   mutmut = {
     "2.5.1" = import ./overrides/mutmut/2.5.1;
   };
+  mwoauth = {
+    "0.4.0" = import ./overrides/mwoauth/0.4.0;
+  };
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
