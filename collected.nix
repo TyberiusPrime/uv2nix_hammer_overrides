32ea78304c973f6b712e12d5ae7fac322080aad5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:18:36 +0000

    autogenerated overwrites for sqlalchemy-vertica-python==0.6.3

--- collected.nix
+++ collected.nix
@@ -8683,6 +8683,9 @@
   sqlalchemy-utils = {
     "0.36.1" = import ./overrides/sqlalchemy-utils/0.36.1;
   };
+  sqlalchemy-vertica-python = {
+    "0.6.3" = import ./overrides/sqlalchemy-vertica-python/0.6.3;
+  };
   sqlitedict = {
     "2.1.0" = import ./overrides/sqlitedict/2.1.0;
   };
