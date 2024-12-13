91870519ea9f7af77b3bf1d05c5addae02bfee20
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 02:02:35 +0000

    autogenerated overwrites for python-fsutil==0.14.1

--- collected.nix
+++ collected.nix
@@ -7324,6 +7324,9 @@
   python-freeipa = {
     "1.0.8" = import ./overrides/python-freeipa/1.0.8;
   };
+  python-fsutil = {
+    "0.14.1" = import ./overrides/python-fsutil/0.14.1;
+  };
   python-gammu = {
     "3.2.4" = import ./overrides/python-gammu/3.2.4;
   };
