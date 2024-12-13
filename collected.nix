1fcf8595b06dbb5dc316fffbec1de5d933aa2350
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:15:39 +0000

    autogenerated overwrites for dict2xml==1.7.6

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  dict2xml = {
+    "1.7.6" = import ./overrides/dict2xml/1.7.6;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
