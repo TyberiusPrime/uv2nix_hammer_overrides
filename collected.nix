ebd3960e50723db2b80519755911c27ad2d8dc50
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 22:43:06 +0000

    autogenerated overwrites for pyserial==3.5

--- collected.nix
+++ collected.nix
@@ -7113,6 +7113,9 @@
   pysendfile = {
     "2.0.1" = import ./overrides/pysendfile/2.0.1;
   };
+  pyserial = {
+    "3.5" = import ./overrides/pyserial/3.5;
+  };
   pysftp = {
     "0.2.9" = import ./overrides/pysftp/0.2.9;
   };
