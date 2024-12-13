5e00a9d6478375bf0f140355a81e17a2bcdb0950
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 04:43:23 +0000

    autogenerated overwrites for pytest-textual-snapshot==1.0.0

--- collected.nix
+++ collected.nix
@@ -7248,6 +7248,9 @@
   pytest-subtesthack = {
     "0.2.0" = import ./overrides/pytest-subtesthack/0.2.0;
   };
+  pytest-textual-snapshot = {
+    "1.0.0" = import ./overrides/pytest-textual-snapshot/1.0.0;
+  };
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
