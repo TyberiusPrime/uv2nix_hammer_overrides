80657b69747a5b21cb67dab2bd9a1608071bc0de
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 12:50:25 +0000

    autogenerated overwrites for sphinx-js==3.2.2

--- collected.nix
+++ collected.nix
@@ -8613,6 +8613,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-js = {
+    "3.2.2" = import ./overrides/sphinx-js/3.2.2;
+  };
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
