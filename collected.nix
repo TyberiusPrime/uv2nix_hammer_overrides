4e9b5b5f561c9732e706b929a0ce09a884a6cf18
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:18:43 +0000

    autogenerated overwrites for dask-kubernetes==2024.8.0

--- collected.nix
+++ collected.nix
@@ -1677,6 +1677,9 @@
   dashing = {
     "0.1.0" = import ./overrides/dashing/0.1.0;
   };
+  dask-kubernetes = {
+    "2024.8.0" = import ./overrides/dask-kubernetes/2024.8.0;
+  };
   data = {
     "0.4" = import ./overrides/data/0.4;
   };
