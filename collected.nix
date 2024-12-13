7c350cc4e6b0bc4e3e536b0ef9bd97d5465ef1ee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:40:38 +0000

    autogenerated overwrites for django-activity-stream==2.0.0

--- collected.nix
+++ collected.nix
@@ -1861,6 +1861,9 @@
   dj-static = {
     "0.0.6" = import ./overrides/dj-static/0.0.6;
   };
+  django-activity-stream = {
+    "2.0.0" = import ./overrides/django-activity-stream/2.0.0;
+  };
   django-admin-extra-buttons = {
     "1.5.8" = import ./overrides/django-admin-extra-buttons/1.5.8;
   };
