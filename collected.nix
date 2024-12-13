69a39a967474164a50675691aa900c11b30e5ef5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:06:29 +0000

    autogenerated overwrites for deal==4.24.4

--- collected.nix
+++ collected.nix
@@ -1722,6 +1722,9 @@
   dd-aliyun-python-sdk-core = {
     "2.8.2" = import ./overrides/dd-aliyun-python-sdk-core/2.8.2;
   };
+  deal = {
+    "4.24.4" = import ./overrides/deal/4.24.4;
+  };
   deap = {
     "1.4.1" = import ./overrides/deap/1.4.1;
   };
