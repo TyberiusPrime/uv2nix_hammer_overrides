80bb04cba29dc93a5c6b9d2944cfe79333cdf4af
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:10:32 +0000

    autogenerated overwrites for dogtail==1.0.1

--- collected.nix
+++ collected.nix
@@ -2334,6 +2334,9 @@
   docx2txt = {
     "0.8" = import ./overrides/docx2txt/0.8;
   };
+  dogtail = {
+    "1.0.1" = import ./overrides/dogtail/1.0.1;
+  };
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
