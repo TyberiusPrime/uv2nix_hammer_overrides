f6f76ea76e64ab8291a71a8b84473875a849accf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:39:18 +0000

    autogenerated overwrites for devito==4.8.11

--- collected.nix
+++ collected.nix
@@ -1782,6 +1782,9 @@
   device-detector = {
     "5.0.1" = import ./overrides/device-detector/5.0.1;
   };
+  devito = {
+    "4.8.11" = import ./overrides/devito/4.8.11;
+  };
   dexy = {
     "2.0.9" = import ./overrides/dexy/2.0.9;
   };
