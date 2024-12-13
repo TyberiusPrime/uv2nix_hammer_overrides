f3a970207fea16bbf4da80c7405a64579a7204cd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:24:44 +0000

    autogenerated overwrites for aiocron==1.8

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aiocron = {
+    "1.8" = import ./overrides/aiocron/1.8;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
