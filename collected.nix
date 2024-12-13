373954f27119e88929b650cf0f9c50c9175dbbdd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 18:28:09 +0000

    autogenerated overwrites for pytextrank==3.3.0

--- collected.nix
+++ collected.nix
@@ -7251,6 +7251,9 @@
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
+  pytextrank = {
+    "3.3.0" = import ./overrides/pytextrank/3.3.0;
+  };
   pytg = {
     "0.4.10" = import ./overrides/pytg/0.4.10;
   };
