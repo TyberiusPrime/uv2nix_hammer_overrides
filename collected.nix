60b6e681d0164dffdb87f2d016c88103e85e4169
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:05:18 +0000

    autogenerated overwrites for sqlacodegen==2.3.0.post1

--- collected.nix
+++ collected.nix
@@ -8667,6 +8667,9 @@
   spyne = {
     "2.14.0" = import ./overrides/spyne/2.14.0;
   };
+  sqlacodegen = {
+    "2.3.0.post1" = import ./overrides/sqlacodegen/2.3.0.post1;
+  };
   sqlalchemy = {
     "1.3.13" = import ./overrides/sqlalchemy/1.3.13;
     "1.3.20" = import ./overrides/sqlalchemy/1.3.20;
