c721d385086a760a6450991d71e6434a1d0ba239
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:09:27 +0000

    autogenerated overwrites for linkeddeepdict==1.1.0

--- collected.nix
+++ collected.nix
@@ -4425,6 +4425,9 @@
   linetable = {
     "0.0.3" = import ./overrides/linetable/0.0.3;
   };
+  linkeddeepdict = {
+    "1.1.0" = import ./overrides/linkeddeepdict/1.1.0;
+  };
   linkheader = {
     "0.4.3" = import ./overrides/linkheader/0.4.3;
   };
