57a7f0c8aa943031cb168eeef077579c8e37a56b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:34:32 +0000

    autogenerated overwrites for dom-toml==2.0.0

--- collected.nix
+++ collected.nix
@@ -2337,6 +2337,9 @@
   doit-py = {
     "0.5.0" = import ./overrides/doit-py/0.5.0;
   };
+  dom-toml = {
+    "2.0.0" = import ./overrides/dom-toml/2.0.0;
+  };
   dopy = {
     "0.3.7" = import ./overrides/dopy/0.3.7;
   };
