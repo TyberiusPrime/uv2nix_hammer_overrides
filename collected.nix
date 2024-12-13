4e902376a2eb2474373e51006427dc60f1f65729
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:27:26 +0000

    autogenerated overwrites for kobo==0.37.0

--- collected.nix
+++ collected.nix
@@ -4202,6 +4202,9 @@
   knnimpute = {
     "0.1.0" = import ./overrides/knnimpute/0.1.0;
   };
+  kobo = {
+    "0.37.0" = import ./overrides/kobo/0.37.0;
+  };
   kotti = {
     "2.0.9" = import ./overrides/kotti/2.0.9;
   };
