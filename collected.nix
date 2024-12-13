42b0f558758dda0e3ce68a2ca24cb59a0799aec4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:38:43 +0000

    autogenerated overwrites for pyroute2==0.7.12

--- collected.nix
+++ collected.nix
@@ -7075,6 +7075,9 @@
   pyric = {
     "0.1.6.3" = import ./overrides/pyric/0.1.6.3;
   };
+  pyroute2 = {
+    "0.7.12" = import ./overrides/pyroute2/0.7.12;
+  };
   pyroute2-core = {
     "0.6.13" = import ./overrides/pyroute2-core/0.6.13;
   };
