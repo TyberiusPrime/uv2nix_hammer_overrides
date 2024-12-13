36e80df3ae16ff3f6980c2077261a26a4bdf538f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:35:26 +0000

    autogenerated overwrites for python-mimeparse==2.0.0

--- collected.nix
+++ collected.nix
@@ -7406,6 +7406,9 @@
   python-lzo = {
     "1.15" = import ./overrides/python-lzo/1.15;
   };
+  python-mimeparse = {
+    "2.0.0" = import ./overrides/python-mimeparse/2.0.0;
+  };
   python-modargs = {
     "1.7" = import ./overrides/python-modargs/1.7;
   };
