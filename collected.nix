f8b59e578e05565adc6ecf3e9e1da9a0e38429cf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:40:01 +0000

    autogenerated overwrites for mwxml==0.3.4

--- collected.nix
+++ collected.nix
@@ -5061,6 +5061,9 @@
   mutmut = {
     "2.5.1" = import ./overrides/mutmut/2.5.1;
   };
+  mwxml = {
+    "0.3.4" = import ./overrides/mwxml/0.3.4;
+  };
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
