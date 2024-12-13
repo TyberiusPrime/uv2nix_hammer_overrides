3a98a5e9fce5c9f2d74ebcf9f0b3c21d23832319
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:18:24 +0000

    autogenerated overwrites for azure-graphrbac==0.61.1

--- collected.nix
+++ collected.nix
@@ -542,6 +542,9 @@
   azure-cognitiveservices-speech = {
     "1.40.0" = import ./overrides/azure-cognitiveservices-speech/1.40.0;
   };
+  azure-graphrbac = {
+    "0.61.1" = import ./overrides/azure-graphrbac/0.61.1;
+  };
   azureml-automl-core = {
     "1.57.0" = import ./overrides/azureml-automl-core/1.57.0;
   };
