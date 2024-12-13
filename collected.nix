05523788a46aa9c3314e2b0966aae838fd6f7a7c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:33:12 +0000

    autogenerated overwrites for nocasedict==2.0.4

--- collected.nix
+++ collected.nix
@@ -5286,6 +5286,9 @@
   nmslib = {
     "2.1.1" = import ./overrides/nmslib/2.1.1;
   };
+  nocasedict = {
+    "2.0.4" = import ./overrides/nocasedict/2.0.4;
+  };
   nodejs = {
     "0.1.1" = import ./overrides/nodejs/0.1.1;
   };
