1e62a4035db5f552390e24b056124cdbaa6df1e9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:43:14 +0000

    autogenerated overwrites for scrapy-deltafetch==2.0.1

--- collected.nix
+++ collected.nix
@@ -8242,6 +8242,9 @@
   scrapeasy = {
     "0.12" = import ./overrides/scrapeasy/0.12;
   };
+  scrapy-deltafetch = {
+    "2.0.1" = import ./overrides/scrapy-deltafetch/2.0.1;
+  };
   screed = {
     "1.1.3" = import ./overrides/screed/1.1.3;
   };
