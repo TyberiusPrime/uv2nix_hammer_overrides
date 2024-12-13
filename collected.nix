78c008c48b50076367aaf7674db23cda00dc36aa
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 03:55:53 +0000

    autogenerated overwrites for in-n-out==0.2.1

--- collected.nix
+++ collected.nix
@@ -3727,6 +3727,9 @@
   imutils = {
     "0.5.4" = import ./overrides/imutils/0.5.4;
   };
+  in-n-out = {
+    "0.2.1" = import ./overrides/in-n-out/0.2.1;
+  };
   infi-execute = {
     "0.1.8" = import ./overrides/infi-execute/0.1.8;
   };
