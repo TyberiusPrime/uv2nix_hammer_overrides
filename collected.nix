bdb25f07bde01299f8eadfaed5c551d5874baf99
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 21:14:25 +0000

    autogenerated overwrites for scikit-optimize==0.10.2

--- collected.nix
+++ collected.nix
@@ -8211,6 +8211,9 @@
     "0.24.2" = import ./overrides/scikit-learn/0.24.2;
     "1.2.2" = import ./overrides/scikit-learn/1.2.2;
   };
+  scikit-optimize = {
+    "0.10.2" = import ./overrides/scikit-optimize/0.10.2;
+  };
   scikit-surgerycore = {
     "0.7.2" = import ./overrides/scikit-surgerycore/0.7.2;
   };
