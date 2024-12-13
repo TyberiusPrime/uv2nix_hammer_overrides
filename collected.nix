ff8a048f5def2f7a2bc59326434b1ae85e0841d3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:22:09 +0000

    autogenerated overwrites for tcolorpy==0.1.6

--- collected.nix
+++ collected.nix
@@ -8909,6 +8909,9 @@
   tbx = {
     "1.9.4" = import ./overrides/tbx/1.9.4;
   };
+  tcolorpy = {
+    "0.1.6" = import ./overrides/tcolorpy/0.1.6;
+  };
   tdqm = {
     "0.0.1" = import ./overrides/tdqm/0.0.1;
   };
