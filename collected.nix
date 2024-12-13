cee42af8d20036581506f280db44bfc55ca27d60
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 13:34:23 +0000

    autogenerated overwrites for hurry.workflow==3.0.2

--- collected.nix
+++ collected.nix
@@ -3583,6 +3583,9 @@
   hunspell = {
     "0.5.5" = import ./overrides/hunspell/0.5.5;
   };
+  hurry-workflow = {
+    "3.0.2" = import ./overrides/hurry-workflow/3.0.2;
+  };
   husl = {
     "4.0.3" = import ./overrides/husl/4.0.3;
   };
