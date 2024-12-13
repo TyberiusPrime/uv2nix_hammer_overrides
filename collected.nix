256ab5859b27837f5989904aa7918a47f22bc4b1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 08:54:14 +0000

    autogenerated overwrites for djangorestframework-jwt==1.11.0

--- collected.nix
+++ collected.nix
@@ -2240,6 +2240,9 @@
   djangorestframework-jsonschema = {
     "0.1.1" = import ./overrides/djangorestframework-jsonschema/0.1.1;
   };
+  djangorestframework-jwt = {
+    "1.11.0" = import ./overrides/djangorestframework-jwt/1.11.0;
+  };
   djangotoolbox = {
     "1.8.0" = import ./overrides/djangotoolbox/1.8.0;
   };
