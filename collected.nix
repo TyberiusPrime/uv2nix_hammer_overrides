5337d2e6cce3995a7f1aaf6ff0bc6a2beda6c8f6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 14:58:55 +0000

    autogenerated overwrites for faster-fifo==1.4.7

--- collected.nix
+++ collected.nix
@@ -2688,6 +2688,9 @@
   fastecdsa = {
     "2.3.2" = import ./overrides/fastecdsa/2.3.2;
   };
+  faster-fifo = {
+    "1.4.7" = import ./overrides/faster-fifo/1.4.7;
+  };
   fastimport = {
     "0.9.14" = import ./overrides/fastimport/0.9.14;
   };
