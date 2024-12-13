c6eb486be3d93f897f57c23241f20ba61d8845e3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:42:02 +0000

    autogenerated overwrites for translate-toolkit==3.13.3

--- collected.nix
+++ collected.nix
@@ -9293,6 +9293,9 @@
   transifex-client = {
     "0.14.4" = import ./overrides/transifex-client/0.14.4;
   };
+  translate-toolkit = {
+    "3.13.3" = import ./overrides/translate-toolkit/3.13.3;
+  };
   translitcodec = {
     "0.7.0" = import ./overrides/translitcodec/0.7.0;
   };
