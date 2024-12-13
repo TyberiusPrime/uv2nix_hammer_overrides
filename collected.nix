4a5272a2356c315554ed861d7f7beef8c73218dd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:26:09 +0000

    autogenerated overwrites for xmltramp2==3.1.1

--- collected.nix
+++ collected.nix
@@ -10025,6 +10025,9 @@
   xmlbuilder = {
     "1.0" = import ./overrides/xmlbuilder/1.0;
   };
+  xmltramp2 = {
+    "3.1.1" = import ./overrides/xmltramp2/3.1.1;
+  };
   xmpppy = {
     "0.7.1" = import ./overrides/xmpppy/0.7.1;
   };
