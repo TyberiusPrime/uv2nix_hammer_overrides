102561ef141e07580ad21a2c001c073ffbf3133c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:53:10 +0000

    autogenerated overwrites for openimis-be-calculation==1.7.1

--- collected.nix
+++ collected.nix
@@ -5543,6 +5543,9 @@
   openidc-client = {
     "0.6.0" = import ./overrides/openidc-client/0.6.0;
   };
+  openimis-be-calculation = {
+    "1.7.1" = import ./overrides/openimis-be-calculation/1.7.1;
+  };
   openmdao = {
     "3.34.2" = import ./overrides/openmdao/3.34.2;
   };
