d220344864c31e16049fd42884f54af00c815d6c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 07:38:07 +0000

    autogenerated overwrites for aioftp==0.22.3

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aioftp = {
+    "0.22.3" = import ./overrides/aioftp/0.22.3;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
