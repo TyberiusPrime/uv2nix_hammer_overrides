405ec352f227c929c90c2bdd70899fd5e99a35f2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:14:14 +0000

    autogenerated overwrites for aiobotocore==2.14.0

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aiobotocore = {
+    "2.14.0" = import ./overrides/aiobotocore/2.14.0;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
