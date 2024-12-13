97465131b9a55f9b46fe2af19665ea0ec5737041
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:06:07 +0000

    autogenerated overwrites for py-cui==0.1.6

--- collected.nix
+++ collected.nix
@@ -6306,6 +6306,9 @@
   py-bcrypt = {
     "0.4" = import ./overrides/py-bcrypt/0.4;
   };
+  py-cui = {
+    "0.1.6" = import ./overrides/py-cui/0.1.6;
+  };
   py-dag = {
     "3.0.1" = import ./overrides/py-dag/3.0.1;
   };
