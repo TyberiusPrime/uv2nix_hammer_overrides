130c14306ffddad04847332f04ad8453a41cb9f6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:37:47 +0000

    autogenerated overwrites for parsedmarc==8.15.0

--- collected.nix
+++ collected.nix
@@ -5723,6 +5723,9 @@
   parse = {
     "1.6.5" = import ./overrides/parse/1.6.5;
   };
+  parsedmarc = {
+    "8.15.0" = import ./overrides/parsedmarc/8.15.0;
+  };
   parsimonious = {
     "0.9.0" = import ./overrides/parsimonious/0.9.0;
   };
