43ce41368db91fec853397050e16d3ed14d3c7ae
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 05:03:37 +0000

    autogenerated overwrites for django-manager-utils==3.1.5

--- collected.nix
+++ collected.nix
@@ -2036,6 +2036,9 @@
   django-m2m-history = {
     "0.3.6" = import ./overrides/django-m2m-history/0.3.6;
   };
+  django-manager-utils = {
+    "3.1.5" = import ./overrides/django-manager-utils/3.1.5;
+  };
   django-markdown-deux = {
     "1.0.6" = import ./overrides/django-markdown-deux/1.0.6;
   };
