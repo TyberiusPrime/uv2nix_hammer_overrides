792fc1a39eb0621373d353070dfa4ba9fc74e4a5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 15:06:32 +0000

    autogenerated overwrites for aiodhcpwatcher==1.0.2

--- collected.nix
+++ collected.nix
@@ -95,6 +95,9 @@
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
+  aiodhcpwatcher = {
+    "1.0.2" = import ./overrides/aiodhcpwatcher/1.0.2;
+  };
   aiohttp = {
     "3.8.3" = import ./overrides/aiohttp/3.8.3;
     "3.8.6" = import ./overrides/aiohttp/3.8.6;
