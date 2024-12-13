24725820b1ca7dad97fd1b910da4f8c606361267
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:48:46 +0000

    autogenerated overwrites for types-aiobotocore-iot-roborunner==2.12.2

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-iot-roborunner = {
+    "2.12.2" = import ./overrides/types-aiobotocore-iot-roborunner/2.12.2;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
