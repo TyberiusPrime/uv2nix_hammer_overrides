1280eba73ed85f134faabccef15a93c15d3a9cb1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 17:26:28 +0000

    autogenerated overwrites for georss-tfs-incidents-client==0.4

--- collected.nix
+++ collected.nix
@@ -3137,6 +3137,9 @@
   geometric = {
     "1.0.2" = import ./overrides/geometric/1.0.2;
   };
+  georss-tfs-incidents-client = {
+    "0.4" = import ./overrides/georss-tfs-incidents-client/0.4;
+  };
   getch = {
     "1.0" = import ./overrides/getch/1.0;
   };
