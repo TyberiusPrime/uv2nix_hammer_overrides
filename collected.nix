3fbc7212b4cf742aa102f484aca5c10533d5f4ad
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:55:29 +0000

    autogenerated overwrites for py-canary==0.5.4

--- collected.nix
+++ collected.nix
@@ -6306,6 +6306,9 @@
   py-bcrypt = {
     "0.4" = import ./overrides/py-bcrypt/0.4;
   };
+  py-canary = {
+    "0.5.4" = import ./overrides/py-canary/0.5.4;
+  };
   py-dag = {
     "3.0.1" = import ./overrides/py-dag/3.0.1;
   };
