412be59af5d1ddaf25fd366d5802a28fa522bb82
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 06:50:01 +0000

    autogenerated overwrites for cookies==2.2.1

--- collected.nix
+++ collected.nix
@@ -1403,6 +1403,9 @@
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
+  cookies = {
+    "2.2.1" = import ./overrides/cookies/2.2.1;
+  };
   coqui-trainer = {
     "0.0.5" = import ./overrides/coqui-trainer/0.0.5;
   };
