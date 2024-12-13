02d3a484a0d7e676d94658501300092616b5d0e5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:38:04 +0000

    autogenerated overwrites for cexprtk==0.4.1

--- collected.nix
+++ collected.nix
@@ -978,6 +978,9 @@
   cenpy = {
     "1.0.1" = import ./overrides/cenpy/1.0.1;
   };
+  cexprtk = {
+    "0.4.1" = import ./overrides/cexprtk/0.4.1;
+  };
   cf-units = {
     "3.2.0" = import ./overrides/cf-units/3.2.0;
   };
