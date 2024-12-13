0fac22620162651ca97d202be1d114fc644b0ad4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:17:29 +0000

    autogenerated overwrites for llama-index-graph-stores-neo4j==0.3.2

--- collected.nix
+++ collected.nix
@@ -4455,6 +4455,9 @@
   ll-xist = {
     "5.76" = import ./overrides/ll-xist/5.76;
   };
+  llama-index-graph-stores-neo4j = {
+    "0.3.2" = import ./overrides/llama-index-graph-stores-neo4j/0.3.2;
+  };
   llist = {
     "0.8" = import ./overrides/llist/0.8;
   };
