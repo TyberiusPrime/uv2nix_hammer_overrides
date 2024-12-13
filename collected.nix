6d2cec9e9cd16453ec70a59de3577914b3d2e22e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 23:13:32 +0000

    autogenerated overwrites for s3pathlib==2.3.1

--- collected.nix
+++ collected.nix
@@ -8096,6 +8096,9 @@
   s3keyring = {
     "0.2.4" = import ./overrides/s3keyring/0.2.4;
   };
+  s3pathlib = {
+    "2.3.1" = import ./overrides/s3pathlib/2.3.1;
+  };
   sabyenc = {
     "3.3.6" = import ./overrides/sabyenc/3.3.6;
   };
