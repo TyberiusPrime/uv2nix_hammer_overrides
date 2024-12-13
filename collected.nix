bb67fa227bfbe11883c9bd707ffb6c3507a927c3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 19:37:42 +0000

    autogenerated overwrites for cognite-sdk-experimental==1.0.0

--- collected.nix
+++ collected.nix
@@ -1232,6 +1232,9 @@
   coffeescript = {
     "2.0.3" = import ./overrides/coffeescript/2.0.3;
   };
+  cognite-sdk-experimental = {
+    "1.0.0" = import ./overrides/cognite-sdk-experimental/1.0.0;
+  };
   coincurve = {
     "15.0.0" = import ./overrides/coincurve/15.0.0;
   };
