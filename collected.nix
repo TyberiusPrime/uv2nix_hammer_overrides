b4263cb1b0c07571b31657066f9bea795d9159a8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 20:56:43 +0000

    autogenerated overwrites for syst3m==2.19.3

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  syst3m = {
+    "2.19.3" = import ./overrides/syst3m/2.19.3;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
