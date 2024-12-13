f65685fa4326d90e1e20e69e72ed01eb76f2a24a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 01:05:45 +0000

    autogenerated overwrites for ase==3.23.0

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  ase = {
+    "3.23.0" = import ./overrides/ase/3.23.0;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
