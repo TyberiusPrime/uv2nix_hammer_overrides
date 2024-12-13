34eeacdb0f0cd90dfd74bd797e1f8c7bd2de4dc0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-13 00:06:28 +0000

    autogenerated overwrites for aio-geojson-nsw-rfs-incidents==0.7

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aio-geojson-nsw-rfs-incidents = {
+    "0.7" = import ./overrides/aio-geojson-nsw-rfs-incidents/0.7;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
