590fa13254d9df81de349347ce73d3c9013c9e5c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:46:34 +0000

    autogenerated overwrites for rdflib-sqlalchemy==0.5.4

--- collected.nix
+++ collected.nix
@@ -7790,6 +7790,9 @@
   rcsb-utils-io = {
     "1.49" = import ./overrides/rcsb-utils-io/1.49;
   };
+  rdflib-sqlalchemy = {
+    "0.5.4" = import ./overrides/rdflib-sqlalchemy/0.5.4;
+  };
   rdp = {
     "0.8" = import ./overrides/rdp/0.8;
   };
