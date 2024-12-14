b7c852271c3643e3e8ea3114e2989cc518b429c0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:30:26 +0000

    autogenerated overwrites for gevent-websocket==0.10.1

--- collected.nix
+++ collected.nix
@@ -3146,6 +3146,9 @@
   getkey = {
     "0.6.5" = import ./overrides/getkey/0.6.5;
   };
+  gevent-websocket = {
+    "0.10.1" = import ./overrides/gevent-websocket/0.10.1;
+  };
   gexml = {
     "1.2.0" = import ./overrides/gexml/1.2.0;
   };
