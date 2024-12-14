f297127bc68da0e63afd049b1dc31669d47998ea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:58:13 +0000

    autogenerated overwrites for aioambient==2024.8.0

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aioambient = {
+    "2024.8.0" = import ./overrides/aioambient/2024.8.0;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
