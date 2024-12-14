de793e94f32e31e3fbee257cd113639b24f46d30
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:57:10 +0000

    autogenerated overwrites for implicit==0.7.2

--- overrides/implicit/0.7.2/default.nix
+++ overrides/implicit/0.7.2/default.nix
@@ -1,8 +1,3 @@
-{ pkgs, ... }:
-old: {
-  buildInputs = old.buildInputs or [ ] ++ [
-    pkgs.cudaPackages.libcurand
-    pkgs.cudaPackages_11.cuda_cudart
-    pkgs.cudaPackages_11.libcublas
-  ];
-}
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.cudaPackages.libcurand pkgs.cudaPackages_11.cuda_cudart pkgs.cudaPackages_11.libcublas];} else {buildInputs = old.buildInputs or [] ++ [pkgs.cudaPackages.libcurand pkgs.cudaPackages_11.cuda_cudart pkgs.cudaPackages_11.libcublas];nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cmake = [];cython = [];ninja = [];scikit-build = [];scipy = [];setuptools = [];wheel = [];});}
+        
