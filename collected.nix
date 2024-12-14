e422f7469cf75e5fef7e6d8fea205cd0e3242000
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 05:48:02 +0000

    autogenerated overwrites for ariadne==0.23.0

--- collected.nix
+++ collected.nix
@@ -364,6 +364,9 @@
   arguments = {
     "76" = import ./overrides/arguments/76;
   };
+  ariadne = {
+    "0.23.0" = import ./overrides/ariadne/0.23.0;
+  };
   armstrong-utils-backends = {
     "1.1.1" = import ./overrides/armstrong-utils-backends/1.1.1;
   };
