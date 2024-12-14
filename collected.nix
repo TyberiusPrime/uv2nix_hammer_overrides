f8bba546a279ffa9fcef5b0ffc215f6399354463
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 19:40:56 +0000

    autogenerated overwrites for upload==0.1.1

--- collected.nix
+++ collected.nix
@@ -9561,6 +9561,9 @@
   update-copyright = {
     "0.6.2" = import ./overrides/update-copyright/0.6.2;
   };
+  upload = {
+    "0.1.1" = import ./overrides/upload/0.1.1;
+  };
   upsetplot = {
     "0.9.0" = import ./overrides/upsetplot/0.9.0;
   };
