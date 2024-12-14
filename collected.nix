bf0eae363f00f884af49d266d1a14a25261fdeea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:28:07 +0000

    autogenerated overwrites for cert-chain-resolver==1.3.0

--- collected.nix
+++ collected.nix
@@ -978,6 +978,9 @@
   cenpy = {
     "1.0.1" = import ./overrides/cenpy/1.0.1;
   };
+  cert-chain-resolver = {
+    "1.3.0" = import ./overrides/cert-chain-resolver/1.3.0;
+  };
   cf-units = {
     "3.2.0" = import ./overrides/cf-units/3.2.0;
   };
