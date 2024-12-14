0f0b942df10e4b3f42cde57c86d9e8fead2cf17b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:27:59 +0000

    autogenerated overwrites for dvc-render==1.0.2

--- collected.nix
+++ collected.nix
@@ -2421,6 +2421,9 @@
   durc = {
     "3.1.0" = import ./overrides/durc/3.1.0;
   };
+  dvc-render = {
+    "1.0.2" = import ./overrides/dvc-render/1.0.2;
+  };
   dxpy = {
     "0.382.0" = import ./overrides/dxpy/0.382.0;
   };
