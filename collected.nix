456ae83c20e4f2a46c859c0008b709c335f0cd3c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:06:41 +0000

    autogenerated overwrites for adafruit-circuitpython-portalbase==2.1.1

--- collected.nix
+++ collected.nix
@@ -35,6 +35,9 @@
   active-alchemy = {
     "1.1.0" = import ./overrides/active-alchemy/1.1.0;
   };
+  adafruit-circuitpython-portalbase = {
+    "2.1.1" = import ./overrides/adafruit-circuitpython-portalbase/2.1.1;
+  };
   adafruit-extended-bus = {
     "1.0.2" = import ./overrides/adafruit-extended-bus/1.0.2;
   };
