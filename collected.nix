16c6a00458ecbe6b55dc7c9fb63ad4a837cd2952
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:16:16 +0000

    autogenerated overwrites for sphinx-rtd-dark-mode==1.3.0

--- collected.nix
+++ collected.nix
@@ -8613,6 +8613,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-rtd-dark-mode = {
+    "1.3.0" = import ./overrides/sphinx-rtd-dark-mode/1.3.0;
+  };
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
