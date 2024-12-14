d304420d886b16e2bc5cd5c5c5557bb0b7b1b4c7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 17:53:25 +0000

    autogenerated overwrites for aioasuswrt==1.4.0

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aioasuswrt = {
+    "1.4.0" = import ./overrides/aioasuswrt/1.4.0;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
