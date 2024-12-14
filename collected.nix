0563e0f79b95029a91a7350ed12fb04a893b2c94
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 23:33:35 +0000

    autogenerated overwrites for debuglater==1.4.4

--- collected.nix
+++ collected.nix
@@ -1728,6 +1728,9 @@
   debts = {
     "0.5" = import ./overrides/debts/0.5;
   };
+  debuglater = {
+    "1.4.4" = import ./overrides/debuglater/1.4.4;
+  };
   decore = {
     "0.0.1" = import ./overrides/decore/0.0.1;
   };
