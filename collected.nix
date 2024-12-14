0d9a057fdec6515a48c7fbd99609d0c2a0697a40
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 06:13:07 +0000

    autogenerated overwrites for hsh==1.1.0

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  hsh = {
+    "1.1.0" = import ./overrides/hsh/1.1.0;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
