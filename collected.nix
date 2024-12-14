8ffcb9072c614cc00b7a623975fa31e6271e3c09
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 05:17:14 +0000

    autogenerated overwrites for opencensus-ext-azure==1.1.13

--- collected.nix
+++ collected.nix
@@ -5519,6 +5519,9 @@
   openapi-codec = {
     "1.3.2" = import ./overrides/openapi-codec/1.3.2;
   };
+  opencensus-ext-azure = {
+    "1.1.13" = import ./overrides/opencensus-ext-azure/1.1.13;
+  };
   opencmiss-utils = {
     "0.4.2" = import ./overrides/opencmiss-utils/0.4.2;
   };
