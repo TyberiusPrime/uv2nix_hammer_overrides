cd94c557914b55ced354335abb7490fc0c12704f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:42:17 +0000

    autogenerated overwrites for invenio-cache==1.3.0

--- collected.nix
+++ collected.nix
@@ -3805,6 +3805,9 @@
   intervaltree = {
     "3.1.0" = import ./overrides/intervaltree/3.1.0;
   };
+  invenio-cache = {
+    "1.3.0" = import ./overrides/invenio-cache/1.3.0;
+  };
   invenio-github = {
     "1.5.2" = import ./overrides/invenio-github/1.5.2;
   };
