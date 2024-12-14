feb348128d194d062640bdab2838fc960b6aa855
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 06:11:23 +0000

    autogenerated overwrites for hstspreload==2024.9.1

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  hstspreload = {
+    "2024.9.1" = import ./overrides/hstspreload/2024.9.1;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
