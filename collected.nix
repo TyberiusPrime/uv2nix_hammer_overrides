4ed3ef9a1e3a7427b550880cd4cc39a61615f8fd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:06:58 +0000

    autogenerated overwrites for gpuctypes==0.3.0

--- collected.nix
+++ collected.nix
@@ -3286,6 +3286,9 @@
   gpt4all = {
     "2.8.2" = import ./overrides/gpt4all/2.8.2;
   };
+  gpuctypes = {
+    "0.3.0" = import ./overrides/gpuctypes/0.3.0;
+  };
   gpustat = {
     "1.1.1" = import ./overrides/gpustat/1.1.1;
   };
