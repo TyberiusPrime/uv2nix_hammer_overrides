982189bd335f288c059281d54cbf441bf66d2491
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:02:34 +0000

    autogenerated overwrites for baytune==0.5.0

--- collected.nix
+++ collected.nix
@@ -644,6 +644,9 @@
   bayespy = {
     "0.6.2" = import ./overrides/bayespy/0.6.2;
   };
+  baytune = {
+    "0.5.0" = import ./overrides/baytune/0.5.0;
+  };
   bcf = {
     "1.9.0" = import ./overrides/bcf/1.9.0;
   };
