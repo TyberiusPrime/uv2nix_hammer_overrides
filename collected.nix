88905b15e78aaf20e9d63585078fc368b1a86e7e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:04:36 +0000

    autogenerated overwrites for cmaes==0.11.1

--- collected.nix
+++ collected.nix
@@ -1190,6 +1190,9 @@
   cloudshell-snmp = {
     "3.3.0" = import ./overrides/cloudshell-snmp/3.3.0;
   };
+  cmaes = {
+    "0.11.1" = import ./overrides/cmaes/0.11.1;
+  };
   cmapy = {
     "0.6.6" = import ./overrides/cmapy/0.6.6;
   };
