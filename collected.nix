7e8e1743df63a3a6fe1ff6c7519c2f7fde276e9c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 23:49:13 +0000

    autogenerated overwrites for segno==1.6.1

--- collected.nix
+++ collected.nix
@@ -8266,6 +8266,9 @@
   seantis-plonetools = {
     "0.18" = import ./overrides/seantis-plonetools/0.18;
   };
+  segno = {
+    "1.6.1" = import ./overrides/segno/1.6.1;
+  };
   selector = {
     "0.10.1" = import ./overrides/selector/0.10.1;
   };
