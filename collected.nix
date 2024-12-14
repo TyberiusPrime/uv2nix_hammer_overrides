16264b7ac98a8b1223c7d20809ed25a997aa985e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:03:32 +0000

    autogenerated overwrites for dbus-signature-pyparsing==0.4.1

--- collected.nix
+++ collected.nix
@@ -1716,6 +1716,9 @@
   dbstream = {
     "0.1.25" = import ./overrides/dbstream/0.1.25;
   };
+  dbus-signature-pyparsing = {
+    "0.4.1" = import ./overrides/dbus-signature-pyparsing/0.4.1;
+  };
   dbutils = {
     "1.3" = import ./overrides/dbutils/1.3;
   };
