13d8f9d6586bffdbf4691d9c1fd6836a02edd672
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:07:29 +0000

    autogenerated overwrites for atpublic==5.0

--- collected.nix
+++ collected.nix
@@ -464,6 +464,9 @@
   atomman = {
     "1.4.11" = import ./overrides/atomman/1.4.11;
   };
+  atpublic = {
+    "5.0" = import ./overrides/atpublic/5.0;
+  };
   auroranoaa = {
     "0.0.3" = import ./overrides/auroranoaa/0.0.3;
   };
