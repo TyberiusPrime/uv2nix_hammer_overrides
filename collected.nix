e2fbdedecf13e0831b9b9bc795ba7f0c524d3ec8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 04:44:45 +0000

    autogenerated overwrites for buildcatrust==0.3.0

--- collected.nix
+++ collected.nix
@@ -861,6 +861,9 @@
   btrfs = {
     "14.1" = import ./overrides/btrfs/14.1;
   };
+  buildcatrust = {
+    "0.3.0" = import ./overrides/buildcatrust/0.3.0;
+  };
   busrt = {
     "0.1.2" = import ./overrides/busrt/0.1.2;
   };
