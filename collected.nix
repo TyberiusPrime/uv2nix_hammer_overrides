a6562d6fab0f381e0ca21ce49fdf884c6c1bacd7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 09:51:03 +0000

    autogenerated overwrites for influxdb3-python==0.8.0

--- collected.nix
+++ collected.nix
@@ -3742,6 +3742,9 @@
   inflection = {
     "0.3.1" = import ./overrides/inflection/0.3.1;
   };
+  influxdb3-python = {
+    "0.8.0" = import ./overrides/influxdb3-python/0.8.0;
+  };
   infrae-cache = {
     "1.0.1" = import ./overrides/infrae-cache/1.0.1;
   };
