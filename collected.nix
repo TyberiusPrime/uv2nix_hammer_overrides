387acaa8dab9ec2aeea55c706c1de6b63693c3c3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:06:47 +0000

    autogenerated overwrites for eventsourcing==9.3.2

--- collected.nix
+++ collected.nix
@@ -2607,6 +2607,9 @@
   events = {
     "0.3" = import ./overrides/events/0.3;
   };
+  eventsourcing = {
+    "9.3.2" = import ./overrides/eventsourcing/9.3.2;
+  };
   ewmh = {
     "0.1.6" = import ./overrides/ewmh/0.1.6;
   };
