5bf85fa6c2bf63a8ad9c8c98027a56e65b5ba41f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:47:02 +0000

    autogenerated overwrites for pytest-tornado==0.8.1

--- collected.nix
+++ collected.nix
@@ -7248,6 +7248,9 @@
   pytest-subtesthack = {
     "0.2.0" = import ./overrides/pytest-subtesthack/0.2.0;
   };
+  pytest-tornado = {
+    "0.8.1" = import ./overrides/pytest-tornado/0.8.1;
+  };
   pytest-watch = {
     "4.2.0" = import ./overrides/pytest-watch/4.2.0;
   };
