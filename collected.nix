3bd2326f6998d452e9c6ad216995bb9f0f2cf594
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:45:16 +0000

    autogenerated overwrites for pynpm==0.2.0

--- collected.nix
+++ collected.nix
@@ -6913,6 +6913,9 @@
     "0.6.4" = import ./overrides/pynisher/0.6.4;
     "1.0.10" = import ./overrides/pynisher/1.0.10;
   };
+  pynpm = {
+    "0.2.0" = import ./overrides/pynpm/0.2.0;
+  };
   pynsq = {
     "0.9.1" = import ./overrides/pynsq/0.9.1;
   };
