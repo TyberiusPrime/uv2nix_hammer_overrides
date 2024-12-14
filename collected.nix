eb202e33e709dd406b4b2f664f84a8b4df4fa451
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:30:46 +0000

    autogenerated overwrites for selectolax==0.3.21

--- collected.nix
+++ collected.nix
@@ -8266,6 +8266,9 @@
   seantis-plonetools = {
     "0.18" = import ./overrides/seantis-plonetools/0.18;
   };
+  selectolax = {
+    "0.3.21" = import ./overrides/selectolax/0.3.21;
+  };
   selector = {
     "0.10.1" = import ./overrides/selector/0.10.1;
   };
