77b605d6618f69b69394da055f1f7c9b69ee36ac
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:26:08 +0000

    autogenerated overwrites for neurokit2==0.2.10

--- collected.nix
+++ collected.nix
@@ -5202,6 +5202,9 @@
   networkx = {
     "2.2" = import ./overrides/networkx/2.2;
   };
+  neurokit2 = {
+    "0.2.10" = import ./overrides/neurokit2/0.2.10;
+  };
   neuronpy = {
     "0.1.6" = import ./overrides/neuronpy/0.1.6;
   };
