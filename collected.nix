fe5e36006c94f190fa1032f0b6d961f8f967a2a3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:28:36 +0000

    autogenerated overwrites for openapi-core==0.19.4

--- collected.nix
+++ collected.nix
@@ -5519,6 +5519,9 @@
   openapi-codec = {
     "1.3.2" = import ./overrides/openapi-codec/1.3.2;
   };
+  openapi-core = {
+    "0.19.4" = import ./overrides/openapi-core/0.19.4;
+  };
   opencmiss-utils = {
     "0.4.2" = import ./overrides/opencmiss-utils/0.4.2;
   };
