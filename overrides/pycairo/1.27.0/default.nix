686fa73c9f84a443197c2beb26b41c5caa3e7ff9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:40:59 +0000

    autogenerated overwrites for PyGObject==3.48.2

--- overrides/pycairo/1.27.0/default.nix
+++ overrides/pycairo/1.27.0/default.nix
@@ -1,29 +1,8 @@
-{
-  resolveBuildSystem,
-  helpers,
-  pkgs,
-  ...
-}:
-old:
-if (helpers.isWheel old) then
-  { buildInputs = old.buildInputs or [ ] ++ [ pkgs.cairo ]; }
-else
-  {
-    buildInputs = old.buildInputs or [ ] ++ [ pkgs.cairo ];
-    nativeBuildInputs =
-      old.nativeBuildInputs or [ ]
-      ++ [
-        pkgs.meson
-        pkgs.pkg-config
-      ]
-      ++ (resolveBuildSystem { meson-python = [ ]; });
-    preBuild = [
-      old.preBuild or ""
-      ''
-        cd /build
-        # find the first directory containing either pyproject.toml or setup.py
-        buildDir=$(find . -maxdepth 1 -type d -exec test -e "{}/pyproject.toml" -o -e "{}/setup.py" \; -print -quit)
-        cd $buildDir
-      ''
-    ];
-  }
+{final, helpers, pkgs, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {buildInputs = old.buildInputs or [] ++ [pkgs.cairo];} else {buildInputs = old.buildInputs or [] ++ [pkgs.cairo];nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.meson pkgs.pkg-config] ++ ( resolveBuildSystem {meson-python = [];ninja = [];});preBuild = [old.preBuild or "" ''
+                cd /build
+                # find the first directory containing either pyproject.toml or setup.py
+                buildDir=$(find . -maxdepth 1 -type d -exec test -e "{}/pyproject.toml" -o -e "{}/setup.py" \; -print -quit)
+                cd $buildDir
+                ''];}
+        
