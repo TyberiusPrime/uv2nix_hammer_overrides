29dd7ceb812618c89e909f5ac185e98c515236c1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:02:15 +0000

    autogenerated overwrites for toil==7.0.0

--- collected.nix
+++ collected.nix
@@ -9154,6 +9154,9 @@
   tlv8 = {
     "0.10.0" = import ./overrides/tlv8/0.10.0;
   };
+  toil = {
+    "7.0.0" = import ./overrides/toil/7.0.0;
+  };
   toil-lib = {
     "1.1.8" = import ./overrides/toil-lib/1.1.8;
   };
