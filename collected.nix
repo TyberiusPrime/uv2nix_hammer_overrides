4fda40d443b275dae5c955badb5381f3004b136a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:02:02 +0000

    autogenerated overwrites for gspread-pandas==3.3.0

--- collected.nix
+++ collected.nix
@@ -3350,6 +3350,9 @@
   gsignals = {
     "0.2.1" = import ./overrides/gsignals/0.2.1;
   };
+  gspread-pandas = {
+    "3.3.0" = import ./overrides/gspread-pandas/3.3.0;
+  };
   gssapi = {
     "1.8.3" = import ./overrides/gssapi/1.8.3;
   };
