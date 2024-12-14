9e62ac5e6c019857065b708d31f62ee8bc0ea255
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:10:22 +0000

    autogenerated overwrites for poppy-core==0.11.5

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  poppy-core = {
+    "0.11.5" = import ./overrides/poppy-core/0.11.5;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
