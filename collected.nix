f98eaf6003b50c5462d1df4ed48bc2acb41e1bec
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:29:11 +0000

    autogenerated overwrites for elgato==5.1.2

--- collected.nix
+++ collected.nix
@@ -2496,6 +2496,9 @@
   elasticutils = {
     "0.10.3" = import ./overrides/elasticutils/0.10.3;
   };
+  elgato = {
+    "5.1.2" = import ./overrides/elgato/5.1.2;
+  };
   eli5 = {
     "0.13.0" = import ./overrides/eli5/0.13.0;
   };
