e425ae6e679f0ac8b3e084c1ecf56355fb3504a9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 05:45:51 +0000

    autogenerated overwrites for fireflyalgorithm==0.4.5

--- collected.nix
+++ collected.nix
@@ -2778,6 +2778,9 @@
   fire = {
     "0.6.0" = import ./overrides/fire/0.6.0;
   };
+  fireflyalgorithm = {
+    "0.4.5" = import ./overrides/fireflyalgorithm/0.4.5;
+  };
   fireworks = {
     "2.0.3" = import ./overrides/fireworks/2.0.3;
   };
