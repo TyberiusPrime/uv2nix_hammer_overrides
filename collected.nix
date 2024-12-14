3ee2971f0f62c9a0d4a911d76a5d6e746ad7cf7c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:38:12 +0000

    autogenerated overwrites for defusedxml==0.7.1

--- collected.nix
+++ collected.nix
@@ -1758,6 +1758,9 @@
   defopt = {
     "6.4.0" = import ./overrides/defopt/6.4.0;
   };
+  defusedxml = {
+    "0.7.1" = import ./overrides/defusedxml/0.7.1;
+  };
   delfick-error = {
     "1.8" = import ./overrides/delfick-error/1.8;
   };
