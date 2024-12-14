aa80763e374e86ff699448e114d8e7edea28b9e0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 17:27:22 +0000

    autogenerated overwrites for stardist==0.9.1

--- collected.nix
+++ collected.nix
@@ -8711,6 +8711,9 @@
   sslyze = {
     "6.0.0" = import ./overrides/sslyze/6.0.0;
   };
+  stardist = {
+    "0.9.1" = import ./overrides/stardist/0.9.1;
+  };
   starkbank-ecdsa = {
     "2.2.0" = import ./overrides/starkbank-ecdsa/2.2.0;
   };
