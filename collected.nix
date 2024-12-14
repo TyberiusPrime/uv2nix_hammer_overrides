a80e893232ac26a7e7a95cdd4aae918fa8436ea8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:47:42 +0000

    autogenerated overwrites for polywrap-core==0.1.2

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  polywrap-core = {
+    "0.1.2" = import ./overrides/polywrap-core/0.1.2;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
