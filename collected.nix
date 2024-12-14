d6b28798dc8b2353c89d9e0944250a75853e8717
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:35:29 +0000

    autogenerated overwrites for clvm-rs==0.8.0

--- collected.nix
+++ collected.nix
@@ -1190,6 +1190,9 @@
   cloudshell-snmp = {
     "3.3.0" = import ./overrides/cloudshell-snmp/3.3.0;
   };
+  clvm-rs = {
+    "0.8.0" = import ./overrides/clvm-rs/0.8.0;
+  };
   cmapy = {
     "0.6.6" = import ./overrides/cmapy/0.6.6;
   };
