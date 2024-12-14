aa1687faee9f597dad0d3a526ec6a3a8ff8f86ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 19:46:14 +0000

    autogenerated overwrites for scikit-allel==1.3.11

--- collected.nix
+++ collected.nix
@@ -8195,6 +8195,9 @@
   sciencedates = {
     "1.5.0" = import ./overrides/sciencedates/1.5.0;
   };
+  scikit-allel = {
+    "1.3.11" = import ./overrides/scikit-allel/1.3.11;
+  };
   scikit-bio = {
     "0.6.2" = import ./overrides/scikit-bio/0.6.2;
   };
