8c220ceaf322a755f439187783a8a88b9802d8b7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:00:51 +0000

    autogenerated overwrites for flake8-comprehensions==3.15.0

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-comprehensions = {
+    "3.15.0" = import ./overrides/flake8-comprehensions/3.15.0;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
