083975915c8d60ec1d898471384812bf9797f384
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:18:48 +0000

    autogenerated overwrites for databricks-api==0.9.0

--- collected.nix
+++ collected.nix
@@ -1680,6 +1680,9 @@
   data = {
     "0.4" = import ./overrides/data/0.4;
   };
+  databricks-api = {
+    "0.9.0" = import ./overrides/databricks-api/0.9.0;
+  };
   datacleaner = {
     "0.1.5" = import ./overrides/datacleaner/0.1.5;
   };
