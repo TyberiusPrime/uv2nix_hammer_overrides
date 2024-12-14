37b88e334d19be1a01ccdafd78bd2a04e74c9555
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:51:24 +0000

    autogenerated overwrites for uri==2.0.1

--- collected.nix
+++ collected.nix
@@ -9573,6 +9573,9 @@
   upyun = {
     "2.5.5" = import ./overrides/upyun/2.5.5;
   };
+  uri = {
+    "2.0.1" = import ./overrides/uri/2.0.1;
+  };
   urlnorm = {
     "1.1.4" = import ./overrides/urlnorm/1.1.4;
   };
