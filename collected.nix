81de3804a800bbd65bbeccc2dde67c9978a7fbe5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:12:20 +0000

    autogenerated overwrites for fastbencode==0.3.1

--- collected.nix
+++ collected.nix
@@ -2679,6 +2679,9 @@
   fastapi-cli = {
     "0.0.5" = import ./overrides/fastapi-cli/0.0.5;
   };
+  fastbencode = {
+    "0.3.1" = import ./overrides/fastbencode/0.3.1;
+  };
   fastcache = {
     "1.1.0" = import ./overrides/fastcache/1.1.0;
   };
