f14fa901131b1426adc0bab7a060c3a7a4f2a87a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:18:59 +0000

    autogenerated overwrites for polyline==2.0.2

--- collected.nix
+++ collected.nix
@@ -6054,6 +6054,9 @@
   polygon3 = {
     "3.0.9.1" = import ./overrides/polygon3/3.0.9.1;
   };
+  polyline = {
+    "2.0.2" = import ./overrides/polyline/2.0.2;
+  };
   portpicker = {
     "1.3.1" = import ./overrides/portpicker/1.3.1;
   };
