f587846a3563b33a95f5938650111497565fb243
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:21:03 +0000

    autogenerated overwrites for singledispatch==4.1.0

--- collected.nix
+++ collected.nix
@@ -8450,6 +8450,9 @@
   singer-python = {
     "5.1.5" = import ./overrides/singer-python/5.1.5;
   };
+  singledispatch = {
+    "4.1.0" = import ./overrides/singledispatch/4.1.0;
+  };
   singleton-decorator = {
     "1.0.0" = import ./overrides/singleton-decorator/1.0.0;
   };
