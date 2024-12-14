8d0fd46ee480e2d4d38bdd811f21c50130c4034d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:44:42 +0000

    autogenerated overwrites for lscsoft-glue==4.0.0

--- collected.nix
+++ collected.nix
@@ -4541,6 +4541,9 @@
   lru-dict = {
     "1.2.0" = import ./overrides/lru-dict/1.2.0;
   };
+  lscsoft-glue = {
+    "4.0.0" = import ./overrides/lscsoft-glue/4.0.0;
+  };
   lucene-querybuilder = {
     "0.2" = import ./overrides/lucene-querybuilder/0.2;
   };
