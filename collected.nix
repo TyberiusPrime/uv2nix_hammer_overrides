08e83d73b0bcfd3d8d895ab74483c6c3058cf4d1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:19:31 +0000

    autogenerated overwrites for sense-core==0.2.93

--- collected.nix
+++ collected.nix
@@ -8275,6 +8275,9 @@
   semgrep = {
     "1.68.0" = import ./overrides/semgrep/1.68.0;
   };
+  sense-core = {
+    "0.2.93" = import ./overrides/sense-core/0.2.93;
+  };
   sentinels = {
     "1.0.0" = import ./overrides/sentinels/1.0.0;
   };
