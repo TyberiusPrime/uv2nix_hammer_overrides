3544ec6c32a09b9fdcfeec63a253464679a3fa0a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:36:28 +0000

    autogenerated overwrites for klein==24.8.0

--- collected.nix
+++ collected.nix
@@ -4199,6 +4199,9 @@
   kiwi = {
     "10.1.8" = import ./overrides/kiwi/10.1.8;
   };
+  klein = {
+    "24.8.0" = import ./overrides/klein/24.8.0;
+  };
   knnimpute = {
     "0.1.0" = import ./overrides/knnimpute/0.1.0;
   };
