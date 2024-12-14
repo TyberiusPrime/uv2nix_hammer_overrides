0aa6a7e9ec63a6bf9e70e09a77b869940d703c68
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:36:24 +0000

    autogenerated overwrites for jupyterhub-tmpauthenticator==1.0.0

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyterhub-tmpauthenticator = {
+    "1.0.0" = import ./overrides/jupyterhub-tmpauthenticator/1.0.0;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
