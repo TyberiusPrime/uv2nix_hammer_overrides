b51c8b3af56fa0a0b21fe3a09e33a24ce7f24d63
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:54:05 +0000

    autogenerated overwrites for marshmallow3-annotations==1.1.0

--- collected.nix
+++ collected.nix
@@ -4640,6 +4640,9 @@
   marshmallow-polyfield = {
     "5.11" = import ./overrides/marshmallow-polyfield/5.11;
   };
+  marshmallow3-annotations = {
+    "1.1.0" = import ./overrides/marshmallow3-annotations/1.1.0;
+  };
   martor = {
     "1.6.44" = import ./overrides/martor/1.6.44;
   };
