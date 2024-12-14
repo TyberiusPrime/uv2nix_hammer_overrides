79c6cfb0ad60be36e5e44c23b9932d1c56a3a87b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:34:25 +0000

    autogenerated overwrites for scikit-spatial==7.2.0

--- collected.nix
+++ collected.nix
@@ -8211,6 +8211,9 @@
     "0.24.2" = import ./overrides/scikit-learn/0.24.2;
     "1.2.2" = import ./overrides/scikit-learn/1.2.2;
   };
+  scikit-spatial = {
+    "7.2.0" = import ./overrides/scikit-spatial/7.2.0;
+  };
   scikit-surgerycore = {
     "0.7.2" = import ./overrides/scikit-surgerycore/0.7.2;
   };
