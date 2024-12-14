7d01837ddd66d5051f9600a36c1c89b5f1c5c15b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:51:56 +0000

    autogenerated overwrites for dataclass-wizard==0.22.3

--- collected.nix
+++ collected.nix
@@ -1680,6 +1680,9 @@
   data = {
     "0.4" = import ./overrides/data/0.4;
   };
+  dataclass-wizard = {
+    "0.22.3" = import ./overrides/dataclass-wizard/0.22.3;
+  };
   datacleaner = {
     "0.1.5" = import ./overrides/datacleaner/0.1.5;
   };
