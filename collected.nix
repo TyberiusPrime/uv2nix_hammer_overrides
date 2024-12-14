d328d1203eceb86bb56590bd7f545f4a4a56b501
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:53:55 +0000

    autogenerated overwrites for pytest==8.3.3

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest = {
+    "8.3.3" = import ./overrides/pytest/8.3.3;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
