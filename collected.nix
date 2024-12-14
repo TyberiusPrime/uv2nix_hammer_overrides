78a42c4643c395a3f0c5447cbec479b3f7172e4f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:18:25 +0000

    autogenerated overwrites for hunter==3.7.0

--- collected.nix
+++ collected.nix
@@ -3583,6 +3583,9 @@
   hunspell = {
     "0.5.5" = import ./overrides/hunspell/0.5.5;
   };
+  hunter = {
+    "3.7.0" = import ./overrides/hunter/3.7.0;
+  };
   husl = {
     "4.0.3" = import ./overrides/husl/4.0.3;
   };
