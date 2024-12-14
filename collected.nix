2c856a48bee83841a0d37c714ca05410463de163
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:18:29 +0000

    autogenerated overwrites for resotolib==3.9.0

--- collected.nix
+++ collected.nix
@@ -7913,6 +7913,9 @@
   resolver = {
     "0.2.1" = import ./overrides/resolver/0.2.1;
   };
+  resotolib = {
+    "3.9.0" = import ./overrides/resotolib/3.9.0;
+  };
   restible = {
     "0.12.1" = import ./overrides/restible/0.12.1;
   };
