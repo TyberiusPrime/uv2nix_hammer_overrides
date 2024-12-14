fc1bf04f0fbc4e45fe0b7b892d2e213eb83766e9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:53:33 +0000

    autogenerated overwrites for adafruit-circuitpython-busdevice==5.2.9

--- collected.nix
+++ collected.nix
@@ -35,6 +35,9 @@
   active-alchemy = {
     "1.1.0" = import ./overrides/active-alchemy/1.1.0;
   };
+  adafruit-circuitpython-busdevice = {
+    "5.2.9" = import ./overrides/adafruit-circuitpython-busdevice/5.2.9;
+  };
   adafruit-extended-bus = {
     "1.0.2" = import ./overrides/adafruit-extended-bus/1.0.2;
   };
