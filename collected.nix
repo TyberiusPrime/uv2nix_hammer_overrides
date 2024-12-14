9e49e9df3056daae4dbe7c69a9a5e7787867d384
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:46:13 +0000

    autogenerated overwrites for snowflake-sqlalchemy==1.6.1

--- collected.nix
+++ collected.nix
@@ -8544,6 +8544,9 @@
   snaptime = {
     "0.2.4" = import ./overrides/snaptime/0.2.4;
   };
+  snowflake-sqlalchemy = {
+    "1.6.1" = import ./overrides/snowflake-sqlalchemy/1.6.1;
+  };
   snownlp = {
     "0.12.3" = import ./overrides/snownlp/0.12.3;
   };
