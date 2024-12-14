f669cdbe1659766e0c2c8c03b85fd852dd82e062
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 13:07:12 +0000

    autogenerated overwrites for moesifapi==1.5.0

--- collected.nix
+++ collected.nix
@@ -4898,6 +4898,9 @@
   modulemd = {
     "1.3.3" = import ./overrides/modulemd/1.3.3;
   };
+  moesifapi = {
+    "1.5.0" = import ./overrides/moesifapi/1.5.0;
+  };
   moksha-common = {
     "1.2.5" = import ./overrides/moksha-common/1.2.5;
   };
