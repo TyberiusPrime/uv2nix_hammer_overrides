482396edd8c35804b8bc02e05752914d72f296f1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:42:33 +0000

    autogenerated overwrites for click-datetime==0.4.0

--- collected.nix
+++ collected.nix
@@ -1113,6 +1113,9 @@
   click-completion = {
     "0.5.2" = import ./overrides/click-completion/0.5.2;
   };
+  click-datetime = {
+    "0.4.0" = import ./overrides/click-datetime/0.4.0;
+  };
   click-didyoumean = {
     "0.0.3" = import ./overrides/click-didyoumean/0.0.3;
   };
