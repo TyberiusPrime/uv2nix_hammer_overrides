6b9e5ae19c7d08e49546e54643dd5cfaba918c1e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:24:46 +0000

    autogenerated overwrites for tasklogger==1.2.0

--- collected.nix
+++ collected.nix
@@ -8900,6 +8900,9 @@
   tasklib = {
     "2.5.1" = import ./overrides/tasklib/2.5.1;
   };
+  tasklogger = {
+    "1.2.0" = import ./overrides/tasklogger/1.2.0;
+  };
   tba-api-v3client = {
     "3.8.0" = import ./overrides/tba-api-v3client/3.8.0;
   };
