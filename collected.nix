f64fbfb8c16046edce41c5ee9fb813fba2864af9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:22:16 +0000

    autogenerated overwrites for ochre==0.4.0

--- collected.nix
+++ collected.nix
@@ -5426,6 +5426,9 @@
   object-clerk = {
     "0.1.1" = import ./overrides/object-clerk/0.1.1;
   };
+  ochre = {
+    "0.4.0" = import ./overrides/ochre/0.4.0;
+  };
   octobot-commons = {
     "1.9.58" = import ./overrides/octobot-commons/1.9.58;
   };
