98a31c7a28cf7b56eb49232e147716fe102ea63b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:10:07 +0000

    autogenerated overwrites for cherrypy-cors==1.7.0

--- collected.nix
+++ collected.nix
@@ -1029,6 +1029,9 @@
   chempy = {
     "0.9.0" = import ./overrides/chempy/0.9.0;
   };
+  cherrypy-cors = {
+    "1.7.0" = import ./overrides/cherrypy-cors/1.7.0;
+  };
   chiki-base = {
     "0.0.3" = import ./overrides/chiki-base/0.0.3;
   };
