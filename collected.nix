231b35d248854516ce732cc0c313bd7703e5894a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:19:26 +0000

    autogenerated overwrites for pytest-randomly==3.15.0

--- collected.nix
+++ collected.nix
@@ -7239,6 +7239,9 @@
   pytest-raisesregexp = {
     "2.1" = import ./overrides/pytest-raisesregexp/2.1;
   };
+  pytest-randomly = {
+    "3.15.0" = import ./overrides/pytest-randomly/3.15.0;
+  };
   pytest-remfiles = {
     "0.0.2" = import ./overrides/pytest-remfiles/0.0.2;
   };
