d9714f0ffc3a716d38e49fa95c6fa83f0206c239
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:08:30 +0000

    autogenerated overwrites for allianceauth==4.3.0

--- collected.nix
+++ collected.nix
@@ -187,6 +187,9 @@
   aliyun-python-sdk-core-v3 = {
     "2.13.33" = import ./overrides/aliyun-python-sdk-core-v3/2.13.33;
   };
+  allianceauth = {
+    "4.3.0" = import ./overrides/allianceauth/4.3.0;
+  };
   allset = {
     "1.0.1" = import ./overrides/allset/1.0.1;
   };
