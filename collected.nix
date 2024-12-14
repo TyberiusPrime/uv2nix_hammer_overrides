922adb239a041ced432a36b1531f0915aec65cbe
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:51:16 +0000

    autogenerated overwrites for pytest-annotate==1.0.5

--- collected.nix
+++ collected.nix
@@ -7209,6 +7209,9 @@
   pytelegraf = {
     "0.3.3" = import ./overrides/pytelegraf/0.3.3;
   };
+  pytest-annotate = {
+    "1.0.5" = import ./overrides/pytest-annotate/1.0.5;
+  };
   pytest-black = {
     "0.3.12" = import ./overrides/pytest-black/0.3.12;
   };
