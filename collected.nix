94111ec780662e48a542d80d153285ee88d7acf8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 23:43:06 +0000

    autogenerated overwrites for python-ipmi==0.5.5

--- collected.nix
+++ collected.nix
@@ -7366,6 +7366,9 @@
   python-ioc = {
     "1.13.0" = import ./overrides/python-ioc/1.13.0;
   };
+  python-ipmi = {
+    "0.5.5" = import ./overrides/python-ipmi/0.5.5;
+  };
   python-iptables = {
     "1.0.1" = import ./overrides/python-iptables/1.0.1;
   };
