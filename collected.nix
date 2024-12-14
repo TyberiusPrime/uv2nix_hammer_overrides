f68b2baf6f1b14c4483e081d5511ed8b0ffe5a3d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:11:47 +0000

    autogenerated overwrites for certipy==0.1.3

--- collected.nix
+++ collected.nix
@@ -978,6 +978,9 @@
   cenpy = {
     "1.0.1" = import ./overrides/cenpy/1.0.1;
   };
+  certipy = {
+    "0.1.3" = import ./overrides/certipy/0.1.3;
+  };
   cf-units = {
     "3.2.0" = import ./overrides/cf-units/3.2.0;
   };
