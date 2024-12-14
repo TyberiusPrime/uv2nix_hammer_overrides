f9ab1a6cff24e6676b6fbf4beba44fb31571da1c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:58:38 +0000

    autogenerated overwrites for sshfs==2024.6.0

--- collected.nix
+++ collected.nix
@@ -8708,6 +8708,9 @@
   ssh = {
     "1.8.0" = import ./overrides/ssh/1.8.0;
   };
+  sshfs = {
+    "2024.6.0" = import ./overrides/sshfs/2024.6.0;
+  };
   sslyze = {
     "6.0.0" = import ./overrides/sslyze/6.0.0;
   };
