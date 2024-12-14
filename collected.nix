6dcce0bba8898aa306cc4cc725d78271f0d2b9cc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 02:36:39 +0000

    autogenerated overwrites for tsfresh==0.20.3

--- collected.nix
+++ collected.nix
@@ -9338,6 +9338,9 @@
   tsam = {
     "2.3.3" = import ./overrides/tsam/2.3.3;
   };
+  tsfresh = {
+    "0.20.3" = import ./overrides/tsfresh/0.20.3;
+  };
   ttkwidgets = {
     "0.13.0" = import ./overrides/ttkwidgets/0.13.0;
   };
