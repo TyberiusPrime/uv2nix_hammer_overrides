4020a815b7418df0c2aa44657bb90504f13a5f31
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:29:02 +0000

    autogenerated overwrites for exif==1.6.0

--- collected.nix
+++ collected.nix
@@ -2625,6 +2625,9 @@
   execo = {
     "2.8" = import ./overrides/execo/2.8;
   };
+  exif = {
+    "1.6.0" = import ./overrides/exif/1.6.0;
+  };
   expects = {
     "0.9.0" = import ./overrides/expects/0.9.0;
   };
