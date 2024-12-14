72f1ad069f8270489e1bc24622efec16d6c21bcc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 13:36:22 +0000

    autogenerated overwrites for returns==0.23.0

--- collected.nix
+++ collected.nix
@@ -7928,6 +7928,9 @@
   retry-decorator = {
     "1.1.1" = import ./overrides/retry-decorator/1.1.1;
   };
+  returns = {
+    "0.23.0" = import ./overrides/returns/0.23.0;
+  };
   returns-decorator = {
     "1.1" = import ./overrides/returns-decorator/1.1;
   };
