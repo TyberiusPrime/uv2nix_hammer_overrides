9dfe3d85dfd2537376f26a657943b801e939dd51
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:17:55 +0000

    autogenerated overwrites for flake8-eradicate==1.5.0

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-eradicate = {
+    "1.5.0" = import ./overrides/flake8-eradicate/1.5.0;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
