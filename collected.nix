21e3695704a56a3a7878941783c06ad3c04cddb9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:41:27 +0000

    autogenerated overwrites for graphtage==0.3.1

--- collected.nix
+++ collected.nix
@@ -3314,6 +3314,9 @@
     "1.1.3" = import ./overrides/graphql-server-core/1.1.3;
     "2.0.0" = import ./overrides/graphql-server-core/2.0.0;
   };
+  graphtage = {
+    "0.3.1" = import ./overrides/graphtage/0.3.1;
+  };
   greenlet = {
     "3.1.0" = import ./overrides/greenlet/3.1.0;
   };
