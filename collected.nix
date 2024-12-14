592045f31bf822cc4fbc466d403b4521ff5e980e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:56:12 +0000

    autogenerated overwrites for pylsqpack==0.3.18

--- collected.nix
+++ collected.nix
@@ -6837,6 +6837,9 @@
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
+  pylsqpack = {
+    "0.3.18" = import ./overrides/pylsqpack/0.3.18;
+  };
   pylzma = {
     "0.5.0" = import ./overrides/pylzma/0.5.0;
   };
