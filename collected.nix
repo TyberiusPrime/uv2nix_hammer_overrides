53bc58777478f59eb8c3b773d9620e5e1a67754c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 09:34:20 +0000

    autogenerated overwrites for georss-generic-client==0.8

--- collected.nix
+++ collected.nix
@@ -3137,6 +3137,9 @@
   geometric = {
     "1.0.2" = import ./overrides/geometric/1.0.2;
   };
+  georss-generic-client = {
+    "0.8" = import ./overrides/georss-generic-client/0.8;
+  };
   getch = {
     "1.0" = import ./overrides/getch/1.0;
   };
