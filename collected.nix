7d5a12a7f3f8731e3caad9ccd595b2e317823c4f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:41:38 +0000

    autogenerated overwrites for flake8-commas==4.0.0

--- collected.nix
+++ collected.nix
@@ -2793,6 +2793,9 @@
   flake8-blind-except = {
     "0.2.1" = import ./overrides/flake8-blind-except/0.2.1;
   };
+  flake8-commas = {
+    "4.0.0" = import ./overrides/flake8-commas/4.0.0;
+  };
   flake8-mutable = {
     "1.2.0" = import ./overrides/flake8-mutable/1.2.0;
   };
