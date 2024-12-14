4e945d8990cc1a7662a2f8caa6a281b0e41d0474
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:44:20 +0000

    autogenerated overwrites for pyln-client==24.8.1

--- collected.nix
+++ collected.nix
@@ -6834,6 +6834,9 @@
   pylint-flask = {
     "0.6" = import ./overrides/pylint-flask/0.6;
   };
+  pyln-client = {
+    "24.8.1" = import ./overrides/pyln-client/24.8.1;
+  };
   pyls-isort = {
     "0.2.2" = import ./overrides/pyls-isort/0.2.2;
   };
