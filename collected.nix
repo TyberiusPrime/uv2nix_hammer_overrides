fe8079faf8c91ced878393a962d08711de267a15
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:08:29 +0000

    autogenerated overwrites for wagtail-localize==1.10

--- collected.nix
+++ collected.nix
@@ -9793,6 +9793,9 @@
   wagon = {
     "1.0.1" = import ./overrides/wagon/1.0.1;
   };
+  wagtail-localize = {
+    "1.10" = import ./overrides/wagtail-localize/1.10;
+  };
   wai-common = {
     "0.0.44" = import ./overrides/wai-common/0.0.44;
   };
