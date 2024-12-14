de6077696e1f8f0dc897ab23da19e34269a7b4d5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 01:55:22 +0000

    autogenerated overwrites for datar==0.15.6

--- collected.nix
+++ collected.nix
@@ -1689,6 +1689,9 @@
   datapipelines = {
     "1.0.7" = import ./overrides/datapipelines/1.0.7;
   };
+  datar = {
+    "0.15.6" = import ./overrides/datar/0.15.6;
+  };
   datashape = {
     "0.5.2" = import ./overrides/datashape/0.5.2;
   };
