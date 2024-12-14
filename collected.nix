467a49e53afd7b1b726c7f4cfe0d1a34db0f0f04
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 19:30:05 +0000

    autogenerated overwrites for python-snap7==2.0.0

--- collected.nix
+++ collected.nix
@@ -7469,6 +7469,9 @@
   python-smpp = {
     "0.1.9" = import ./overrides/python-smpp/0.1.9;
   };
+  python-snap7 = {
+    "2.0.0" = import ./overrides/python-snap7/2.0.0;
+  };
   python-speech-features = {
     "0.6" = import ./overrides/python-speech-features/0.6;
   };
