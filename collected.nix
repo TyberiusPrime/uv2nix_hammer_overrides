85594af2c499e32976aadf8d8be30200407447c9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:05:33 +0000

    autogenerated overwrites for hass-nabucasa==0.81.1

--- collected.nix
+++ collected.nix
@@ -3408,6 +3408,9 @@
   hash-ring = {
     "1.3.1" = import ./overrides/hash-ring/1.3.1;
   };
+  hass-nabucasa = {
+    "0.81.1" = import ./overrides/hass-nabucasa/0.81.1;
+  };
   hbmqtt = {
     "0.9.6" = import ./overrides/hbmqtt/0.9.6;
   };
