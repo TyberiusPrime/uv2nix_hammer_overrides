ad0fb5a3041f95f9e1504d34bdc4fdc79196cd5b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 06:46:58 +0000

    autogenerated overwrites for clihelper==1.7.6

--- collected.nix
+++ collected.nix
@@ -1122,6 +1122,9 @@
   clickhouse-sqlalchemy = {
     "0.3.2" = import ./overrides/clickhouse-sqlalchemy/0.3.2;
   };
+  clihelper = {
+    "1.7.6" = import ./overrides/clihelper/1.7.6;
+  };
   clime = {
     "0.3.1" = import ./overrides/clime/0.3.1;
   };
