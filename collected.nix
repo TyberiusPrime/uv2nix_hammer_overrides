9a38e5268125d9eecbb4c68ecbf128270b1c788f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 02:10:23 +0000

    autogenerated overwrites for sympy==1.13.2

--- collected.nix
+++ collected.nix
@@ -8864,6 +8864,9 @@
   sym = {
     "0.3.5" = import ./overrides/sym/0.3.5;
   };
+  sympy = {
+    "1.13.2" = import ./overrides/sympy/1.13.2;
+  };
   systemcmd = {
     "3.0.1" = import ./overrides/systemcmd/3.0.1;
   };
