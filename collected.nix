0bc0d682942293f23542a37b7256fd803d95d1b0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:03:24 +0000

    autogenerated overwrites for simple-term-menu==1.6.4

--- collected.nix
+++ collected.nix
@@ -8411,6 +8411,9 @@
   simple-rlp = {
     "0.1.3" = import ./overrides/simple-rlp/0.1.3;
   };
+  simple-term-menu = {
+    "1.6.4" = import ./overrides/simple-term-menu/1.6.4;
+  };
   simple-translation = {
     "0.8.6" = import ./overrides/simple-translation/0.8.6;
   };
