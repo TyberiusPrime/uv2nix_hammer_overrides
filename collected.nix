4c7486bb270c0497ca63ecfb95f16df17711a23e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 19:28:13 +0000

    autogenerated overwrites for pytest-relaxed==2.0.2

--- collected.nix
+++ collected.nix
@@ -7239,6 +7239,9 @@
   pytest-raisesregexp = {
     "2.1" = import ./overrides/pytest-raisesregexp/2.1;
   };
+  pytest-relaxed = {
+    "2.0.2" = import ./overrides/pytest-relaxed/2.0.2;
+  };
   pytest-remfiles = {
     "0.0.2" = import ./overrides/pytest-remfiles/0.0.2;
   };
