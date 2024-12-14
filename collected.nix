8280f143f780964c538b387fcdcd423a67f9ce87
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:19:26 +0000

    autogenerated overwrites for mne==1.8.0

--- collected.nix
+++ collected.nix
@@ -4850,6 +4850,9 @@
   mmhash3 = {
     "3.0.1" = import ./overrides/mmhash3/3.0.1;
   };
+  mne = {
+    "1.8.0" = import ./overrides/mne/1.8.0;
+  };
   mo-dots = {
     "4.22.21108" = import ./overrides/mo-dots/4.22.21108;
   };
