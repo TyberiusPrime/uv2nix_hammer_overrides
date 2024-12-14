cee748d9feec25218cbcfa9104a1d6dddb2f8b41
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:40:16 +0000

    autogenerated overwrites for py7zr==0.22.0

--- collected.nix
+++ collected.nix
@@ -6351,6 +6351,9 @@
   py3rijndael = {
     "0.3.3" = import ./overrides/py3rijndael/0.3.3;
   };
+  py7zr = {
+    "0.22.0" = import ./overrides/py7zr/0.22.0;
+  };
   pyacoustid = {
     "1.3.0" = import ./overrides/pyacoustid/1.3.0;
   };
