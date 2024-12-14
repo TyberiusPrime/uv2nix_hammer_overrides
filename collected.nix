52ab8437ec33c11181b5872ce67760cc06dcce9d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:33:24 +0000

    autogenerated overwrites for jupyter-cache==1.0.0

--- collected.nix
+++ collected.nix
@@ -4066,6 +4066,9 @@
   julius = {
     "0.2.7" = import ./overrides/julius/0.2.7;
   };
+  jupyter-cache = {
+    "1.0.0" = import ./overrides/jupyter-cache/1.0.0;
+  };
   jupyter-contrib-core = {
     "0.4.2" = import ./overrides/jupyter-contrib-core/0.4.2;
   };
