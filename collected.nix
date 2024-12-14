105d6a16c7a867c107b26c178d03c65a4c9eaf36
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:56:01 +0000

    autogenerated overwrites for deserialize==2.0.1

--- collected.nix
+++ collected.nix
@@ -1776,6 +1776,9 @@
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
+  deserialize = {
+    "2.0.1" = import ./overrides/deserialize/2.0.1;
+  };
   dev0s = {
     "3.36.6" = import ./overrides/dev0s/3.36.6;
   };
