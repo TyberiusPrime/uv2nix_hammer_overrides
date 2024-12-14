1e2f5a735d69cc7eb84fe9623258fc5b2d0266c9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 06:12:17 +0000

    autogenerated overwrites for essentials-openapi==1.0.9

--- collected.nix
+++ collected.nix
@@ -2568,6 +2568,9 @@
   esptool = {
     "4.7.0" = import ./overrides/esptool/4.7.0;
   };
+  essentials-openapi = {
+    "1.0.9" = import ./overrides/essentials-openapi/1.0.9;
+  };
   etcd3 = {
     "0.12.0" = import ./overrides/etcd3/0.12.0;
   };
