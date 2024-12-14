a4558cb168915cb26d539ba9a1ca8901bbf5e2ce
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:46:14 +0000

    autogenerated overwrites for pycap==2.6.0

--- collected.nix
+++ collected.nix
@@ -6444,6 +6444,9 @@
   pycairo = {
     "1.27.0" = import ./overrides/pycairo/1.27.0;
   };
+  pycap = {
+    "2.6.0" = import ./overrides/pycap/2.6.0;
+  };
   pycatch22 = {
     "0.4.5" = import ./overrides/pycatch22/0.4.5;
   };
