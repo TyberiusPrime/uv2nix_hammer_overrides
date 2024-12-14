e488828a5bd993794aafb511581f66ce6e48bb84
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:21:13 +0000

    autogenerated overwrites for aiohttp-wsgi==0.10.0

--- collected.nix
+++ collected.nix
@@ -117,6 +117,9 @@
   aiohttp-swagger3 = {
     "0.8.0" = import ./overrides/aiohttp-swagger3/0.8.0;
   };
+  aiohttp-wsgi = {
+    "0.10.0" = import ./overrides/aiohttp-wsgi/0.10.0;
+  };
   aiolivisi = {
     "0.0.19" = import ./overrides/aiolivisi/0.0.19;
   };
