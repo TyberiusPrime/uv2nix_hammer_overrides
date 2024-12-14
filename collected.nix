9141773c68ccaf905308fbf760e1053acf6a3693
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:04:03 +0000

    autogenerated overwrites for nevergrad==1.0.4

--- collected.nix
+++ collected.nix
@@ -5205,6 +5205,9 @@
   neuronpy = {
     "0.1.6" = import ./overrides/neuronpy/0.1.6;
   };
+  nevergrad = {
+    "1.0.4" = import ./overrides/nevergrad/1.0.4;
+  };
   nextcord = {
     "2.6.0" = import ./overrides/nextcord/2.6.0;
   };
