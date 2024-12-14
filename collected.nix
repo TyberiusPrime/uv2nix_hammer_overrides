8ed108b99f51b8f4d9609824e3eacf5be74cb340
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 04:13:17 +0000

    autogenerated overwrites for jupyterlab-server==2.27.3

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyterlab-server = {
+    "2.27.3" = import ./overrides/jupyterlab-server/2.27.3;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
