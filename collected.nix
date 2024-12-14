443ee3ba2075c1f554dedaffd00295f9d4a978a8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:15:58 +0000

    autogenerated overwrites for intreehooks==1.0

--- collected.nix
+++ collected.nix
@@ -3805,6 +3805,9 @@
   intervaltree = {
     "3.1.0" = import ./overrides/intervaltree/3.1.0;
   };
+  intreehooks = {
+    "1.0" = import ./overrides/intreehooks/1.0;
+  };
   invenio-github = {
     "1.5.2" = import ./overrides/invenio-github/1.5.2;
   };
