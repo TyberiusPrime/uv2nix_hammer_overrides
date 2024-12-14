c63e024174f87f9ad2940cf1f7067d6044a0fbdb
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:19:29 +0000

    autogenerated overwrites for classutils==1.18.2

--- collected.nix
+++ collected.nix
@@ -1101,6 +1101,9 @@
   classproperties = {
     "0.2.0" = import ./overrides/classproperties/0.2.0;
   };
+  classutils = {
+    "1.18.2" = import ./overrides/classutils/1.18.2;
+  };
   cleandev-config-loader = {
     "0.3.5" = import ./overrides/cleandev-config-loader/0.3.5;
   };
