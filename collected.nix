4b60d7b0bd0f1495dfd8f4ebe43f36dc51081b62
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:18:50 +0000

    autogenerated overwrites for openevsewifi==1.1.2

--- collected.nix
+++ collected.nix
@@ -5537,6 +5537,9 @@
   openerp-web = {
     "7.0.406" = import ./overrides/openerp-web/7.0.406;
   };
+  openevsewifi = {
+    "1.1.2" = import ./overrides/openevsewifi/1.1.2;
+  };
   openhomedevice = {
     "2.3.1" = import ./overrides/openhomedevice/2.3.1;
   };
