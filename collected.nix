4111b94639bef2fbdf3eddf370cf44a1f847b938
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:04:12 +0000

    autogenerated overwrites for pulp-glue==0.28.2

--- collected.nix
+++ collected.nix
@@ -6264,6 +6264,9 @@
   pubnub = {
     "8.1.0" = import ./overrides/pubnub/8.1.0;
   };
+  pulp-glue = {
+    "0.28.2" = import ./overrides/pulp-glue/0.28.2;
+  };
   pupynere = {
     "1.0.15" = import ./overrides/pupynere/1.0.15;
   };
