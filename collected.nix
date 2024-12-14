2f09ae2f0d0774f295e1da6b8370199676c134a4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:24:09 +0000

    autogenerated overwrites for tksheet==7.2.15

--- collected.nix
+++ collected.nix
@@ -9139,6 +9139,9 @@
   tkitfile = {
     "0.0.1.2" = import ./overrides/tkitfile/0.0.1.2;
   };
+  tksheet = {
+    "7.2.15" = import ./overrides/tksheet/7.2.15;
+  };
   tls-client = {
     "1.0.1" = import ./overrides/tls-client/1.0.1;
   };
