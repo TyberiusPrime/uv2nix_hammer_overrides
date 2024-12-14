e5c816284a2616bd9703b0cc7b1107177f87b87e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 02:48:56 +0000

    autogenerated overwrites for pytest-selenium==4.1.0

--- collected.nix
+++ collected.nix
@@ -7242,6 +7242,9 @@
   pytest-remfiles = {
     "0.0.2" = import ./overrides/pytest-remfiles/0.0.2;
   };
+  pytest-selenium = {
+    "4.1.0" = import ./overrides/pytest-selenium/4.1.0;
+  };
   pytest-services = {
     "2.2.1" = import ./overrides/pytest-services/2.2.1;
   };
