6d6355f624d8f36a00aa9b311071ea9076b5ac37
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:26:22 +0000

    autogenerated overwrites for dictdiffer==0.9.0

--- collected.nix
+++ collected.nix
@@ -1791,6 +1791,9 @@
   dhooks = {
     "1.1.4" = import ./overrides/dhooks/1.1.4;
   };
+  dictdiffer = {
+    "0.9.0" = import ./overrides/dictdiffer/0.9.0;
+  };
   dictionaryutils = {
     "3.4.10" = import ./overrides/dictionaryutils/3.4.10;
   };
