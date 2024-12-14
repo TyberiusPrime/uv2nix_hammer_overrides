f21b7d4445fa721e23756de3ae55173bf6440fea
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:32:08 +0000

    autogenerated overwrites for glad2==2.0.7

--- collected.nix
+++ collected.nix
@@ -3198,6 +3198,9 @@
   giturlparse-py = {
     "0.0.5" = import ./overrides/giturlparse-py/0.0.5;
   };
+  glad2 = {
+    "2.0.7" = import ./overrides/glad2/2.0.7;
+  };
   glob2 = {
     "0.7" = import ./overrides/glob2/0.7;
   };
