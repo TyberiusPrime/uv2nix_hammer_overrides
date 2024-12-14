d240115fbb2c81454a1ffc6f85304541b2a23f8a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 09:13:40 +0000

    autogenerated overwrites for kneed==0.8.5

--- collected.nix
+++ collected.nix
@@ -4199,6 +4199,9 @@
   kiwi = {
     "10.1.8" = import ./overrides/kiwi/10.1.8;
   };
+  kneed = {
+    "0.8.5" = import ./overrides/kneed/0.8.5;
+  };
   knnimpute = {
     "0.1.0" = import ./overrides/knnimpute/0.1.0;
   };
