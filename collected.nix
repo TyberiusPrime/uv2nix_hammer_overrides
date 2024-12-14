fb885464fa774b3efae9e74ddee0d81a2383f6e1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:36:47 +0000

    autogenerated overwrites for nameparser==1.1.3

--- collected.nix
+++ collected.nix
@@ -5099,6 +5099,9 @@
   nad-receiver = {
     "0.3.0" = import ./overrides/nad-receiver/0.3.0;
   };
+  nameparser = {
+    "1.1.3" = import ./overrides/nameparser/1.1.3;
+  };
   names = {
     "0.3.0" = import ./overrides/names/0.3.0;
   };
