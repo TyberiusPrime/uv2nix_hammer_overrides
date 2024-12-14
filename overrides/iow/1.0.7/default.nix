98db5fb7ae6c344c25fe55fa6dbbf064bde9afee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:38:15 +0000

    autogenerated overwrites for iow==1.0.7

--- overrides/iow/1.0.7/default.nix
+++ overrides/iow/1.0.7/default.nix
@@ -1,14 +1,3 @@
-{ helpers, resolveBuildSystem, ... }:
-old:
-if (helpers.isWheel old) then
-  { }
-else
-  {
-    nativeBuildInputs =
-      old.nativeBuildInputs or [ ]
-      ++ (resolveBuildSystem {
-        cython = [ ];
-        numpy = [ ];
-        setuptools = [ ];
-      });
-  }
+{final, helpers, resolveBuildSystem, ...}
+        : old: if (helpers.isWheel old) then {} else {nativeBuildInputs = old.nativeBuildInputs or [] ++ ( resolveBuildSystem {cython = [];numpy = [];setuptools = [];});}
+        
