53a9c9cadcfaab9d878545c896938014b2f8ad67
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 19:56:25 +0000

    autogenerated overwrites for sphinx-gallery==0.17.1

--- collected.nix
+++ collected.nix
@@ -8613,6 +8613,9 @@
   sphinx-fortran = {
     "1.1.1" = import ./overrides/sphinx-fortran/1.1.1;
   };
+  sphinx-gallery = {
+    "0.17.1" = import ./overrides/sphinx-gallery/0.17.1;
+  };
   sphinx-serve = {
     "1.0.1" = import ./overrides/sphinx-serve/1.0.1;
   };
