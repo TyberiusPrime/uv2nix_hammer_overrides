457795f501e25a86dcea74480b37b90b00a024cf
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:28:27 +0000

    autogenerated overwrites for google-cloud-iot==2.9.2

--- collected.nix
+++ collected.nix
@@ -3243,6 +3243,9 @@
   google-apputils = {
     "0.4.2" = import ./overrides/google-apputils/0.4.2;
   };
+  google-cloud-iot = {
+    "2.9.2" = import ./overrides/google-cloud-iot/2.9.2;
+  };
   google-gax = {
     "0.12.5" = import ./overrides/google-gax/0.12.5;
   };
