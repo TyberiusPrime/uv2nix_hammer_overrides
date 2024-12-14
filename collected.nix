ea2cb30468bbd7ae4e011627eaf922bd90415be4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:24:09 +0000

    autogenerated overwrites for laundrify-aio==1.2.2

--- collected.nix
+++ collected.nix
@@ -4257,6 +4257,9 @@
   latlon = {
     "1.0.2" = import ./overrides/latlon/1.0.2;
   };
+  laundrify-aio = {
+    "1.2.2" = import ./overrides/laundrify-aio/1.2.2;
+  };
   lava-tool = {
     "0.11.1" = import ./overrides/lava-tool/0.11.1;
   };
