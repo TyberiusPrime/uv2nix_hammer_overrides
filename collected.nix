116413983e94426ba3474fe267acbcb3f7e1f521
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 04:12:28 +0000

    autogenerated overwrites for indexed-bzip2==1.6.0

--- collected.nix
+++ collected.nix
@@ -3727,6 +3727,9 @@
   imutils = {
     "0.5.4" = import ./overrides/imutils/0.5.4;
   };
+  indexed-bzip2 = {
+    "1.6.0" = import ./overrides/indexed-bzip2/1.6.0;
+  };
   infi-execute = {
     "0.1.8" = import ./overrides/infi-execute/0.1.8;
   };
