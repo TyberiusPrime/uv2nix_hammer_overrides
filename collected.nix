d0628c9e3d15a0ed3fc246475b73e40913fb239a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 06:19:58 +0000

    autogenerated overwrites for django-browserid==2.0.2

--- collected.nix
+++ collected.nix
@@ -1904,6 +1904,9 @@
   django-bower = {
     "5.2.0" = import ./overrides/django-bower/5.2.0;
   };
+  django-browserid = {
+    "2.0.2" = import ./overrides/django-browserid/2.0.2;
+  };
   django-cacheops = {
     "7.0.2" = import ./overrides/django-cacheops/7.0.2;
   };
