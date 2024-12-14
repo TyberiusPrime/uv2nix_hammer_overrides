e6d36ccaa8b595c65acaf568ec4b1c7ae62bdd41
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:23:08 +0000

    autogenerated overwrites for aioredis==2.0.1

--- collected.nix
+++ collected.nix
@@ -132,6 +132,9 @@
   aiopubsub = {
     "2.1.5" = import ./overrides/aiopubsub/2.1.5;
   };
+  aioredis = {
+    "2.0.1" = import ./overrides/aioredis/2.0.1;
+  };
   aiosasl = {
     "0.5.0" = import ./overrides/aiosasl/0.5.0;
   };
