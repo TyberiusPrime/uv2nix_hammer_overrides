bc512527e9cf393b6d6be135384d81f72ae192f7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 05:52:59 +0000

    autogenerated overwrites for coolname==2.2.0

--- collected.nix
+++ collected.nix
@@ -1403,6 +1403,9 @@
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
+  coolname = {
+    "2.2.0" = import ./overrides/coolname/2.2.0;
+  };
   coqui-trainer = {
     "0.0.5" = import ./overrides/coqui-trainer/0.0.5;
   };
