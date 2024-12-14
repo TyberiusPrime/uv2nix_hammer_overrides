5937b62ca9a18f0ebd81c14d5d5676339ddbcda6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 10:46:37 +0000

    autogenerated overwrites for aria2p==0.12.0

--- collected.nix
+++ collected.nix
@@ -364,6 +364,9 @@
   arguments = {
     "76" = import ./overrides/arguments/76;
   };
+  aria2p = {
+    "0.12.0" = import ./overrides/aria2p/0.12.0;
+  };
   armstrong-utils-backends = {
     "1.1.1" = import ./overrides/armstrong-utils-backends/1.1.1;
   };
