91693cf921d63710f4c0c61eb45e194a200c572a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:44:59 +0000

    autogenerated overwrites (commit to be squashed)

--- collected.nix
+++ collected.nix
@@ -4290,6 +4290,9 @@
   lcogt-logging = {
     "0.3.2" = import ./overrides/lcogt-logging/0.3.2;
   };
+  ldapdomaindump = {
+    "0.9.4" = import ./overrides/ldapdomaindump/0.9.4;
+  };
   lepl = {
     "5.1.3" = import ./overrides/lepl/5.1.3;
   };
