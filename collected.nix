8fcf4b066d3f8468cbc5c3eee8faa45223f82d2a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:38:31 +0000

    autogenerated overwrites for emtable==0.0.14

--- collected.nix
+++ collected.nix
@@ -2523,6 +2523,9 @@
   empyrical = {
     "0.5.5" = import ./overrides/empyrical/0.5.5;
   };
+  emtable = {
+    "0.0.14" = import ./overrides/emtable/0.0.14;
+  };
   encodec = {
     "0.1.1" = import ./overrides/encodec/0.1.1;
   };
