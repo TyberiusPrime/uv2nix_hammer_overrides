5915597bd7ecaddc353c453f0fa48e2da5ec464a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:13:02 +0000

    autogenerated overwrites for oras==0.2.2

--- collected.nix
+++ collected.nix
@@ -5587,6 +5587,9 @@
   opuslib = {
     "3.0.1" = import ./overrides/opuslib/3.0.1;
   };
+  oras = {
+    "0.2.2" = import ./overrides/oras/0.2.2;
+  };
   orcid = {
     "1.0.3" = import ./overrides/orcid/1.0.3;
   };
