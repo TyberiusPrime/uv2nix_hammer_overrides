e5f73dd113b650739909c421666b2a3a87ed6054
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:13:01 +0000

    autogenerated overwrites for thttp==1.3.0

--- collected.nix
+++ collected.nix
@@ -9057,6 +9057,9 @@
   thriftpy2 = {
     "0.5.2" = import ./overrides/thriftpy2/0.5.2;
   };
+  thttp = {
+    "1.3.0" = import ./overrides/thttp/1.3.0;
+  };
   thunder-python = {
     "1.4.2" = import ./overrides/thunder-python/1.4.2;
   };
