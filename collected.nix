906eda7beddb13cd196bac698394faecd0d9d4a9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 17:08:16 +0000

    autogenerated overwrites for yandexcloud==0.316.0

--- collected.nix
+++ collected.nix
@@ -10064,6 +10064,9 @@
   yanc = {
     "0.3.3" = import ./overrides/yanc/0.3.3;
   };
+  yandexcloud = {
+    "0.316.0" = import ./overrides/yandexcloud/0.316.0;
+  };
   yapsy = {
     "1.12.2" = import ./overrides/yapsy/1.12.2;
   };
