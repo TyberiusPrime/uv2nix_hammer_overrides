fe97204bb3d373e9b2eab0fedf0fcb93bb804941
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:29:46 +0000

    autogenerated overwrites for attributedict==0.3.0

--- collected.nix
+++ collected.nix
@@ -464,6 +464,9 @@
   atomman = {
     "1.4.11" = import ./overrides/atomman/1.4.11;
   };
+  attributedict = {
+    "0.3.0" = import ./overrides/attributedict/0.3.0;
+  };
   auroranoaa = {
     "0.0.3" = import ./overrides/auroranoaa/0.0.3;
   };
