024f16c2219b7f7382ebb203063058e4368210ba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:14:46 +0000

    autogenerated overwrites for eth-brownie==1.20.6

--- collected.nix
+++ collected.nix
@@ -2580,6 +2580,9 @@
   etgen = {
     "1.1.4" = import ./overrides/etgen/1.1.4;
   };
+  eth-brownie = {
+    "1.20.6" = import ./overrides/eth-brownie/1.20.6;
+  };
   ethereum = {
     "2.3.2" = import ./overrides/ethereum/2.3.2;
   };
