598d2b296638b1a4c4ba69e39ccc25206e43600a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 17:57:37 +0000

    autogenerated overwrites for djangocms-snippet==3.1.1

--- collected.nix
+++ collected.nix
@@ -2225,6 +2225,9 @@
   djangoapidec = {
     "1.8" = import ./overrides/djangoapidec/1.8;
   };
+  djangocms-snippet = {
+    "3.1.1" = import ./overrides/djangocms-snippet/3.1.1;
+  };
   djangocodemirror = {
     "2.1.0" = import ./overrides/djangocodemirror/2.1.0;
   };
