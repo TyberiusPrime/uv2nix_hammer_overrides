9213ea2ee0bed46c52479922ca4a5283152aa042
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 04:37:46 +0000

    autogenerated overwrites for html-text==0.6.2

--- collected.nix
+++ collected.nix
@@ -3513,6 +3513,9 @@
   hsaudiotag3k = {
     "1.1.3.post1" = import ./overrides/hsaudiotag3k/1.1.3.post1;
   };
+  html-text = {
+    "0.6.2" = import ./overrides/html-text/0.6.2;
+  };
   html2markdown = {
     "0.1.7" = import ./overrides/html2markdown/0.1.7;
   };
