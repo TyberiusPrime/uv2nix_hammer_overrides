a90ef57c17397c7aeeac55ddde7a148c56b6c489
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:51:53 +0000

    autogenerated overwrites for flytekit==1.13.5

--- collected.nix
+++ collected.nix
@@ -2938,6 +2938,9 @@
   fluent = {
     "0.10.0" = import ./overrides/fluent/0.10.0;
   };
+  flytekit = {
+    "1.13.5" = import ./overrides/flytekit/1.13.5;
+  };
   fnvhash = {
     "0.1.0" = import ./overrides/fnvhash/0.1.0;
   };
