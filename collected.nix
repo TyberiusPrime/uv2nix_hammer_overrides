d303e988f29bc721e04d42805b83c7c1641f8214
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 00:17:30 +0000

    autogenerated overwrites for jupyter-telemetry==0.1.0

--- collected.nix
+++ collected.nix
@@ -4075,6 +4075,9 @@
   jupyter-existing-provisioner = {
     "0.1.3" = import ./overrides/jupyter-existing-provisioner/0.1.3;
   };
+  jupyter-telemetry = {
+    "0.1.0" = import ./overrides/jupyter-telemetry/0.1.0;
+  };
   jws = {
     "0.1.3" = import ./overrides/jws/0.1.3;
   };
