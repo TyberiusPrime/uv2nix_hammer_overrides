7db04dd58518af22c9da52fb0badc58ba23cd197
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:17:24 +0000

    autogenerated overwrites for xmod==1.8.1

--- collected.nix
+++ collected.nix
@@ -10025,6 +10025,9 @@
   xmlbuilder = {
     "1.0" = import ./overrides/xmlbuilder/1.0;
   };
+  xmod = {
+    "1.8.1" = import ./overrides/xmod/1.8.1;
+  };
   xmpppy = {
     "0.7.1" = import ./overrides/xmpppy/0.7.1;
   };
