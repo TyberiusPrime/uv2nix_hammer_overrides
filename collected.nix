613ab82ade8a74ccd194c38f0fcac556f5631fba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:40:48 +0000

    autogenerated overwrites for python-ndn==0.4.2

--- collected.nix
+++ collected.nix
@@ -7412,6 +7412,9 @@
   python-multipart = {
     "0.0.5" = import ./overrides/python-multipart/0.0.5;
   };
+  python-ndn = {
+    "0.4.2" = import ./overrides/python-ndn/0.4.2;
+  };
   python-nest = {
     "4.2.0" = import ./overrides/python-nest/4.2.0;
   };
