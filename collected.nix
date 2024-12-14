ccc919f8963b7e7e5b3d4b6b2b3b81832fc855f4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:12:40 +0000

    autogenerated overwrites for aioairzone==0.9.2

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aioairzone = {
+    "0.9.2" = import ./overrides/aioairzone/0.9.2;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
