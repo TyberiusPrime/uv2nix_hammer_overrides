3417c97354f6ae862606c22ea052c8ca49506d2e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 07:52:13 +0000

    autogenerated overwrites for napari-plugin-engine==0.2.0

--- collected.nix
+++ collected.nix
@@ -5120,6 +5120,9 @@
   nanotime = {
     "0.5.2" = import ./overrides/nanotime/0.5.2;
   };
+  napari-plugin-engine = {
+    "0.2.0" = import ./overrides/napari-plugin-engine/0.2.0;
+  };
   nats-py = {
     "2.9.0" = import ./overrides/nats-py/2.9.0;
   };
