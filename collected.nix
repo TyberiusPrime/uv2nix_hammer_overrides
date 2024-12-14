3b51f3c58a1a94b4883bf0e839d73c218b49e192
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:04:02 +0000

    autogenerated overwrites for hikari-lightbulb==2.3.5.post0

--- collected.nix
+++ collected.nix
@@ -3465,6 +3465,9 @@
   hierplace = {
     "1.1.0" = import ./overrides/hierplace/1.1.0;
   };
+  hikari-lightbulb = {
+    "2.3.5.post0" = import ./overrides/hikari-lightbulb/2.3.5.post0;
+  };
   hitchbuild = {
     "0.6.3" = import ./overrides/hitchbuild/0.6.3;
   };
