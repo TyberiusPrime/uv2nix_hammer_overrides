3007930a7f53c7e026c789e12bba0e180f779ab2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:02:22 +0000

    autogenerated overwrites for myjwt==2.1.0

--- collected.nix
+++ collected.nix
@@ -5064,6 +5064,9 @@
   mybad = {
     "0.2.0" = import ./overrides/mybad/0.2.0;
   };
+  myjwt = {
+    "2.1.0" = import ./overrides/myjwt/2.1.0;
+  };
   mypy-extensions = {
     "0.4.4" = import ./overrides/mypy-extensions/0.4.4;
   };
