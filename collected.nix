b59871f96f2b203b16033ece50c52cb7264200cc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 14:21:37 +0000

    autogenerated overwrites for tag-expressions==2.0.0

--- collected.nix
+++ collected.nix
@@ -8885,6 +8885,9 @@
   tables = {
     "3.8.0" = import ./overrides/tables/3.8.0;
   };
+  tag-expressions = {
+    "2.0.0" = import ./overrides/tag-expressions/2.0.0;
+  };
   tailer = {
     "0.4.1" = import ./overrides/tailer/0.4.1;
   };
