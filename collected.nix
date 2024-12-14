52664c215db4ac7174a4d5c6ea2414fb597749cd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:33:00 +0000

    autogenerated overwrites for github-to-sqlite==2.9

--- collected.nix
+++ collected.nix
@@ -3182,6 +3182,9 @@
   github-flask = {
     "3.2.0" = import ./overrides/github-flask/3.2.0;
   };
+  github-to-sqlite = {
+    "2.9" = import ./overrides/github-to-sqlite/2.9;
+  };
   gitignore-parser = {
     "0.1.11" = import ./overrides/gitignore-parser/0.1.11;
   };
