8f783f2803a51877640fec231538582be79646ef
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 03:08:02 +0000

    autogenerated overwrites for audioread==3.0.1

--- collected.nix
+++ collected.nix
@@ -464,6 +464,9 @@
   atomman = {
     "1.4.11" = import ./overrides/atomman/1.4.11;
   };
+  audioread = {
+    "3.0.1" = import ./overrides/audioread/3.0.1;
+  };
   auroranoaa = {
     "0.0.3" = import ./overrides/auroranoaa/0.0.3;
   };
