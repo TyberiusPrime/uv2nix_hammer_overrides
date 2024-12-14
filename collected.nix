a41938fa5570099871bd461cec85619987f30d63
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 05:25:08 +0000

    autogenerated overwrites for deep-translator==1.11.4

--- collected.nix
+++ collected.nix
@@ -1737,6 +1737,9 @@
   dedupe-levenshtein-search = {
     "1.4.5" = import ./overrides/dedupe-levenshtein-search/1.4.5;
   };
+  deep-translator = {
+    "1.11.4" = import ./overrides/deep-translator/1.11.4;
+  };
   deeplake = {
     "3.9.22" = import ./overrides/deeplake/3.9.22;
   };
