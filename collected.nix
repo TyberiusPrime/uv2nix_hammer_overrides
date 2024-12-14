0b075e2ae9878d1d497285af175bfa0047dec6b3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 12:01:32 +0000

    autogenerated overwrites for lmcloud==1.2.2

--- collected.nix
+++ collected.nix
@@ -4462,6 +4462,9 @@
     "0.36.0" = import ./overrides/llvmlite/0.36.0;
     "0.41.1" = import ./overrides/llvmlite/0.41.1;
   };
+  lmcloud = {
+    "1.2.2" = import ./overrides/lmcloud/1.2.2;
+  };
   lmtpd = {
     "6.2.0" = import ./overrides/lmtpd/6.2.0;
   };
