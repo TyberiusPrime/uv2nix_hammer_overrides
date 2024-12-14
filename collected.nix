18de753b2fe7c552be6a069b593f50a25504f8f0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 04:19:22 +0000

    autogenerated overwrites for control==0.10.1

--- collected.nix
+++ collected.nix
@@ -1400,6 +1400,9 @@
   contextvars = {
     "2.4" = import ./overrides/contextvars/2.4;
   };
+  control = {
+    "0.10.1" = import ./overrides/control/0.10.1;
+  };
   conversionkit = {
     "0.3.4" = import ./overrides/conversionkit/0.3.4;
   };
