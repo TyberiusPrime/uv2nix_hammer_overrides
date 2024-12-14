15c060a4a502ba34ff7703f297da7ccecaa64a91
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 15:57:22 +0000

    autogenerated overwrites for asf-search==8.0.1

--- collected.nix
+++ collected.nix
@@ -394,6 +394,9 @@
   asciitree = {
     "0.3.3" = import ./overrides/asciitree/0.3.3;
   };
+  asf-search = {
+    "8.0.1" = import ./overrides/asf-search/8.0.1;
+  };
   asks = {
     "2.4.12" = import ./overrides/asks/2.4.12;
     "3.0.0" = import ./overrides/asks/3.0.0;
