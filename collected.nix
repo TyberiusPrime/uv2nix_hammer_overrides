5d5fd377b013f12c0c4da33b64f26fc437413eee
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:49:30 +0000

    autogenerated overwrites for django-selectable==1.4.0

--- collected.nix
+++ collected.nix
@@ -2129,6 +2129,9 @@
   django-rtk = {
     "0.6.0" = import ./overrides/django-rtk/0.6.0;
   };
+  django-selectable = {
+    "1.4.0" = import ./overrides/django-selectable/1.4.0;
+  };
   django-sendfile = {
     "0.3.11" = import ./overrides/django-sendfile/0.3.11;
   };
