386bdc1c4fa16d80494c4bf86c64f3e7c52a27fd
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 20:25:44 +0000

    autogenerated overwrites for sev-snp-measure==0.0.11

--- collected.nix
+++ collected.nix
@@ -8308,6 +8308,9 @@
   setuptools-scm = {
     "8.1.0" = import ./overrides/setuptools-scm/8.1.0;
   };
+  sev-snp-measure = {
+    "0.0.11" = import ./overrides/sev-snp-measure/0.0.11;
+  };
   sewar = {
     "0.4.6" = import ./overrides/sewar/0.4.6;
   };
