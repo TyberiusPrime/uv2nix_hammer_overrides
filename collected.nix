8ca1ce9670129983c29babae435fa45ae9cf5a7b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:30:58 +0000

    autogenerated overwrites for airtable-python-wrapper==0.15.3

--- collected.nix
+++ collected.nix
@@ -153,6 +153,9 @@
   aircv = {
     "1.4.6" = import ./overrides/aircv/1.4.6;
   };
+  airtable-python-wrapper = {
+    "0.15.3" = import ./overrides/airtable-python-wrapper/0.15.3;
+  };
   aldryn-boilerplates = {
     "0.8.0" = import ./overrides/aldryn-boilerplates/0.8.0;
   };
