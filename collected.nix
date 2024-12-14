08dbdfe2db8b374aa403f81877333c32229fabfc
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 22:25:14 +0000

    autogenerated overwrites for django-webtest==1.9.12

--- collected.nix
+++ collected.nix
@@ -2222,6 +2222,9 @@
   django-vkontakte-users = {
     "0.8.7" = import ./overrides/django-vkontakte-users/0.8.7;
   };
+  django-webtest = {
+    "1.9.12" = import ./overrides/django-webtest/1.9.12;
+  };
   djangoapidec = {
     "1.8" = import ./overrides/djangoapidec/1.8;
   };
