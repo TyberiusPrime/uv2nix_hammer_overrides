94b9ead203f94fbf7707fdc13e7ffa75ac69ada9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 13:54:30 +0000

    autogenerated overwrites for razorpay==1.4.2

--- collected.nix
+++ collected.nix
@@ -7787,6 +7787,9 @@
   rauth = {
     "0.7.3" = import ./overrides/rauth/0.7.3;
   };
+  razorpay = {
+    "1.4.2" = import ./overrides/razorpay/1.4.2;
+  };
   rcsb-utils-io = {
     "1.49" = import ./overrides/rcsb-utils-io/1.49;
   };
