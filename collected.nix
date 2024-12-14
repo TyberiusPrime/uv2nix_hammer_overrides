05eec5eb9ee576f0446d60f98385e1634f8d087d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 08:01:58 +0000

    autogenerated overwrites for stanio==0.5.1

--- collected.nix
+++ collected.nix
@@ -8711,6 +8711,9 @@
   sslyze = {
     "6.0.0" = import ./overrides/sslyze/6.0.0;
   };
+  stanio = {
+    "0.5.1" = import ./overrides/stanio/0.5.1;
+  };
   starkbank-ecdsa = {
     "2.2.0" = import ./overrides/starkbank-ecdsa/2.2.0;
   };
