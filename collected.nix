13eda55819aa5329fd68b98646b6ccbf764ad1b8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 09:32:33 +0000

    autogenerated overwrites for hypothesis==6.112.0

--- collected.nix
+++ collected.nix
@@ -3607,6 +3607,9 @@
   hyperloglog = {
     "0.0.14" = import ./overrides/hyperloglog/0.0.14;
   };
+  hypothesis = {
+    "6.112.0" = import ./overrides/hypothesis/6.112.0;
+  };
   hyrule = {
     "0.6.0" = import ./overrides/hyrule/0.6.0;
   };
