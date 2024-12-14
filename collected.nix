a81b8a27cd5b654128a84540a09ad9e382c0366a
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 18:51:11 +0000

    autogenerated overwrites for infi.exceptools==0.2.15

--- collected.nix
+++ collected.nix
@@ -3727,6 +3727,9 @@
   imutils = {
     "0.5.4" = import ./overrides/imutils/0.5.4;
   };
+  infi-exceptools = {
+    "0.2.15" = import ./overrides/infi-exceptools/0.2.15;
+  };
   infi-execute = {
     "0.1.8" = import ./overrides/infi-execute/0.1.8;
   };
