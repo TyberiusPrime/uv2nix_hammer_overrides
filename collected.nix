b84636dbfba01e3181988a9a0d847b25b462a1e5
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 23:58:21 +0000

    autogenerated overwrites for luxtronik==0.3.14

--- collected.nix
+++ collected.nix
@@ -4547,6 +4547,9 @@
   luhn = {
     "0.2.0" = import ./overrides/luhn/0.2.0;
   };
+  luxtronik = {
+    "0.3.14" = import ./overrides/luxtronik/0.3.14;
+  };
   lyrebird = {
     "3.0.5" = import ./overrides/lyrebird/3.0.5;
   };
