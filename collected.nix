bc93edb559cd6b0adf0bc265e1401c835732522f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 21:07:46 +0000

    autogenerated overwrites for invenio-assets==3.0.3

--- collected.nix
+++ collected.nix
@@ -3805,6 +3805,9 @@
   intervaltree = {
     "3.1.0" = import ./overrides/intervaltree/3.1.0;
   };
+  invenio-assets = {
+    "3.0.3" = import ./overrides/invenio-assets/3.0.3;
+  };
   invenio-github = {
     "1.5.2" = import ./overrides/invenio-github/1.5.2;
   };
