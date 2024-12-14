e1ff817740cf811a4fb079d5ae2304ad802653a0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 11:02:08 +0000

    autogenerated overwrites for interpret-core==0.6.3

--- collected.nix
+++ collected.nix
@@ -3796,6 +3796,9 @@
   intermake = {
     "1.0.0.78" = import ./overrides/intermake/1.0.0.78;
   };
+  interpret-core = {
+    "0.6.3" = import ./overrides/interpret-core/0.6.3;
+  };
   interruptingcow = {
     "0.8" = import ./overrides/interruptingcow/0.8;
   };
