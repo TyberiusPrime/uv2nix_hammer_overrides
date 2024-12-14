7161fa6aeddf37bacfbf07bbd1653cb63fc3531d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-14 16:10:44 +0000

    autogenerated overwrites for sqlglot==25.20.1

--- collected.nix
+++ collected.nix
@@ -8683,6 +8683,9 @@
   sqlalchemy-utils = {
     "0.36.1" = import ./overrides/sqlalchemy-utils/0.36.1;
   };
+  sqlglot = {
+    "25.20.1" = import ./overrides/sqlglot/25.20.1;
+  };
   sqlitedict = {
     "2.1.0" = import ./overrides/sqlitedict/2.1.0;
   };
