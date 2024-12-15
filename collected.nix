656d7211de8308bce87ebfb39bf3431e0c287ff7
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:26:52 +0000

    autogenerated overwrites for dependencies==7.7.0

--- collected.nix
+++ collected.nix
@@ -1773,6 +1773,9 @@
   demjson3 = {
     "3.0.6" = import ./overrides/demjson3/3.0.6;
   };
+  dependencies = {
+    "7.7.0" = import ./overrides/dependencies/7.7.0;
+  };
   derpconf = {
     "0.8.4" = import ./overrides/derpconf/0.8.4;
   };
