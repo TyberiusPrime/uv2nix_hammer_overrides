41abeced465acfb6fe81f18b666f7ec7be90dd9e
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 04:06:01 +0000

    autogenerated overwrites for roifile==2024.5.24

--- collected.nix
+++ collected.nix
@@ -7997,6 +7997,9 @@
   rocketmq-client-python = {
     "2.0.0" = import ./overrides/rocketmq-client-python/2.0.0;
   };
+  roifile = {
+    "2024.5.24" = import ./overrides/roifile/2024.5.24;
+  };
   roku = {
     "4.1.0" = import ./overrides/roku/4.1.0;
   };
