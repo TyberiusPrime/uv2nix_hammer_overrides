2e4fefb1fc91da682aaca3d49cba940218abb2f1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:24:55 +0000

    autogenerated overwrites for zipcodes==1.2.0

--- collected.nix
+++ collected.nix
@@ -10148,6 +10148,9 @@
   zhconv = {
     "1.4.3" = import ./overrides/zhconv/1.4.3;
   };
+  zipcodes = {
+    "1.2.0" = import ./overrides/zipcodes/1.2.0;
+  };
   zipseeker = {
     "1.0.11" = import ./overrides/zipseeker/1.0.11;
   };
