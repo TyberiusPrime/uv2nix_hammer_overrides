22a6fb5776d714c48a7a809014dba6386eb157d9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:30:44 +0000

    autogenerated overwrites for pytest-flask==1.3.0

--- collected.nix
+++ collected.nix
@@ -7224,6 +7224,9 @@
   pytest-expect = {
     "1.1.0" = import ./overrides/pytest-expect/1.1.0;
   };
+  pytest-flask = {
+    "1.3.0" = import ./overrides/pytest-flask/1.3.0;
+  };
   pytest-json = {
     "0.4.0" = import ./overrides/pytest-json/0.4.0;
   };
