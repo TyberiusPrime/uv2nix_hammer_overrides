2950c6c15e0e164ceb0151958404ca90ad7b0cba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 02:49:42 +0000

    autogenerated overwrites for tentaclio==1.3.0

--- collected.nix
+++ collected.nix
@@ -8981,6 +8981,9 @@
   tensorflow-io-gcs-filesystem = {
     "0.37.1" = import ./overrides/tensorflow-io-gcs-filesystem/0.37.1;
   };
+  tentaclio = {
+    "1.3.0" = import ./overrides/tentaclio/1.3.0;
+  };
   termcolor = {
     "1.1.0" = import ./overrides/termcolor/1.1.0;
   };
