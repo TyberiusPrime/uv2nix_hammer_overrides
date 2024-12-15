bfb074699c6f23a985ac510be6880891f4226496
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 03:56:15 +0000

    autogenerated overwrites for aliyun-python-sdk-sts==3.1.2

--- collected.nix
+++ collected.nix
@@ -187,6 +187,9 @@
   aliyun-python-sdk-core-v3 = {
     "2.13.33" = import ./overrides/aliyun-python-sdk-core-v3/2.13.33;
   };
+  aliyun-python-sdk-sts = {
+    "3.1.2" = import ./overrides/aliyun-python-sdk-sts/3.1.2;
+  };
   allset = {
     "1.0.1" = import ./overrides/allset/1.0.1;
   };
