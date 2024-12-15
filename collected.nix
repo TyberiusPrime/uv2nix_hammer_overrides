ed8f506533880d76c2e6d718fe4ec3e6db3ddaed
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:24:35 +0000

    autogenerated overwrites for click-log==0.4.0

--- collected.nix
+++ collected.nix
@@ -1116,6 +1116,9 @@
   click-didyoumean = {
     "0.0.3" = import ./overrides/click-didyoumean/0.0.3;
   };
+  click-log = {
+    "0.4.0" = import ./overrides/click-log/0.4.0;
+  };
   clickhouse-cli = {
     "0.3.9" = import ./overrides/clickhouse-cli/0.3.9;
   };
