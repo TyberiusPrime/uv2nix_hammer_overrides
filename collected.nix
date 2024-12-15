40e32ab80438e29edea33b04d522100883424412
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 04:15:04 +0000

    autogenerated overwrites for towncrier==24.8.0

--- collected.nix
+++ collected.nix
@@ -9248,6 +9248,9 @@
   total-ordering = {
     "0.1.0" = import ./overrides/total-ordering/0.1.0;
   };
+  towncrier = {
+    "24.8.0" = import ./overrides/towncrier/24.8.0;
+  };
   toyplot = {
     "1.0.3" = import ./overrides/toyplot/1.0.3;
   };
