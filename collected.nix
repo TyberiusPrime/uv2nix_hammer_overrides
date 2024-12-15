c0a8e962706f3314c88f31d9c1c0762596aed91d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:25:00 +0000

    autogenerated overwrites for drmaa==0.7.9

--- collected.nix
+++ collected.nix
@@ -2388,6 +2388,9 @@
   drfdocs = {
     "0.0.11" = import ./overrides/drfdocs/0.0.11;
   };
+  drmaa = {
+    "0.7.9" = import ./overrides/drmaa/0.7.9;
+  };
   dsinternals = {
     "1.2.4" = import ./overrides/dsinternals/1.2.4;
   };
