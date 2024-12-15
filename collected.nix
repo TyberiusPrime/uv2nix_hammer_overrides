274e32ca0fd94a7e302cb9cfd017d13ab8f6bad3
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:34:59 +0000

    autogenerated overwrites for cloudsplaining==0.6.3

--- collected.nix
+++ collected.nix
@@ -1190,6 +1190,9 @@
   cloudshell-snmp = {
     "3.3.0" = import ./overrides/cloudshell-snmp/3.3.0;
   };
+  cloudsplaining = {
+    "0.6.3" = import ./overrides/cloudsplaining/0.6.3;
+  };
   cmapy = {
     "0.6.6" = import ./overrides/cmapy/0.6.6;
   };
