63f7241c26bc48576ba55971bfe027ba4f436bb1
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:02:43 +0000

    autogenerated overwrites for rarfile==4.2

--- collected.nix
+++ collected.nix
@@ -7775,6 +7775,9 @@
   rapidtables = {
     "0.1.11" = import ./overrides/rapidtables/0.1.11;
   };
+  rarfile = {
+    "4.2" = import ./overrides/rarfile/4.2;
+  };
   rask = {
     "0.1.5" = import ./overrides/rask/0.1.5;
   };
