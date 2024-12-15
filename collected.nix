7d20cde669de15d1ec69bed9a2a5886969784db2
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:04:59 +0000

    autogenerated overwrites for unasync==0.6.0

--- collected.nix
+++ collected.nix
@@ -9482,6 +9482,9 @@
   umi-tools = {
     "1.1.5" = import ./overrides/umi-tools/1.1.5;
   };
+  unasync = {
+    "0.6.0" = import ./overrides/unasync/0.6.0;
+  };
   uncleengineer = {
     "0.2" = import ./overrides/uncleengineer/0.2;
   };
