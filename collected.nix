d3738868a21de59cca061fb8467d689ff61e475c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 02:22:48 +0000

    autogenerated overwrites for pywaze==1.1.0

--- collected.nix
+++ collected.nix
@@ -7628,6 +7628,9 @@
   pywatchman = {
     "2.0.0" = import ./overrides/pywatchman/2.0.0;
   };
+  pywaze = {
+    "1.1.0" = import ./overrides/pywaze/1.1.0;
+  };
   pywebdav = {
     "0.9.8" = import ./overrides/pywebdav/0.9.8;
   };
