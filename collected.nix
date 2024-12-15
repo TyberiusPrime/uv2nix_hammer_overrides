d84a49782adc214f3e414863a75173950273cc15
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:55:44 +0000

    autogenerated overwrites for naver-config==0.0.2

--- collected.nix
+++ collected.nix
@@ -5135,6 +5135,9 @@
   naturalsort = {
     "1.5.1" = import ./overrides/naturalsort/1.5.1;
   };
+  naver-config = {
+    "0.0.2" = import ./overrides/naver-config/0.0.2;
+  };
   nbexec = {
     "0.2.0" = import ./overrides/nbexec/0.2.0;
   };
