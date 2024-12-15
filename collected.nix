5b8ba9c491a194feba4ae0929303a1d5025f353f
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:06:02 +0000

    autogenerated overwrites for openai==1.44.1

--- collected.nix
+++ collected.nix
@@ -5510,6 +5510,9 @@
   opcua = {
     "0.98.13" = import ./overrides/opcua/0.98.13;
   };
+  openai = {
+    "1.44.1" = import ./overrides/openai/1.44.1;
+  };
   openai-clip = {
     "1.0.1" = import ./overrides/openai-clip/1.0.1;
   };
