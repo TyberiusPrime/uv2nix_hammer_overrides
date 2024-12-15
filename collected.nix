e2f98842f98a78d442f4941166e22eff7a657c2b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 01:20:26 +0000

    autogenerated overwrites for aiida==1.0.1

--- collected.nix
+++ collected.nix
@@ -92,6 +92,9 @@
   aiee = {
     "0.3.2" = import ./overrides/aiee/0.3.2;
   };
+  aiida = {
+    "1.0.1" = import ./overrides/aiida/1.0.1;
+  };
   aiocqhttp = {
     "1.4.4" = import ./overrides/aiocqhttp/1.4.4;
   };
