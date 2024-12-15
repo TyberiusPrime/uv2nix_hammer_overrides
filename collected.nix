54d84d98d2ab5483b1e452d68d06a2f2bb710e8c
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 02:01:39 +0000

    autogenerated overwrites for blockfrost-python==0.6.0

--- collected.nix
+++ collected.nix
@@ -785,6 +785,9 @@
   blockchain-etl-common = {
     "1.7.1" = import ./overrides/blockchain-etl-common/1.7.1;
   };
+  blockfrost-python = {
+    "0.6.0" = import ./overrides/blockfrost-python/0.6.0;
+  };
   blosc2 = {
     "2.0.0" = import ./overrides/blosc2/2.0.0;
     "2.7.1" = import ./overrides/blosc2/2.7.1;
