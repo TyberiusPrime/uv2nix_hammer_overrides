aa76d92f8a49ef296cf8881c4537ca5c6bd5ac25
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 02:32:37 +0000

    autogenerated overwrites for async-timeout==4.0.3

--- collected.nix
+++ collected.nix
@@ -428,6 +428,9 @@
   async-cache = {
     "1.1.1" = import ./overrides/async-cache/1.1.1;
   };
+  async-timeout = {
+    "4.0.3" = import ./overrides/async-timeout/4.0.3;
+  };
   async-upnp-client = {
     "0.40.0" = import ./overrides/async-upnp-client/0.40.0;
   };
