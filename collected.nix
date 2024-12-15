cb4d5e0e2a5f6b42b654991b1217b273f5842208
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:06:27 +0000

    autogenerated overwrites for types-aiobotocore-mobile==2.13.2

--- collected.nix
+++ collected.nix
@@ -9443,6 +9443,9 @@
   typelike = {
     "0.0.32" = import ./overrides/typelike/0.0.32;
   };
+  types-aiobotocore-mobile = {
+    "2.13.2" = import ./overrides/types-aiobotocore-mobile/2.13.2;
+  };
   typing = {
     "3.5.2.2" = import ./overrides/typing/3.5.2.2;
   };
