fa39ed4a6235e3ff5967d1185425833f5ecac3f0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-12-15 00:23:16 +0000

    autogenerated overwrites for qcodes==0.48.0

--- collected.nix
+++ collected.nix
@@ -7691,6 +7691,9 @@
   qcloud-image = {
     "1.0.0" = import ./overrides/qcloud-image/1.0.0;
   };
+  qcodes = {
+    "0.48.0" = import ./overrides/qcodes/0.48.0;
+  };
   qds-sdk = {
     "1.16.1" = import ./overrides/qds-sdk/1.16.1;
   };
