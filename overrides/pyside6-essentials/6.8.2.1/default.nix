8900087c0e67824aae85d36473c8202a015dcd8b
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-03-11 15:17:15 +0000

    autogenerated overwrites (commit to be squashed)

--- overrides/pyside6-essentials/6.8.2.1/default.nix
+++ overrides/pyside6-essentials/6.8.2.1/default.nix
@@ -1,3 +1,3 @@
-{helpers, ...}
-        : old: {dontWrapQtApps = true;}
+{helpers, pkgs, ...}
+        : old: {buildInputs = old.buildInputs or [] ++ [pkgs.cairo pkgs.dbus pkgs.fontconfig pkgs.freetype pkgs.pango pkgs.unixODBC];dontWrapQtApps = true;}
         
