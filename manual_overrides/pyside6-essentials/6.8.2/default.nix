624cd80adb6bd391701a08c4a00b9c9f54fd5bd8
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2025-03-12 09:33:02 +0000

    pyside6-essentials 6.8.2

---
+++ manual_overrides/pyside6-essentials/6.8.2/default.nix
@@ -0,0 +1,18 @@
+{
+  dontWrapQtApps = true;
+  buildInputs = (old.buildInputs or [ ]) ++ [
+    pkgs.qt6.full
+    pkgs.gtk3
+    pkgs.libxkbcommon
+    pkgs.postgresql.lib
+  ];
+  autoPatchelfIgnoreMissingDeps = [
+    "libmysqlclient.so.21"
+    "libmimerapi.so"
+    "libQt6EglFsKmsGbmSupport.so*"
+  ];
+  # Somehow `autopatchelf` does not find the `shiboken6` library.
+  preFixup = ''
+    addAutoPatchelfSearchPath ${final.shiboken6}/${final.python.sitePackages}/shiboken6
+  '';
+}
