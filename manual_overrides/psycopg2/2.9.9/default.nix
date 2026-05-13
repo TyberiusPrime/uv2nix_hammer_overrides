024dd550fef52d34653173b74b12d648979712e4
Jairo Llopis <jairo@moduon.team>
2026-05-13 09:42:40 +0100

    fix(psycopg2): allow building on modern Nixpkgs
    Fixes https://github.com/TyberiusPrime/uv2nix_hammer_overrides/issues/17
    
    @moduon MT-14562

--- manual_overrides/psycopg2/2.9.9/default.nix
+++ manual_overrides/psycopg2/2.9.9/default.nix
@@ -1,15 +1,13 @@
-{
-  #from nixpkgs...
-  postPatch = ''
-    # Preferably upstream would not depend on pg_config because config scripts are incompatible with cross-compilation, however postgresql's pc file is lacking information.
-    # some linker flags are added but the linker ignores them because they're incompatible
-    # https://github.com/psycopg/psycopg2/blob/89005ac5b849c6428c05660b23c5a266c96e677d/setup.py
-    substituteInPlace setup.py \
-      --replace-fail "self.pg_config_exe = self.build_ext.pg_config" 'self.pg_config_exe = "${pkgs.lib.getDev pkgs.buildPackages.postgresql}/bin/pg_config"'
-  '';
-  env = {
-    PGDATABASE = "psycopg2_test";
-  };
+(pkgs.lib.optionalAttrs (!(helpers.isWheel old)) {
+  buildInputs =
+    (old.buildInputs or [ ])
+    ++ [ pkgs.libpq ]
+    ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ pkgs.openssl ];
 
-  buildInputs = [ pkgs.postgresql ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.openssl ];
-}
+  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+    pkgs.postgresql
+    pkgs.libpq.pg_config
+  ];
+
+  env.PGDATABASE = "psycopg2_test";
+})
